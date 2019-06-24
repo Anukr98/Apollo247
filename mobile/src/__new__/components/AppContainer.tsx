b2ddbcbeb8a9ff49fdaf8cecd47a41d41d98128a
Kabir Sarin <sarink87@gmail.com>
2019-06-24 23:28:26 +0530

    Add BackButton, split AppContainer components

--- mobile/src/__new__/components/AppContainer.tsx
+++ mobile/src/__new__/components/AppContainer.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
-import { NativeRouter, Route } from 'react-router-native';
+import { NativeRouter, Route, BackButton } from 'react-router-native';
 import { IntlProvider } from 'react-intl';
 import { Onboarding } from 'app/src/__new__/components/Onboarding';
 import { Login } from './Login';
 import { ConsultRoom } from 'app/src/__new__/components/ConsultRoom';
 import { appRoutes } from 'app/src/__new__/helpers/appRoutes';
+import { View } from 'react-native';
 
 const enMessages = {
   welcome: 'Welome! :)',
+};
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Route exact path="/" component={Onboarding} />
+      <Route path={appRoutes.onboarding()} component={Onboarding} />
+      <Route path={appRoutes.login()} component={Login} />
+      <Route path={appRoutes.consultRoom()} component={ConsultRoom} />
+    </View>
+  );
 };
 
 export const AppContainer: React.FC = () => {
   return (
     <IntlProvider locale="en" messages={enMessages}>
       <NativeRouter>
-        <Route exact path="/" component={Onboarding} />
-        <Route path={appRoutes.onboarding()} component={Onboarding} />
-        <Route path={appRoutes.login()} component={Login} />
-        <Route path={appRoutes.consultRoom()} component={ConsultRoom} />
+        <BackButton>
+          <App />
+        </BackButton>
       </NativeRouter>
     </IntlProvider>
   );
