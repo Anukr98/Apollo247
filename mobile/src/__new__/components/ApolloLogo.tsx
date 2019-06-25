256650dd39b9b7a3c4a3a0b323e3245da0cb5cdf
Kabir Sarin <sarink87@gmail.com>
2019-06-25 17:15:20 +0530

    Build Icons component

---
+++ mobile/src/__new__/components/ApolloLogo.tsx
@@ -0,0 +1,11 @@
+import { Image, ImageProps, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+export const ApolloLogo: React.FC<ImageProps> = (props) => (
+  <Image
+    style={{ width: 77, height: 57, left: width - 97, top: 20 }}
+    {...props}
+    source={require('app/src/__new__/images/apollo/apollo_logo.png')}
+  />
+);
