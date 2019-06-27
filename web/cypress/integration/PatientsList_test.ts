55f7d9a0a6985e17f4f57ccf77c796c103e0d478
Kabir Sarin <sarink87@gmail.com>
2019-06-27 23:11:40 +0530

    Add mock PatientsList test

--- web/cypress/integration/PatientsList_test.ts
+++ web/cypress/integration/PatientsList_test.ts
@@ -1,8 +1,20 @@
 import { clientBaseUrl, clientRoutes } from 'helpers/clientRoutes';
+import { apiRoutes } from 'helpers/apiRoutes';
+import { getPatientsFixture } from 'cypress/fixtures/patientsFixtures';
 
 describe('PatientsList', () => {
-  it('Shows PatientsList', () => {
+  it('Shows the PatientsList', () => {
     cy.visit(`${clientBaseUrl}${clientRoutes.patients()}`);
-    cy.get(`[data-cypress='PatientsList']`).should('exist');
+    cy.contains('h3', 'Patients List').should('exist');
+  });
+
+  it('Renders each patient', () => {
+    const patientsListQueryResult = getPatientsFixture();
+    cy.server();
+    cy.route('POST', apiRoutes.graphql(), patientsListQueryResult);
+    cy.visit(`${clientBaseUrl}${clientRoutes.patients()}`);
+    patientsListQueryResult.data.getPatients!.patients.forEach((patient) => {
+      cy.contains('div', `${patient.firstName} ${patient.lastName}`);
+    });
   });
 });
