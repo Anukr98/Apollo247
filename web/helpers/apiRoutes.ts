dd67e14879cb63ebeb23c8a9515f9b7c5dc49725
Kabir Sarin <sarink87@gmail.com>
2019-07-03 18:45:09 +0530

    Add local env and modify routes accordingly

--- web/helpers/apiRoutes.ts
+++ web/helpers/apiRoutes.ts
@@ -1,5 +1,11 @@
-const apiBaseUrl = () =>
-  process.env.NODE_ENV === 'test' ? '' : `//localhost:${process.env.API_GATEWAY_PORT}`;
+const apiBaseUrl = () => {
+  const env = process.env.NODE_ENV;
+  const port = process.env.API_GATEWAY_PORT === '80' ? '' : `:${process.env.API_GATEWAY_PORT}`;
+  if (env === 'test') return `//localhost${port}`;
+  if (env === 'local') return `//localhost${port}`;
+  if (env === 'development') return `//api.aph.popcornapps.com${port}`;
+  console.error('Invalid NODE_ENV environment variable configuration');
+};
 
 export const apiRoutes = {
   graphql: () => `${apiBaseUrl()}/graphql`,
--- web/helpers/clientRoutes.ts
+++ web/helpers/clientRoutes.ts
@@ -1,4 +1,11 @@
-export const clientBaseUrl = `http://localhost:${process.env.WEB_CLIENT_PORT}`;
+export const clientBaseUrl = () => {
+  const env = process.env.NODE_ENV;
+  const port = process.env.WEB_CLIENT_PORT === '80' ? '' : `:${process.env.WEB_CLIENT_PORT}`;
+  if (env === 'test') return `//localhost${port}`;
+  if (env === 'local') return `//localhost${port}`;
+  if (env === 'development') return `//patients-web.aph.popcornapps.com${port}`;
+  console.error('Invalid NODE_ENV environment variable configuration');
+};
 
 export const clientRoutes = {
   welcome: () => '/',
