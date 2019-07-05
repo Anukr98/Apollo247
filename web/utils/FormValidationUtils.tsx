f0a6a0a4072abaad12ae90ad52dab0c5ae472d67
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2019-07-05 14:14:10 +0530

    dob validation fixed.

--- web/utils/FormValidationUtils.tsx
+++ web/utils/FormValidationUtils.tsx
@@ -21,20 +21,21 @@ export const isNameValid = (name: string) => {
   return /^[a-zA-Z ]*$/.test(name.trim()) && name.trim().length > 2;
 };
 
-export const isDobValid = (dob: string) => {
-  return (
-    /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/.test(dob) &&
-    dob
-      .split('/')
-      .reverse()
-      .join('/') <
-      new Date()
-        .toLocaleDateString('en-GB', { year: 'numeric', month: 'numeric', day: 'numeric' })
-        .replace(/ /g, '/')
-        .split('/')
-        .reverse()
-        .join('/')
-  );
+export const isDobValid = (ddmmyyyy: string) => {
+  const isCorrectFormat = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/.test(ddmmyyyy);
+  if (!isCorrectFormat) return false;
+
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+
+  const dateParts = ddmmyyyy.split('/');
+  const dd = parseInt(dateParts[0]);
+  const mm = parseInt(dateParts[1]);
+  const yyyy = parseInt(dateParts[2]);
+
+  const isValidDate = new Date(yyyy, dd, mm) < currentDate;
+
+  return isValidDate;
 };
 
 export const isEmailValid = (email: string) => {
