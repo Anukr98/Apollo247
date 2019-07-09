3f1e5021ee84f49f704c8bf9cd7b89a5ebfa4407
Sushil Chhetri <chhetrisushil@gmail.com>
2019-07-10 01:09:07 +0530

    setting up doctors page

---
+++ packages/web-doctors/src/helpers/clientRoutes.ts
@@ -0,0 +1,16 @@
+export const clientBaseUrl = () => {
+  const env = process.env.NODE_ENV;
+  const port = process.env.WEB_CLIENT_PORT === '80' ? '' : `:${process.env.WEB_CLIENT_PORT}`;
+  if (env === 'test') return `//localhost${port}`;
+  if (env === 'local') return `//localhost${port}`;
+  if (env === 'development') return `//patients-web.aph.popcornapps.com${port}`;
+  console.error('Invalid NODE_ENV environment variable configuration');
+};
+
+export const clientRoutes = {
+  welcome: () => '/',
+  patients: () => '/patients',
+  consultRoom: () => '#',
+  testsAndMedicine: () => '#',
+  healthRecords: () => '#',
+};
