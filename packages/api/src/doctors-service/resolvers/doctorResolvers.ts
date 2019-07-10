43ac79d0ddbe703a239789ef17052e56dcca468f
sushma <sushma.voleti@popcornapps.com>
2019-07-10 23:18:04 +0530

    Doctors has access to Portal
    Search by doctors  mobilenumber. Tells whether a soctor can access to APP

--- packages/api/src/doctors-service/resolvers/doctorResolvers.ts
+++ packages/api/src/doctors-service/resolvers/doctorResolvers.ts
@@ -1,20 +1,41 @@
 import gql from 'graphql-tag';
 import { Resolver } from 'doctors-service/doctors-service';
+import DoctorsData from '../data/doctors.json';
+import { JsonValue } from 'apollo-utilities';
+import { isNull } from 'util';
 
 export const doctorTypeDefs = gql`
+  type Doctor {
+    firstName: String
+    lastName: String
+    mobileNumber: String
+    experience: String
+  }
+
   extend type Query {
-    sayHello: String
+    getDoctorsData: [Doctor]
+    hasAccess(mobileNumber: String): Boolean
   }
 `;
 
-//const sayHello = 'Hello world!';
+const getDoctorsData: Resolver<any> = async (parent, args): Promise<JSON> => {
+  return JSON.parse(JSON.stringify(DoctorsData));
+};
 
-const sayHello: Resolver<any> = async (parent, args): Promise<String> => {
-  return 'Hello world!';
+const hasAccess: Resolver<any, { mobileNumber: string }> = async (
+  parent,
+  args
+): Promise<Boolean> => {
+  let authorized = DoctorsData.find((item) => {
+    return item.mobileNumber == args.mobileNumber;
+  });
+  if (isNull(authorized)) return false;
+  return true;
 };
 
 export const doctorResolvers = {
   Query: {
-    sayHello,
+    getDoctorsData,
+    hasAccess,
   },
 };
