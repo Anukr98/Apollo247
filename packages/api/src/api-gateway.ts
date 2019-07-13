c3ca6243f1895609ff3e8394995ef6e4bc55e0e5
Kabir Sarin <sarink87@gmail.com>
2019-07-13 13:16:50 +0530

    Refactor api-gateway

--- packages/api/src/api-gateway.ts
+++ packages/api/src/api-gateway.ts
@@ -2,49 +2,22 @@ import { ApolloServer, AuthenticationError } from 'apollo-server';
 import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { GraphQLExecutor } from 'apollo-server-core';
 import * as firebaseAdmin from 'firebase-admin';
-import { auth } from 'firebase-admin';
 
 interface GatewayContext {
   firebaseUid: string;
   mobileNumber: string;
 }
 
-interface GatewayError {
-  message: string;
-}
-
-function wait<R, E>(promise: Promise<R>): [R, E] {
-  return (promise.then((data: R) => [data, null], (err: E) => [null, err]) as any) as [R, E];
-}
+const env = process.env.NODE_ENV as 'local' | 'development';
+const port = process.env.WEB_CLIENT_PORT === '80' ? '' : `:${process.env.WEB_CLIENT_PORT}`;
+const envToCorsOrigin = {
+  local: '*', // `http://localhost${port}`,
+  development: '*', // 'http://patients-web.aph.popcornapps.com'
+  // staging: '',
+  // production: ''
+};
 
 (async () => {
-  const firebase = firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.applicationDefault(),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-  });
-  await Promise.all([firebase]);
-
-  async function validateToken(jwt: any): Promise<GatewayContext> {
-    const [firebaseIdToken, firebaseIdTokenError] = await wait<
-      auth.DecodedIdToken,
-      firebaseAdmin.FirebaseError
-    >(firebase.auth().verifyIdToken(jwt));
-    if (firebaseIdTokenError) {
-      throw new AuthenticationError(`${firebaseIdTokenError.code}`);
-    } else {
-      const [firebaseUser, firebaseUserError] = await wait(
-        firebase.auth().getUser(firebaseIdToken.uid)
-      );
-      if (firebaseUserError) {
-        throw new AuthenticationError(firebaseUserError);
-      }
-      return {
-        firebaseUid: firebaseIdToken.uid,
-        mobileNumber: firebaseUser.phoneNumber!,
-      };
-    }
-  }
-
   const gateway = new ApolloGateway({
     serviceList: [
       { name: 'profiles', url: 'http://profiles-service/graphql' },
@@ -64,37 +37,52 @@ function wait<R, E>(promise: Promise<R>): [R, E] {
     },
   });
 
-  const env = process.env.NODE_ENV as 'local' | 'development';
-  const port = process.env.WEB_CLIENT_PORT === '80' ? '' : `:${process.env.WEB_CLIENT_PORT}`;
-  const envToCorsOrigin = {
-    local: '*', // `http://localhost${port}`,
-    development: '*', // 'http://patients-web.aph.popcornapps.com'
-    // staging: '',
-    // production: ''
-  };
-
   const config = await gateway.load();
   const schema = config.schema;
   const executor = config.executor as GraphQLExecutor;
+
+  const firebase = firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.applicationDefault(),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  });
+
   const server = new ApolloServer({
     cors: { origin: envToCorsOrigin[env] },
     schema,
     executor,
     context: async ({ req }) => {
-      if (
-        (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'local') &&
-        req.body.operationName == 'IntrospectionQuery'
-      ) {
+      const isLocal = process.env.NODE_ENV == 'local';
+      const isDevelopment = process.env.NODE_ENV == 'development';
+      const isSchemaIntrospectionQuery = req.body.operationName == 'IntrospectionQuery';
+      if ((isLocal || isDevelopment) && isSchemaIntrospectionQuery) {
         return { firebaseUid: '', mobileNumber: '' };
       }
-      const token = req.headers.authorization || '';
-      const [firebaseData, error] = await wait<GatewayContext, GatewayError>(validateToken(token));
-      if (error) {
-        throw error;
-      }
-      return firebaseData;
+
+      const jwt = req.headers.authorization || '';
+
+      const firebaseIdToken = await firebase
+        .auth()
+        .verifyIdToken(jwt)
+        .catch((error: firebaseAdmin.FirebaseError) => {
+          throw new AuthenticationError(error.code);
+        });
+
+      const firebaseUser = await firebase
+        .auth()
+        .getUser(firebaseIdToken.uid)
+        .catch((error: firebaseAdmin.FirebaseError) => {
+          throw new AuthenticationError(error.message);
+        });
+
+      const gatewayContext: GatewayContext = {
+        firebaseUid: firebaseUser.uid,
+        mobileNumber: firebaseUser.phoneNumber || '',
+      };
+
+      return gatewayContext;
     },
   });
+
   server.listen(process.env.API_GATEWAY_PORT).then(({ url }) => {
     console.log(`🚀 api gateway ready at ${url}`);
   });
