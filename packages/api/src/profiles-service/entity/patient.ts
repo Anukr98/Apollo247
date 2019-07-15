e63aeae515ee7e8b94c9bbc0b072131f545cf8a4
Kabir Sarin <sarink87@gmail.com>
2019-07-15 20:29:07 +0530

    Remove ErrorMsgs enum from patients.ts

--- packages/api/src/profiles-service/entity/patient.ts
+++ packages/api/src/profiles-service/entity/patient.ts
@@ -18,15 +18,6 @@ export enum Relation {
   OTHER = 'OTHER',
 }
 
-export enum ErrorMsgs {
-  INVALID_TOKEN = 'INVALID_TOKEN',
-  INVALID_MOBILE_NUMBER = 'INVALID_MOBILE_NUMBER',
-  PRISM_AUTH_TOKEN_ERROR = 'PRISM_AUTH_TOKEN_ERROR',
-  PRISM_GET_USERS_ERROR = 'PRISM_GET_USERS_ERROR',
-  UPDATE_PROFILE_ERROR = 'UPDATE_PROFILE_ERROR',
-  PRISM_NO_DATA = 'PRISM_NO_DATA',
-}
-
 @Entity()
 export class Patient extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
