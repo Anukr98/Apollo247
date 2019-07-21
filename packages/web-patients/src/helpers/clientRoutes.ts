5a000be4a9c63c5d6453793ff47ddd87b981a652
Kabir Sarin <sarink87@gmail.com>
2019-07-21 16:30:29 +0530

    Add typedef for process.env and update web-patients urls

--- packages/web-patients/src/helpers/clientRoutes.ts
+++ packages/web-patients/src/helpers/clientRoutes.ts
@@ -2,7 +2,7 @@ export const clientBaseUrl = () => {
   const env = process.env.NODE_ENV;
   const port = process.env.WEB_PATIENTS_PORT === '80' ? '' : `:${process.env.WEB_PATIENTS_PORT}`;
   if (env === 'local') return `http://localhost${port}`;
-  if (env === 'development') return `http://patients-web.aph.popcornapps.com${port}`;
+  if (env === 'dev') return `http://web-patients.aph.popcornapps.com${port}`;
   console.error('Invalid NODE_ENV environment variable configuration');
 };
 
