28665cc4be120391f020040060ebbbac5d8c83ed
Kabir Sarin <sarink87@gmail.com>
2019-07-21 16:00:48 +0530

    Update webpack config and packge.json scripts in web-doctors

--- packages/web-doctors/webpack.config.js
+++ packages/web-doctors/webpack.config.js
@@ -5,113 +5,104 @@ const DotenvWebpack = require('dotenv-webpack');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-module.exports = (env, argv) => {
-  if (!env) throw new Error('Invalid webpack env!');
+const envFile = path.resolve(__dirname, '../../.env');
+const dotEnvConfig = dotenv.config({ path: envFile });
+if (dotEnvConfig.error) throw dotEnvConfig.error;
+Object.values(dotEnvConfig).forEach((val, KEY) => (process.env[KEY] = val));
+const isLocal = process.env.NODE_ENV === 'local';
+const isProduction = process.env.NODE_ENV === 'production';
 
-  const envFile = path.resolve(__dirname, `../../${env}.env`);
-  console.log(`Loading ${envFile}...\n`);
+const distDir = path.resolve(__dirname, 'dist');
 
-  const dotEnvConfig = dotenv.config({ path: envFile });
-  if (dotEnvConfig.error) throw dotEnvConfig.error;
-  Object.values(dotEnvConfig).forEach((val, KEY) => (process.env[KEY] = val));
-  const isTest = process.env.NODE_ENV === 'test';
-  const isLocal = process.env.NODE_ENV === 'local';
-  const isProduction = process.env.NODE_ENV === 'production';
+const plugins = [
+  new DotenvWebpack({ path: envFile }),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    chunks: ['index'],
+    template: './index.html',
+    inject: true,
+  }),
+];
+if (isLocal) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
 
-  const distDir = path.resolve(__dirname, 'dist');
+const rhlBabelLoader = {
+  loader: 'babel-loader',
+  options: {
+    plugins: ['react-hot-loader/babel'],
+  },
+};
+const tsLoader = { loader: 'awesome-typescript-loader' };
+const urlLoader = {
+  loader: 'url-loader',
+  options: {
+    limit: 16384,
+    fallback: 'file-loader',
+    name: '[path][name]-[hash:6].[ext]',
+  },
+};
 
-  const plugins = [
-    new DotenvWebpack({ path: envFile }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      chunks: ['index'],
-      template: './index.html',
-      inject: true,
-    }),
-  ];
-  if (isTest || isLocal) {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
 
-  const rhlBabelLoader = {
-    loader: 'babel-loader',
-    options: {
-      plugins: ['react-hot-loader/babel'],
-    },
-  };
-  const tsLoader = { loader: 'awesome-typescript-loader' };
-  const urlLoader = {
-    loader: 'url-loader',
-    options: {
-      limit: 16384,
-      fallback: 'file-loader',
-      name: '[path][name]-[hash:6].[ext]',
-    },
-  };
+  context: path.resolve(__dirname, 'src'),
 
-  return {
-    mode: isProduction ? 'production' : 'development',
+  entry: { index: ['index.tsx'] },
 
-    context: path.resolve(__dirname, 'src'),
+  output: {
+    publicPath: '/', // Where you uploaded your bundled files. (Relative to server root)
+    path: distDir, // Local disk directory to store all your output files (Absolute path)
+    filename: '[name]-[hash:6].bundle.js',
+  },
 
-    entry: { index: ['index.tsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [/node_modules/],
+        use: isProduction ? [tsLoader] : [rhlBabelLoader, tsLoader],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        use: [urlLoader],
+      },
+    ],
+  },
 
-    output: {
-      publicPath: '/', // Where you uploaded your bundled files. (Relative to server root)
-      path: distDir, // Local disk directory to store all your output files (Absolute path)
-      filename: '[name]-[hash:6].bundle.js',
-    },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
+    alias: isLocal
+      ? {
+          'react-dom': '@hot-loader/react-dom',
+        }
+      : undefined,
+  },
 
-    module: {
-      rules: [
-        {
-          test: /\.(j|t)sx?$/,
-          include: [path.resolve(__dirname, 'src')],
-          exclude: [/node_modules/],
-          use: isProduction ? [tsLoader] : [rhlBabelLoader, tsLoader],
+  optimization: {
+    // Enable these for tree-shaking capabilities.
+    // Also set `"sideEffects": false` in `package.json`
+    sideEffects: true,
+    usedExports: true,
+  },
+
+  devServer: isLocal
+    ? {
+        publicPath: '/', // URL path where the webpack files are served from
+        contentBase: distDir, // A directory to serve files non-webpack files from (Absolute path)
+        host: '0.0.0.0',
+        port: process.env.WEB_DOCTORS_PORT,
+        disableHostCheck: true,
+        hot: true,
+        inline: true,
+        historyApiFallback: true,
+        watchOptions: {
+          ignored: [/node_modules([\\]+|\/)+(?!@aph)/],
         },
-        {
-          test: /\.(png|jpg|jpeg|svg|gif)$/,
-          use: [urlLoader],
-        },
-      ],
-    },
+      }
+    : undefined,
 
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
-      alias:
-        isTest || isLocal
-          ? {
-              'react-dom': '@hot-loader/react-dom',
-            }
-          : undefined,
-    },
-
-    optimization: {
-      // Enable these for tree-shaking capabilities.
-      // Also set `"sideEffects": false` in `package.json`
-      sideEffects: true,
-      usedExports: true,
-    },
-
-    devServer:
-      isTest || isLocal
-        ? {
-            publicPath: '/', // URL path where the webpack files are served from
-            contentBase: distDir, // A directory to serve files non-webpack files from (Absolute path)
-            host: '0.0.0.0',
-            port: process.env.WEB_DOCTORS_PORT,
-            disableHostCheck: true,
-            hot: true,
-            inline: true,
-            historyApiFallback: true,
-            watchOptions: {
-              ignored: [/node_modules([\\]+|\/)+(?!@aph)/],
-            },
-          }
-        : undefined,
-
-    plugins,
-  };
+  plugins,
 };
