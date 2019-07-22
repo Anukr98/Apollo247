9560b18a7eb51fb20bfd387f39f186c731a16b4e
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2019-07-22 18:11:45 +0530

    integrated past searches api

--- packages/web-patients/src/components/PastSearches.tsx
+++ packages/web-patients/src/components/PastSearches.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/styles';
 import { Theme, Grid, Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useQueryWithSkip } from 'hooks/apolloHooks';
+import { GET_PAST_SEARCHES } from 'graphql/pastsearches';
+import { GetPastSearches } from 'graphql/types/getPastSearches';
+import { SEARCH_TYPE } from 'graphql/types/globalTypes';
+import _uniqueId from 'lodash/uniqueId';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme: Theme) => {
   return createStyles({
@@ -32,37 +39,70 @@ const useStyles = makeStyles((theme: Theme) => {
   });
 });
 
-export const PastSearches: React.FC = (props) => {
+interface PastSearchProps {
+  speciality: (specialitySelected: string) => void;
+  disableFilter: (disableFilters: boolean) => void;
+}
+
+export const PastSearches: React.FC<PastSearchProps> = (props) => {
   const classes = useStyles();
+  const { data, loading, error } = useQueryWithSkip<GetPastSearches>(GET_PAST_SEARCHES);
+  const { speciality, disableFilter } = props;
+
+  if (loading) {
+    return <LinearProgress variant="query" />;
+  }
+  if (error) {
+    return <div>Error....</div>;
+  }
+
+  const pastSearches =
+    data && data.getPastSearches ? (
+      data.getPastSearches.map((searchDetails) => {
+        return searchDetails && searchDetails.searchType === SEARCH_TYPE.DOCTOR ? (
+          <Grid item xs={3} key={_uniqueId('psearch_doctor_')}>
+            <Link to={`/doctor-details/${searchDetails.typeId}`}>
+              <div className={classes.contentBox}>
+                <Avatar
+                  alt={(searchDetails && searchDetails.name) || ''}
+                  src={(searchDetails && searchDetails.image) || ''}
+                  className={classes.bigAvatar}
+                />
+                {(searchDetails && searchDetails.name) || ''}
+              </div>
+            </Link>
+          </Grid>
+        ) : (
+          <Grid
+            item
+            xs={3}
+            title={(searchDetails && searchDetails.name) || ''}
+            onClick={(e) => {
+              speciality(e.currentTarget.title);
+              disableFilter(false);
+            }}
+            key={_uniqueId('psearch_spl_')}
+          >
+            <div className={classes.contentBox}>
+              <Avatar
+                alt={(searchDetails && searchDetails.name) || ''}
+                src={(searchDetails && searchDetails.image) || ''}
+                className={classes.bigAvatar}
+              />
+              {(searchDetails && searchDetails.name) || ''}
+            </div>
+          </Grid>
+        );
+      })
+    ) : (
+      <div>No data found....</div>
+    );
 
   return (
     <div className={classes.root}>
       <div className={classes.searchList}>
         <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <div className={classes.contentBox}>
-              <Avatar alt="" src={require('images/ic_mascot.png')} className={classes.bigAvatar} />
-              Dr. Alok Mehta
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className={classes.contentBox}>
-              <Avatar alt="" src={require('images/ic_mascot.png')} className={classes.bigAvatar} />
-              Dr. Vikas Siddeshwar
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className={classes.contentBox}>
-              <Avatar alt="" src={require('images/ic_mascot.png')} className={classes.bigAvatar} />
-              General Physician
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className={classes.contentBox}>
-              <Avatar alt="" src={require('images/ic_mascot.png')} className={classes.bigAvatar} />
-              Cardiology
-            </div>
-          </Grid>
+          {pastSearches}
         </Grid>
       </div>
     </div>
