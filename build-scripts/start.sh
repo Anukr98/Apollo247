1528fae43f2c0052669bfb5ca140f6b4598a2216
Kabir Sarin <sarink87@gmail.com>
2019-07-23 18:24:20 +0530

    Make sure to stop the old containers

--- build-scripts/start.sh
+++ build-scripts/start.sh
@@ -2,7 +2,9 @@
 # $1 refers the environment: 'local' | 'dev' | 'qa' | 'production'
 # This script should run while ssh'ed into the remote server
 
+cd apollo-hospitals
 docker-compose stop
+cd ..
 rm -rf apollo-hospitals
 tar -xzf aph-$1.tar.gz
 mv -v aph-$1.tar.gz aph-$1-$(date +%Y-%m-%d_%H-%M-%S).tar.gz
