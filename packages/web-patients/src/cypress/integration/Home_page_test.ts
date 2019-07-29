ee5aa2e5308f09f2c6b36e0bd9f9962b80adf1d2
Kabir Sarin <sarink87@gmail.com>
2019-07-29 14:23:09 +0530

    Refactor home_page/login tests

--- packages/web-patients/src/cypress/integration/Home_page_test.ts
+++ packages/web-patients/src/cypress/integration/Home_page_test.ts
@@ -2,219 +2,37 @@ import { clientRoutes } from 'helpers/clientRoutes';
 import { jane, john, jimmy } from 'cypress/fixtures/patientsFixtures';
 import { Relation } from 'graphql/types/globalTypes';
 
-describe('Home page', () => {
+describe('Home page (when signed out)', () => {
   beforeEach(() => {
-    // Add a wait so we finish attempting to authenticate
+    cy.signOut();
     cy.visitAph(clientRoutes.welcome()).wait(500);
   });
 
-  it('Clicking "Consult a doctor" when not signed in renders sign in popup', () => {
-    cy.signOut();
-    cy.visitAph(clientRoutes.welcome()).wait(500);
+  it('Clicking "Consult a doctor" renders sign in popup', () => {
     cy.contains(/consult a doctor/i).click();
     cy.get('[data-cypress="SignIn"]').should('be.visible');
   });
+});
+
+describe('Home page (when signed in)', () => {
+  const me = { ...jimmy, relation: Relation.ME };
+  const patients = [jane, john, me];
+
+  beforeEach(() => {
+    cy.signIn(patients);
+    cy.visitAph(clientRoutes.welcome()).wait(500);
+  });
 
   it('All the profiles should be visible after logging in', () => {
-    const me = { ...jimmy, relation: Relation.ME };
-    const patients = [jane, john, me];
-    cy.signIn(patients);
-    cy.visitAph(clientRoutes.welcome())
-      .wait(500)
-      .get('[data-cypress="HeroBanner"]')
+    cy.get('[data-cypress="HeroBanner"]')
       .contains(me.firstName!.toLowerCase())
       .click({ force: true });
     patients.forEach(({ firstName }) => cy.contains(firstName!.toLowerCase()));
     cy.contains('button', /add member/i);
   });
-  it('Apollo logo is displayed', () => {
-    cy.get('header').find('img');
-  });
 
-  // it('Available tabs in the header', () => {
-  //   // These don't exist yet
-  //   cy.get('header').contains('Consult Room');
-  //   cy.get('header').contains('Health Records');
-  //   cy.get('header').contains('Tests & Medicines');
-  // });
-
-  it('should display profile icon', () => {
-    cy.get('header')
-      .find('div[class*="userCircle"]')
-      .should('exist');
+  it('Shows "me" selected in hello dropdown', () => {
+    cy.contains('hello');
+    cy.contains(me!.firstName!.toLowerCase());
   });
-
-  it('Button for consult a doctor', () => {
-    cy.get('[data-cypress="HeroBanner"]')
-      .find('span')
-      .contains('Consult a doctor')
-      .should('exist');
-  });
-
-  it('Name of the logged in user should be visible beside Hello', () => {
-    cy.get('h1')
-      .contains('there!')
-      .should('exist');
-  });
-
-  it('Clicking "Consult a doctor" when not signed in renders sign in popup', () => {
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]').should('be.visible');
-  });
-
-  it('Are you not feeling well', () => {
-    cy.contains('p', 'Not feeling well today').should('exist');
-  });
-
-  it('All the profiles should be visible after logging in', () => {
-    const me = { ...jimmy, relation: Relation.ME };
-    const patients = [jane, john, me];
-    cy.signIn(patients);
-    cy.visitAph(clientRoutes.welcome())
-      .wait(500)
-      .get('[data-cypress="HeroBanner"]')
-      .find('div[role*="button"]')
-      .contains(me.firstName!.toLowerCase())
-      .click({ force: true });
-
-    patients.forEach(({ firstName }) => cy.contains(firstName!.toLowerCase())); //passes, though doesn't display right?
-    //played around endlessly with line 141-153 of HeroBanner, to no avail.
-    //Proper capitalization displays in console, though.
-  });
-
-  it('Add member option verified', () => {
-    const me = { ...jane, relation: Relation.ME };
-    const patients = [jimmy, john, me];
-    cy.signIn(patients);
-    cy.visitAph(clientRoutes.welcome()).wait(500);
-    cy.get('[data-cypress="HeroBanner"]')
-      .find('div[role*="button"]')
-      .contains(me.firstName!.toLowerCase())
-      .click({ force: true });
-    cy.get('button[class*="addMember"]')
-      .contains('Add Member')
-      .should('exist');
-  });
-
-  it('links of search specialist & star doctors in horizontal', () => {
-    cy.get('a').contains('Find specialist');
-    cy.get('a').contains('Search Medicine');
-    cy.get('a').contains('Book a test');
-    cy.get('a').contains('Who are star doctors');
-  });
-  it('Prefix "+91" should be displayed before the mobile number input field', () => {
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('p')
-      .contains('+91')
-      .should('exist');
-  });
-  it('There should be validation upon entering anything non-numerical', () => {
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('test');
-    cy.get('input[name*="mobileNumber"]')
-      .contains('test')
-      .should('not.exist');
-  });
-  it('Next arrow should be disabled upon entering anything invalid number', () => {
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('0123456789')
-      .find('button[type*="submit"]')
-      .should('not.exist');
-  });
-  it('There should be validation upon entering mobile number starting with zero', () => {
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('0123456789');
-    cy.get('div[class*="MuiFormHelperText-filled"]').should(
-      'contain',
-      'This seems like a wrong number'
-    );
-  });
-  it('Ten digit numbers starting with non-zero should be considered valid', () => {
-    //fails with any number starting w/ 0-5
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('1234567890');
-    cy.get('div[class*="MuiFormHelperText-filled"]').should(
-      'contain',
-      'OTP will be sent to this number'
-    );
-  });
-  it('Validation error message should be in red font', () => {
-    //chai-colors must be properly imported for test to pass
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('0123456789');
-    cy.get('div[class*="MuiFormHelperText-filled"]')
-      .contains('This seems like a wrong number')
-      .should('have.css', 'color')
-      .and('be.colored', '#890000');
-  });
-  it('Validation success message should be in green font', () => {
-    //chai-colors must be properly imported for test to pass
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('9876543210');
-    cy.get('div[class*="MuiFormHelperText-filled"]')
-      .contains('OTP will be sent to this number')
-      .should('have.css', 'color')
-      .and('be.colored', 'rgba(2,71,91,0.6)');
-  });
-
-  it('Resend OTP hyperlink should be displayed, clicking on which resends OTP to the provided mobile number', () => {
-    //fails because this doesn't trigger firebase captcha exception?
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('9876543210');
-    cy.get('button[type*="submit"]')
-      .click()
-      .wait(5000);
-    cy.get('button[class*="resendBtn"]')
-      .should('contain', 'Resend OTP')
-      .find('input[name*="mobileNumber"]')
-      .type('555');
-    cy.get('button[class*="resendBtn"]').click();
-    cy.get('input[name*="mobileNumber"]')
-      .should('not')
-      .contains('555');
-  });
-
-  it('After clicking  Resend OTP, header text should be changed to "Type in the OTP that has been resent to you for authentication"', () => {
-    //fails, because that doesn't exist yet
-    cy.contains(/consult a doctor/i).click();
-    cy.get('[data-cypress="SignIn"]')
-      .find('input[name*="mobileNumber"]')
-      .type('9876543210');
-    cy.get('button[type*="submit"]')
-      .click()
-      .wait(5000);
-    cy.get('button[class*="resendBtn"]')
-      .find('p')
-      .contains('Type in the OTP sent to you, to authenticate')
-      .should('exist');
-
-    cy.get('button[class*="resendBtn"]').click();
-    cy.get('input[name*="mobileNumber"]')
-      .find('p')
-      .contains('Type in the OTP sent to you, to authenticate')
-      .should('exist');
-  });
-
-  //OTP should be received twice on the provided mobile number,
-  // once as soon as we redirect to OTP screen and the other upon clicking on Resend OTP
-
-  // There should be a back arrow on the top left of the screen,
-  // clicking on which user should redirect to the previous screen (Enter mobile number screen)
-
-  // API-21 all
 });
--- packages/web-patients/src/cypress/integration/Login_test.tsx
+++ packages/web-patients/src/cypress/integration/Login_test.tsx
@@ -1,23 +1,75 @@
 import { clientRoutes } from 'helpers/clientRoutes';
-import { Relation } from 'graphql/types/globalTypes';
 import { jane, john } from 'cypress/fixtures/patientsFixtures';
+import { Relation } from 'graphql/types/globalTypes';
 
 describe('Login', () => {
-  const johnMe = { ...john, relation: Relation.ME };
-  const patients = [jane, johnMe];
-
   beforeEach(() => {
-    cy.signIn(patients);
+    cy.signOut();
     cy.visitAph(clientRoutes.welcome()).wait(500);
-  });
-
-  it('Can do a real login with firebase', () => {
-    cy.get('[data-cypress="Navigation"]').should('not.exist');
-
     cy.get('[data-cypress="Header"]')
       .find('[class*="userCircle"]')
-      .click()
-      .wait(2000);
+      .click();
+  });
+
+  it('Prefix "+91" should be displayed before the mobile number input field', () => {
+    cy.get('[data-cypress="SignIn"]').contains('+91');
+  });
+
+  it('There should be validation upon entering anything non-numerical', () => {
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[name*="mobileNumber"]')
+      .type('test');
+    cy.get('input[name*="mobileNumber"]')
+      .contains('test')
+      .should('not.exist');
+    cy.get('[data-cypress="SignIn"]')
+      .find('button[type="submit"]')
+      .should('be.disabled');
+  });
+
+  it('Next arrow should be disabled upon entering an invalid number', () => {
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[name*="mobileNumber"]')
+      .type('0123456789');
+    cy.get('[data-cypress="SignIn"]')
+      .find('button[type="submit"]')
+      .should('be.disabled');
+  });
+
+  it('There should be validation upon entering mobile number starting with zero', () => {
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[name*="mobileNumber"]')
+      .type('0123456789');
+    cy.get('[data-cypress="SignIn"]')
+      .find('button[type="submit"]')
+      .should('be.disabled');
+    cy.contains('This seems like a wrong number');
+  });
+
+  it('Ten digit numbers starting with non-zero should be considered valid', () => {
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[name*="mobileNumber"]')
+      .type('934567890');
+    cy.get('[data-cypress="SignIn"]')
+      .find('button[type="submit"]')
+      .should('be.enabled');
+    cy.contains('OTP will be sent to this number');
+  });
+});
+
+describe('Login (Firebase)', () => {
+  beforeEach(() => {
+    const johnMe = { ...john, relation: Relation.ME };
+    const patients = [jane, johnMe];
+    cy.signIn(patients);
+    cy.visitAph(clientRoutes.welcome()).wait(500);
+    cy.get('[data-cypress="Header"]')
+      .find('[class*="userCircle"]')
+      .click();
+  });
+
+  it('Firebase: Can do a real login', () => {
+    cy.get('[data-cypress="Navigation"]').should('not.exist');
 
     cy.get('[data-cypress="SignIn"]')
       .find('input[type="tel"]')
@@ -37,13 +89,36 @@ describe('Login', () => {
       .should('not.exist');
 
     cy.get('[data-cypress="Navigation"]').should('exist');
-
-    cy.clearFirebaseDb();
   });
 
-  it('Shows "me" selected in hello dropdown', () => {
-    const me = patients.find((p) => p.relation === Relation.ME);
-    cy.contains('hello');
-    cy.contains(me!.firstName!.toLowerCase());
+  it('Firebase: Resend OTP', () => {
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[type="tel"]')
+      .type('9999999999')
+      .get('button[type="submit"]')
+      .click()
+      .find('[class*="MuiCircularProgress"]')
+      .should('not.exist');
+
+    cy.contains('Type in the OTP sent to you, to authenticate');
+
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[type*="tel"]')
+      .each(($el) => cy.wrap($el).type('9'));
+
+    cy.get('[data-cypress="SignIn"]')
+      .should('be.visible')
+      .contains('button', 'Resend OTP')
+      .click();
+
+    cy.get('[data-cypress="SignIn"]')
+      .find('input[type*="tel"]')
+      .each(($el) => cy.wrap($el).should('be.empty'));
+
+    cy.get('[data-cypress="SignIn"]')
+      .find('[class*="MuiCircularProgress"]')
+      .should('not.exist');
+
+    cy.contains('Type in the OTP that has been resent to you for authentication');
   });
 });
