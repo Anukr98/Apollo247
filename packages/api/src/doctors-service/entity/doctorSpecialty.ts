6873e8657fc6544307455c457de58a2f67740496
sushma <sushma.voleti@popcornapps.com>
2019-07-30 17:32:46 +0530

    renamed Speciality to Specialty

---
+++ packages/api/src/doctors-service/entity/doctorSpecialty.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  OneToMany,
+} from 'typeorm';
+import { Doctor } from 'doctors-service/entity/doctor';
+
+@Entity()
+export class DoctorSpecialty extends BaseEntity {
+  @Column()
+  createdDate: Date;
+
+  @PrimaryGeneratedColumn('uuid')
+  id: String;
+
+  @Column()
+  name: String;
+
+  @OneToMany((type) => Doctor, (doctor) => doctor.specialty)
+  doctor: Doctor[];
+
+  @Column({ nullable: true })
+  updatedDate: Date;
+
+  @BeforeInsert()
+  updateDateCreation() {
+    this.createdDate = new Date();
+  }
+
+  @BeforeUpdate()
+  updateDateUpdate() {
+    this.updatedDate = new Date();
+  }
+}
