8c4cb8db01d9081082f0aef5129a72a17d71eff4
Mamatha V <mamatha.vattimalla@popcornapps.com>
2019-07-30 11:25:30 +0530

    Resolved PR issues

--- packages/mobile-patients/index.js
+++ packages/mobile-patients/index.js
@@ -1,5 +1,3 @@
-// GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
-
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import { AppContainer } from '@aph/mobile-patients/src/components/AppContainer';
