62e2a312d2c8b6350175b96964be7e0cfd3c2e52
Ramarao <ramarao.jaligama@popcornapps.com>
2019-08-02 19:42:44 +0530

    consult room UI

--- packages/web-doctors/src/components/ConsultRoom.tsx
+++ packages/web-doctors/src/components/ConsultRoom.tsx
@@ -24,19 +24,30 @@ const useStyles = makeStyles((theme: Theme) => {
       top: 0,
     },
     chatContainer: {
-      border: '1px solid #ff0000',
       marginTop: 50,
       minHeight: 500,
     },
     petient: {
-      color: '#ff0000',
-      backgroundColor: '#fff',
+      color: '#0087ba',
       textAlign: 'left',
+      backgroundColor: '#fff',
+      padding: 15,
+      fontWeight: theme.typography.fontWeightMedium,
+      display: 'inline-block',
+      borderRadius: 5,
+      boxShadow: '0 2px 4px 0 #00000026',
+      minWidth: 120,
     },
     doctor: {
-      color: '#00ff00',
-      backgroundColor: '#fff',
-      textAlign: 'right',
+      backgroundColor: '#f0f4f5',
+      padding: 15,
+      color: '#02475b',
+      fontWeight: theme.typography.fontWeightMedium,
+      display: 'inline-block',
+      borderRadius: 5,
+      boxShadow: '0 2px 4px 0 #00000026',
+      minWidth: 120,
+      marginRight: 30,
     },
     boldTxt: {
       fontWeight: 700,
@@ -52,8 +63,131 @@ const useStyles = makeStyles((theme: Theme) => {
       align: 'left',
     },
     showIncomingBox: {
-      color: "#f00",
-    }
+      color: '#f00',
+    },
+    container: {
+      maxWidth: 1064,
+      margin: 'auto',
+      position: 'relative',
+      boxShadow: '0 5px 20px 0 rgba(128, 128, 128, 0.3)',
+      backgroundColor: '#f7f7f7',
+      minHeight: 700,
+    },
+    breadcrumbs: {
+      marginLeft: 20,
+      marginRight: 20,
+      fontSize: 13,
+      paddingTop: 36,
+      paddingBottom: 11,
+      fontWeight: 600,
+      color: '#02475b',
+      textTransform: 'uppercase',
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+
+      [theme.breakpoints.down('xs')]: {
+        position: 'fixed',
+        zIndex: 2,
+        top: 0,
+        width: '100%',
+        backgroundColor: theme.palette.common.white,
+        margin: 0,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.1)',
+      },
+    },
+    timeLeft: {
+      fontSize: 12,
+      fontWeight: 500,
+      color: 'rgba(2, 71, 91, 0.6)',
+      textTransform: 'capitalize',
+    },
+    backArrow: {
+      cursor: 'pointer',
+      marginRight: 50,
+      [theme.breakpoints.up(1220)]: {
+        position: 'absolute',
+        left: -82,
+        top: 20,
+        width: 48,
+        height: 48,
+        lineHeight: '36px',
+        borderRadius: '50%',
+        textAlign: 'center',
+        backgroundColor: '#02475b',
+      },
+      '& img': {
+        verticalAlign: 'bottom',
+      },
+    },
+    whiteArrow: {
+      verticalAlign: 'middle',
+      [theme.breakpoints.down(1220)]: {
+        display: 'none',
+      },
+    },
+    blackArrow: {
+      verticalAlign: 'middle',
+      [theme.breakpoints.up(1220)]: {
+        display: 'none',
+      },
+    },
+    docterChat: {
+      display: 'block',
+      width: '100%',
+      textAlign: 'right',
+      margin: '5px 5px 10px 5px',
+    },
+    patientChat: {
+      display: 'block',
+      maxWidth: '50%',
+      margin: '5px 5px 10px 70px',
+      position: 'relative',
+
+      '& img': {
+        position: 'absolute',
+        left: -50,
+        top: 5,
+        width: 40,
+        borderRadius: '50%',
+      },
+    },
+    incomingContainer: {
+      textAlign: 'right',
+      paddingRight: 20,
+    },
+    incomingBtn: {
+      position: 'relative',
+      width: 170,
+      height: 168,
+      float: 'right',
+      borderRadius: 10,
+      boxShadow: '0 5px 20px 0 rgba(0,0,0,0.6)',
+      overflow: 'hidden',
+      '& img': {
+        maxWidth: '100%',
+      },
+      '& div': {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,0.2)',
+        top: 0,
+        textAlign: 'center',
+        paddingTop: 10,
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 500,
+      },
+    },
+    endcall: {
+      position: 'absolute',
+      width: 40,
+      bottom: 20,
+    },
   };
 });
 interface MessagesProps {
@@ -109,7 +243,7 @@ export const ConsultRoom: React.FC = (props) => {
       if (messages.length !== newmessage.length) {
         setMessages(newmessage);
         const lastMessage = newmessage[newmessage.length - 1];
-        if(lastMessage && lastMessage.message === 'callme'){
+        if (lastMessage && lastMessage.message === 'callme') {
           setIsCalled(true);
         }
       }
@@ -139,9 +273,11 @@ export const ConsultRoom: React.FC = (props) => {
       leftComponent++;
       rightComponent = 0;
       return (
-        <div className={classes.doctor}>
-          {leftComponent == 1 && <span className={classes.boldTxt}>Me: </span>}
-          <span>{rowData.message}</span>
+        <div className={classes.docterChat}>
+          <div className={classes.doctor}>
+            {leftComponent == 1 && <span className={classes.boldTxt}></span>}
+            <span>{rowData.message}</span>
+          </div>
         </div>
       );
     }
@@ -149,9 +285,15 @@ export const ConsultRoom: React.FC = (props) => {
       leftComponent = 0;
       rightComponent++;
       return (
-        <div className={classes.petient}>
-          {rightComponent == 1 && <span className={classes.boldTxt}>Sai: </span>}
-          <span>{rowData.message}</span>
+        <div className={classes.patientChat}>
+          <div className={classes.petient}>
+            {rightComponent == 1 && (
+              <span className={classes.boldTxt}>
+                <img src={require('images/ic_patientchat.png')} />
+              </span>
+            )}
+            <span>{rowData.message}</span>
+          </div>
         </div>
       );
     }
@@ -170,28 +312,45 @@ export const ConsultRoom: React.FC = (props) => {
       <div className={classes.headerSticky}>
         <Header />
       </div>
-      {isCalled && 
-         <div> 
-         <Button variant="text" className={classes.sendMsgBtn}>
-           Incoming Call
-         </Button>
-       </div>
-      }
-     
-      <div>
-        <div className={classes.chatContainer}>{messagessHtml}</div>
+      <div className={classes.container}>
+        <div className={classes.breadcrumbs}>
+          <div>
+            <div className={classes.backArrow}>
+              <img className={classes.blackArrow} src={require('images/ic_back.svg')} />
+              <img className={classes.whiteArrow} src={require('images/ic_back_white.svg')} />
+            </div>
+          </div>
+          CONSULT ROOM{' '}
+          <span className={classes.timeLeft}> &nbsp; | &nbsp; Consult Duration 00:25</span>
+        </div>
         <div>
-          <AphInput
-            className={classes.inputWidth}
-            inputProps={{ type: 'text' }}
-            value={messageText}
-            onChange={(event) => {
-              setMessageText(event.currentTarget.value);
-            }}
-          />
-          <Button variant="text" className={classes.sendMsgBtn} onClick={() => send()}>
-            Send
-          </Button>
+          <div className={classes.chatContainer}>{messagessHtml}</div>
+          <div>
+            {isCalled && (
+              <div className={classes.incomingContainer}>
+                <div className={classes.incomingBtn}>
+                  <img src={require('images/ic_patientchat.png')} />
+                  <div>
+                    <span>Ringing</span>
+                    <img src={require('images/ic_stopcall.svg')} className={classes.endcall} />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <div>
+            <AphInput
+              className={classes.inputWidth}
+              inputProps={{ type: 'text' }}
+              value={messageText}
+              onChange={(event) => {
+                setMessageText(event.currentTarget.value);
+              }}
+            />
+            <Button variant="text" className={classes.sendMsgBtn} onClick={() => send()}>
+              Send
+            </Button>
+          </div>
         </div>
       </div>
     </div>
