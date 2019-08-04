50cd92ebd10aefb042fcb0c3434487c3a6207d91
BarkerAW83 <BarkerAW@gmail.com>
2019-08-04 12:08:51 -0400

    "toIsoString function elevated from isDateValid func for use elsewhere"

--- packages/universal/src/aphValidators.ts
+++ packages/universal/src/aphValidators.ts
@@ -11,35 +11,29 @@ export const isNameValid = (name: string) => {
 
 export const aphClientDateFormat = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
 
-export const isDobValid = (ddmmyyyy: string) => {
-  const toIsoString = (ddmmyyyy: string) => {
-    const dateParts = ddmmyyyy.split('/');
-    const dd = dateParts[0];
-    const mm = dateParts[1];
-    const yyyy = dateParts[2];
-    return `${yyyy}-${mm}-${dd}`;
-  };
-
-  const today = new Date();
-  const dob = new Date(toIsoString(ddmmyyyy));
-  return today > dob;
-};
-
-export const isDateValid = (ddmmyyyy: string) => {
-  const isCorrectFormat = aphClientDateFormat.test(ddmmyyyy);
-  console.log('DATE INPUT IS: ', ddmmyyyy);
-  console.log('IS CORRECT FORMAT? : ', isCorrectFormat);
-  if (!isCorrectFormat) return false;
-
+export const toIsoString = (ddmmyyyy: string) => {
   const dateParts = ddmmyyyy.split('/');
   const dd = dateParts[0];
   const mm = dateParts[1];
   const yyyy = dateParts[2];
-  const dateStr = `${yyyy}-${mm}-${dd}`;
-  const isValidJsDate = !isNaN(new Date(dateStr).valueOf());
+  return `${yyyy}-${mm}-${dd}`;
+};
+export const isDobValid = (ddmmyyyy: string) => {
+  if (!isDateValid(ddmmyyyy)) return false;
+  const isoStr = toIsoString(ddmmyyyy);
+  const date = new Date(isoStr);
+  const isInPast = new Date() > date;
+  return isInPast;
+};
+export const isDateValid = (ddmmyyyy: string) => {
+  const isCorrectFormat = aphClientDateFormat.test(ddmmyyyy);
+  if (!isCorrectFormat) return false;
+  const isoStr = toIsoString(ddmmyyyy); //
+  const date = new Date(isoStr);
+  const isValidJsDate = !isNaN(date.valueOf());
   // https://stackoverflow.com/questions/12756159/regex-and-iso8601-formatted-datetime
   const isValidCalendarDate = /(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))/.test(
-    dateStr
+    isoStr
   );
   return isValidJsDate && isValidCalendarDate;
 };
