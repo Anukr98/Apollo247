fe2d9e53f829b6c7f800fb2c3b3858301c66a91e
sushma <sushma.voleti@popcornapps.com>
2019-08-05 22:35:10 +0530

    added removeTeamDoctor API

--- packages/api/src/doctors-service/resolvers/starTeam.ts
+++ packages/api/src/doctors-service/resolvers/starTeam.ts
@@ -4,12 +4,14 @@ import { DoctorsServiceContext } from 'doctors-service/doctorsServiceContext';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/AphErrorMessages';
 import { StarTeamRepository } from 'doctors-service/repositories/starTeamRepository';
-import { StarTeam } from 'doctors-service/entities/';
+import { StarTeam, Doctor } from 'doctors-service/entities/';
 import { isUndefined } from 'util';
+import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
 
 export const starTeamTypeDefs = gql`
   extend type Mutation {
     makeTeamDoctorActive(associatedDoctor: String, starDoctor: String): Boolean
+    removeTeamDoctorFromStarTeam(associatedDoctor: String, starDoctor: String): DoctorDetails
   }
 `;
 
@@ -41,8 +43,38 @@ const makeTeamDoctorActive: Resolver<
   return updatedDoctorDetails.isActive;
 };
 
+const removeTeamDoctorFromStarTeam: Resolver<
+  null,
+  { associatedDoctor: string; starDoctor: string },
+  DoctorsServiceContext,
+  Doctor
+> = async (parent, args, { dbConnect, currentUser }) => {
+  const starRepo = dbConnect.getCustomRepository(StarTeamRepository);
+  const doctorDetails = (await starRepo.getTeamDoctorData(
+    args.associatedDoctor,
+    args.starDoctor
+  )) as StarTeam;
+
+  if (isUndefined(doctorDetails)) throw new AphError(AphErrorMessages.INSUFFICIENT_PREVILAGES);
+
+  const isAuthorized = doctorDetails.starDoctor.id === currentUser.id;
+  if (!isAuthorized) throw new AphError(AphErrorMessages.INSUFFICIENT_PREVILAGES);
+
+  await starRepo.removeFromStarteam(<string>doctorDetails.id);
+
+  let doctordata: Doctor;
+  try {
+    const doctorRepository = dbConnect.getCustomRepository(DoctorRepository);
+    doctordata = (await doctorRepository.findById(args.starDoctor)) as Doctor;
+  } catch (getProfileError) {
+    throw new AphError(AphErrorMessages.GET_PROFILE_ERROR, undefined, { getProfileError });
+  }
+  return doctordata;
+};
+
 export const starTeamResolvers = {
   Mutation: {
     makeTeamDoctorActive,
+    removeTeamDoctorFromStarTeam,
   },
 };
