900403ab1c98914be5413c62d15c5aa30a4567a8
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2019-08-08 15:46:19 +0530

    Bottom TabBar modified

--- packages/mobile-patients/src/components/TabBar.tsx
+++ packages/mobile-patients/src/components/TabBar.tsx
@@ -9,40 +9,59 @@ import {
   MyHealth,
   Person,
   ShoppingCart,
+  PersonFocused,
+  MyHealthFocused,
+  ConsultationRoomFocused,
+  ShoppingCartFocused,
 } from '@aph/mobile-patients/src/components/ui/Icons';
 import { theme } from '@aph/mobile-patients/src/theme/theme';
 import React from 'react';
 import { createBottomTabNavigator, NavigationRouteConfig } from 'react-navigation';
 import { Consult } from '@aph/mobile-patients/src/components/Consult';
 
-const routeToIcon: Partial<Record<keyof typeof AppRoutes, NavigationRouteConfig>> = {
-  ConsultRoom: <ConsultationRoom />,
-  HealthRecords: <MyHealth />,
-  Medicine: <ShoppingCart />,
-  MyAccount: <Person />,
+const routeToIcon: Partial<Record<string, NavigationRouteConfig>> = {
+  'CONSULT ROOM': <ConsultationRoom />,
+  'HEALTH RECORDS': <MyHealth />,
+  'TESTS & MEDICINES': <ShoppingCart />,
+  'MY ACCOUNT': <Person />,
 };
 
-const routeConfigMap: Partial<Record<keyof typeof AppRoutes, NavigationRouteConfig>> = {
-  ConsultRoom: Consult,
-  HealthRecords: HealthRecords,
-  Medicine: Medicine,
-  MyAccount: MyAccount,
+const routeToFocusedIcon: Partial<Record<string, NavigationRouteConfig>> = {
+  'CONSULT ROOM': <ConsultationRoomFocused />,
+  'HEALTH RECORDS': <MyHealthFocused />,
+  'TESTS & MEDICINES': <ShoppingCartFocused />,
+  'MY ACCOUNT': <PersonFocused />,
+};
+
+const routeConfigMap: Partial<Record<string, NavigationRouteConfig>> = {
+  'CONSULT ROOM': ConsultRoom,
+  'HEALTH RECORDS': HealthRecords,
+  'TESTS & MEDICINES': Medicine,
+  'MY ACCOUNT': MyAccount,
 };
 
 export const TabBar = createBottomTabNavigator(routeConfigMap, {
   defaultNavigationOptions: ({ navigation }) => ({
-    tabBarIcon: () => routeToIcon[navigation.state.routeName as AppRoute],
+    tabBarIcon: ({ focused }) =>
+      focused
+        ? routeToFocusedIcon[navigation.state.routeName as AppRoute]
+        : routeToIcon[navigation.state.routeName as AppRoute],
   }),
   tabBarOptions: {
     activeTintColor: theme.colors.TAB_BAR_ACTIVE_TINT_COLOR,
     inactiveTintColor: theme.colors.TAB_BAR_INACTIVE_TINT_COLOR,
+    activeBackgroundColor: theme.colors.LIGHT_BLUE,
+    style: {
+      borderTopColor: 'transparent',
+    },
     labelStyle: {
+      borderTopColor: 'transparent',
       fontFamily: 'IBMPlexSans-SemiBold',
-      fontSize: 8,
+      fontSize: 7,
       letterSpacing: 0.5,
       textAlign: 'center',
-      textTransform: 'uppercase',
-      top: -2,
+      marginTop: 9,
+      marginBottom: 7,
     },
   },
 });
