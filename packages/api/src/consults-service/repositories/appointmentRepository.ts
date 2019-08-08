ac364057876e256c4ffbde101e2822f3b81569aa
adityakvs <aditya.kvs@popcornapps.com>
2019-08-08 12:21:39 +0530

    Added availability logic for search by name functionality

--- packages/api/src/consults-service/repositories/appointmentRepository.ts
+++ packages/api/src/consults-service/repositories/appointmentRepository.ts
@@ -1,8 +1,8 @@
-import { EntityRepository, Repository, Between } from 'typeorm';
+import { EntityRepository, Repository, Between, In } from 'typeorm';
 import { Appointment } from 'consults-service/entities/appointment';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
-import { differenceInMinutes } from 'date-fns';
+import { differenceInMinutes, addMinutes, addHours, getMinutes, getHours } from 'date-fns';
 import { format } from 'date-fns';
 
 @EntityRepository(Appointment)
@@ -70,6 +70,82 @@ export class AppointmentRepository extends Repository<Appointment> {
       }
       prev += 15;
       return prev;
+    });
+  }
+
+  getNextPossibleSlots(currDateTime: Date) {
+    const currentMinutes = getMinutes(currDateTime);
+    let nextMins = Math.ceil(currentMinutes / 15) * 15;
+    const nextHour = nextMins === 60 ? getHours(addHours(currDateTime, 1)) : getHours(currDateTime);
+    nextMins = nextMins === 60 ? 0 : nextMins;
+    const startTime = `${nextHour.toString()}:${nextMins.toString()}`;
+
+    let startDateTime = new Date(`${format(currDateTime, 'yyyy-MM-dd').toString()} ${startTime}`);
+
+    const possibleSlots = Array(4)
+      .fill(0)
+      .map(() => {
+        const tempTime = startDateTime;
+        startDateTime = addMinutes(startDateTime, 15);
+        const stTimeHours = tempTime
+          .getHours()
+          .toString()
+          .padStart(2, '0');
+        const stTimeMins = tempTime
+          .getMinutes()
+          .toString()
+          .padStart(2, '0');
+        return `${stTimeHours}:${stTimeMins}`;
+      });
+
+    return possibleSlots;
+  }
+
+  findDoctorsNextAvailability(doctorIds: string[]) {
+    const curDate = new Date();
+    const curEndDate = addMinutes(curDate, 60);
+
+    const possibleTimeSlots = this.getNextPossibleSlots(curDate);
+
+    const availabiltyData = doctorIds.map((doctorId) => {
+      return {
+        doctorId,
+        availabileInMinutes: { online: 60, physical: 60 },
+        possibleSlots: possibleTimeSlots,
+      };
+    });
+
+    return this.find({
+      where: {
+        doctorId: doctorIds.length ? In(doctorIds) : '',
+        appointmentDateTime: Between(curDate, curEndDate),
+      },
+    }).then((appointments) => {
+      if (appointments) {
+        appointments.map((appt) => {
+          const aptTime = format(new Date(appt.appointmentDateTime), 'HH:mm').toString();
+          const index = availabiltyData.findIndex((avail) => {
+            return avail.doctorId === appt.doctorId;
+          });
+          if (index >= 0) {
+            const isSlotBooked = availabiltyData[index].possibleSlots.indexOf(aptTime);
+            if (isSlotBooked >= 0) {
+              availabiltyData[index].possibleSlots.splice(isSlotBooked, 1);
+            }
+          }
+        });
+      }
+
+      availabiltyData.map((avail) => {
+        const availDateTime = new Date(
+          `${format(curDate, 'yyyy-MM-dd').toString()} ${avail.possibleSlots[0]}`
+        );
+        const availInMins = differenceInMinutes(availDateTime, curDate);
+        avail.availabileInMinutes.online = availInMins;
+        avail.availabileInMinutes.physical = availInMins;
+      });
+
+      return availabiltyData;
     });
   }
 }
---
+++ packages/api/src/doctors-service/resolvers/searchDoctorAndSpecialtyByName.ts
@@ -0,0 +1,138 @@
+import gql from 'graphql-tag';
+import { Resolver } from 'api-gateway';
+import { DoctorsServiceContext } from 'doctors-service/doctorsServiceContext';
+import { Doctor, DoctorSpecialty } from 'doctors-service/entities/';
+
+import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
+import { DoctorSpecialtyRepository } from 'doctors-service/repositories/doctorSpecialtyRepository';
+import { AppointmentRepository } from 'consults-service/repositories/appointmentRepository';
+
+import { AphError } from 'AphError';
+import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
+
+export const searchDoctorAndSpecialtyByNameTypeDefs = gql`
+  type AvailabilityInMinutes {
+    online: String
+    physical: String
+  }
+  type DoctorAvailability {
+    doctorId: ID!
+    availabileInMinutes: AvailabilityInMinutes
+    possibleSlots: [String]
+  }
+  type PossibleSearchMatches {
+    doctors: [DoctorDetails]
+    doctorsAvailability: [DoctorAvailability]
+    specialties: [DoctorSpecialty]
+  }
+  type OtherDoctors {
+    doctors: [DoctorDetails]
+    doctorsAvailability: [DoctorAvailability]
+  }
+  type SearchDoctorAndSpecialtyByNameResult {
+    doctors: [DoctorDetails]
+    doctorsAvailability: [DoctorAvailability]
+    specialties: [DoctorSpecialty]
+    possibleMatches: PossibleSearchMatches
+    otherDoctors: OtherDoctors
+  }
+
+  extend type Query {
+    SearchDoctorAndSpecialtyByName(searchText: String!): SearchDoctorAndSpecialtyByNameResult
+  }
+`;
+
+type AvailabilityInMinutes = {
+  online: number;
+  physical: number;
+};
+type DoctorAvailability = {
+  doctorId: string;
+  availabileInMinutes: AvailabilityInMinutes;
+  possibleSlots: string[];
+};
+
+type OtherDoctors = {
+  doctors: Doctor[] | undefined;
+  doctorsAvailability: DoctorAvailability[] | undefined;
+};
+
+type PossibleSearchMatches = {
+  doctors: Doctor[] | undefined;
+  doctorsAvailability: DoctorAvailability[] | undefined;
+  specialties: DoctorSpecialty[] | undefined;
+};
+
+type SearchDoctorAndSpecialtyByNameResult = {
+  doctors: Doctor[];
+  specialties: DoctorSpecialty[];
+  possibleMatches: PossibleSearchMatches;
+  otherDoctors: OtherDoctors;
+};
+
+const SearchDoctorAndSpecialtyByName: Resolver<
+  null,
+  { searchText: string },
+  DoctorsServiceContext,
+  SearchDoctorAndSpecialtyByNameResult
+> = async (parent, args, { doctorsDb, consultsDb }) => {
+  const searchTextLowerCase = args.searchText.trim().toLowerCase();
+
+  let matchedDoctors, doctorsAvailability, matchedSpecialties;
+  let possibleDoctors, possibleSpecialties, possibleDoctorsAvailability;
+  let otherDoctors, otherDoctorsAvailability;
+  try {
+    const doctorRepository = doctorsDb.getCustomRepository(DoctorRepository);
+    const specialtyRepository = doctorsDb.getCustomRepository(DoctorSpecialtyRepository);
+    const appointmentRepository = consultsDb.getCustomRepository(AppointmentRepository);
+
+    matchedDoctors = await doctorRepository.searchByName(searchTextLowerCase);
+    const matchedDoctorIds = matchedDoctors.map((doctor) => {
+      return doctor.id;
+    });
+    doctorsAvailability = await appointmentRepository.findDoctorsNextAvailability(matchedDoctorIds);
+    matchedSpecialties = await specialtyRepository.searchByName(searchTextLowerCase);
+
+    //fetch other doctors only if there is one matched doctor
+    if (matchedDoctors.length === 1) {
+      otherDoctors = await doctorRepository.searchBySpecialty(matchedDoctors[0].specialty.id);
+      const otherDoctorIds = otherDoctors.map((doctor) => {
+        return doctor.id;
+      });
+      otherDoctorsAvailability = await appointmentRepository.findDoctorsNextAvailability(
+        otherDoctorIds
+      );
+    }
+
+    //fetch possible doctors only if there are not matched doctors and specialties
+    if (matchedDoctors.length === 0 && matchedSpecialties.length === 0) {
+      possibleDoctors = await doctorRepository.searchByName('');
+      const possibleDoctorIds = possibleDoctors.map((doctor) => {
+        return doctor.id;
+      });
+      possibleDoctorsAvailability = await appointmentRepository.findDoctorsNextAvailability(
+        possibleDoctorIds
+      );
+      possibleSpecialties = await specialtyRepository.searchByName('');
+    }
+  } catch (searchError) {
+    throw new AphError(AphErrorMessages.SEARCH_DOCTOR_ERROR, undefined, { searchError });
+  }
+  return {
+    doctors: matchedDoctors,
+    doctorsAvailability: doctorsAvailability,
+    specialties: matchedSpecialties,
+    possibleMatches: {
+      doctors: possibleDoctors,
+      doctorsAvailability: possibleDoctorsAvailability,
+      specialties: possibleSpecialties,
+    },
+    otherDoctors: { doctors: otherDoctors, doctorsAvailability: otherDoctorsAvailability },
+  };
+};
+
+export const searchDoctorAndSpecialtyByNameResolvers = {
+  Query: {
+    SearchDoctorAndSpecialtyByName,
+  },
+};
