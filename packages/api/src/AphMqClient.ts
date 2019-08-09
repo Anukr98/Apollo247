b363166942e1e97c9b7127dad615707f0a3699b1
Kabir Sarin <sarink87@gmail.com>
2019-08-09 17:46:58 +0530

    Switch between azure and rabbit based on env vars

--- packages/api/src/AphMqClient.ts
+++ packages/api/src/AphMqClient.ts
@@ -2,7 +2,7 @@ import url from 'url';
 import rhea, { EventContext, ConnectionOptions } from 'rhea';
 
 export enum AphMqMessageTypes {
-  BOOK_APPOINTMENT = 'BOOK_APPOINTMENT',
+  TEST = 'TEST',
 }
 
 export type AphMqMessage<T, P> = {
@@ -19,9 +19,6 @@ const container = rhea.create_container();
 
 export const AphMqClient = {
   connect: function() {
-    const azureConnectionString =
-      'Endpoint=sb://apollo-hospitals.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/eX+5EqSEyXQ1iLjt7dt3jJ0YlAWw3imtgmV1CAKcYk=';
-
     const parseConnectionString = (connString: string) => {
       const parsed: Record<string, string> = connString.split(';').reduce((acc, part) => {
         const splitIndex = part.indexOf('=');
@@ -33,29 +30,31 @@ export const AphMqClient = {
       return parsed;
     };
 
-    const parsedAzureConn = parseConnectionString(azureConnectionString);
+    const USE_AZURE_SB = process.env.USE_AZURE_SERVICE_BUS === 'true';
+    const parsedAzureConn = parseConnectionString(process.env.AZURE_SERVICE_BUS_CONNECTION_STRING);
 
-    const connectionOptions: ConnectionOptions =
-      process.env.NODE_ENV === 'local'
-        ? {
-            host: 'localhost',
-            username: 'rabbitmq',
-            password: 'rabbitmq',
-            port: 5672,
-            reconnect_limit: 100,
-          }
-        : {
-            transport: 'tls',
-            host: url.parse(parsedAzureConn.Endpoint).hostname,
-            hostname: url.parse(parsedAzureConn.Endpoint).hostname,
-            username: parsedAzureConn.SharedAccessKeyName,
-            password: parsedAzureConn.SharedAccessKey,
-            port: 5671,
-            reconnect_limit: 100,
-          };
+    const connectionOptions: ConnectionOptions = USE_AZURE_SB
+      ? {
+          transport: 'tls',
+          host: url.parse(parsedAzureConn.Endpoint).hostname,
+          hostname: url.parse(parsedAzureConn.Endpoint).hostname,
+          username: parsedAzureConn.SharedAccessKeyName,
+          password: parsedAzureConn.SharedAccessKey,
+          port: parseInt(process.env.AZURE_SERVICE_BUS_PORT, 10),
+          reconnect_limit: 100,
+        }
+      : {
+          transport: 'tcp',
+          host: process.env.RABBITMQ_HOST,
+          username: process.env.RABBITMQ_USER,
+          password: process.env.RABBITMQ_PASSWORD,
+          port: parseInt(process.env.RABBITMQ_PORT, 10),
+          reconnect_limit: 100,
+        };
 
-    const queueName = 'entity-actions';
+    const queueName = process.env.MESSAGE_QUEUE_QUEUE_NAME;
     container.on('connection_open', (context: EventContext) => {
+      console.log('connected to message queue!');
       context.connection.open_receiver({ autoaccept: false, source: queueName });
       context.connection.open_sender({ target: queueName });
     });
