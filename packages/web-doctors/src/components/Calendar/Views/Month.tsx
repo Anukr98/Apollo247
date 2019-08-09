22496b446a3a3ea336d0ca94f0bb1ad6a6e00aee
Sushil Chhetri <sushil.chhetri@popcornapps.com>
2019-08-10 00:43:29 +0530

    Added api integration for timeline and month view

--- packages/web-doctors/src/components/Calendar/Views/Month.tsx
+++ packages/web-doctors/src/components/Calendar/Views/Month.tsx
@@ -3,8 +3,8 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { makeStyles } from '@material-ui/styles';
-import { GetDoctorAppointments } from 'graphql/types/GetDoctorAppointments';
-import { addMinutes } from 'date-fns/esm';
+import { GetDoctorAppointments, GetDoctorAppointments_getDoctorAppointments_appointmentsHistory } from 'graphql/types/GetDoctorAppointments';
+import { addMinutes, format } from 'date-fns/esm';
 
 const useStyles = makeStyles(() => {
   return {
@@ -35,12 +35,12 @@ const eventsAdapter = (data: GetDoctorAppointments) => {
   let eventList: MonthEvent[] = [];
 
   if (data && data.getDoctorAppointments) {
-    eventList = (data.getDoctorAppointments.appointmentsHistory || []).map((appointment) => {
-      const { id, appointmentType: title, appointmentDateTime } = appointment;
+    eventList = (data.getDoctorAppointments.appointmentsHistory || []).map((appointment: GetDoctorAppointments_getDoctorAppointments_appointmentsHistory | null) => {
+      const { id, appointmentDateTime, patientInfo } = appointment!;
       const start = new Date(appointmentDateTime);
       return {
         id,
-        title,
+        title: `${format(start, 'hh:mm aa')} ${patientInfo!.firstName} ${patientInfo!.lastName}`,
         start,
         end: addMinutes(start, 15),
       };
