a5fb24e8fb04ad1cb3db94aa01932c1af918b481
anuppopcornapps <anup.purushottam@popcornapps.com>
2019-08-09 07:22:25 +0530

    styles code cleanup
    styles code cleanup + callpoover build

--- packages/web-doctors/src/components/CallPopover.tsx
+++ packages/web-doctors/src/components/CallPopover.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/styles';
 import React, { useState } from 'react';
 import Popover from '@material-ui/core/Popover';
 import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -20,6 +21,30 @@ const useStyles = makeStyles((theme: Theme) => {
     helpWrap: {
       paddingBottom: 0,
     },
+    breadcrumbs: {
+      marginLeft: 20,
+      marginRight: 20,
+      fontSize: 13,
+      padding: '35px 20px',
+      fontWeight: 600,
+      color: '#02475b',
+      textTransform: 'uppercase',
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+      [theme.breakpoints.down('xs')]: {
+        position: 'fixed',
+        zIndex: 2,
+        top: 0,
+        width: '100%',
+        backgroundColor: theme.palette.common.white,
+        margin: 0,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.1)',
+      },
+    },
     consultButton: {
       fontSize: 13,
       fontWeight: theme.typography.fontWeightBold,
@@ -35,6 +60,44 @@ const useStyles = makeStyles((theme: Theme) => {
         backgroundColor: '#e28913',
       },
     },
+    timeLeft: {
+      fontSize: 12,
+      fontWeight: 500,
+      color: 'rgba(2, 71, 91, 0.6)',
+      textTransform: 'capitalize',
+      position: 'relative',
+      top: -1,
+    },
+    backArrow: {
+      cursor: 'pointer',
+      marginRight: 50,
+      [theme.breakpoints.up(1220)]: {
+        position: 'absolute',
+        left: -82,
+        top: 20,
+        width: 48,
+        height: 48,
+        lineHeight: '36px',
+        borderRadius: '50%',
+        textAlign: 'center',
+        backgroundColor: '#02475b',
+      },
+      '& img': {
+        verticalAlign: 'bottom',
+      },
+    },
+    whiteArrow: {
+      verticalAlign: 'middle',
+      [theme.breakpoints.down(1220)]: {
+        display: 'none',
+      },
+    },
+    blackArrow: {
+      verticalAlign: 'middle',
+      [theme.breakpoints.up(1220)]: {
+        display: 'none',
+      },
+    },
     loginForm: {
       width: 280,
       minHeight: 290,
@@ -42,6 +105,10 @@ const useStyles = makeStyles((theme: Theme) => {
       borderRadius: 10,
       boxShadow: '0 5px 40px 0 rgba(0, 0, 0, 0.3)',
       backgroundColor: theme.palette.common.white,
+    },
+    consultButtonContainer: {
+      position: 'absolute',
+      right: 0,
     },
     cross: {
       position: 'absolute',
@@ -140,78 +207,94 @@ export const CallPopover: React.FC<CallPopoverProps> = (props) => {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
   return (
-    <span>
-      <span style={{ color: 'red' }}>{`${minutes}:${seconds}`}</span>
-      <Button
-        className={classes.consultButton}
-        onClick={() => {
-          !startAppointment ? startInterval(900) : stopInterval();
-          setStartAppointment(!startAppointment);
-        }}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path fill="#fff" d="M8 5v14l11-7z" />
-        </svg>
-        {startAppointment ? 'End Consult' : 'Start Consult'}
-      </Button>
-      <Button
-        className={classes.consultIcon}
-        aria-describedby={id}
-        variant="contained"
-        onClick={(e) => handleClick(e)}
-      >
-        <img src={require('images/ic_call.svg')} />
-      </Button>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-      >
-        <Paper className={classes.loginForm}>
-          <Button className={classes.cross}>
-            <img src={require('images/ic_cross.svg')} alt="" onClick={() => handleClose()} />
-          </Button>
-          <div className={`${classes.loginFormWrap} ${classes.helpWrap}`}>
-            <p>How do you want to talk to the patient?</p>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.needHelp}
-              onClick={() => {
-                handleClose();
-                props.setStartConsultAction(false);
-              }}
-            >
-              <img src={require('images/call_popup.svg')} alt="" />
-              AUDIO CALL
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.needHelp}
-              onClick={() => {
-                handleClose();
-                props.setStartConsultAction(true);
-              }}
-            >
-              <img src={require('images/video_popup.svg')} alt="" />
-              VIDEO CALL
-            </Button>
+    <div className={classes.breadcrumbs}>
+      <div>
+        <Link to="/calendar">
+          <div className={classes.backArrow}>
+            <img className={classes.blackArrow} src={require('images/ic_back.svg')} />
+            <img className={classes.whiteArrow} src={require('images/ic_back_white.svg')} />
           </div>
-        </Paper>
-      </Popover>
-      <Button className={classes.consultIcon}>
-        <img src={require('images/ic_more.svg')} />
-      </Button>
-    </span>
+        </Link>
+      </div>
+      CONSULT ROOM &nbsp; | &nbsp;
+      <span className={classes.timeLeft}>
+        Time to Consult <b>00:25</b>
+      </span>
+      <div className={classes.consultButtonContainer}>
+        <span>
+          <span style={{ color: 'red' }}>{`${minutes}:${seconds}`}</span>
+          <Button
+            className={classes.consultButton}
+            onClick={() => {
+              !startAppointment ? startInterval(900) : stopInterval();
+              setStartAppointment(!startAppointment);
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="#fff" d="M8 5v14l11-7z" />
+            </svg>
+            {startAppointment ? 'End Consult' : 'Start Consult'}
+          </Button>
+          <Button
+            className={classes.consultIcon}
+            aria-describedby={id}
+            variant="contained"
+            onClick={(e) => handleClick(e)}
+          >
+            <img src={require('images/ic_call.svg')} />
+          </Button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <Paper className={classes.loginForm}>
+              <Button className={classes.cross}>
+                <img src={require('images/ic_cross.svg')} alt="" onClick={() => handleClose()} />
+              </Button>
+              <div className={`${classes.loginFormWrap} ${classes.helpWrap}`}>
+                <p>How do you want to talk to the patient?</p>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.needHelp}
+                  onClick={() => {
+                    handleClose();
+                    props.setStartConsultAction(false);
+                  }}
+                >
+                  <img src={require('images/call_popup.svg')} alt="" />
+                  AUDIO CALL
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.needHelp}
+                  onClick={() => {
+                    handleClose();
+                    props.setStartConsultAction(true);
+                  }}
+                >
+                  <img src={require('images/video_popup.svg')} alt="" />
+                  VIDEO CALL
+                </Button>
+              </div>
+            </Paper>
+          </Popover>
+          <Button className={classes.consultIcon}>
+            <img src={require('images/ic_more.svg')} />
+          </Button>
+        </span>
+      </div>
+    </div>
   );
 };
--- packages/web-doctors/src/components/ConsultTabs.tsx
+++ packages/web-doctors/src/components/ConsultTabs.tsx
@@ -7,7 +7,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import { ConsultRoom } from 'components/ConsultRoom';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -33,68 +32,6 @@ const useStyles = makeStyles((theme: Theme) => {
       boxShadow: '0 5px 20px 0 rgba(128, 128, 128, 0.3)',
       backgroundColor: '#f7f7f7',
       minHeight: 500,
-    },
-    breadcrumbs: {
-      marginLeft: 20,
-      marginRight: 20,
-      fontSize: 13,
-      padding: '35px 20px',
-      fontWeight: 600,
-      color: '#02475b',
-      textTransform: 'uppercase',
-      position: 'relative',
-      display: 'flex',
-      alignItems: 'center',
-      [theme.breakpoints.down('xs')]: {
-        position: 'fixed',
-        zIndex: 2,
-        top: 0,
-        width: '100%',
-        backgroundColor: theme.palette.common.white,
-        margin: 0,
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingBottom: 20,
-        boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.1)',
-      },
-    },
-    timeLeft: {
-      fontSize: 12,
-      fontWeight: 500,
-      color: 'rgba(2, 71, 91, 0.6)',
-      textTransform: 'capitalize',
-      position: 'relative',
-      top: -1,
-    },
-    backArrow: {
-      cursor: 'pointer',
-      marginRight: 50,
-      [theme.breakpoints.up(1220)]: {
-        position: 'absolute',
-        left: -82,
-        top: 20,
-        width: 48,
-        height: 48,
-        lineHeight: '36px',
-        borderRadius: '50%',
-        textAlign: 'center',
-        backgroundColor: '#02475b',
-      },
-      '& img': {
-        verticalAlign: 'bottom',
-      },
-    },
-    whiteArrow: {
-      verticalAlign: 'middle',
-      [theme.breakpoints.down(1220)]: {
-        display: 'none',
-      },
-    },
-    blackArrow: {
-      verticalAlign: 'middle',
-      [theme.breakpoints.up(1220)]: {
-        display: 'none',
-      },
     },
     tabsRoot: {
       backgroundColor: theme.palette.common.white,
@@ -122,72 +59,11 @@ const useStyles = makeStyles((theme: Theme) => {
       backgroundColor: '#00b38e',
       height: 4,
     },
-    consultButtonContainer: {
-      position: 'absolute',
-      right: 0,
-    },
-    consultButton: {
-      fontSize: 12,
-      fontWeight: theme.typography.fontWeightMedium,
-      color: '#fff',
-      padding: '6px 16px',
-      backgroundColor: '#fc9916',
-      marginLeft: 20,
-      marginRight: 10,
-      borderRadius: 15,
-      boxShadow: '0 2px 4px 0 rgba(0,0,0,0.2)',
-      '&:hover': {
-        backgroundColor: '#e28913',
-      },
-    },
-    consultIcon: {
-      padding: 6,
-      backgroundColor: 'transparent',
-      margin: '0 5px',
-      minWidth: 20,
-    },
     DisplayNone: {
       display: 'none !important',
     },
     typography: {
       padding: theme.spacing(2),
-    },
-    loginForm: {
-      width: 280,
-      minHeight: 290,
-      padding: 20,
-      borderRadius: 10,
-      boxShadow: '0 5px 40px 0 rgba(0, 0, 0, 0.3)',
-      backgroundColor: theme.palette.common.white,
-    },
-    cross: {
-      position: 'absolute',
-      right: 0,
-      top: '10px',
-      fontSize: '18px',
-      color: '#02475b',
-    },
-    loginFormWrap: {
-      padding: '30px 0 50px 0',
-      '& p': {
-        fontSize: 17,
-        fontWeight: 500,
-        lineHeight: 1.41,
-        color: theme.palette.secondary.main,
-        marginTop: 10,
-        marginBottom: 10,
-      },
-    },
-    helpWrap: {
-      paddingBottom: 0,
-    },
-    needHelp: {
-      padding: '8px',
-      width: '100%',
-      marginTop: 15,
-      borderRadius: '5px',
-      boxShadow: 'none',
-      backgroundColor: '#fc9916',
     },
   };
 });
@@ -216,26 +92,10 @@ export const ConsultTabs: React.FC = (props) => {
         <Header />
       </div>
       <div className={classes.container}>
-        <div className={classes.breadcrumbs}>
-          <div>
-            <Link to="/calendar">
-              <div className={classes.backArrow}>
-                <img className={classes.blackArrow} src={require('images/ic_back.svg')} />
-                <img className={classes.whiteArrow} src={require('images/ic_back_white.svg')} />
-              </div>
-            </Link>
-          </div>
-          CONSULT ROOM &nbsp; | &nbsp;
-          <span className={classes.timeLeft}>
-            Time to Consult <b>00:25</b>
-          </span>
-          <div className={classes.consultButtonContainer}>
-            <CallPopover
-              setStartConsultAction={(flag: boolean) => setStartConsultAction(flag)}
-              // startAppointmentAction={(flag: boolean) => startAppointmentAction(flag)}
-            />
-          </div>
-        </div>
+        <CallPopover
+          setStartConsultAction={(flag: boolean) => setStartConsultAction(flag)}
+          // startAppointmentAction={(flag: boolean) => startAppointmentAction(flag)}
+        />
         <div>
           <div className={showTabs ? '' : classes.DisplayNone}>
             <Tabs
