64e17a74569815931baa972a8d3e13b5b718bc88
Ramarao <ramarao.jaligama@popcornapps.com>
2019-08-10 21:39:14 +0530

    Checkups added

--- packages/web-doctors/src/components/Calendar.tsx
+++ packages/web-doctors/src/components/Calendar.tsx
@@ -127,8 +127,8 @@ const dataAdapter = (data: GetDoctorAppointments | undefined) => {
         isNew: !!newPatientsList && newPatientsList.includes(patientId),
         details: {
           patientName: `${patientInfo!.firstName} ${patientInfo!.lastName}`,
-          checkups: [],
-          avatar: '',
+          checkups: ['Fever', 'Cold & Cough'],
+          avatar: require('images/ic_patientchat.png'),
         },
       };
     }
