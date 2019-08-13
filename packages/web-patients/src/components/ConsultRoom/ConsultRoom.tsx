42643de0c0d01e23241345d52e4ffa96c7996381
malleshpopcornapps <mallesh.tirugudu@popcornapps.com>
2019-08-13 10:48:42 +0530

    Fixed dropdown alignment issue

--- packages/web-patients/src/components/ConsultRoom/ConsultRoom.tsx
+++ packages/web-patients/src/components/ConsultRoom/ConsultRoom.tsx
@@ -87,7 +87,8 @@ const useStyles = makeStyles((theme: Theme) => {
       fontSize: 50,
       fontWeight: 600,
       lineHeight: '66px',
-      padding: '2px 0 7px',
+      paddingTop: 2,
+      paddingBottom: 7,
       [theme.breakpoints.down('xs')]: {
         fontSize: 30,
         lineHeight: '46px',
