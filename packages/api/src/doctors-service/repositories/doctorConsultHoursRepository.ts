84594756b820870eaa18dde471725c17467ce4da
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2019-08-17 15:39:10 +0530

    added physical slots availability api

--- packages/api/src/doctors-service/repositories/doctorConsultHoursRepository.ts
+++ packages/api/src/doctors-service/repositories/doctorConsultHoursRepository.ts
@@ -4,6 +4,10 @@ import { ConsultHours } from 'doctors-service/entities';
 @EntityRepository(ConsultHours)
 export class DoctorConsultHoursRepository extends Repository<ConsultHours> {
   getConsultHours(doctor: string, weekDay: string) {
-    return this.find({ where: { doctor, weekDay } });
+    return this.find({ where: { doctor, weekDay, consultMode: 'ONLINE' } });
+  }
+
+  getPhysicalConsultHours(doctor: string, weekDay: string, facility: string) {
+    return this.find({ where: { doctor, weekDay, facility, consultMode: 'BOTH' } });
   }
 }
