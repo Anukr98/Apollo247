284009d846ace1d3bc28ce9b3663ffb36f17968b
adityakvs <aditya.kvs@popcornapps.com>
2019-08-19 07:01:56 +0530

    Added get past searches api

--- packages/api/src/profiles-service/resolvers/getPatientPastSearches.ts
+++ packages/api/src/profiles-service/resolvers/getPatientPastSearches.ts
@@ -3,20 +3,20 @@ import { Resolver } from 'api-gateway';
 import { ProfilesServiceContext } from 'profiles-service/profilesServiceContext';
 import { SEARCH_TYPE } from 'profiles-service/entities';
 import { SearchHistoryRepository } from 'profiles-service/repositories/searchHistoryRepository';
-// import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
-// import { DoctorSpecialtyRepository } from 'doctors-service/repositories/doctorSpecialtyRepository';
+import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
+import { DoctorSpecialtyRepository } from 'doctors-service/repositories/doctorSpecialtyRepository';
 
 export const getPatientPastSearchesTypeDefs = gql`
   extend type Query {
-    getPatientPastSearches(patientId: ID!): [SearchData]
+    getPatientPastSearches(patientId: ID!): [SearchData]!
   }
 `;
 
 export type SearchData = {
   searchType: SEARCH_TYPE;
-  typeId: String;
-  name: String;
-  image: String;
+  typeId: string;
+  name: string;
+  image: string;
 };
 
 const getPatientPastSearches: Resolver<
@@ -26,9 +26,48 @@ const getPatientPastSearches: Resolver<
   SearchData[]
 > = async (parent, args, { profilesDb, doctorsDb }) => {
   const searchHistoryRepository = profilesDb.getCustomRepository(SearchHistoryRepository);
-  const recentSearches = await searchHistoryRepository.getRecentSearchHistory(args.patientId);
-  console.log(recentSearches);
-  return [];
+  const recentSearches = await searchHistoryRepository.getPatientRecentSearchHistory(
+    args.patientId
+  );
+  const pastSearches: SearchData[] = [];
+  if (recentSearches.length > 0) {
+    //get search data ids
+    const searchTypeIds = recentSearches.map((search) => {
+      return search.typeId;
+    });
+
+    //To convert array into an object
+    const searchObject: { [index: string]: SearchData } = {};
+    const convertArrayToObject = (array: SearchData[]) =>
+      array.reduce((searchObject, item: SearchData) => {
+        searchObject[item.typeId] = item;
+        return searchObject;
+      }, searchObject);
+
+    //get all searched doctors details
+    const doctorsRepository = doctorsDb.getCustomRepository(DoctorRepository);
+    const searchDoctorsArray = await doctorsRepository.getSearchDoctorsByIds(searchTypeIds);
+    const searchDoctorsObject = convertArrayToObject(searchDoctorsArray);
+
+    //get all searched specialties details
+    const specialtiesRepo = doctorsDb.getCustomRepository(DoctorSpecialtyRepository);
+    const searchSpecialtiesArray = await specialtiesRepo.getSearchSpecialtiesByIds(searchTypeIds);
+    const searchSpecialtiesObject = convertArrayToObject(searchSpecialtiesArray);
+
+    //creating response as array of SearchData
+    recentSearches.forEach((item) => {
+      let searchItem;
+      if (item.type == 'DOCTOR') {
+        searchItem = searchDoctorsObject[item.typeId];
+        searchItem.searchType = SEARCH_TYPE.DOCTOR;
+      } else {
+        searchItem = searchSpecialtiesObject[item.typeId];
+        searchItem.searchType = SEARCH_TYPE.SPECIALTY;
+      }
+      pastSearches.push(searchItem);
+    });
+  }
+  return pastSearches;
 };
 
 export const getPatientPastSearchesResolvers = {
