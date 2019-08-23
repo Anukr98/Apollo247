e6e7cb73015c8286f3c67503cc99e24143de4c43
Kabir Sarin <sarink87@gmail.com>
2019-08-23 14:06:14 +0530

    Add a bunch of fake specialties

--- packages/api/src/doctors-service/database/factories/doctorSpecialtyFactory.ts
+++ packages/api/src/doctors-service/database/factories/doctorSpecialtyFactory.ts
@@ -1,7 +1,34 @@
 import { DoctorSpecialty } from 'doctors-service/entities';
 import _sample from 'lodash/sample';
 
-const allDoctorSpecialties = ['Gynecology'];
+const allDoctorSpecialties = [
+  'Anesthesiologist',
+  'Cardiologist',
+  'Dermatologist',
+  'Gastroenterologist',
+  'Hematologist/Oncologist',
+  'Internal Medicine Physician',
+  'Nephrologist',
+  'Neurologist',
+  'Neurosurgeon',
+  'Obstetrician',
+  'Gynecologist',
+  'Occupational Medicine Physician',
+  'Ophthalmologist',
+  'Oral and Maxillofacial Surgeon',
+  'Orthopaedic Surgeon',
+  'Otolaryngologist',
+  'Pathologist',
+  'Pediatrician',
+  'Plastic Surgeon',
+  'Podiatrist',
+  'Psychiatrist',
+  'Pulmonary Medicine Physician',
+  'Radiation Onconlogist',
+  'Diagnostic Radiologist',
+  'Rheumatologist',
+  'Urologist',
+];
 export const randomDoctorSpecialty = () => _sample(allDoctorSpecialties) as string;
 
 export const buildDoctorSpecialty = (attrs: Partial<DoctorSpecialty> = {}) => {
