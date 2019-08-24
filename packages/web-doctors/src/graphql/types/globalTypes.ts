4b7d0fd820ce8f6d028f07ad608aebc46c097f4e
Sushil Chhetri <sushil.chhetri@popcornapps.com>
2019-08-24 14:38:18 +0530

    auto generated type file

--- packages/web-doctors/src/graphql/types/globalTypes.ts
+++ packages/web-doctors/src/graphql/types/globalTypes.ts
@@ -7,7 +7,6 @@
 //==============================================================
 
 export enum APPOINTMENT_TYPE {
-  BOTH = "BOTH",
   ONLINE = "ONLINE",
   PHYSICAL = "PHYSICAL",
 }
@@ -69,7 +68,7 @@ export enum STATUS {
   COMPLETED = "COMPLETED",
   CONFIRMED = "CONFIRMED",
   IN_PROGRESS = "IN_PROGRESS",
-  MISSED = "MISSED",
+  NO_SHOW = "NO_SHOW",
   PENDING = "PENDING",
 }
 
