4699dfbd8d9d69efd0800b6e165ca1046596d829
anuppopcornapps <anup.purushottam@popcornapps.com>
2019-08-26 10:45:28 +0530

    added props for details page
    added props for details page

--- packages/web-doctors/src/components/case-sheet/UserCard.tsx
+++ packages/web-doctors/src/components/case-sheet/UserCard.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, Typography, Divider } from '@material-ui/core';
-
-export const UserCard: React.FC = () => {
+interface UserInfoObject {
+  patientId: string;
+  image: string;
+  name: string;
+  age: number;
+  gender: string;
+  location: string;
+  uhid: string;
+  appointmentId: string;
+}
+interface CasesheetInfoObj {
+  userInfo: UserInfoObject;
+}
+interface CasesheetInfoProps {
+  casesheetInfo: CasesheetInfoObj;
+}
+export const UserCard: React.FC<CasesheetInfoProps> = (props) => {
   return (
-    <Card>
+    (props.casesheetInfo && props.casesheetInfo.userInfo) &&
+    (<Card>
       <CardMedia
         component="img"
         image={require('images/ic_patientchat.png')}
@@ -11,19 +27,21 @@ export const UserCard: React.FC = () => {
       />
       <CardContent>
         <Typography gutterBottom variant="h4" component="h2">
-          Seema Singh
+          {(props.casesheetInfo.userInfo.name && props.casesheetInfo.userInfo.name !== '')  &&  props.casesheetInfo.userInfo.name}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          56, F, Mumbai
+          {(props.casesheetInfo.userInfo.age)  &&  props.casesheetInfo.userInfo.age}, 
+          {(props.casesheetInfo.userInfo.gender && props.casesheetInfo.userInfo.gender !== '')  &&  props.casesheetInfo.userInfo.gender}, 
+          {(props.casesheetInfo.userInfo.location && props.casesheetInfo.userInfo.location !== '')  &&  props.casesheetInfo.userInfo.location}
         </Typography>
         <Divider />
         <Typography variant="body2" color="textSecondary" component="p">
-          UHID: 012345
+          UHID: {(props.casesheetInfo.userInfo.uhid && props.casesheetInfo.userInfo.uhid !== '')  &&  props.casesheetInfo.userInfo.uhid}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          Appt ID: 98765
+          Appt ID:  {(props.casesheetInfo.userInfo.appointmentId && props.casesheetInfo.userInfo.appointmentId !== '')  &&  props.casesheetInfo.userInfo.appointmentId}
         </Typography>
       </CardContent>
-    </Card>
+    </Card>)
   );
 };
