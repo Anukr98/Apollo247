31103419a915db01131bff482ec1841fd14dfe56
sushma <sushma.voleti@popcornapps.com>
2019-08-27 18:54:40 +0530

    Case=sheet bug fix

--- packages/api/src/consults-service/resolvers/caseSheet.ts
+++ packages/api/src/consults-service/resolvers/caseSheet.ts
@@ -280,8 +280,6 @@ const getCaseSheet: Resolver<
   const patientDetails = await patientRepo.getPatientDetails(appointmentData.patientId);
   if (patientDetails == null) throw new AphError(AphErrorMessages.INVALID_APPOINTMENT_ID);
 
-  console.log(patientDetails);
-
   //get past appointment details
   const pastAppointments = await appointmentRepo.getPastAppointments(
     appointmentData.doctorId,
