6d033ad85bdb705013f4c6a8850a3e2063444cbc
Kabir Sarin <sarink87@gmail.com>
2019-08-27 15:44:51 +0530

    Get local storage working by setting publicAccessLevel

--- packages/api/src/AphStorageClient.ts
+++ packages/api/src/AphStorageClient.ts
@@ -2,37 +2,59 @@ import { createBlobService } from 'azure-storage';
 import uuid from 'uuid/v4';
 
 const container = process.env.BLOB_STORAGE_CONTAINER_NAME;
+const publicAccessLevel = 'container';
 
 const blobService = createBlobService(process.env.AZURE_STORAGE_CONNECTION_STRING);
 
-const createContainerPromise = new Promise((resolve, reject) =>
-  blobService.createContainerIfNotExists(container, (error, result, response) => {
-    console.log('creating blob container...', container);
-    if (error) throw error;
-    if (result) console.log(`container "${container}" already exists, skipping creation`);
-    if (!result) console.log(`container "${container}" created successfully`);
-    if (response) console.log('response', response);
-    resolve();
-  })
-);
+const deleteContainer = () =>
+  process.env.NODE_ENV !== 'local'
+    ? Promise.resolve()
+    : new Promise((resolve, reject) =>
+        blobService.deleteContainerIfExists(container, (error, result, response) => {
+          console.log('deleting blob container...');
+          if (result) console.log(`container "${container}" deleted successfully`);
+          if (!result) console.log(`container "${container}" doesn't exist, skipping deletion`);
+          if (response) console.log('response', response);
+          resolve();
+        })
+      );
 
-const testBlobConnectionPromise = new Promise((resolve, reject) =>
-  blobService.getServiceProperties((error, result, response) => {
-    console.log('testing blob connection...');
-    if (error) throw error;
-    if (result) console.log('result', result);
-    if (response) console.log('response', response);
-    resolve();
-  })
-);
+const createContainer = () =>
+  new Promise((resolve, reject) =>
+    blobService.createContainerIfNotExists(
+      container,
+      { publicAccessLevel },
+      (error, result, response) => {
+        console.log('creating blob container...');
+        if (error) throw error;
+        if (result) console.log(`container "${container}" created successfully`);
+        if (!result) console.log(`container "${container}" already exists, skipping creation`);
+        if (response) console.log('response', response);
+        resolve();
+      }
+    )
+  );
 
-const initStorage = () => Promise.all([createContainerPromise, testBlobConnectionPromise]);
+const testBlobConnection = () =>
+  new Promise((resolve, reject) =>
+    blobService.getServiceProperties((error, result, response) => {
+      console.log('testing blob connection...');
+      if (error) throw error;
+      if (result) console.log('result', result);
+      if (response) console.log('response', response);
+      resolve();
+    })
+  );
+
+const initStoragePromise = testBlobConnection().then(() =>
+  deleteContainer().then(() => createContainer())
+);
 
 // Idk why we can't import this from azure-storage, just find the type by following function paramters
 type BlobResult = Parameters<Parameters<typeof blobService.createBlockBlobFromLocalFile>[3]>[1];
 
 const uploadBlob = async ({ name = uuid(), file }: { name?: string; file: string }) => {
-  await initStorage();
+  await initStoragePromise;
   return new Promise<BlobResult>((resolve, reject) =>
     blobService.createBlockBlobFromLocalFile(container, name, file, (error, result) =>
       !error ? resolve(result) : reject(error)
