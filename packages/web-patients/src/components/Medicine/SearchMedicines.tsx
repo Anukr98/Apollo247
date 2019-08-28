b3b2fd7f014940200680b179064b4b423f0e063c
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2019-08-28 11:52:43 +0530

    removed unused import

--- packages/web-patients/src/components/Medicine/SearchMedicines.tsx
+++ packages/web-patients/src/components/Medicine/SearchMedicines.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Theme, CircularProgress } from '@material-ui/core';
 import { Link } from 'react-router-dom';
