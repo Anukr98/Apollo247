726eb7e0b64b4333213a0b43a0ccde8b2f624005
anuppopcornapps <anup.purushottam@popcornapps.com>
2019-09-04 19:17:22 +0530

    added instruction
    added instruction

--- packages/web-doctors/src/components/case-sheet/panels/OtherInstructions.tsx
+++ packages/web-doctors/src/components/case-sheet/panels/OtherInstructions.tsx
@@ -28,10 +28,10 @@ interface OptionType {
 }
 
 let suggestions: OptionType[] = [
-  { instruction: 'Drink more water in a day', __typename: 'OtherInstructions' },
-  { instruction: 'Eat 1 apple every morning', __typename: 'OtherInstructions' },
-  { instruction: 'Eat more fruits', __typename: 'OtherInstructions' },
-  { instruction: 'Do some exercise in morning', __typename: 'OtherInstructions' },
+  { instruction: 'Drink more water in a day.', __typename: 'OtherInstructions' },
+  { instruction: 'Eat 1 apple every morning.', __typename: 'OtherInstructions' },
+  { instruction: 'Eat more fruits.', __typename: 'OtherInstructions' },
+  { instruction: 'Do some exercise in morning.', __typename: 'OtherInstructions' },
 ];
 
 // const suggestions: OptionType[] = [];
