c68500340a09340d5fa6833d68684dc0757202ee
sushma <sushma.voleti@popcornapps.com>
2019-09-08 17:43:25 +0530

    Inprogress email templates
    Inprogress email templates

---
+++ packages/api/src/notifications-service/resolvers/email.ts
@@ -0,0 +1,65 @@
+import gql from 'graphql-tag';
+import { Resolver } from 'api-gateway';
+import { MailMessage } from 'types/notificationMessageTypes';
+
+export const emailTypeDefs = gql``;
+
+export async function buildEmail(message: MailMessage) {
+  const lib = require('pepipost');
+  //const configuration = lib.Configuration;
+  const controller = lib.EmailController;
+  const apiKey = '0e396e4e9b5247d267c9a536cd154869';
+  const mailContent = message.mailContent;
+
+  const body = new lib.EmailBody();
+
+  body.personalizations = [];
+  body.personalizations[0] = new lib.Personalizations();
+  body.personalizations[0].recipient = message.toEmailMailId;
+  // body.personalizations[0].xApiheaderCc = '123';
+  //body.personalizations[0].xApiheader = '12';
+  body.personalizations[0].attributes = JSON.parse('{"name":"Ravi"}');
+  body.personalizations[0].attachments = [];
+
+  body.personalizations[0].recipientCc = ['sushma.voleti@popcornapps.com'];
+
+  body.tags = 'tagsTransnodejs';
+  body.from = new lib.From();
+  body.from.fromEmail = 'info@pepisandbox.com';
+  body.from.fromName = 'Apollo Hospitals';
+  body.subject = message.subject;
+  body.content = mailContent; //'Hello There, This is a test email from apollo Hospitals [% name %]';
+  body.settings = new lib.Settings();
+
+  body.settings.footer = 1;
+  body.settings.clicktrack = 1;
+  body.settings.opentrack = 1;
+  body.settings.unsubscribe = 1;
+  body.settings.bcc = '';
+  body.replyToId = 'sriram.kanchan@popcornapps.com';
+
+  console.log(apiKey, body);
+  const promise = await controller.createSendEmail(apiKey, body);
+  console.log(promise);
+  /*await promise.then(
+      (response: string) => {
+        response = '1';
+      },
+      (err: string) => {
+        console.log(err);
+      }
+    );*/
+  return <String>'success';
+}
+
+const sendEmail: Resolver<null, {}, NotificationsServiceContext, string> = async (
+  parent,
+  {},
+  { patientsDb, consultsDb }
+) => {
+  console.log(buildEmail());
+  return '';
+};
+export const emailResolvers = {
+  Query: { sendEmail },
+};
