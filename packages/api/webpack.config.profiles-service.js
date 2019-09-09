2149d860c52270dbe23a03b3bfdcac5e90b5a937
Kabir Sarin <sarink87@gmail.com>
2019-09-09 17:20:26 +0530

    Re-jig our hot reload scheme

--- packages/api/webpack.config.profiles-service.js
+++ packages/api/webpack.config.profiles-service.js
@@ -3,6 +3,7 @@ const makeWebpackConfig = require('./webpack-make-config');
 module.exports = makeWebpackConfig({
   nodemonPluginArgs: {
     script: 'dist/profiles-service.bundle.js',
+    watch: ['dist/profiles-service.bundle.js', 'dist/profiles-db-seeds.bundle.js'],
   },
   webpackConfigOptions: {
     entry: {
