ae4dca377e358feaec3e42f1df03de14c5dacc12
anuppopcornapps <anup.purushottam@popcornapps.com>
2019-09-10 02:12:27 +0530

    removed disabled condition for start consult
    removed disabled condition for start consult

--- packages/web-doctors/src/components/CallPopover.tsx
+++ packages/web-doctors/src/components/CallPopover.tsx
@@ -1043,13 +1043,13 @@ export const CallPopover: React.FC<CallPopoverProps> = (props) => {
           ) : (
             <Button
               className={classes.consultButton}
-              disabled={
-                startAppointmentButton ||
-                disableOnTransfer ||
-                appointmentInfo!.appointmentState !== 'NEW' ||
-                (appointmentInfo!.status !== STATUS.IN_PROGRESS &&
-                  appointmentInfo!.status !== STATUS.PENDING)
-              }
+              // disabled={
+              //   startAppointmentButton ||
+              //   disableOnTransfer ||
+              //   appointmentInfo!.appointmentState !== 'NEW' ||
+              //   (appointmentInfo!.status !== STATUS.IN_PROGRESS &&
+              //     appointmentInfo!.status !== STATUS.PENDING)
+              // }
               onClick={() => {
                 !startAppointment ? onStartConsult() : onStopConsult();
                 !startAppointment ? startInterval(900) : stopInterval();
