27e9de1331acafa0095bd247e73520a27e4850fe
Kabir Sarin <sarink87@gmail.com>
2019-09-09 17:10:37 +0530

    Go back to atl, add watch to nodemonplugin args

--- packages/api/webpack-make-config.js
+++ packages/api/webpack-make-config.js
@@ -25,9 +25,13 @@ module.exports = ({ nodemonPluginArgs, webpackConfigOptions }) => {
       cwd: process.cwd(),
     }),
   ];
-  if (isLocal) plugins.push(new NodemonPlugin(nodemonPluginArgs));
+  if (isLocal) {
+    plugins.push(
+      new NodemonPlugin({ watch: path.resolve(__dirname, 'dist'), ...nodemonPluginArgs })
+    );
+  }
 
-  const tsLoader = { loader: 'ts-loader' };
+  const tsLoader = { loader: 'awesome-typescript-loader' };
 
   return {
     target: 'node',
