ec785d3d40fb7f3e6023a2a1607b764e7f43609e
vishalkalambe-Popcornapps <vishalkalambe@popcornapps.com>
2019-09-11 11:50:41 +0530

    format fix
    format fix

--- packages/web-doctors/src/components/FeesTab.tsx
+++ packages/web-doctors/src/components/FeesTab.tsx
@@ -329,9 +329,7 @@ export const FeesTab: React.FC<FeesProps> = (props) => {
                     <div className={classes.topBorder}>
                       <Grid container alignItems="flex-start" spacing={0}>
                         <Grid item lg={12} sm={12} xs={12}>
-                          <div className={classes.accountDetailsHeading}>
-                            Account Holder’s Name
-                          </div>
+                          <div className={classes.accountDetailsHeading}>Account Holder’s Name</div>
                           <Typography variant="h5" className={classes.accountDetails}>
                             {data.bankAccount[0]!.accountHolderName}
                           </Typography>
