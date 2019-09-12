db70d96ab71ff32784353f4f83354ae50c941465
sankeeth <sankeerth.obulam@popcornapps.com>
2019-09-12 17:35:36 +0530

    Fixing reschudule issues

---
+++ packages/mobile-patients/src/components/Consult/ChatDataList.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+export interface ChatDataListProps {}
+
+export const ChatDataList: React.FC<ChatDataListProps> = (props) => {
+  return(
+
+  );
+};
