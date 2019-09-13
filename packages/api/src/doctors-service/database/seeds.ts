55e49d17e2fe9256d1cc73da8bc55a761d5136dc
Kabir Sarin <sarink87@gmail.com>
2019-09-13 11:04:33 +0530

    Add delegateNumber to seeds

--- packages/api/src/doctors-service/database/seeds.ts
+++ packages/api/src/doctors-service/database/seeds.ts
@@ -92,6 +92,7 @@ import { CaseSheetRepository } from 'consults-service/repositories/caseSheetRepo
     specialty: _sample(doctorSpecialties),
     doctorType: DoctorType.JUNIOR,
     mobileNumber: '+919999999999', // OTP is 999999
+    delegateNumber: '+91123456789',
     isActive: true, // Don't forget to set this to true or you won't be able to log in!
     onlineStatus: DOCTOR_ONLINE_STATUS.ONLINE,
     firebaseToken: 'dJYFTV7MJGWK2EdziTtr5zUPm243', // This is actually the firebaseuid, not the token
