0bc516d4f880ce513cd00c65e500e13e68d4f81e
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2019-09-13 23:05:35 +0530

    generated types

--- packages/web-doctors/src/graphql/consults.ts
+++ packages/web-doctors/src/graphql/consults.ts
@@ -54,3 +54,47 @@ export const REMOVE_FROM_CONSULT_QUEUE = gql`
     }
   }
 `;
+
+export const SAVE_PATIENT_FAMILY_HISTORY = gql`
+  mutation SavePatientFamilyHistory($patientFamilyHistoryInput: PatientFamilyHistoryInput) {
+    savePatientFamilyHistory(patientFamilyHistoryInput: $patientFamilyHistoryInput) {
+      patientFamilyHistory {
+        id
+        description
+      }
+    }
+  }
+`;
+
+export const GET_PATIENT_FAMILY_HISTORY = gql`
+  query GetPatientFamilyHistoryList($patientId: String!) {
+    getPatientFamilyHistoryList(patientId: $patientId) {
+      familyHistoryList {
+        id
+        description
+      }
+    }
+  }
+`;
+
+export const SAVE_PATIENT_LIFE_STYLE = gql`
+  mutation SavePatientLifeStyle($patientLifeStyleInput: PatientLifeStyleInput) {
+    savePatientLifeStyle(patientLifeStyleInput: $patientLifeStyleInput) {
+      patientLifeStyle {
+        id
+        description
+      }
+    }
+  }
+`;
+
+export const GET_PATIENT_LIFE_STYLE = gql`
+  query GetPatientLifeStyleList($patientId: String) {
+    getPatientLifeStyleList(patientId: $patientId) {
+      lifeStyleList {
+        id
+        description
+      }
+    }
+  }
+`;
---
+++ packages/web-doctors/src/graphql/types/GetPatientLifeStyleList.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetPatientLifeStyleList
+// ====================================================
+
+export interface GetPatientLifeStyleList_getPatientLifeStyleList_lifeStyleList {
+  __typename: "PatientLifeStyles";
+  id: string;
+  description: string | null;
+}
+
+export interface GetPatientLifeStyleList_getPatientLifeStyleList {
+  __typename: "PatientLifeStyleListResult";
+  lifeStyleList: GetPatientLifeStyleList_getPatientLifeStyleList_lifeStyleList[] | null;
+}
+
+export interface GetPatientLifeStyleList {
+  getPatientLifeStyleList: GetPatientLifeStyleList_getPatientLifeStyleList;
+}
+
+export interface GetPatientLifeStyleListVariables {
+  patientId?: string | null;
+}
