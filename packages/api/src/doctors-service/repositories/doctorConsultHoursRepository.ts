8ce0ff5c0f4064bae7cefcbf6da6fb99241554bd
adityakvs <aditya.kvs@popcornapps.com>
2019-09-17 17:01:53 +0530

    added consult hours insert method

--- packages/api/src/doctors-service/repositories/doctorConsultHoursRepository.ts
+++ packages/api/src/doctors-service/repositories/doctorConsultHoursRepository.ts
@@ -5,6 +5,11 @@ import { addDays, addMilliseconds, format } from 'date-fns';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
 
+type DayTimes = {
+  startTime: string;
+  endTime: string;
+};
+
 @EntityRepository(ConsultHours)
 export class DoctorConsultHoursRepository extends Repository<ConsultHours> {
   getConsultHours(doctor: string, weekDay: string) {
@@ -35,47 +40,56 @@ export class DoctorConsultHoursRepository extends Repository<ConsultHours> {
     return this.count({ where: [{ doctor, consultMode }, { doctor, consultMode: 'BOTH' }] });
   }
 
-  getDayStarAndEndTimesArray(dayAllTimings: string) {
+  getDayStarAndEndTimesArray(dayAllTimings: string): DayTimes[] {
     const delimiter = '\r\n';
     const arrayTimings = dayAllTimings.split(delimiter);
     const startEndTimes = arrayTimings.map((timings) => {
       const timesArray = timings.split('-');
       const istStartTime = timesArray[0].toString().trim();
-      const istEndTime = timesArray[0].toString().trim();
-      //addMilliseconds(appointment.appointmentDateTime, 19800000);
-      const istStartDate = new Date(format(new Date(), 'yyyy-MM-dd') + ' ' + istStartTime + '');
-      const utcStartDate = addMilliseconds(istStartDate, -19800000);
+      const istEndTime = timesArray[1].toString().trim();
+      const istStartDate = new Date(format(new Date(), 'yyyy-MM-dd') + ' ' + istStartTime);
       const istEndDate = new Date(format(new Date(), 'yyyy-MM-dd') + ' ' + istEndTime);
+      const utcStartDate = addMilliseconds(istStartDate, -19800000);
       const utcEndDate = addMilliseconds(istEndDate, -19800000);
-      console.log(istStartTime, istEndTime);
-      console.log(istStartDate, istEndDate);
-      console.log(utcStartDate, utcEndDate);
-      console.log(
-        istStartDate.getUTCHours() + ':' + istStartDate.getUTCMinutes(),
-        istEndDate.getUTCHours() + ':' + istEndDate.getUTCMinutes()
-      );
+      const utcStartTime = format(utcStartDate, 'HH:mm');
+      const utcEndTime = format(utcEndDate, 'HH:mm');
+      console.log(utcStartTime, utcEndTime);
       console.log('----------------------------------------');
       return {
-        startTime: timesArray[0].toString().trim(),
-        endTime: timesArray[1].toString().trim(),
+        startTime: utcStartTime,
+        endTime: utcEndTime,
       };
     });
-    //console.log(startEndTimes);
+    return startEndTimes;
   }
 
   async insertOrUpdateAllConsultHours(availabilityData: any[]) {
-    //insert/update new doctors
+    const consultHours: Partial<ConsultHours>[] = [];
     availabilityData.forEach((consultHourData) => {
       const weekDays = consultHourData.weekDays.toString();
+      const arrWeekDays = weekDays.split(',');
       const availability = consultHourData.availability.toString();
-      this.getDayStarAndEndTimesArray(availability);
-      //console.log(weekDays);
+      const arrDayTimes = this.getDayStarAndEndTimesArray(availability);
+
+      arrWeekDays.forEach((weekDay: WeekDay) => {
+        arrDayTimes.forEach((dayTimes: DayTimes) => {
+          const dayConsultHour: Partial<ConsultHours> = {
+            weekDay: weekDay,
+            startTime: dayTimes.startTime,
+            endTime: dayTimes.endTime,
+            ...consultHourData,
+          };
+          consultHours.push(dayConsultHour);
+        });
+      });
     });
 
-    // return this.save(consultHours).catch((saveConsultHoursError) => {
-    //   throw new AphError(AphErrorMessages.SAVE_CONSULT_HOURS_ERROR, undefined, {
-    //     saveConsultHoursError,
-    //   });
-    // });
+    console.log(consultHours.length);
+
+    return this.save(consultHours).catch((saveConsultHoursError) => {
+      throw new AphError(AphErrorMessages.SAVE_CONSULT_HOURS_ERROR, undefined, {
+        saveConsultHoursError,
+      });
+    });
   }
 }
