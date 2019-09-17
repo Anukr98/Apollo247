f2c8f9c4976923c17b5b2342c3b4def5d8dd1ce3
anuppopcornapps <anup.purushottam@popcornapps.com>
2019-09-17 20:14:38 +0530

    error msg changes
    error msg changes

--- packages/web-doctors/src/components/JuniorDoctors/JDCallPopover.tsx
+++ packages/web-doctors/src/components/JuniorDoctors/JDCallPopover.tsx
@@ -1071,44 +1071,44 @@ export const JDCallPopover: React.FC<CallPopoverProps> = (props) => {
       (status, response) => {}
     );
 
-    let folloupDateTime = '';
-    if (
-      followUpDate &&
-      followUpDate.length > 0 &&
-      followUpDate[0] !== null &&
-      followUpDate[0] !== ''
-    ) {
-      folloupDateTime = followUpDate[0] ? new Date(followUpDate[0]).toISOString() : '';
-    } else if (followUp[0] && followUpAfterInDays[0] !== 'Custom') {
-      const apptdateTime = new Date(props.appointmentDateTime);
-      folloupDateTime = new Date(
-        apptdateTime.getTime() + parseInt(followUpAfterInDays[0]) * 24 * 60 * 60 * 1000
-      ).toISOString();
-    }
-    const followupObj: any = {
-      appointmentId: props.appointmentId,
-      folloupDateTime: folloupDateTime,
-      doctorId: props.doctorId,
-      caseSheetId: props.caseSheetId,
-      doctorInfo: currentPatient,
-      pdfUrl: props.prescriptionPdf,
-    };
-    if (folloupDateTime !== '') {
-      setTimeout(() => {
-        pubnub.publish(
-          {
-            message: {
-              id: props.doctorId,
-              message: followupconsult,
-              transferInfo: followupObj,
-            },
-            channel: channel,
-            storeInHistory: true,
-          },
-          (status, response) => {}
-        );
-      }, 100);
-    }
+    // let folloupDateTime = '';
+    // if (
+    //   followUpDate &&
+    //   followUpDate.length > 0 &&
+    //   followUpDate[0] !== null &&
+    //   followUpDate[0] !== ''
+    // ) {
+    //   folloupDateTime = followUpDate[0] ? new Date(followUpDate[0]).toISOString() : '';
+    // } else if (followUp[0] && followUpAfterInDays[0] !== 'Custom') {
+    //   const apptdateTime = new Date(props.appointmentDateTime);
+    //   folloupDateTime = new Date(
+    //     apptdateTime.getTime() + parseInt(followUpAfterInDays[0]) * 24 * 60 * 60 * 1000
+    //   ).toISOString();
+    // }
+    // const followupObj: any = {
+    //   appointmentId: props.appointmentId,
+    //   folloupDateTime: folloupDateTime,
+    //   doctorId: props.doctorId,
+    //   caseSheetId: props.caseSheetId,
+    //   doctorInfo: currentPatient,
+    //   pdfUrl: props.prescriptionPdf,
+    // };
+    // if (folloupDateTime !== '') {
+    //   setTimeout(() => {
+    //     pubnub.publish(
+    //       {
+    //         message: {
+    //           id: props.doctorId,
+    //           message: followupconsult,
+    //           transferInfo: followupObj,
+    //         },
+    //         channel: channel,
+    //         storeInHistory: true,
+    //       },
+    //       (status, response) => {}
+    //     );
+    //   }, 100);
+    // }
   };
   const transferConsultAction = () => {
     if (isEmpty(transferReason)) {
