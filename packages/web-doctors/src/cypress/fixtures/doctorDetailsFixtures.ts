7323c3d16231bc9411fe7c796940f0bcf02205dc
Kabir Sarin <sarink87@gmail.com>
2019-09-17 11:44:07 +0530

    Update doctorDetailsFixtures so pages quit blowing up

--- packages/web-doctors/src/cypress/fixtures/doctorDetailsFixtures.ts
+++ packages/web-doctors/src/cypress/fixtures/doctorDetailsFixtures.ts
@@ -1,27 +1,30 @@
 import { GetDoctorDetails_getDoctorDetails } from 'graphql/types/GetDoctorDetails';
-import { DOCTOR_ONLINE_STATUS, Salutation, DoctorType } from 'graphql/types/globalTypes';
+import { DOCTOR_ONLINE_STATUS, Salutation, DoctorType, Gender } from 'graphql/types/globalTypes';
 
 type Doctor = GetDoctorDetails_getDoctorDetails;
 
 export const jrKabir: Doctor = {
   __typename: 'DoctorDetails',
+  id: 'befa91a6-adb0-488d-a148-cc84ce3cacac',
+  firstName: 'Kabir',
+  lastName: 'Sarin',
+  emailAddress: 'kabir@sarink.net',
+  doctorType: DoctorType.JUNIOR,
+  // mobileNumber: '+919999999999', // OTP is 999999
+  mobileNumber: '+919296858696', // OTP is 772345
+  delegateNumber: '+91123456789',
+  isActive: true, // Don't forget to set this to true or you won't be able to log in!
+  onlineStatus: DOCTOR_ONLINE_STATUS.ONLINE,
+  // firebaseToken: 'dJYFTV7MJGWK2EdziTtr5zUPm243', // This is actually the firebaseuid, not the token
+  firebaseToken: 'gnUQsiiGlrS0NuGgF62RsmvG1HF3',
+
   awards: '',
   city: '',
   country: '',
   dateOfBirth: '',
-  doctorType: DoctorType.JUNIOR,
-  delegateNumber: '',
-  emailAddress: '',
   experience: '',
-  firebaseToken: '',
-  firstName: 'Kabir',
-  isActive: true,
-  id: '1',
   languages: 'Typescript, Javascript, Ruby, Python, C, GraphQL',
-  lastName: 'Sarin',
-  mobileNumber: '+919296858696',
   onlineConsultationFees: '500',
-  onlineStatus: DOCTOR_ONLINE_STATUS.ONLINE,
   photoUrl: '',
   physicalConsultationFees: '200',
   qualification: '',
