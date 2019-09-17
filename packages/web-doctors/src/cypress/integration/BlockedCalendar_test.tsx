01a35924bfc21738bcdb1e0d6a13fd2ee349cf58
Kabir Sarin <sarink87@gmail.com>
2019-09-17 11:49:11 +0530

    Add all slots test

--- packages/web-doctors/src/cypress/integration/BlockedCalendar_test.tsx
+++ packages/web-doctors/src/cypress/integration/BlockedCalendar_test.tsx
@@ -13,12 +13,46 @@ describe('BlockedCalendar', () => {
     cy.contains(/add blocked hours/i);
   });
 
-  it('Should convert UTC dates to IST for display purposes', () => {
+  it('Should convert UTC dates to IST for display purposes (same-day)', () => {
     const doctorId = srKabir.id;
     const startUtcFromDb = '2019-09-18T10:00:00.000Z';
     const endUtcFromDb = '2019-09-18T11:00:00.000Z';
     const displayDateIst = 'Wed, 09/18/2019';
     const displayTimeIst = '3:30 PM - 4:30 PM';
+    cy.mockAphGraphqlOps({
+      operations: {
+        GetBlockedCalendar: {
+          getBlockedCalendar: {
+            __typename: 'BlockedCalendarResult',
+            blockedCalendar: [
+              {
+                __typename: 'BlockedCalendarItem',
+                id: 1,
+                doctorId,
+                start: startUtcFromDb,
+                end: endUtcFromDb,
+              },
+            ],
+          },
+        },
+      },
+    });
+    cy.contains(/add blocked hours/i).should('exist');
+    cy.get('[data-cypress="BlockedCalendar"]')
+      .find('[class*="MuiCircularProgress"]')
+      .should('exist')
+      .find('[class*="MuiCircularProgress"]')
+      .should('not.exist');
+    cy.contains(displayDateIst).should('exist');
+    cy.contains(displayTimeIst).should('exist');
+  });
+
+  it('Should display "All slots" for multi-day block', () => {
+    const doctorId = srKabir.id;
+    const startUtcFromDb = '2019-09-18T10:00:00.000Z';
+    const endUtcFromDb = '2019-09-19T11:00:00.000Z';
+    const displayDateIst = '09/18/2019 - 09/19/2019';
+    const displayTimeIst = 'All slots';
     cy.mockAphGraphqlOps({
       operations: {
         GetBlockedCalendar: {
