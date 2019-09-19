d16e1f0ce5f32765371d7dce1a437c7876ddcbda
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2019-09-19 19:02:07 +0530

    Cancel Order fixed, Prescription upload UI fixed

--- packages/mobile-patients/src/components/ui/Spinner.tsx
+++ packages/mobile-patients/src/components/ui/Spinner.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { ActivityIndicator, View, Platform } from 'react-native';
+import { ActivityIndicator, View, Platform, StyleProp, ViewStyle } from 'react-native';
 
-export interface SpinnerProps {}
+export interface SpinnerProps {
+  style?: StyleProp<ViewStyle>;
+}
 
 export const Spinner: React.FC<SpinnerProps> = (props) => {
   return (
     <View
-      style={{
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundColor: Platform.OS === 'ios' ? 'rgba(0,0,0, 0.3)' : '',
-        alignSelf: 'center',
-        justifyContent: 'center',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 10,
-        elevation: 20,
-      }}
+      style={[
+        {
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundColor: Platform.OS === 'ios' ? 'rgba(0,0,0, 0.3)' : '',
+          alignSelf: 'center',
+          justifyContent: 'center',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: 10,
+          elevation: 20,
+        },
+        props.style,
+      ]}
     >
       <ActivityIndicator animating={true} size="large" color="green" />
     </View>
