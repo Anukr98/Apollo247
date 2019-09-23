2416c644db76a4f170e8b3f69e96c72b4c3b9c16
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2019-09-23 15:25:12 +0530

    added function to reschedule

--- packages/api/src/consults-service/repositories/rescheduleAppointmentRepository.ts
+++ packages/api/src/consults-service/repositories/rescheduleAppointmentRepository.ts
@@ -1,7 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, Connection } from 'typeorm';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
 import { RescheduleAppointmentDetails, TRANSFER_STATUS } from 'consults-service/entities';
+import { sendNotification, NotificationType } from 'notifications-service/resolvers/notifications';
+import { ConsultServiceContext } from 'consults-service/consultServiceContext';
 
 @EntityRepository(RescheduleAppointmentDetails)
 export class RescheduleAppointmentRepository extends Repository<RescheduleAppointmentDetails> {
@@ -21,5 +23,46 @@ export class RescheduleAppointmentRepository extends Repository<RescheduleAppoin
 
   updateReschedule(id: string, rescheduleStatus: TRANSFER_STATUS) {
     return this.update(id, { rescheduleStatus });
+  }
+
+  async rescheduleAppointment(rescheduleAppointmentAttrs: Partial<RescheduleAppointmentDetails>) {
+    const recCount = await this.count({
+      where: {
+        appointment: rescheduleAppointmentAttrs.appointment,
+        rescheduleStatus: TRANSFER_STATUS.INITIATED,
+      },
+    });
+    if (recCount > 0) {
+      throw new AphError(AphErrorMessages.RESCHEDULE_APPOINTMENT_ERROR, undefined, {});
+    }
+    const createReschdule = this.create(rescheduleAppointmentAttrs)
+      .save()
+      .catch((createErrors) => {
+        throw new AphError(AphErrorMessages.RESCHEDULE_APPOINTMENT_ERROR, undefined, {
+          createErrors,
+        });
+      });
+
+    if (rescheduleAppointmentAttrs.appointment) {
+      // send notification
+      const pushNotificationInput = {
+        appointmentId: rescheduleAppointmentAttrs.appointment.id,
+        notificationType: NotificationType.INITIATE_RESCHEDULE,
+      };
+      const context: Partial<ConsultServiceContext> = {};
+      if (!context.consultsDb || !context.patientsDb || !context.doctorsDb) {
+        throw new AphError(AphErrorMessages.RESCHEDULE_APPOINTMENT_ERROR, undefined, {});
+      }
+
+      const notificationResult = sendNotification(
+        pushNotificationInput,
+        context.patientsDb,
+        context.consultsDb,
+        context.doctorsDb
+      );
+
+      console.log(notificationResult, 'notificationResult');
+    }
+    return createReschdule;
   }
 }
