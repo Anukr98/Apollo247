aeb3e5823c9ef25889bded94799d1ccc4356236d
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2019-09-25 17:00:34 +0530

    Common Spinner and alert in context

---
+++ packages/mobile-patients/src/components/UIElementsProvider.tsx
@@ -0,0 +1,135 @@
+import { BottomPopUp } from '@aph/mobile-patients/src/components/ui/BottomPopUp';
+import { Spinner } from '@aph/mobile-patients/src/components/ui/Spinner';
+import { theme } from '@aph/mobile-patients/src/theme/theme';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import {
+  BackHandler,
+  StyleProp,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  okButtonStyle: {
+    paddingHorizontal: 25,
+    backgroundColor: 'transparent',
+    marginTop: 8,
+    marginBottom: 20,
+    alignItems: 'flex-end',
+  },
+  okButtonTextStyle: {
+    paddingTop: 16,
+    ...theme.viewStyles.yellowTextStyle,
+  },
+});
+
+export interface UIElementsContextProps {
+  setLoading: ((isLoading: boolean) => void) | null;
+  showAphAlert: ((params: AphAlertParams) => void) | null;
+  hideAphAlert: (() => void) | null;
+}
+
+export const UIElementsContext = createContext<UIElementsContextProps>({
+  setLoading: null,
+  showAphAlert: null,
+  hideAphAlert: null,
+});
+
+type AphAlertParams = {
+  title?: string;
+  description?: string;
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  onPressOk?: () => void;
+};
+
+export const UIElementsProvider: React.FC = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [isAlertVisible, setAlertVisible] = useState(false);
+  const [alertParams, setAlertParams] = useState<AphAlertParams>({});
+
+  useEffect(() => {
+    if (isAlertVisible || loading) {
+      BackHandler.addEventListener('hardwareBackPress', handleBack);
+    } else {
+      BackHandler.removeEventListener('hardwareBackPress', handleBack);
+    }
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBack);
+    };
+  }, [isAlertVisible, loading]);
+
+  const handleBack = async () => {
+    console.log('handleBack Called');
+    // setLoading(false);
+    setAlertVisible(false);
+    return true;
+  };
+
+  const renderLoading = () => {
+    return loading && <Spinner />;
+  };
+
+  const renderAphAlert = () => {
+    const renderOkButton = (
+      <TouchableOpacity
+        style={styles.okButtonStyle}
+        onPress={() => {
+          if (alertParams.onPressOk) {
+            setAlertVisible(false);
+            alertParams.onPressOk();
+          } else {
+            setAlertVisible(false);
+            setAlertParams({});
+          }
+        }}
+      >
+        <Text style={styles.okButtonTextStyle}>OK, GOT IT</Text>
+      </TouchableOpacity>
+    );
+
+    return (
+      isAlertVisible && (
+        <BottomPopUp
+          title={alertParams.title}
+          description={alertParams.description}
+          style={alertParams.style}
+        >
+          {alertParams.children ? alertParams.children : renderOkButton}
+        </BottomPopUp>
+      )
+    );
+  };
+
+  const showAphAlert = (params: AphAlertParams) => {
+    setAlertParams(params);
+    setAlertVisible(true);
+  };
+
+  const hideAphAlert = () => {
+    setAlertVisible(false);
+  };
+
+  return (
+    <UIElementsContext.Provider
+      value={{
+        setLoading,
+        showAphAlert,
+        hideAphAlert,
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }} pointerEvents={loading ? 'none' : 'auto'}>
+          {props.children}
+          {renderLoading()}
+        </View>
+        {renderAphAlert()}
+      </View>
+    </UIElementsContext.Provider>
+  );
+};
+
+export const useUIElements = () => useContext<UIElementsContextProps>(UIElementsContext);
