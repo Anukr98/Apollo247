6237b5af496ef783e2c36305b873125a41337a27
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2019-10-17 16:21:30 +0530

    added required params, changed sorting order, modified query for doctor block slots

--- packages/api/src/doctors-service/repositories/blockedCalendarItemRepository.ts
+++ packages/api/src/doctors-service/repositories/blockedCalendarItemRepository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository, MoreThanOrEqual, LessThanOrEqual } from 'typeorm';
 import { BlockedCalendarItem } from 'doctors-service/entities';
-import { addDays, format } from 'date-fns';
+//import { addDays, format } from 'date-fns';
 
 @EntityRepository(BlockedCalendarItem)
 export class BlockedCalendarItemRepository extends Repository<BlockedCalendarItem> {
@@ -11,14 +11,14 @@ export class BlockedCalendarItemRepository extends Repository<BlockedCalendarIte
   }
 
   getBlockedSlots(slot: Date, doctorId: string) {
-    const inputStartDate = format(addDays(slot, -1), 'yyyy-MM-dd');
-    const currentStartDate = new Date(inputStartDate + 'T18:30');
-    const currentEndDate = new Date(format(slot, 'yyyy-MM-dd').toString() + 'T18:29');
+    // const inputStartDate = format(addDays(slot, -1), 'yyyy-MM-dd');
+    // const currentStartDate = new Date(inputStartDate + 'T18:30');
+    // const currentEndDate = new Date(format(slot, 'yyyy-MM-dd').toString() + 'T18:29');
+    // start: MoreThanOrEqual(currentStartDate),
+    // end: LessThanOrEqual(currentEndDate),
     return this.find({
       where: {
         doctorId,
-        start: MoreThanOrEqual(currentStartDate),
-        end: LessThanOrEqual(currentEndDate),
       },
     });
   }
