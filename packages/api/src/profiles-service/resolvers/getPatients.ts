c2f4bd25f5b7ff11b112393fcca56766e6614dae
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2019-10-22 08:55:39 +0530

    added api to get patient details by id and mobile number

--- packages/api/src/profiles-service/resolvers/getPatients.ts
+++ packages/api/src/profiles-service/resolvers/getPatients.ts
@@ -1,20 +1,62 @@
 import gql from 'graphql-tag';
+import { Resolver } from 'api-gateway';
+import { ProfilesServiceContext } from 'profiles-service/profilesServiceContext';
+import { Patient } from 'profiles-service/entities';
+import { PatientRepository } from 'profiles-service/repositories/patientRepository';
+import { AphError } from 'AphError';
+import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
 
 export const getPatientTypeDefs = gql`
-  type GetPatientsResult {
-    patients: [Patient!]!
+  type PatientInfo {
+    patients: Patient
+  }
+  type PatientList {
+    patients: [Patient]
   }
   extend type Query {
-    getPatients: GetPatientsResult
+    getPatientById(patientId: String): PatientInfo
+    getPatientByMobile(mobileNumber: String): PatientList
   }
 `;
 
-const getPatients = () => {
-  return { patients: [] };
+type PatientInfo = {
+  patients: Patient;
+};
+type PatientList = {
+  patients: Patient[];
+};
+
+const getPatientById: Resolver<
+  null,
+  { patientId: string },
+  ProfilesServiceContext,
+  PatientInfo
+> = async (parent, args, { profilesDb }) => {
+  const patientRepo = profilesDb.getCustomRepository(PatientRepository);
+  const patients = await patientRepo.findById(args.patientId);
+  if (!patients) {
+    throw new AphError(AphErrorMessages.INVALID_PATIENT_ID, undefined, {});
+  }
+  return { patients };
+};
+
+const getPatientByMobile: Resolver<
+  null,
+  { mobileNumber: string },
+  ProfilesServiceContext,
+  PatientList
+> = async (parent, args, { profilesDb }) => {
+  const patientRepo = profilesDb.getCustomRepository(PatientRepository);
+  const patients = await patientRepo.findByMobileNumber(args.mobileNumber);
+  if (!patients) {
+    throw new AphError(AphErrorMessages.INVALID_PATIENT_ID, undefined, {});
+  }
+  return { patients };
 };
 
 export const getPatientResolvers = {
   Query: {
-    getPatients,
+    getPatientById,
+    getPatientByMobile,
   },
 };
