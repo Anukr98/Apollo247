7e8e1698b2f42e06150bb1885b6681293f8de637
Ramana Odela <ramana.odela@popcornapps.com>
2019-11-04 11:04:15 +0530

    updated the edit prompt issue
    updated the edit prompt issue

--- packages/web-doctors/src/components/CallPopover.tsx
+++ packages/web-doctors/src/components/CallPopover.tsx
@@ -1299,7 +1299,7 @@ export const CallPopover: React.FC<CallPopoverProps> = (props) => {
     <div className={classes.stickyHeader}>
       <div className={classes.breadcrumbs}>
         <div>
-          {props.appointmentStatus !== 'COMPLETED' && (
+          {(props.appointmentStatus !== 'COMPLETED' || isClickedOnEdit) && (
             <Prompt message="Are you sure to exit?" when={props.startAppointment}></Prompt>
           )}
           <Link to="/calendar">
