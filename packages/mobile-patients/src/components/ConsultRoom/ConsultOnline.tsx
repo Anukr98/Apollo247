f41b5bb836f6dc4522da7861523cbd91c0d5cc3e
sankeeth <sankeerth.obulam@popcornapps.com>
2019-11-12 16:54:07 +0530

    Changed pub nub keys and added bugfender

--- packages/mobile-patients/src/components/ConsultRoom/ConsultOnline.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultOnline.tsx
@@ -28,6 +28,7 @@ import { useApolloClient } from 'react-apollo-hooks';
 import { StyleSheet, Text, View } from 'react-native';
 import { CalendarView, CALENDAR_TYPE } from '../ui/CalendarView';
 import { CommonLogEvent } from '../../FunctionHelpers/DeviceHelper';
+import { AppRoutes } from '../NavigatorContainer';
 
 const styles = StyleSheet.create({
   selectedButtonView: {
@@ -367,7 +368,7 @@ export const ConsultOnline: React.FC<ConsultOnlineProps> = (props) => {
               selectedCTA === onlineCTA[0] ? styles.selectedButtonText : null,
             ]}
             onPress={() => {
-              CommonLogEvent('CONSULT_ONLINE', 'Consult Now clicked');
+              CommonLogEvent(AppRoutes.DoctorDetails, 'Consult Now clicked');
               setselectedCTA(onlineCTA[0]);
               props.setisConsultOnline(true);
               // props.setselectedTimeSlot('');
@@ -389,7 +390,7 @@ export const ConsultOnline: React.FC<ConsultOnlineProps> = (props) => {
               selectedCTA === onlineCTA[1] ? styles.selectedButtonText : null,
             ]}
             onPress={() => {
-              CommonLogEvent('CONSULT_ONLINE', 'Schedule For Later clicked');
+              CommonLogEvent(AppRoutes.DoctorDetails, 'Schedule For Later clicked');
               fetchSlots();
               setselectedCTA(onlineCTA[1]);
               props.setisConsultOnline(false);
