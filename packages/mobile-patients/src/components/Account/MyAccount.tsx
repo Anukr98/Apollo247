54641174e61cd17e91dafa0fffeee54a1c763058
sankeeth <sankeerth.obulam@popcornapps.com>
2019-11-22 10:34:49 +0530

    Production push

--- packages/mobile-patients/src/components/Account/MyAccount.tsx
+++ packages/mobile-patients/src/components/Account/MyAccount.tsx
@@ -382,7 +382,7 @@ export const MyAccount: React.FC<MyAccountProps> = (props) => {
                 paddingTop: 10,
               }}
             >
-              QA V 1.0(54)
+              PRO V 1.0(16)
             </Text>
           </View>
         </Animated.ScrollView>
