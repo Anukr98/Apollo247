51d2632282bbcda17f6b50cf4de1fed1c8291c43
srknalabothu <57083939+srknalabothu@users.noreply.github.com>
2019-11-28 11:31:30 +0530

    formatChanges

--- packages/web-patients/src/components/VisitClinic.tsx
+++ packages/web-patients/src/components/VisitClinic.tsx
@@ -1,34 +1,23 @@
 import { makeStyles } from '@material-ui/styles';
 import { Theme, MenuItem, CircularProgress, Grid } from '@material-ui/core';
 import React, { useState, useEffect, useContext } from 'react';
-import {
-  AphButton,
-  AphSelect,
-  AphDialog,
-  AphDialogTitle
-} from '@aph/web-ui-components';
+import { AphButton, AphSelect, AphDialog, AphDialogTitle } from '@aph/web-ui-components';
 import { AphCalendar } from 'components/AphCalendar';
 import { DayTimeSlots } from 'components/DayTimeSlots';
 import Scrollbars from 'react-custom-scrollbars';
 import _uniqueId from 'lodash/uniqueId';
 import {
   GetDoctorDetailsById as DoctorDetails,
-  GetDoctorDetailsById_getDoctorDetailsById_doctorHospital as Facility
+  GetDoctorDetailsById_getDoctorDetailsById_doctorHospital as Facility,
 } from 'graphql/types/GetDoctorDetailsById';
 import {
   GetDoctorPhysicalAvailableSlots,
-  GetDoctorPhysicalAvailableSlotsVariables
+  GetDoctorPhysicalAvailableSlotsVariables,
 } from 'graphql/types/GetDoctorPhysicalAvailableSlots';
-import {
-  GET_DOCTOR_PHYSICAL_AVAILABLE_SLOTS,
-  BOOK_APPOINTMENT
-} from 'graphql/doctors';
+import { GET_DOCTOR_PHYSICAL_AVAILABLE_SLOTS, BOOK_APPOINTMENT } from 'graphql/doctors';
 import { useQueryWithSkip } from 'hooks/apolloHooks';
 import { Mutation } from 'react-apollo';
-import {
-  BookAppointment,
-  BookAppointmentVariables
-} from 'graphql/types/BookAppointment';
+import { BookAppointment, BookAppointmentVariables } from 'graphql/types/BookAppointment';
 import { APPOINTMENT_TYPE } from 'graphql/types/globalTypes';
 import { useAllCurrentPatients } from 'hooks/authHooks';
 import { clientRoutes } from 'helpers/clientRoutes';
@@ -41,7 +30,7 @@ const useStyles = makeStyles((theme: Theme) => {
   return {
     root: {
       width: '100%',
-      overflow: 'hidden'
+      overflow: 'hidden',
     },
     consultGroup: {
       boxShadow: '0 2px 4px 0 rgba(128, 128, 128, 0.3)',
@@ -59,14 +48,14 @@ const useStyles = makeStyles((theme: Theme) => {
       borderRadius: 10,
       marginBottom: 10,
       '& p': {
-        marginTop: 0
-      }
+        marginTop: 0,
+      },
     },
     actions: {
       paddingTop: 5,
       paddingBottom: 4,
       marginLeft: -8,
-      marginRight: -8
+      marginRight: -8,
     },
     button: {
       fontSize: 16,
@@ -76,19 +65,19 @@ const useStyles = makeStyles((theme: Theme) => {
       textTransform: 'none',
       borderRadius: 10,
       paddingLeft: 10,
-      paddingRight: 10
+      paddingRight: 10,
     },
     buttonActive: {
       backgroundColor: '#00b38e',
       color: theme.palette.common.white,
       '&:hover': {
         backgroundColor: '#00b38e',
-        color: theme.palette.common.white
-      }
+        color: theme.palette.common.white,
+      },
     },
     buttonDisable: {
       backgroundColor: '#fed984',
-      boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2) !important'
+      boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2) !important',
     },
     bottomActions: {
       padding: '10px 15px 15px 15px',
@@ -97,34 +86,34 @@ const useStyles = makeStyles((theme: Theme) => {
       '& button': {
         borderRadius: 10,
         textTransform: 'none',
-        minWidth: 288
-      }
+        minWidth: 288,
+      },
     },
     customScrollBar: {
       paddingTop: 10,
       paddingLeft: 20,
-      paddingRight: 20
+      paddingRight: 20,
     },
     timeSlots: {
-      paddingTop: 5
+      paddingTop: 5,
     },
     menuSelected: {
       backgroundColor: 'transparent !important',
-      color: '#00b38e !important'
+      color: '#00b38e !important',
     },
     selectMenuRoot: {
       '& svg': {
         color: '#00b38e',
-        fontSize: 30
-      }
+        fontSize: 30,
+      },
     },
     selectMenuItem: {
       color: theme.palette.secondary.dark,
-      fontWeight: 600
+      fontWeight: 600,
     },
     selectedAddress: {
       paddingTop: 20,
-      display: 'flex'
+      display: 'flex',
     },
     clinicAddress: {
       fontSize: 13,
@@ -133,7 +122,7 @@ const useStyles = makeStyles((theme: Theme) => {
       lineHeight: 1.54,
       letterSpacing: 0.33,
       paddingRight: 20,
-      width: '78%'
+      width: '78%',
     },
     clinicDistance: {
       marginLeft: 'auto',
@@ -145,28 +134,28 @@ const useStyles = makeStyles((theme: Theme) => {
       borderLeft: '0.5px solid rgba(2,71,91,0.3)',
       paddingLeft: 15,
       width: '22%',
-      textAlign: 'right'
+      textAlign: 'right',
     },
     noSlotsAvailable: {
       fontSize: 14,
       color: '#0087ba',
       fontWeight: 500,
       lineHeight: 1.71,
-      padding: 6
+      padding: 6,
     },
     circlularProgress: {
       display: 'flex',
       padding: 20,
-      justifyContent: 'center'
+      justifyContent: 'center',
     },
     noDataAvailable: {
       padding: 20,
       fontSize: 14,
       fontWeight: 500,
-      color: '#0087ba'
+      color: '#0087ba',
     },
     confirmationColor: {
-      color: '#fcb716'
+      color: '#fcb716',
     },
     dialogBody: {
       padding: 20,
@@ -174,16 +163,16 @@ const useStyles = makeStyles((theme: Theme) => {
       fontWeight: 500,
       fontSize: 14,
       '& span': {
-        fontWeight: 'bold'
-      }
+        fontWeight: 'bold',
+      },
     },
     dialogActions: {
       padding: 16,
       textAlign: 'center',
       '& button': {
-        minWidth: 288
-      }
-    }
+        minWidth: 288,
+      },
+    },
   };
 });
 
@@ -196,7 +185,7 @@ interface VisitClinicProps {
   doctorDetails: DoctorDetails;
 }
 
-export const VisitClinic: React.FC<VisitClinicProps> = props => {
+export const VisitClinic: React.FC<VisitClinicProps> = (props) => {
   const classes = useStyles();
   const [dateSelected, setDateSelected] = useState<string>('');
   const [timeSelected, setTimeSelected] = useState<string>('');
@@ -236,34 +225,27 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
     lateNightSlots: number[] = [];
 
   const apiDateFormat =
-    dateSelected === ''
-      ? new Date().toISOString().substring(0, 10)
-      : getYyMmDd(dateSelected);
+    dateSelected === '' ? new Date().toISOString().substring(0, 10) : getYyMmDd(dateSelected);
 
   const morningTime = getIstTimestamp(new Date(apiDateFormat), '12:01');
   const afternoonTime = getIstTimestamp(new Date(apiDateFormat), '17:01');
   const eveningTime = getIstTimestamp(new Date(apiDateFormat), '21:01');
 
   const doctorId =
-    doctorDetails &&
-    doctorDetails.getDoctorDetailsById &&
-    doctorDetails.getDoctorDetailsById.id
+    doctorDetails && doctorDetails.getDoctorDetailsById && doctorDetails.getDoctorDetailsById.id
       ? doctorDetails.getDoctorDetailsById.id
       : '';
 
   const clinics: Facility[] = [];
   if (doctorDetails && doctorDetails.getDoctorDetailsById) {
-    _forEach(
-      doctorDetails.getDoctorDetailsById.doctorHospital,
-      hospitalDetails => {
-        if (
-          hospitalDetails.facility.facilityType === 'CLINIC' ||
-          hospitalDetails.facility.facilityType === 'HOSPITAL'
-        ) {
-          clinics.push(hospitalDetails);
-        }
+    _forEach(doctorDetails.getDoctorDetailsById.doctorHospital, (hospitalDetails) => {
+      if (
+        hospitalDetails.facility.facilityType === 'CLINIC' ||
+        hospitalDetails.facility.facilityType === 'HOSPITAL'
+      ) {
+        clinics.push(hospitalDetails);
       }
-    );
+    });
   }
   const getDistance = (
     fromLat: string | null,
@@ -271,12 +253,7 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
     toLat: string,
     toLong: string
   ): string | null => {
-    if (
-      fromLat != null &&
-      fromLong != null &&
-      toLat != null &&
-      toLong != null
-    ) {
+    if (fromLat != null && fromLong != null && toLat != null && toLong != null) {
       const toRadian = (n: number) => (n * Math.PI) / 180;
       let toLatitude = parseFloat(toLat);
       let toLongitude = parseFloat(toLong);
@@ -328,9 +305,7 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
     }
   };
   const defaultClinicId =
-    clinics.length > 0 && clinics[0] && clinics[0].facility
-      ? clinics[0].facility.id
-      : '';
+    clinics.length > 0 && clinics[0] && clinics[0].facility ? clinics[0].facility.id : '';
 
   const { data, loading, error } = useQueryWithSkip<
     GetDoctorPhysicalAvailableSlots,
@@ -340,10 +315,10 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
       DoctorPhysicalAvailabilityInput: {
         doctorId: doctorId,
         availableDate: apiDateFormat,
-        facilityId: defaultClinicId
-      }
+        facilityId: defaultClinicId,
+      },
     },
-    fetchPolicy: 'network-only'
+    fetchPolicy: 'network-only',
   });
 
   useEffect(() => {
@@ -359,16 +334,11 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
   }
 
   if (error) {
-    return (
-      <div className={classes.noDataAvailable}>
-        Unable to load Available slots.
-      </div>
-    );
+    return <div className={classes.noDataAvailable}>Unable to load Available slots.</div>;
   }
 
-  const availableSlots =
-    (data && data.getDoctorPhysicalAvailableSlots.availableSlots) || [];
-  availableSlots.map(slot => {
+  const availableSlots = (data && data.getDoctorPhysicalAvailableSlots.availableSlots) || [];
+  availableSlots.map((slot) => {
     // const slotTimeUtc = new Date(new Date(`${apiDateFormat} ${slot}:00`).toISOString()).getTime();
     // const localTimeOffset = new Date().getTimezoneOffset() * 60000;
     // const slotTime = new Date(slotTimeUtc - localTimeOffset).getTime();
@@ -376,10 +346,8 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
     const currentTime = new Date(new Date().toISOString()).getTime();
     if (slotTime > currentTime) {
       if (slotTime < morningTime) morningSlots.push(slotTime);
-      else if (slotTime >= morningTime && slotTime < afternoonTime)
-        afternoonSlots.push(slotTime);
-      else if (slotTime >= afternoonTime && slotTime < eveningTime)
-        eveningSlots.push(slotTime);
+      else if (slotTime >= morningTime && slotTime < afternoonTime) afternoonSlots.push(slotTime);
+      else if (slotTime >= afternoonTime && slotTime < eveningTime) eveningSlots.push(slotTime);
       else lateNightSlots.push(slotTime);
     }
   });
@@ -392,17 +360,9 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
     timeSelected === '';
   const defaultClinicAddress =
     clinics.length > 0 && clinics[0] && clinics[0].facility
-      ? `${
-          clinics[0].facility.streetLine1 ? clinics[0].facility.streetLine1 : ''
-        } ${
-          clinics[0].facility.streetLine2
-            ? `${clinics[0].facility.streetLine2}`
-            : ''
-        } ${
-          clinics[0].facility.streetLine3
-            ? `${clinics[0].facility.streetLine3}`
-            : ''
-        }`
+      ? `${clinics[0].facility.streetLine1 ? clinics[0].facility.streetLine1 : ''} ${
+          clinics[0].facility.streetLine2 ? `${clinics[0].facility.streetLine2}` : ''
+        } ${clinics[0].facility.streetLine3 ? `${clinics[0].facility.streetLine3}` : ''}`
       : '';
 
   return (
@@ -413,9 +373,7 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
             <Grid item sm={6}>
               <div className={classes.consultGroup}>
                 <AphCalendar
-                  getDate={(dateSelected: string) =>
-                    setDateSelected(dateSelected)
-                  }
+                  getDate={(dateSelected: string) => setDateSelected(dateSelected)}
                   selectedDate={new Date(apiDateFormat)}
                 />
               </div>
@@ -423,21 +381,19 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
             <Grid item sm={6}>
               <div className={`${classes.consultGroup} ${classes.timeSlots}`}>
                 <AphSelect
-                  value={
-                    clinicSelected === '' ? defaultClinicId : clinicSelected
-                  }
-                  onChange={e => {
+                  value={clinicSelected === '' ? defaultClinicId : clinicSelected}
+                  onChange={(e) => {
                     setClinicSelected(e.target.value as string);
                   }}
                   MenuProps={{
                     anchorOrigin: {
                       vertical: 'top',
-                      horizontal: 'right'
+                      horizontal: 'right',
                     },
                     transformOrigin: {
                       vertical: 'top',
-                      horizontal: 'right'
-                    }
+                      horizontal: 'right',
+                    },
                   }}
                 >
                   {clinics.map((clinicDetails: Facility) => {
@@ -445,11 +401,7 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
                       <MenuItem
                         classes={{ selected: classes.menuSelected }}
                         key={_uniqueId('clinic_')}
-                        value={
-                          (clinicDetails.facility.id &&
-                            clinicDetails.facility.id) ||
-                          ''
-                        }
+                        value={(clinicDetails.facility.id && clinicDetails.facility.id) || ''}
                       >
                         {clinicDetails.facility.name}
                       </MenuItem>
@@ -458,12 +410,10 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
                 </AphSelect>
                 <div className={classes.selectedAddress}>
                   <div className={classes.clinicAddress}>
-                    {clinicAddress === ''
-                      ? defaultClinicAddress
-                      : clinicAddress}
+                    {clinicAddress === '' ? defaultClinicAddress : clinicAddress}
                   </div>
                   <div className={classes.clinicDistance}>
-                    <img src={require('images/ic_location.svg')} alt='' />
+                    <img src={require('images/ic_location.svg')} alt="" />
                     <br />
                     {getClinicDistance(
                       clinicSelected || defaultClinicId,
@@ -482,7 +432,7 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
                     eveningSlots={eveningSlots}
                     latenightSlots={lateNightSlots}
                     doctorName={doctorName}
-                    timeSelected={timeSelected => setTimeSelected(timeSelected)}
+                    timeSelected={(timeSelected) => setTimeSelected(timeSelected)}
                   />
                 ) : (
                   <div className={classes.noSlotsAvailable}>
@@ -505,33 +455,31 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
                 `${apiDateFormat} ${timeSelected.padStart(5, '0')}:00`
               ).toISOString(),
               appointmentType: APPOINTMENT_TYPE.PHYSICAL,
-              hospitalId: defaultClinicId
-            }
+              hospitalId: defaultClinicId,
+            },
           }}
           onCompleted={() => {
             setMutationLoading(false);
             setIsDialogOpen(true);
           }}
-          onError={error => {
+          onError={(error) => {
             alert(error);
           }}
         >
-          {mutate => (
+          {(mutate) => (
             <AphButton
-              color='primary'
+              color="primary"
               disabled={disableSubmit || mutationLoading || isDialogOpen}
-              onClick={e => {
+              onClick={(e) => {
                 setMutationLoading(true);
                 mutate();
               }}
               className={
-                disableSubmit || mutationLoading || isDialogOpen
-                  ? classes.buttonDisable
-                  : ''
+                disableSubmit || mutationLoading || isDialogOpen ? classes.buttonDisable : ''
               }
             >
               {mutationLoading ? (
-                <CircularProgress size={22} color='secondary' />
+                <CircularProgress size={22} color="secondary" />
               ) : (
                 `PAY Rs. ${physicalConsultationFees}`
               )}
@@ -544,16 +492,15 @@ export const VisitClinic: React.FC<VisitClinicProps> = props => {
         disableBackdropClick
         disableEscapeKeyDown
         onClose={() => setIsDialogOpen(false)}
-        maxWidth='sm'
+        maxWidth="sm"
       >
         <AphDialogTitle>Appointment Confirmation</AphDialogTitle>
         <div className={classes.dialogBody}>
-          Your appointment has been successfully booked with{' '}
-          <span>Dr. {doctorName}</span>
+          Your appointment has been successfully booked with <span>Dr. {doctorName}</span>
         </div>
         <div className={classes.dialogActions}>
           <AphButton
-            color='primary'
+            color="primary"
             onClick={() => {
               setIsDialogOpen(false);
             }}
