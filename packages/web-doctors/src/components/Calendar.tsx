5785dbefa5184ce3e0f51d7bc87a51b71c887cd4
ramanaodela1 <45425060+ramanaodela1@users.noreply.github.com>
2019-11-29 14:34:12 +0530

    updated text changes in sd and Jd .
    updated text changes in sd and Jd .

--- packages/web-doctors/src/components/Calendar.tsx
+++ packages/web-doctors/src/components/Calendar.tsx
@@ -312,6 +312,7 @@ export const Calendar: React.FC = () => {
     pollInterval: pageRefreshTimeInSeconds * 1000,
     notifyOnNetworkStatusChange: true,
   });
+  // console.log('lastName:', 'Dr' + '.' + currentPatient!.lastName);
 
   return (
     <div className={classes.welcome}>
@@ -322,7 +323,7 @@ export const Calendar: React.FC = () => {
         <div className={classes.container}>
           <div className={classes.tabHeading}>
             <Typography variant="h1">{`hello  ${(
-              (currentPatient && currentPatient!.lastName) ||
+              (currentPatient && 'Dr' + '.' + currentPatient!.lastName) ||
               ''
             ).toLowerCase()} :)`}</Typography>
             {viewSelection === 'day' ? (
