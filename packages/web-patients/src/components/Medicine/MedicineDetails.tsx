404fb7acdbce56ccb001b83d4b71229100ea241b
malleshpopcornapps <popcorn@PCL-MALLESH.localdomain>
2019-12-03 18:15:17 +0530

    Integrated image gallery for medicine images

--- packages/web-patients/src/components/Medicine/MedicineDetails.tsx
+++ packages/web-patients/src/components/Medicine/MedicineDetails.tsx
@@ -5,7 +5,7 @@ import { Header } from 'components/Header';
 import { clientRoutes } from 'helpers/clientRoutes';
 import Scrollbars from 'react-custom-scrollbars';
 import { MedicineFilter } from 'components/Medicine/MedicineFilter';
-import ImageGallery from 'react-image-gallery';
+import { MedicineImageGallery } from 'components/Medicine/MedicineImageGallery';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -116,54 +116,6 @@ const useStyles = makeStyles((theme: Theme) => {
       borderRadius: 5,
       display: 'flex',
     },
-    imageGalleryGroup: {
-      width: 290,
-      '& .image-gallery-slides': {
-        border: 'solid 1px rgba(151,151,151,0.24)',
-        borderRadius: 10,
-        overflow: 'hidden',
-      },
-      '& .image-gallery-thumbnails': {
-        paddingTop: 20,
-        '& a': {
-          width: 60,
-          backgroundColor: theme.palette.common.white,
-          boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2)',
-          borderRadius: 10,
-          padding: 8,
-          border: '1px solid transparent',
-          marginLeft: 8,
-          '&.active': {
-            border: '1px solid #00b38e',
-          },
-          '&:first-child': {
-            marginLeft: 0,
-          },
-        },
-      },
-      '& .image-gallery-bullets': {
-        position: 'static',
-        marginTop: 8,
-        '& >div': {
-          display: 'flex',
-          justifyContent: 'center',
-        },
-        '& button': {
-          padding: 0,
-          width: 8,
-          height: 8,
-          backgroundColor: '#0087ba',
-          opacity: 0.4,
-          border: '2px solid #fff',
-          boxShadow: 'none',
-          margin: '0 2px',
-          '&.active': {
-            border: '4px solid #0087ba',
-            opacity: 1,
-          },
-        },
-      },
-    },
     productDetails: {
       paddingLeft: 20,
       width: 'calc(100% - 290px)',
@@ -228,20 +180,6 @@ const useStyles = makeStyles((theme: Theme) => {
 export const MedicineDetails: React.FC = (props) => {
   const classes = useStyles();
   const [tabValue, setTabValue] = React.useState<number>(0);
-  const images = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-  ];
 
   return (
     <div className={classes.welcome}>
@@ -266,15 +204,7 @@ export const MedicineDetails: React.FC = (props) => {
               <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 195px'}>
                 <div className={classes.customScroll}>
                   <div className={classes.productInformation}>
-                    <div className={classes.imageGalleryGroup}>
-                      <ImageGallery
-                        items={images}
-                        showFullscreenButton={false}
-                        showPlayButton={false}
-                        showNav={false}
-                        showBullets={true}
-                      />
-                    </div>
+                    <MedicineImageGallery />
                     <div className={classes.productDetails}>
                       <h2>Crocin Advance Tab</h2>
                       <div className={classes.textInfo}>
