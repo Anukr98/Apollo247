81e3e5bd2d80703936f62490b5e3b23f2720505b
Vishal Anil Kalambe <vishal.kalambe@popcornapps.com>
2019-12-04 12:29:28 +0530

    UI changes
    UI changes

--- packages/web-doctors/src/components/case-sheet/panels/OtherInstructions.tsx
+++ packages/web-doctors/src/components/case-sheet/panels/OtherInstructions.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Typography, Chip, Theme } from "@material-ui/core";
+import { Typography, Chip, Theme, Grid } from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/styles";
 import { InputBase, Button } from "@material-ui/core";
 import { AphButton } from "@aph/web-ui-components";
@@ -90,6 +90,37 @@ const useStyles = makeStyles((theme: Theme) =>
         padding: 10
       }
     },
+    othersBtnfavContainer: {
+      border: "1px solid rgba(2, 71, 91, 0.15)",
+      backgroundColor: "rgba(0,0,0,0.02)",
+      borderRadius: 5,
+      padding: 10
+    },
+    othersBtnfav: {
+      height: "auto",
+      borderBottom: "1px solid rgba(2, 71, 91, 0.15)",
+      fontWeight: 600,
+      fontSize: 14,
+      color: "#02475b !important",
+      padding: "5px 0",
+      "& img": {
+        float: "right",
+        paddingTop: 5
+      },
+      "&:focus": {
+        backgroundColor: "rgba(0,0,0,0.02)"
+      },
+      "&:last-child": {
+        borderBottom: "none"
+      },
+      "& span": {
+        display: "inline-block",
+        width: "100%",
+        textAlign: "left",
+        whiteSpace: "normal",
+        padding: 10
+      }
+    },
     btnAddDoctor: {
       backgroundColor: "transparent",
       boxShadow: "none",
@@ -104,6 +135,9 @@ const useStyles = makeStyles((theme: Theme) =>
       "& img": {
         marginRight: 8
       }
+    },
+    chip: {
+      background: "transparent"
     }
   })
 );
@@ -147,121 +181,128 @@ export const OtherInstructions: React.FC = () => {
 
   return (
     <Typography component="div" className={classes.contentContainer}>
-      <Typography component="div" className={classes.column}>
-        <Typography component="h5" variant="h5">
-          Instructions to the patient
-        </Typography>
-        <Typography component="div" className={classes.listContainer}>
-          {selectedValues !== null &&
-            selectedValues.length > 0 &&
-            selectedValues!.map(
-              (item, idx) =>
-                item.instruction!.trim() !== "" && (
-                  <Chip
-                    className={classes.othersBtn}
-                    key={idx}
-                    label={item!.instruction}
-                    onDelete={() => handleDelete(item, idx)}
-                    deleteIcon={
-                      <img
-                        src={
-                          caseSheetEdit && require("images/ic_cancel_green.svg")
+      <Grid container spacing={1}>
+        <Grid item lg={6} xs={12}>
+          <Typography component="div" className={classes.column}>
+            <Typography component="h5" variant="h5">
+              Instructions to the patient
+            </Typography>
+            <Typography component="div" className={classes.listContainer}>
+              {selectedValues !== null &&
+                selectedValues.length > 0 &&
+                selectedValues!.map(
+                  (item, idx) =>
+                    item.instruction!.trim() !== "" && (
+                      <Chip
+                        className={classes.othersBtn}
+                        key={idx}
+                        label={item!.instruction}
+                        onDelete={() => handleDelete(item, idx)}
+                        deleteIcon={
+                          <img
+                            src={
+                              caseSheetEdit &&
+                              require("images/ic_cancel_green.svg")
+                            }
+                            alt=""
+                          />
                         }
-                        alt=""
                       />
-                    }
-                  />
-                )
-            )}
-        </Typography>
-      </Typography>
-      {showAddInputText && (
-        <Typography component="div" className={classes.textFieldWrapper}>
-          <InputBase
-            fullWidth
-            className={classes.textFieldColor}
-            placeholder="Enter instruction here.."
-            value={otherInstruct}
-            onChange={e => {
-              setOtherInstruct(e.target.value);
-            }}
-          ></InputBase>
-          <Button
-            className={classes.chatSubmitBtn}
-            onClick={() => {
-              if (otherInstruct.trim() !== "") {
-                selectedValues!.splice(idx, 0, {
-                  instruction: otherInstruct,
-                  __typename: "OtherInstructions"
-                });
-                setSelectedValues(selectedValues);
-                setIdx(selectedValues!.length + 1);
-                setTimeout(() => {
-                  setOtherInstruct("");
-                }, 10);
-              } else {
-                setOtherInstruct("");
-              }
-            }}
-          >
-            <img src={require("images/ic_plus.png")} alt="" />
-          </Button>
-        </Typography>
-      )}
-      {!showAddInputText && caseSheetEdit && (
-        <AphButton
-          className={classes.btnAddDoctor}
-          variant="contained"
-          color="primary"
-          onClick={() => setShowAddInputText(true)}
-        >
-          <img src={require("images/ic_dark_plus.svg")} alt="" /> ADD
-          INSTRUCTIONS
-        </AphButton>
-      )}
-
-      <Typography component="h5" variant="h5">
-        Favourite Instructions
-      </Typography>
-      <Typography component="div" className={classes.listContainer}>
-        {adviceList !== null &&
-          adviceList.length > 0 &&
-          adviceList!.map(
-            (item, idx) =>
-              item.instruction!.trim() !== "" && (
-                <div>
-                  <Chip
-                    className={classes.othersBtn}
-                    key={idx}
-                    label={item!.instruction}
-                  />
-                  <img
-                    src={
-                      caseSheetEdit && require("images/add_doctor_white.svg")
-                    }
-                    onClick={() => {
-                      if (item!.instruction.trim() !== "") {
-                        selectedValues!.splice(idx, 0, {
-                          instruction: item!.instruction,
-                          __typename: "OtherInstructions"
-                        });
-                        setSelectedValues(selectedValues);
-                        setIdx(selectedValues!.length + 1);
-                        setTimeout(() => {
-                          setOtherInstruct("");
-                        }, 10);
-                      } else {
-                        setOtherInstruct("");
-                      }
-                    }}
-                    alt=""
-                  />
-                </div>
-              )
+                    )
+                )}
+            </Typography>
+          </Typography>
+          {showAddInputText && (
+            <Typography component="div" className={classes.textFieldWrapper}>
+              <InputBase
+                fullWidth
+                className={classes.textFieldColor}
+                placeholder="Enter instruction here.."
+                value={otherInstruct}
+                onChange={e => {
+                  setOtherInstruct(e.target.value);
+                }}
+              ></InputBase>
+              <Button
+                className={classes.chatSubmitBtn}
+                onClick={() => {
+                  if (otherInstruct.trim() !== "") {
+                    selectedValues!.splice(idx, 0, {
+                      instruction: otherInstruct,
+                      __typename: "OtherInstructions"
+                    });
+                    setSelectedValues(selectedValues);
+                    setIdx(selectedValues!.length + 1);
+                    setTimeout(() => {
+                      setOtherInstruct("");
+                    }, 10);
+                  } else {
+                    setOtherInstruct("");
+                  }
+                }}
+              >
+                <img src={require("images/ic_plus.png")} alt="" />
+              </Button>
+            </Typography>
           )}
-
-        {}
-      </Typography>
+          {!showAddInputText && caseSheetEdit && (
+            <AphButton
+              className={classes.btnAddDoctor}
+              variant="contained"
+              color="primary"
+              onClick={() => setShowAddInputText(true)}
+            >
+              <img src={require("images/ic_dark_plus.svg")} alt="" /> ADD
+              INSTRUCTIONS
+            </AphButton>
+          )}
+        </Grid>
+        <Grid item lg={6} xs={12}>
+          <Typography component="h5" variant="h5">
+            Favourite Instructions
+          </Typography>
+          <Typography component="div" className={classes.listContainer}>
+            <div className={classes.othersBtnfavContainer}>
+              {adviceList !== null &&
+                adviceList.length > 0 &&
+                adviceList!.map(
+                  (item, idx) =>
+                    item!.instruction!.trim() !== "" && (
+                      <div className={classes.othersBtnfav}>
+                        <Chip
+                          className={classes.chip}
+                          key={idx}
+                          label={item!.instruction}
+                        />
+                        <img
+                          src={
+                            caseSheetEdit &&
+                            require("images/add_doctor_white.svg")
+                          }
+                          onClick={() => {
+                            if (item!.instruction.trim() !== "") {
+                              selectedValues!.splice(idx, 0, {
+                                instruction: item!.instruction,
+                                __typename: "OtherInstructions"
+                              });
+                              setSelectedValues(selectedValues);
+                              setIdx(selectedValues!.length + 1);
+                              setTimeout(() => {
+                                setOtherInstruct("");
+                              }, 10);
+                            } else {
+                              setOtherInstruct("");
+                            }
+                          }}
+                          alt=""
+                        />
+                      </div>
+                    )
+                )}
+            </div>
+          </Typography>
+        </Grid>
+      </Grid>
     </Typography>
   );
 };
