c827910084cdb2a0b8079f8c19481acfe4f5d4ba
malleshpopcornapps <popcorn@PCL-MALLESH.localdomain>
2019-12-11 17:45:00 +0530

    Created UI for order list page

--- packages/web-patients/src/components/Orders/YourOrders.tsx
+++ packages/web-patients/src/components/Orders/YourOrders.tsx
@@ -1,102 +1,27 @@
 import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Theme, Popover } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import Scrollbars from 'react-custom-scrollbars';
-import { clientRoutes } from 'helpers/clientRoutes';
 import { OrderCard } from 'components/Orders/OrderCard';
 import { OrdersMessage } from 'components/Orders/OrdersMessage';
+import { TrackOrders } from 'components/Orders/TrackOrders';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
     root: {
-      paddingTop: 20,
-      paddingBottom: 20,
-      paddingLeft: 20,
-      PaddingRight: 3,
+      padding: 20,
+      paddingLeft: 0,
       display: 'flex',
     },
     leftSection: {
-      width: 328,
-      backgroundColor: theme.palette.common.white,
-      padding: '20px 5px',
-      borderRadius: 5,
-    },
-    rightSection: {
       width: 'calc(100% - 328px)',
       paddingRight: 5,
     },
-    medicineSection: {
-      paddingLeft: 15,
-      paddingRight: 15,
-    },
-    sectionGroup: {
-      marginBottom: 10,
-    },
-    serviceType: {
-      backgroundColor: '#f7f8f5',
+    rightSection: {
+      width: 328,
+      backgroundColor: theme.palette.common.white,
+      padding: '15px 5px',
       borderRadius: 5,
-      padding: 10,
-      paddingBottom: 8,
-      display: 'flex',
-      width: '100%',
-      color: '#02475b',
-      fontSize: 14,
-      fontWeight: 500,
-    },
-    activeMenuTab: {
-      backgroundColor: 'rgba(0,179,142,0.15)',
-    },
-    textVCenter: {
-      alignItems: 'center',
-      minHeight: 54,
-      paddingBottom: 10,
-    },
-    serviceImg: {
-      marginRight: 20,
-      '& img': {
-        maxWidth: 49,
-        verticalAlign: 'middle',
-      },
-    },
-    serviceIcon: {
-      marginRight: 10,
-      '& img': {
-        maxWidth: 24,
-        verticalAlign: 'middle',
-      },
-    },
-    rightArrow: {
-      width: 24,
-      marginLeft: 'auto',
-    },
-    linkText: {
-      letterSpacing: 'normal',
-      paddingRight: 10,
-    },
-    serviceinfoText: {
-      marginTop: 10,
-      fontSize: 12,
-      fontWeight: 500,
-      letterSpacing: 0.04,
-      opacity: 0.6,
-      lineHeight: 1.67,
-      color: '#02475b',
-      borderTop: '0.5px solid rgba(2,71,91,0.2)',
-      paddingTop: 10,
-      paddingBottom: 10,
-      display: 'inline-block',
-      width: '100%',
-    },
-    marginNone: {
-      marginBottom: 'none',
-    },
-    bottomImgGroup: {
-      marginTop: 40,
-      '& img': {
-        maxWidth: '100%',
-        verticalAlign: 'middle',
-      },
     },
     sectionHeader: {
       color: '#02475b',
@@ -104,7 +29,6 @@ const useStyles = makeStyles((theme: Theme) => {
       fontWeight: 500,
       borderBottom: '0.5px solid rgba(2,71,91,0.3)',
       paddingBottom: 10,
-      paddingTop: 10,
       marginBottom: 20,
       display: 'flex',
       alignItems: 'center',
@@ -113,9 +37,6 @@ const useStyles = makeStyles((theme: Theme) => {
     },
     count: {
       marginLeft: 'auto',
-    },
-    orderListing: {
-      paddingTop: 10,
     },
     bottomPopover: {
       overflow: 'initial',
@@ -155,76 +76,18 @@ export const YourOrders: React.FC = (props) => {
   const classes = useStyles();
   const currentPath = window.location.pathname;
   const mascotRef = useRef(null);
-  const [isPopoverOpen] = React.useState<boolean>(true);
+  const [isPopoverOpen] = React.useState<boolean>(false);
 
   return (
     <div className={classes.root}>
       <div className={classes.leftSection}>
-        <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 237px)'}>
-          <div className={classes.medicineSection}>
-            <div className={classes.sectionGroup}>
-              <Link className={classes.serviceType} to={clientRoutes.testsAndMedicine()}>
-                <span className={classes.serviceImg}>
-                  <img src={require('images/ic_medicines.png')} alt="" />
-                </span>
-                <span className={classes.linkText}>Need to find a medicine/ alternative?</span>
-                <span className={classes.rightArrow}>
-                  <img src={require('images/ic_arrow_right.svg')} alt="" />
-                </span>
-              </Link>
-            </div>
-            <div className={classes.sectionGroup}>
-              <Link className={classes.serviceType} to={clientRoutes.prescriptionsLanding()}>
-                <span className={classes.serviceImg}>
-                  <img src={require('images/ic_medicines.png')} alt="" />
-                </span>
-                <span className={classes.linkText}>Do you have a prescription ready?</span>
-                <span className={classes.rightArrow}>
-                  <img src={require('images/ic_arrow_right.svg')} alt="" />
-                </span>
-              </Link>
-            </div>
-            <div className={classes.sectionGroup}>
-              <div className={classes.serviceinfoText}>
-                Get all your medicines, certified using our 5-point system, within 2 hours.
-              </div>
-            </div>
-            <div className={classes.sectionGroup}>
-              <Link className={`${classes.serviceType} ${classes.textVCenter}`} to="#">
-                <span className={classes.serviceIcon}>
-                  <img src={require('images/ic_schedule.svg')} alt="" />
-                </span>
-                <span className={classes.linkText}>Your Med Subscripitons</span>
-                <span className={classes.rightArrow}>
-                  <img src={require('images/ic_arrow_right.svg')} alt="" />
-                </span>
-              </Link>
-            </div>
-            <div className={`${classes.sectionGroup} ${classes.marginNone}`}>
-              <Link
-                className={`${classes.serviceType} ${classes.textVCenter} ${
-                  currentPath === clientRoutes.yourOrders() ? classes.activeMenuTab : ''
-                }`}
-                to={clientRoutes.yourOrders()}
-              >
-                <span className={classes.serviceIcon}>
-                  <img src={require('images/ic_tablets.svg')} alt="" />
-                </span>
-                <span className={classes.linkText}>Your Orders</span>
-                <span className={classes.rightArrow}>
-                  <img src={require('images/ic_arrow_right.svg')} alt="" />
-                </span>
-              </Link>
-            </div>
-            <div className={classes.bottomImgGroup}>
-              <img src={require('images/ic_adbanner_web.png')} alt="" />
-            </div>
-          </div>
-        </Scrollbars>
-      </div>
-      <div className={classes.rightSection}>
         <div className={classes.sectionHeader}>Your Orders</div>
         <OrderCard />
+      </div>
+      <div className={classes.rightSection}>
+        <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 237px)'}>
+          <TrackOrders />
+        </Scrollbars>
       </div>
       <Popover
         open={isPopoverOpen}
