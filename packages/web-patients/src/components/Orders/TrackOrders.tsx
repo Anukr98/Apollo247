18dd697f43a91fd9659a86e3afdd59cdf6f8b969
malleshpopcornapps <popcorn@PCL-MALLESH.localdomain>
2019-12-12 13:14:32 +0530

    Created UI for order summary page

--- packages/web-patients/src/components/Orders/TrackOrders.tsx
+++ packages/web-patients/src/components/Orders/TrackOrders.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Theme, Popover } from '@material-ui/core';
+import { Theme, Popover, Typography, Tabs, Tab } from '@material-ui/core';
 import Scrollbars from 'react-custom-scrollbars';
 import { AphButton, AphDialog, AphDialogTitle, AphDialogClose } from '@aph/web-ui-components';
 import { OrderStatusCard } from 'components/Orders/OrderStatusCard';
 import { CancelOrder } from 'components/Orders/CancelOrder';
 import { ReturnOrder } from 'components/Orders/ReturnOrder';
+import { OrdersSummary } from 'components/Orders/OrderSummary';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -28,30 +29,6 @@ const useStyles = makeStyles((theme: Theme) => {
     headerActions: {
       marginLeft: 'auto',
       marginBottom: -6,
-    },
-    tabGroup: {
-      display: 'flex',
-      alignItems: 'center',
-      borderBottom: '0.5px solid rgba(2,71,91,0.2)',
-    },
-    tabBtn: {
-      width: '50%',
-      boxShadow: 'none',
-      backgroundColor: 'transparent',
-      padding: '8px 0',
-      fontSize: 14,
-      fontWeight: 500,
-      color: '#01475b',
-      textTransform: 'none',
-      borderBottom: '4px solid transparent',
-      borderRadius: 0,
-      minWidth: 'auto',
-      opacity: 0.6,
-    },
-    btnActive: {
-      borderBottom: '4px solid #00b38e',
-      color: '#02475b',
-      opacity: 1,
     },
     moreBtn: {
       border: 0,
@@ -100,11 +77,41 @@ const useStyles = makeStyles((theme: Theme) => {
         },
       },
     },
+    tabsRoot: {
+      borderBottom: '0.5px solid rgba(2,71,91,0.3)',
+      minHeight: 44,
+    },
+    tabRoot: {
+      fontSize: 14,
+      fontWeight: 500,
+      textAlign: 'center',
+      padding: 10,
+      color: '#01475b',
+      opacity: 0.6,
+      minWidth: '50%',
+      textTransform: 'none',
+      minHeight: 44,
+    },
+    tabSelected: {
+      opacity: 1,
+    },
+    tabsIndicator: {
+      backgroundColor: '#00b38e',
+      height: 4,
+    },
+    rootTabContainer: {
+      padding: 0,
+    },
   };
 });
 
+const TabContainer: React.FC = (props) => {
+  return <Typography component="div">{props.children}</Typography>;
+};
+
 export const TrackOrders: React.FC = (props) => {
   const classes = useStyles();
+  const [tabValue, setTabValue] = useState<number>(0);
   const currentPath = window.location.pathname;
   const [moreActionsDialog, setMoreActionsDialog] = React.useState<null | HTMLElement>(null);
   const [isCancelOrderDialogOpen, setIsCancelOrderDialogOpen] = React.useState<boolean>(false);
@@ -148,15 +155,40 @@ export const TrackOrders: React.FC = (props) => {
         </div>
       </div>
       <div className={classes.orderTrackCrads}>
-        <div className={classes.tabGroup}>
-          <AphButton className={`${classes.tabBtn} ${classes.btnActive}`}>Track Order</AphButton>
-          <AphButton className={`${classes.tabBtn}`}>Order Summary</AphButton>
-        </div>
-        <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 276px)'}>
-          <div className={classes.customScroll}>
-            <OrderStatusCard />
-          </div>
-        </Scrollbars>
+        <Tabs
+          value={tabValue}
+          classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
+          onChange={(e, newValue) => {
+            setTabValue(newValue);
+          }}
+        >
+          <Tab
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            label="Track Order"
+          />
+          <Tab
+            classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+            label="Order Summary"
+          />
+        </Tabs>
+        {tabValue === 0 && (
+          <TabContainer>
+            <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 276px)'}>
+              <div className={classes.customScroll}>
+                <OrderStatusCard />
+              </div>
+            </Scrollbars>
+          </TabContainer>
+        )}
+        {tabValue === 1 && (
+          <TabContainer>
+            <Scrollbars autoHide={true} autoHeight autoHeightMax={'calc(100vh - 276px)'}>
+              <div className={classes.customScroll}>
+                <OrdersSummary />
+              </div>
+            </Scrollbars>
+          </TabContainer>
+        )}
       </div>
       <AphDialog open={isCancelOrderDialogOpen} maxWidth="sm">
         <AphDialogClose onClick={() => setIsCancelOrderDialogOpen(false)} />
