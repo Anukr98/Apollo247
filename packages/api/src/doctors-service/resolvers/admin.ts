d995af6b3c0b1e163f217b473286c351eef854b8
sushma <sushma.voleti@popcornapps.com>
2019-12-16 21:52:24 +0530

    Create Doctor - With consult hours

--- packages/api/src/doctors-service/resolvers/admin.ts
+++ packages/api/src/doctors-service/resolvers/admin.ts
@@ -8,6 +8,9 @@ import {
   Gender,
   DoctorAndHospital,
   DoctorSecretary,
+  ConsultHoursData,
+  ConsultHours,
+  ConsultType,
 } from 'doctors-service/entities';
 import {
   AdminDoctor,
@@ -16,19 +19,32 @@ import {
   AdminDoctorAndHospital,
   AdminDoctorSecretaryRepository,
   AdminSecretaryRepository,
+  AdminConsultHoursRepository,
 } from 'doctors-service/repositories/adminRepository';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
 import { isMobileNumberValid, isNameValid, trimObjects } from '@aph/universal/dist/aphValidators';
 import { DoctorSpecialtyRepository } from 'doctors-service/repositories/doctorSpecialtyRepository';
 import { ApiConstants } from 'ApiConstants';
+import format from 'date-fns/format';
+import { areIntervalsOverlapping, isEqual, isAfter } from 'date-fns';
+import { setWeekWithOptions } from 'date-fns/esm/fp';
+import _ from 'lodash';
 
 export const AdminTypeDefs = gql`
+  input ConsultHoursData {
+    weekDay: WeekDay
+    startTime: Time
+    endTime: Time
+    consultMode: ConsultMode
+  }
+
   input DoctorInput {
     awards: String
     city: String
     consultBuffer: Int!
     consultDuration: Int!
+    consultHoursData: [ConsultHoursData]
     country: String
     dateOfBirth: Date
     doctorType: DoctorType!
@@ -90,6 +106,7 @@ type DoctorInput = {
   city: string;
   consultBuffer: number;
   consultDuration: number;
+  consultHoursData: ConsultHoursData[];
   country: string;
   dateOfBirth: Date;
   doctorType: DoctorType;
@@ -141,17 +158,39 @@ const createDoctor: Resolver<null, DoctorInputArgs, DoctorsServiceContext, Docto
     throw new AphError(AphErrorMessages.INVALID_DISPLAY_NAME);
   if (!isMobileNumberValid(doctorInputdata.mobileNumber))
     throw new AphError(AphErrorMessages.INVALID_MOBILE_NUMBER);
+
+  if (!doctorInputdata.facilityId) throw new AphError(AphErrorMessages.INVALID_FACILITY_ID);
+  if (!doctorInputdata.consultHoursData) throw new AphError(AphErrorMessages.INVALID_CONSULT_HOURS);
+
+  const uniqueCalendarItems = _.uniqWith(
+    doctorInputdata.consultHoursData,
+    (item1, item2) =>
+      isEqual(new Date(item1.startTime), new Date(item2.startTime)) &&
+      isEqual(new Date(item1.endTime), new Date(item2.endTime)) &&
+      item1.consultMode === item2.consultMode &&
+      item1.weekDay === item2.weekDay
+  );
+
+  if (uniqueCalendarItems.length != doctorInputdata.consultHoursData.length)
+    throw new AphError(AphErrorMessages.INVALID_DATES);
+
+  const exceptions = await checkOverlapsAndException(0, 0, doctorInputdata.consultHoursData);
+
+  if (exceptions.dateException > 0 || exceptions.overlapCount > 0) {
+    throw new AphError(AphErrorMessages.INVALID_DATES);
+  }
+
   //input validation ends
 
-  const doctorDetails: Partial<Doctor> = {
-    ...doctorInputdata,
-  };
+  const doctorDetails: Partial<Doctor> = { ...doctorInputdata };
 
   //specilaty check
-  const specialtyRepo = doctorsDb.getCustomRepository(DoctorSpecialtyRepository);
-  const specialty = await specialtyRepo.findById(doctorInputdata.specialtyId.toString());
-  if (specialty == null) throw new AphError(AphErrorMessages.GET_SPECIALTIES_ERROR);
-  if (specialty) doctorDetails.specialty = specialty;
+  if (doctorInputdata.specialtyId && doctorInputdata.specialtyId.length > 0) {
+    const specialtyRepo = doctorsDb.getCustomRepository(DoctorSpecialtyRepository);
+    const specialty = await specialtyRepo.findById(doctorInputdata.specialtyId.toString());
+    if (specialty == null) throw new AphError(AphErrorMessages.GET_SPECIALTIES_ERROR);
+    if (specialty) doctorDetails.specialty = specialty;
+  }
 
   //facility Id check
   const facilityRepo = doctorsDb.getCustomRepository(AdminFacility);
@@ -189,7 +228,7 @@ const createDoctor: Resolver<null, DoctorInputArgs, DoctorsServiceContext, Docto
     facility: facility,
   };
   const doctorAndHospitalRepo = doctorsDb.getCustomRepository(AdminDoctorAndHospital);
-  await doctorAndHospitalRepo.createDoctorAndHospital(doctorAndHospital);
+  const doctorHospital = await doctorAndHospitalRepo.createDoctorAndHospital(doctorAndHospital);
 
   if (doctorInputdata.secretaryId) {
     //check if secretary id is valid
@@ -215,7 +254,82 @@ const createDoctor: Resolver<null, DoctorInputArgs, DoctorsServiceContext, Docto
   }
 
   //insert in to consult hours
+  const doctorConsultHours: Partial<ConsultHours>[] = [];
+  doctorInputdata.consultHoursData.forEach((element) => {
+    doctorConsultHours.push({
+      doctor: newDoctorDetails,
+      doctorHospital: doctorHospital,
+      facility: facility,
+      consultMode: element.consultMode,
+      consultType: ConsultType.FIXED,
+      startTime: format(new Date(element.startTime), 'HH:mm'),
+      endTime: format(new Date(element.endTime), 'HH:mm'),
+      weekDay: element.weekDay,
+    });
+  });
+  const consultHoursRepo = doctorsDb.getCustomRepository(AdminConsultHoursRepository);
+  await consultHoursRepo.save(doctorConsultHours);
+
   return newDoctorDetails;
+};
+
+export const checkOverlapsAndException = async (
+  overlapCount: number,
+  dateException: number,
+  consultHoursData: Partial<ConsultHours>[]
+) => {
+  let currentIndex = 0;
+  return new Promise<{ overlapCount: number; dateException: number }>(async (resolve, reject) => {
+    consultHoursData.forEach(async (item, index) => {
+      if (item.startTime && item.endTime) {
+        const start = item.startTime;
+        const end = item.endTime;
+        const itemsList = consultHoursData.filter((currentObject, currentIndex) => {
+          return currentIndex != index;
+        });
+
+        if (isEqual(new Date(start), new Date(end))) {
+          dateException++;
+          resolve({ overlapCount, dateException });
+        }
+        if (isAfter(new Date(start), new Date(end))) {
+          dateException++;
+          resolve({ overlapCount, dateException });
+        }
+
+        const calendarItemsOverlap = doesCalendarItemOverlap(item, itemsList);
+        if (calendarItemsOverlap) {
+          overlapCount++;
+          resolve({ overlapCount, dateException });
+        }
+
+        const consultMode = item.consultMode;
+        currentIndex++;
+        if (currentIndex == consultHoursData.length) resolve({ overlapCount, dateException });
+      }
+    });
+  });
+};
+
+export const doesCalendarItemOverlap = (
+  item: Partial<ConsultHours>,
+  itemsToCheckAgainst: Partial<ConsultHours>[]
+) => {
+  return itemsToCheckAgainst.some(({ startTime, endTime }) => {
+    const convertedStartTime = new Date(startTime!);
+    const convertedEndTime = new Date(endTime!);
+    const convertedStartTime2 = new Date(item.startTime!);
+    const convertedEndTime2 = new Date(item.endTime!);
+
+    try {
+      return areIntervalsOverlapping(
+        { start: convertedStartTime, end: convertedEndTime },
+        { start: convertedStartTime2, end: convertedEndTime2 }
+      );
+    } catch (Exception) {
+      return true;
+    }
+  });
 };
 
 export const AdminResolvers = {
