2882a002e9ab4b1b9d4913b31f45a86cc816ec0f
hithaprani <hithaprani.vakkanti@popcornapps.com>
2019-12-19 11:05:52 +0530

    Fixed search accepting spaces

--- packages/mobile-patients/src/components/Medicines/SearchTestScene.tsx
+++ packages/mobile-patients/src/components/Medicines/SearchTestScene.tsx
@@ -27,7 +27,7 @@ import {
   searchDiagnosticsVariables,
   searchDiagnostics_searchDiagnostics_diagnostics,
 } from '@aph/mobile-patients/src/graphql/types/searchDiagnostics';
-import { aphConsole, g } from '@aph/mobile-patients/src/helpers/helperFunctions';
+import { aphConsole, g, isValidSearch } from '@aph/mobile-patients/src/helpers/helperFunctions';
 import { useAllCurrentPatients, useAuth } from '@aph/mobile-patients/src/hooks/authHooks';
 import { theme } from '@aph/mobile-patients/src/theme/theme';
 import { AxiosResponse } from 'axios';
@@ -238,33 +238,35 @@ export const SearchTestScene: React.FC<SearchTestSceneProps> = (props) => {
   };
 
   const onSearchMedicine = (_searchText: string) => {
-    setSearchText(_searchText);
-    if (!(_searchText && _searchText.length > 2)) {
-      setMedicineList([]);
-      return;
-    }
-    setShowMatchingMedicines(true);
-    setIsLoading(true);
+    if (isValidSearch(_searchText)) {
+      setSearchText(_searchText);
+      if (!(_searchText && _searchText.length > 2)) {
+        setMedicineList([]);
+        return;
+      }
+      setShowMatchingMedicines(true);
+      setIsLoading(true);
 
-    client
-      .query<searchDiagnostics, searchDiagnosticsVariables>({
-        query: SEARCH_DIAGNOSTICS,
-        variables: {
-          searchText: _searchText,
-          city: locationForDiagnostics && locationForDiagnostics.city,
-          patientId: (currentPatient && currentPatient.id) || '',
-        },
-        fetchPolicy: 'no-cache',
-      })
-      .then(({ data }) => {
-        const products = g(data, 'searchDiagnostics', 'diagnostics') || [];
-        setMedicineList(products as searchDiagnostics_searchDiagnostics_diagnostics[]);
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setIsLoading(false);
-        showGenericALert(e);
-      });
+      client
+        .query<searchDiagnostics, searchDiagnosticsVariables>({
+          query: SEARCH_DIAGNOSTICS,
+          variables: {
+            searchText: _searchText,
+            city: locationForDiagnostics && locationForDiagnostics.city,
+            patientId: (currentPatient && currentPatient.id) || '',
+          },
+          fetchPolicy: 'no-cache',
+        })
+        .then(({ data }) => {
+          const products = g(data, 'searchDiagnostics', 'diagnostics') || [];
+          setMedicineList(products as searchDiagnostics_searchDiagnostics_diagnostics[]);
+          setIsLoading(false);
+        })
+        .catch((e) => {
+          setIsLoading(false);
+          showGenericALert(e);
+        });
+    }
   };
 
   const savePastSeacrh = (sku: string, name: string) =>
