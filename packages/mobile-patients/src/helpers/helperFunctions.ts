664e27fe632532158c3581068535ef07c8a652c2
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2019-12-19 19:44:38 +0530

    Medicine payment api updated

--- packages/mobile-patients/src/helpers/helperFunctions.ts
+++ packages/mobile-patients/src/helpers/helperFunctions.ts
@@ -109,6 +109,9 @@ export const getOrderStatusText = (status: MEDICINE_ORDER_STATUS): string => {
     case MEDICINE_ORDER_STATUS.RETURN_INITIATED:
       statusString = 'Return Requested';
       break;
+    case MEDICINE_ORDER_STATUS.PAYMENT_SUCCESS:
+      statusString = 'Payment Success';
+      break;
   }
   return statusString;
 };
@@ -199,22 +202,10 @@ export const divideSlots = (availableSlots: string[], date: Date) => {
 
 export const handleGraphQlError = (
   error: any,
-  message: string = 'Uh oh! An Unknown Error Occurred.'
+  message: string = 'Oops! seems like we are having an issue. Please try again.'
 ) => {
   console.log({ error });
-
-  let formattedError = message;
-  if (typeof error == 'string') {
-    formattedError = error;
-  } else if (typeof error == 'object') {
-    try {
-      const graphqlErr = (error as GraphQLError).message;
-      formattedError = graphqlErr;
-    } catch (e) {
-      console.log({ e });
-    }
-  }
-  Alert.alert('Alert', formattedError);
+  Alert.alert('Uh oh.. :(', message);
 };
 
 export const timeTo12HrFormat = (time: string) => {
