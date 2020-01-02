fb145bf8b700b666a7a332c1129136089a6bf4d6
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-01-02 20:43:42 +0530

    removed unwanted code

--- packages/api/src/consults-service/entities/index.ts
+++ packages/api/src/consults-service/entities/index.ts
@@ -12,7 +12,6 @@ import {
 } from 'typeorm';
 import { IsDate } from 'class-validator';
 import { DoctorType } from 'doctors-service/entities';
-import { type } from 'os';
 
 export enum patientLogSort {
   MOST_RECENT = 'MOST_RECENT',
