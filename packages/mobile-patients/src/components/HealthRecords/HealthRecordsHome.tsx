9d6c3a37848ccbc0c503cb69811fefa60655c0e1
sankeeth <sankeerth.obulam@popcornapps.com>
2020-01-09 11:47:41 +0530

    Scroll related Shadows updated

--- packages/mobile-patients/src/components/HealthRecords/HealthRecordsHome.tsx
+++ packages/mobile-patients/src/components/HealthRecords/HealthRecordsHome.tsx
@@ -340,12 +340,13 @@ export const HealthRecordsHome: React.FC<HealthRecordsHomeProps> = (props) => {
 
   const renderTopView = () => {
     const containerStyle: ViewStyle =
-      scrollOffset > 134
+      scrollOffset > 1
         ? {
             shadowColor: '#808080',
             shadowOffset: { width: 0, height: 0 },
+            zIndex: 1,
             shadowOpacity: 0.4,
-            shadowRadius: 10,
+            shadowRadius: 5,
             elevation: 5,
           }
         : {};
