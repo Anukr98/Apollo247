a8221a68a6045573bb67a023a74c1cf68b5bf371
sankeeth <sankeerth.obulam@popcornapps.com>
2020-01-10 11:10:05 +0530

    Login and OTP API integrated

--- packages/mobile-patients/src/components/AuthProvider.tsx
+++ packages/mobile-patients/src/components/AuthProvider.tsx
@@ -1,7 +1,6 @@
 import { GET_CURRENT_PATIENTS } from '@aph/mobile-patients/src/graphql/profiles';
 import { GetCurrentPatients } from '@aph/mobile-patients/src/graphql/types/GetCurrentPatients';
 import { apiRoutes } from '@aph/mobile-patients/src/helpers/apiRoutes';
-// import { apiRoutes } from '@aph/universal/dist/aphRoutes';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloClient, ApolloQueryResult } from 'apollo-client';
 import { setContext } from 'apollo-link-context';
@@ -36,10 +35,6 @@ export interface AuthContextProps {
   sendOtpError: boolean;
   isSendingOtp: boolean;
 
-  verifyOtp: ((otp: string) => Promise<unknown>) | null;
-  verifyOtpError: boolean;
-  isVerifyingOtp: boolean;
-
   signInError: boolean;
   isSigningIn: boolean;
   signOut: (() => void) | null;
@@ -56,9 +51,6 @@ export const AuthContext = React.createContext<AuthContextProps>({
   sendOtpError: false,
   isSendingOtp: false,
 
-  verifyOtp: null,
-  verifyOtpError: false,
-  isVerifyingOtp: false,
   hasAuthToken: false,
   signInError: false,
   isSigningIn: true,
@@ -91,15 +83,22 @@ const buildApolloClient = (authToken: string, handleUnauthenticated: () => void)
       ...headers,
       Authorization: authToken ? authToken : 'Bearer 3d1833da7020e0602165529446587434',
     },
-    // headers: { ...headers, Authorization: 'Bearer 3d1833da7020e0602165529446587434' },
   }));
-  const httpLink = createHttpLink({ uri: apiRoutes.graphql() });
+  const httpLink = createHttpLink({
+    uri: apiRoutes.graphql(),
+  });
+  console.log(
+    '-------authToken-------',
+    authToken ? authToken : 'Bearer 3d1833da7020e0602165529446587434'
+  );
+
   const link = errorLink.concat(authLink).concat(httpLink);
   const cache = apolloClient ? apolloClient.cache : new InMemoryCache();
-  return new ApolloClient({ link, cache });
+  return new ApolloClient({
+    link,
+    cache,
+  });
 };
-
-let otpVerifier: RNFirebase.ConfirmationResult;
 
 export const AuthProvider: React.FC = (props) => {
   const [authToken, setAuthToken] = useState<string>('');
@@ -116,9 +115,6 @@ export const AuthProvider: React.FC = (props) => {
   const [isSendingOtp, setIsSendingOtp] = useState<AuthContextProps['isSendingOtp']>(false);
   const [sendOtpError, setSendOtpError] = useState<AuthContextProps['sendOtpError']>(false);
 
-  const [isVerifyingOtp, setIsVerifyingOtp] = useState<AuthContextProps['isVerifyingOtp']>(false);
-  const [verifyOtpError, setVerifyOtpError] = useState<AuthContextProps['verifyOtpError']>(false);
-
   const [isSigningIn, setIsSigningIn] = useState<AuthContextProps['isSigningIn']>(false);
   const [signInError, setSignInError] = useState<AuthContextProps['signInError']>(false);
 
@@ -126,34 +122,13 @@ export const AuthProvider: React.FC = (props) => {
 
   const [allPatients, setAllPatients] = useState<AuthContextProps['allPatients']>(null);
 
-  // const sendOtp = (phoneNumber: string, forceResend: boolean = false) => {
-  //   return new Promise(async (resolve, reject) => {
-  //     setIsSendingOtp(true);
-
-  //     const [phoneAuthResult, phoneAuthError] = await wait(
-  //       auth.signInWithPhoneNumber('+91' + phoneNumber, forceResend)
-  //     );
-  //     setIsSendingOtp(false);
-  //     if (phoneAuthError) {
-  //       setSendOtpError(true);
-  //       reject(phoneAuthError);
-  //       return;
-  //     }
-  //     otpVerifier = phoneAuthResult;
-  //     setSendOtpError(false);
-  //     resolve(phoneAuthResult);
-  //   });
-  // };
-
-  const sendOtp = (phoneNumber: string, forceResend: boolean = false) => {
+  const sendOtp = (customToken: string) => {
     return new Promise(async (resolve, reject) => {
       setIsSendingOtp(true);
 
       const [phoneAuthResult, phoneAuthError] = await wait(
         // auth.signInWithPhoneNumber('+91' + phoneNumber, forceResend)
-        auth.signInWithCustomToken(
-          'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU3ODU3NDg3NywiZXhwIjoxNTc4NTc4NDc3LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1pdGJiaEBhcG9sbG8tcGF0aWVudC1pbnRlcmZhY2UuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzdWIiOiJmaXJlYmFzZS1hZG1pbnNkay1pdGJiaEBhcG9sbG8tcGF0aWVudC1pbnRlcmZhY2UuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJ1aWQiOiIrOTE5MDUyOTU5NTI3IiwiY2xhaW1zIjp7InBob25lTnVtYmVyIjoiKzkxOTA1Mjk1OTUyNyJ9fQ.lZHC21UDLN8VBcmQgUQQrMzllUjfsRw4ttzkciCsMnQK6kSp3qO3rYfl8S0zG4CQptAEpZ3SJ4kfr3cw5YrBo86odYbAzp1xHUEUTCFff97ppS2l-PccJEZpxRBaFi8riu1Tko_uAgq6949xEW6fzPQ2eFf__qFtFogyHS3KM87sckGpI5_G2kwYjHRO5Ecwp58qAoiB9AfEKMfUVYKoEUAaf8aVgR9Al2rCuwxTSJ_MCSrzGi6DQ5vT2MS85NzyVa6w7mQh1DnEx4WobRlUEjy4i79NFiTPz5TqWe2qurCDZN3-cGGzrXM5viDAnR2tXhv10S9JF7Kr77kmdVSKLw'
-        )
+        auth.signInWithCustomToken(customToken)
       );
       setIsSendingOtp(false);
       if (phoneAuthError) {
@@ -161,29 +136,8 @@ export const AuthProvider: React.FC = (props) => {
         reject(phoneAuthError);
         return;
       }
-      // otpVerifier = phoneAuthResult;
       setSendOtpError(false);
       resolve(phoneAuthResult);
-    });
-  };
-
-  const verifyOtp = async (otp: string) => {
-    return new Promise(async (resolve, reject) => {
-      if (!otpVerifier) {
-        setSendOtpError(true);
-        reject();
-        return;
-      }
-      setIsVerifyingOtp(true);
-      const [otpAuthResult, otpError] = await wait(otpVerifier.confirm(otp));
-      setVerifyOtpError(Boolean(otpError || !otpAuthResult));
-      if (otpAuthResult) {
-        // console.log('otpAuthResult', otpAuthResult);
-        resolve(otpAuthResult);
-      } else {
-        reject(otpError);
-      }
-      setIsVerifyingOtp(false);
     });
   };
 
@@ -286,10 +240,6 @@ export const AuthProvider: React.FC = (props) => {
             sendOtp,
             sendOtpError,
             isSendingOtp,
-
-            verifyOtp,
-            verifyOtpError,
-            isVerifyingOtp,
 
             signInError,
             isSigningIn,
