bbb0750e187b7fa35e8366f3b322d9ea8ac4e996
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2020-01-13 12:28:16 +0530

    In-app notification after registration removed

--- packages/mobile-patients/src/components/MultiSignup.tsx
+++ packages/mobile-patients/src/components/MultiSignup.tsx
@@ -36,8 +36,7 @@ import moment from 'moment';
 import { StackActions } from 'react-navigation';
 import { NavigationActions } from 'react-navigation';
 import { CommonLogEvent } from '@aph/mobile-patients/src/FunctionHelpers/DeviceHelper';
-import { handleGraphQlError, g } from '@aph/mobile-patients/src/helpers/helperFunctions';
-import { useUIElements } from '@aph/mobile-patients/src/components/UIElementsProvider';
+import { handleGraphQlError } from '@aph/mobile-patients/src/helpers/helperFunctions';
 import { TextInputComponent } from './ui/TextInputComponent';
 
 const { width, height } = Dimensions.get('window');
@@ -127,7 +126,6 @@ export const MultiSignup: React.FC<MultiSignupProps> = (props) => {
   const [verifyingPhoneNumber, setVerifyingPhoneNumber] = useState<boolean>(false);
   const { currentPatient, allCurrentPatients } = useAllCurrentPatients();
   const [backPressCount, setbackPressCount] = useState<number>(0);
-  const { showAphAlert, hideAphAlert } = useUIElements();
   const [referredBy, setReferredBy] = useState<string>();
   const [referral, setReferral] = useState<string>('');
 
@@ -416,26 +414,17 @@ export const MultiSignup: React.FC<MultiSignupProps> = (props) => {
                   AsyncStorage.setItem('gotIt', 'false'),
                   CommonLogEvent(AppRoutes.MultiSignup, 'Navigating to Consult Room'),
                   setTimeout(() => {
-                    showAphAlert!({
-                      title: `Hi ${g(currentPatient, 'firstName') || ''},`,
-                      description:
-                        'Welcome to Apollo24X7. We’re glad you’re here!\nConsult online with our top Apollo doctors now!',
-                      unDismissable: true,
-                      onPressOk: () => {
-                        hideAphAlert!();
-                        props.navigation.dispatch(
-                          StackActions.reset({
-                            index: 0,
-                            key: null,
-                            actions: [
-                              NavigationActions.navigate({
-                                routeName: AppRoutes.ConsultRoom,
-                              }),
-                            ],
-                          })
-                        );
-                      },
-                    });
+                    props.navigation.dispatch(
+                      StackActions.reset({
+                        index: 0,
+                        key: null,
+                        actions: [
+                          NavigationActions.navigate({
+                            routeName: AppRoutes.ConsultRoom,
+                          }),
+                        ],
+                      })
+                    );
                   }, 500))
                 : null}
               {error
