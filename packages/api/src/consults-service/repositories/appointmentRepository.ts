3f1c89650e5bcfebac503743466791558cd1e2d0
sushma <sushma.voleti@popcornapps.com>
2020-01-14 06:44:29 +0530

    code clean up

--- packages/api/src/consults-service/repositories/appointmentRepository.ts
+++ packages/api/src/consults-service/repositories/appointmentRepository.ts
@@ -1374,13 +1374,14 @@ export class AppointmentRepository extends Repository<Appointment> {
   getPatientFutureAppointmentsCount(patientId: string, maxConsultationMinutes: number) {
     return this.createQueryBuilder('appointment')
       .where('appointment.appointmentDateTime > :apptDate', {
-        apptDate: new Date(),
+        apptDate: subMinutes(new Date(), maxConsultationMinutes),
       })
       .andWhere('appointment.patientId = :patientId', { patientId: patientId })
-      .andWhere('appointment.status not in(:status1,:status2,:status3)', {
+      .andWhere('appointment.status not in(:status1,:status2,:status3,:status4)', {
         status1: STATUS.CANCELLED,
         status2: STATUS.PAYMENT_PENDING,
         status3: STATUS.UNAVAILABLE_MEDMANTRA,
+        status4: STATUS.COMPLETED,
       })
       .orderBy('appointment.appointmentDateTime', 'ASC')
       .getCount();
