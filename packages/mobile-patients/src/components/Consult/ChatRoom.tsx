972d2f646e57364a70a5eed778542671efe4b3b8
sankeeth <sankeerth.obulam@popcornapps.com>
2020-01-21 18:52:28 +0530

    Double time messages issues fixed

--- packages/mobile-patients/src/components/Consult/ChatRoom.tsx
+++ packages/mobile-patients/src/components/Consult/ChatRoom.tsx
@@ -678,7 +678,6 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
     if (userAnswers) {
       addToConsultQueueWithAutomatedQuestions(client, userAnswers)
         .then(({ data }: any) => {
-          startJoinTimer(0);
           console.log(data, 'data res, adding');
           const queueData = {
             queueId: data.data.addToConsultQueue && data.data.addToConsultQueue.doctorId,
