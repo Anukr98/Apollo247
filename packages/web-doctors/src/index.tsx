c53b57146cbc35bfc4ab49b37f358d13e9a7eede
tantan-pop <53180348+tantan-pop@users.noreply.github.com>
2020-01-22 14:53:26 +0530

    fixed format
    fixed format

--- packages/web-doctors/src/index.tsx
+++ packages/web-doctors/src/index.tsx
@@ -4,14 +4,19 @@ import 'unfetch/polyfill';
 import { AppContainer } from 'components/AppContainer';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bugsnag from '@bugsnag/js'
-import bugsnagReact from '@bugsnag/plugin-react'
-const bugsnagClient= bugsnag({
-  apiKey:`${process.env.BUGSNAG_API_KEY}`,
-  notifyReleaseStages: [ 'local','development','production', 'staging' ],
-  releaseStage: process.env.NODE_ENV 
-})
-bugsnagClient.use(bugsnagReact, React)
-const ErrorBoundary = bugsnagClient.getPlugin('react')
-bugsnagClient.notify(new Error('Test error'))
-ReactDOM.render(<ErrorBoundary><AppContainer /></ErrorBoundary>, document.getElementById('root'));
+import bugsnag from '@bugsnag/js';
+import bugsnagReact from '@bugsnag/plugin-react';
+const bugsnagClient = bugsnag({
+  apiKey: `${process.env.BUGSNAG_API_KEY}`,
+  notifyReleaseStages: ['local', 'development', 'production', 'staging'],
+  releaseStage: process.env.NODE_ENV,
+});
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin('react');
+bugsnagClient.notify(new Error('Test error'));
+ReactDOM.render(
+  <ErrorBoundary>
+    <AppContainer />
+  </ErrorBoundary>,
+  document.getElementById('root')
+);
