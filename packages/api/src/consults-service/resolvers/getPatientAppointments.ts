c6578c17d880cf0adaa3efa46d8aba63161c100c
sushma <sushma.voleti@popcornapps.com>
2020-01-31 18:12:23 +0530

    code clenup

--- packages/api/src/consults-service/resolvers/getPatientAppointments.ts
+++ packages/api/src/consults-service/resolvers/getPatientAppointments.ts
@@ -6,7 +6,6 @@ import { AppointmentRepository } from 'consults-service/repositories/appointment
 import { PatientRepository } from 'profiles-service/repositories/patientRepository';
 import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
-import { DoctorConsultHoursRepository } from 'doctors-service/repositories/doctorConsultHoursRepository';
 
 export const getPatinetAppointmentsTypeDefs = gql`
   type PatinetAppointments {
