6f60f355758b320f9cda93f45a8d2a1da6446c5b
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-02-03 12:26:14 +0530

    updated query again

--- packages/api/src/doctors-service/repositories/blockedCalendarItemRepository.ts
+++ packages/api/src/doctors-service/repositories/blockedCalendarItemRepository.ts
@@ -13,23 +13,30 @@ export class BlockedCalendarItemRepository extends Repository<BlockedCalendarIte
     const inputStartDate = format(addDays(slot, -1), 'yyyy-MM-dd');
     const currentStartDate = new Date(inputStartDate + 'T18:30');
     const currentEndDate = new Date(format(slot, 'yyyy-MM-dd').toString() + 'T18:29');
+    return this.find({
+      where: {
+        doctorId,
+        start: LessThanOrEqual(currentStartDate),
+        end: MoreThanOrEqual(currentEndDate),
+      },
+    });
     // start: MoreThanOrEqual(currentStartDate),
     // end: LessThanOrEqual(currentEndDate),
-    const flag = process.env.BLOCKED_FIX_WORKING ? process.env.BLOCKED_FIX_WORKING : '0';
-    if (flag == '1') {
-      return this.find({
-        where: {
-          doctorId,
-          start: LessThanOrEqual(currentStartDate),
-          end: MoreThanOrEqual(currentEndDate),
-        },
-      });
-    } else {
-      return this.find({
-        where: {
-          doctorId,
-        },
-      });
-    }
+    // const flag = process.env.BLOCKED_FIX_WORKING ? process.env.BLOCKED_FIX_WORKING : '0';
+    // if (flag == '1') {
+    //   return this.find({
+    //     where: {
+    //       doctorId,
+    //       start: LessThanOrEqual(currentStartDate),
+    //       end: MoreThanOrEqual(currentEndDate),
+    //     },
+    //   });
+    // } else {
+    //   return this.find({
+    //     where: {
+    //       doctorId,
+    //     },
+    //   });
+    // }
   }
 }
