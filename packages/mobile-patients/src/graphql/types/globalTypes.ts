39f07d6257a78f16731405e7336317f5ae4cdf84
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2020-02-07 14:42:21 +0530

    AHQK-937: added UpdatePatientInput API types updated

--- packages/mobile-patients/src/graphql/types/globalTypes.ts
+++ packages/mobile-patients/src/graphql/types/globalTypes.ts
@@ -696,6 +696,7 @@ export interface UpdatePatientInput {
   uhid?: string | null;
   emailAddress?: string | null;
   dateOfBirth?: any | null;
+  referralCode?: string | null;
   relation?: Relation | null;
   photoUrl?: string | null;
 }
