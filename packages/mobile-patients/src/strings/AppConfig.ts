51d1a9c9443098ae99b5eb9e6fcca35d2cbcdecf
sankeeth <sankeerth.obulam@popcornapps.com>
2020-02-07 19:44:36 +0530

    QA Push

--- packages/mobile-patients/src/strings/AppConfig.ts
+++ packages/mobile-patients/src/strings/AppConfig.ts
@@ -99,8 +99,8 @@ const ConfigurationDev = {
   GOOGLE_API_KEY: 'AIzaSyCu4uyf9ln--tU-8V32nnFyfk8GN4koLI0',
   ...PharmaApiConfig.dev,
   ...appStaticVariables,
-  iOS_Version: '1.7',
-  Android_Version: '1.93',
+  iOS_Version: '1.311',
+  Android_Version: '1.311',
   CONDITIONAL_MANAGENET_BASE_URL: 'https://aph.staging.pmt.popcornapps.com',
   BUGSNAG_KEY: '53a0b9fd23719632a22d2c262a06bb4e', //7839e425f4acbd8e6ff3f907281addca <-- popcornapps key
 };
@@ -131,8 +131,8 @@ const ConfigurationQA = {
   GOOGLE_API_KEY: 'AIzaSyCu4uyf9ln--tU-8V32nnFyfk8GN4koLI0',
   ...PharmaApiConfig.prod,
   ...appStaticVariables,
-  iOS_Version: '1.221',
-  Android_Version: '1.24',
+  iOS_Version: '1.311',
+  Android_Version: '1.311',
   CONDITIONAL_MANAGENET_BASE_URL: 'https://aph.staging.pmt.popcornapps.com',
   BUGSNAG_KEY: '53a0b9fd23719632a22d2c262a06bb4e',
 };
