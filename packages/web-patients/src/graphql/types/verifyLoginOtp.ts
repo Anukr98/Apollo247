1b43901d675d5725917b77d8d1796e1c17ad7675
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2020-02-12 18:28:57 +0530

    prettier and new login files for graphql

---
+++ packages/web-patients/src/graphql/types/verifyLoginOtp.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { OtpVerificationInput } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: verifyLoginOtp
+// ====================================================
+
+export interface verifyLoginOtp_verifyLoginOtp {
+  __typename: "OtpVerificationResult";
+  status: boolean;
+  authToken: string | null;
+  isBlocked: boolean | null;
+}
+
+export interface verifyLoginOtp {
+  verifyLoginOtp: verifyLoginOtp_verifyLoginOtp;
+}
+
+export interface verifyLoginOtpVariables {
+  otpVerificationInput?: OtpVerificationInput | null;
+}
