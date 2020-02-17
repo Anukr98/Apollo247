6557f0a167b6b9bd2f32813f5a3a86a658cfc13a
MamathaPopcorn <mamatha.vattimalla@popcornapps.com>
2020-02-17 18:18:29 +0530

    Code updated

--- packages/mobile-doctors/src/components/ConsultRoom/ConsultRoomScreen.tsx
+++ packages/mobile-doctors/src/components/ConsultRoom/ConsultRoomScreen.tsx
@@ -103,31 +103,6 @@ const styles = StyleSheet.create({
     elevation: 10,
     backgroundColor: 'white',
   },
-  imageStyle: {
-    width: 32,
-    height: 32,
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-  },
-  automatedLeftText: {
-    ...theme.viewStyles.text('M', 15, theme.colors.WHITE),
-    paddingTop: 8,
-    paddingBottom: 4,
-    paddingHorizontal: 16,
-    textAlign: 'left',
-  },
-  automatedRightText: {
-    ...theme.viewStyles.text('M', 10, theme.colors.WHITE),
-    paddingHorizontal: 16,
-    paddingVertical: 4,
-    textAlign: 'right',
-  },
-  automatedTextView: {
-    backgroundColor: '#0087ba',
-    marginLeft: 38,
-    borderRadius: 10,
-  },
 });
 
 let connectionCount = 0;
