97f69d58e233d3dbd1830075c4f6494efaa402dc
Ravikiran Gambhire <46347375+rkgambhire@users.noreply.github.com>
2020-02-20 20:16:09 +0530

    Added consoles for doctor sms
    Added consoles for doctor sms

--- packages/api/src/notifications-service/resolvers/notifications.ts
+++ packages/api/src/notifications-service/resolvers/notifications.ts
@@ -929,6 +929,7 @@ export async function sendReminderNotification(
       );
       doctorSMS = doctorSMS.replace('{1}', patientDetails.firstName);
     }
+    console.log('doctorSMS=============', doctorSMS);
     sendNotificationSMS(doctorDetails.mobileNumber, doctorSMS);
     //send doctor sms ends
   } else if (
