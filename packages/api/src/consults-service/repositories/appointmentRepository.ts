fc9c14587f0d57fcc91e7d142e2ad5859974e647
sushma <sushma.voleti@popcornapps.com>
2020-02-23 01:08:24 +0530

    code clean up

--- packages/api/src/consults-service/repositories/appointmentRepository.ts
+++ packages/api/src/consults-service/repositories/appointmentRepository.ts
@@ -40,7 +40,6 @@ import { PatientRepository } from 'profiles-service/repositories/patientReposito
 //import { DoctorNextAvaialbleSlotsRepository } from 'consults-service/repositories/DoctorNextAvaialbleSlotsRepository';
 import { log } from 'customWinstonLogger';
 import { ApiConstants } from 'ApiConstants';
-import { getPatientMedicalRecordsResolvers } from 'profiles-service/resolvers/getPatientMedicalRecords';
 
 @EntityRepository(Appointment)
 export class AppointmentRepository extends Repository<Appointment> {
