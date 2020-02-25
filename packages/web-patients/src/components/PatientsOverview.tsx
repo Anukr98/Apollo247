b1da7bb44ae01049bfe0475d0008bbb1bb2f1598
malleshpopcornapps <popcorn@PCALP-50003.localdomain>
2020-02-25 16:49:40 +0530

    Fixed shadow issue on home page bottom cards

--- packages/web-patients/src/components/PatientsOverview.tsx
+++ packages/web-patients/src/components/PatientsOverview.tsx
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme: Theme) => {
       display: 'flex',
       alignItems: 'center',
       cursor: 'pointer',
+      boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2)',
     },
     totalConsults: {
       width: 40,
