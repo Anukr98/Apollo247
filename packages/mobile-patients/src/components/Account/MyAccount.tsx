6849bb28b368dc08625c16a892ba2f08a82b9249
hithaprani <hithaprani.vakkanti@popcornapps.com>
2020-03-03 12:48:36 +0530

    APP-164: Image fixed in MyAccount

--- packages/mobile-patients/src/components/Account/MyAccount.tsx
+++ packages/mobile-patients/src/components/Account/MyAccount.tsx
@@ -324,7 +324,7 @@ export const MyAccount: React.FC<MyAccountProps> = (props) => {
         >
           <View
             style={{
-              height: 200,
+              minHeight: 200,
               alignItems: 'center',
               // justifyContent: 'center',
               overflow: 'hidden',
@@ -347,7 +347,7 @@ export const MyAccount: React.FC<MyAccountProps> = (props) => {
             ) : (
               <Image
                 source={require('@aph/mobile-patients/src/components/ui/icons/no-photo-icon-round.png')}
-                style={{ top: 10, height: 140, width: '100%' }}
+                style={{ top: 10, height: 200, width: '100%' }}
                 resizeMode={'contain'}
               />
             )}
