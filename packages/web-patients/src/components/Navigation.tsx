c9b6ff9883f8b5b8688e40d9884d99c66c012e11
Alekhya837 <54703416+Alekhya837@users.noreply.github.com>
2020-03-03 15:59:12 +0530

    APP-356-B reverting css related changes

--- packages/web-patients/src/components/Navigation.tsx
+++ packages/web-patients/src/components/Navigation.tsx
@@ -26,9 +26,9 @@ const useStyles = makeStyles((theme: Theme) => {
         [theme.breakpoints.down(900)]: {
           display: 'none',
         },
-        // '&:nth-child(10)': {
-        //   display: 'none',
-        // },
+        '&:nth-child(3)': {
+          display: 'none',
+        },
       },
     },
     menuItemActive: {
@@ -45,7 +45,8 @@ const useStyles = makeStyles((theme: Theme) => {
       },
     },
     notificationBtn: {
-      display: 'inline-block',
+      display: 'none',
+      // display: 'inline-block',
       padding: '33px 20px 31px 20px',
       fontSize: 13,
       fontWeight: 600,
@@ -184,9 +185,9 @@ export const Navigation: React.FC = (props) => {
           <span className={classes.itemCount}>{cartItems.length || 0}</span>
         </span>
       </div>
-      {/* <div className={`${classes.notificationBtn}`}>
+      <div className={`${classes.notificationBtn}`}>
         <img src={require('images/ic_notification.svg')} alt="Notifications" />
-      </div> */}
+      </div>
       <Popover
         open={isCartPopoverOpen}
         anchorEl={cartPopoverRef.current}
