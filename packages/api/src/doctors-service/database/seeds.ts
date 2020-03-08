1607143ad2b8d62d5aa7e3c0dc06cff643d9536b
sushma <sushma.voleti@popcornapps.com>
2020-03-08 22:34:55 +0530

    lint warnings clean up

--- packages/api/src/doctors-service/database/seeds.ts
+++ packages/api/src/doctors-service/database/seeds.ts
@@ -1,6 +1,5 @@
 import { connect } from 'doctors-service/database/connect';
 import { buildDoctor } from 'doctors-service/database/factories/doctorFactory';
-import { DoctorConsultHoursRepository } from 'doctors-service/repositories/doctorConsultHoursRepository';
 import { DoctorHospitalRepository } from 'doctors-service/repositories/doctorHospitalRepository';
 import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
 import { DoctorSpecialtyRepository } from 'doctors-service/repositories/doctorSpecialtyRepository';
