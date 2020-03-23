517a23e5fa4555d21bb066771188f01c3931b41f
Uma Maheswari <uma.maheswari@popcornapps.com>
2020-03-23 10:29:20 +0530

    Created UI for Alert Messages

---
+++ packages/web-patients/src/components/Alerts/Alerts.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Theme } from '@material-ui/core';
+import { AphButton, AphDialogTitle, AphDialog, AphDialogClose } from '@aph/web-ui-components';
+
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    root: {},
+    dialogBox: {
+      '& > div': {
+        display: 'flex',
+        backgroundColor: 'transparent',
+        '& div': {
+          marginTop: 0,
+        },
+      },
+    },
+    defaultWrapper: {
+      width: 466,
+      boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.5)',
+      backgroundColor: '#f7f8f5',
+      textAlign: 'center',
+      borderRadius: 10,
+      '& h3': {
+        boxShadow: '0 5px 20px 0 rgba(128, 128, 128, 0.3)',
+        padding: 15,
+        fontSize: 16,
+        fontWeight: 500,
+        color: '#ffffff',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        margin: 0,
+      },
+      '& p': {
+        fontSize: 14,
+        fontWeight: 500,
+        marginBottom: 26,
+        marginTop: 0,
+      },
+      '& button': {
+        minWidth: 106,
+        fontSize: 13,
+        fontWeight: 600,
+        padding: '2px 13px',
+      },
+    },
+    errorAlertWrapper: {
+      '& h3': {
+        backgroundColor: '#890000',
+      },
+      '& p': {
+        color: '#890000',
+      },
+      '& button': {
+        '&:first-child': {
+          color: '#890000',
+        },
+        '&:last-child': {
+          backgroundColor: '#890000',
+          color: '#fff',
+        },
+      },
+    },
+    successAlertWrapper: {
+      '& h3': {
+        backgroundColor: '#00b38e',
+      },
+      '& p': {
+        color: '#00b38e',
+      },
+      '& button': {
+        '&:first-child': {
+          color: '#00b38e',
+        },
+        '&:last-child': {
+          backgroundColor: '#00b38e',
+          color: '#fff',
+        },
+      },
+    },
+    contentWrapper: {
+      padding: 22,
+    },
+    buttonsWrapper: {
+      display: 'flex',
+      justifyContent: 'space-evenly',
+    },
+    dialogWidth: {
+      maxWidth: 500,
+    },
+  };
+});
+export const Alerts: React.FC = (props) => {
+  const classes = useStyles({});
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(true);
+
+  return (
+    <div className={classes.root}>
+      <AphDialog
+        className={classes.dialogBox}
+        classes={{ paper: classes.dialogWidth }}
+        open={isAlertOpen}
+        maxWidth="md"
+      >
+        <div
+          className={`${classes.defaultWrapper} ${classes.errorAlertWrapper} ${classes.successAlertWrapper}`}
+        >
+          <h3>Success</h3>
+          <div className={classes.contentWrapper}>
+            <p>The file was uploaded successfully.</p>
+            <div className={classes.buttonsWrapper}>
+              <AphButton onClick={() => setIsAlertOpen(false)} title={'Close'}>
+                Ignore
+              </AphButton>
+              <AphButton onClick={() => setIsAlertOpen(false)} title={'Close'}>
+                Done
+              </AphButton>
+            </div>
+          </div>
+        </div>
+      </AphDialog>
+    </div>
+  );
+};
