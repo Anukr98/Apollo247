16307d01f8dec6a7643349a1116b541f42540cda
hithaprani <hithaprani.vakkanti@popcornapps.com>
2020-03-24 18:18:09 +0530

    Revert "APP-762: AM/PM Time fix"
    This reverts commit c26aa33ea49ed9dac212e89122b13fab6d1e3161.

--- packages/mobile-doctors/src/components/ui/DatePicker.tsx
+++ packages/mobile-doctors/src/components/ui/DatePicker.tsx
@@ -90,7 +90,6 @@ export const DatePicker: React.FC<DatePickerProps> = (props) => {
         minimumDate={props.minimumDate}
         maximumDate={props.maximumDate}
         mode={props.mode || 'date'}
-        is24Hour={false}
       />
     </View>
   );
