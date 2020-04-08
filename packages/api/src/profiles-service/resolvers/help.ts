ba40d4a23fe27e14dde3a11f0ed56a7e01fa8468
saiprasad <saiprasad.ashwini@popcornapps.com>
2020-04-08 10:53:17 +0530

    APP-922 : help email template

--- packages/api/src/profiles-service/resolvers/help.ts
+++ packages/api/src/profiles-service/resolvers/help.ts
@@ -15,6 +15,7 @@ import { MEDICINE_ORDER_PAYMENT_TYPE } from 'profiles-service/entities';
 import { DoctorRepository } from 'doctors-service/repositories/doctorRepository';
 import { PatientHelpTicketRepository } from 'profiles-service/repositories/patientHelpTicketsRepository';
 import { PatientHelpTickets } from 'profiles-service/entities';
+import { helpEmailTemplate } from 'helpers/emailTemplates/helpEmailTemplate';
 
 export const helpTypeDefs = gql`
   input HelpEmailInput {
@@ -168,64 +169,12 @@ const sendHelpEmail: Resolver<null, HelpEmailInputArgs, ProfilesServiceContext, 
 
   const medicineOrders = Object.values(formattedOrdersObject);
 
-  const mailContentTemplate = _.template(
-    `<html>
-    <body>
-    <p> #original_sender{<%- patientDetails.mobileNumber %>@apollo247.org} </p>
-    <p> Patient Help Form</p>
-    <ul>
-    <li>Need help with  : <%- helpEmailInput.category %></li>
-    <li>Reason  : <%- helpEmailInput.reason %></li>
-    <li>Comments  : <%- helpEmailInput.comments %></li>
-    <li>Patient Details
-      <ul>
-       <li>First Name : <%- patientDetails.firstName %></li>
-       <li>Last Name : <%- patientDetails.lastName %></li>
-       <li>Customer Id : <%- patientDetails.id %></li>
-       <li>UHID : <%- patientDetails.uhid %></li>
-       <li>Email : <%- patientDetails.emailAddress %></li>
-       <li>Mobile Number : <%- patientDetails.mobileNumber %></li>
-      </ul>
-    </li>
-    <% _.each(appointmentList, function(appointment,index) { %>
-    <li> Appointment: <%- index+1 %>     
-    <ul>
-     <li>Doctor's Name : <%- appointment.doctorName %> </li>
-     <li>Appointment DateTime : <%- appointment.appointmentDateTime %> </li>
-     <li>Appointment Mode : <%- appointment.appointmentMode %> </li>  
-     <li>DoctorType : <%- appointment.doctorType %> </li>    
-    </ul>    
-    </li>
-    <% }); %>
-    <% _.each(medicineOrders, function(order, index) { %>
-    <li> Medicine Order Details: <%- index+1 %> 
-      <ul>      
-          <li>Payment Details : <%- order.paymentMode %></li>
-          <li>Item Details : 
-              <ul>
-                <% _.each(order.lineItems, function(item) { %>
-                    <li>Name: <%- item.name %> , SKU: <%-item.sku%> , OrderId: <%- item.orderautoid %></li>
-                <% }); %>
-                </ul>
-          </li>
-          <li>Date and Time of Order : <%- order.orderDateTime %></li>
-          <% if(order.prescriptionUrl != null) {  %>
-            <li>Prescription : <%- order.prescriptionUrl %></li>
-          <% } %> 
-      </ul>
-    </li>
-    <% }); %>
-
-    </ul>
-    </body> 
-    </html>
-    `
-  );
-  const mailContent = mailContentTemplate({
+  const mailContent = helpEmailTemplate({
     helpEmailInput,
     patientDetails,
     medicineOrders,
     appointmentList,
+    format,
   });
 
   let subjectLine = ApiConstants.PATIENT_HELP_SUBJECT.replace('{1}', helpEmailInput.category);
