a6be376b6566bdfffa34067c7a93c709ccc26178
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-04-14 08:54:47 +0530

    added logic to add more data and search

--- packages/api/src/doctors-service/resolvers/doctorDataElastic.ts
+++ packages/api/src/doctors-service/resolvers/doctorDataElastic.ts
@@ -15,12 +15,59 @@ const insertDataElastic: Resolver<null, {}, DoctorsServiceContext, string> = asy
   { doctorsDb }
 ) => {
   const client = new Client({ node: 'http://104.211.242.175:9200' });
+  const sourceData = {
+    doc: {
+      firstName: 'testdoc1',
+      doctorSlots: [
+        {
+          date: '2020-04-09',
+          slots: [{ id: 1, slot: '2020-04-09T14:30', status: 'BOOKED' }],
+        },
+      ],
+    },
+  };
+
+  const doc1: RequestParams.Update = {
+    index: 'doctors',
+    type: 'posts',
+    id: '1',
+    body: {
+      script: {
+        source:
+          'for (int i = 0; i < ctx._source.doctorSlots.length; ++i) { if(ctx._source.doctorSlots[i].date == params.date) {ctx._source.doctorSlots[i].date = params.newDate; ctx._source.doctorSlots[i].slots.add(params.slot);}}',
+        params: {
+          date: '2020-04-11',
+          newDate: '2020-04-11',
+          slot: {
+            id: 1,
+            slot: '2020-04-11T14:30',
+            status: 'BOOKED',
+          },
+        },
+      },
+    },
+  };
+  await client.index({
+    index: 'doctors',
+    id: '2',
+    body: {
+      firstName: 'testdoc2',
+      lastName: 'last',
+      mobileNumber: '+918019677179',
+      specialty: {
+        id: 1,
+        specialtyName: 'General Physian',
+      },
+    },
+  });
+  //await client.index(doc1);
+  //await client.update(doc1);
   const params: RequestParams.Search = {
     index: 'doctors',
     body: {
       query: {
         match: {
-          firstName: 'test',
+          firstName: 'testdoc1',
         },
       },
     },
@@ -28,7 +75,7 @@ const insertDataElastic: Resolver<null, {}, DoctorsServiceContext, string> = asy
   client
     .search(params)
     .then((result: ApiResponse) => {
-      console.log(result.body.hits.hits);
+      console.log('response = ', result.body.hits.hits);
     })
     .catch((err: Error) => {
       console.log(err);
