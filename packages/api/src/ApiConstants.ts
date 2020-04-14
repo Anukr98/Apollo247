07225e18c1e59fdaa1114be459e40f399cf13a91
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-04-14 08:58:22 +0530

    removed mail id from cc list

--- packages/api/src/ApiConstants.ts
+++ packages/api/src/ApiConstants.ts
@@ -129,7 +129,7 @@ export enum ApiConstants {
 
   //Patient app, help form email configurations
   PATIENT_HELP_SUPPORT_EMAILID = 'sushma.voleti@popcornapps.com',
-  PATIENT_HELP_SUPPORT_CC_EMAILID = 'sriram.kanchan@popcornapps.com,sumeeth.kumar@popcornapps.com',
+  PATIENT_HELP_SUPPORT_CC_EMAILID = 'sriram.kanchan@popcornapps.com',
   PATIENT_HELP_FROM_EMAILID = 'info@apollo247.com', // 'info@pepisandbox.com',
   PATIENT_HELP_FROM_NAME = 'Apollo24*7',
   PATIENT_HELP_SUBJECT = 'Issue raised by customer : {0} - {1}, {2}',
@@ -180,7 +180,7 @@ export enum ApiConstants {
   APPT_MAIL_SUBJECT = 'Your appointment has been booked successfully',
   FROM_NAME = 'Admin',
   PATIENT_APPT_EMAILID = 'sriram.kanchan@popcornapps.com',
-  PATIENT_APPT_CC_EMAILID = 'raj@popcornapps.com,sumeeth.kumar@popcornapps.com',
+  PATIENT_APPT_CC_EMAILID = 'raj@popcornapps.com',
   PATIENT_APPT_CC_EMAILID_TRIGGER = 'raj@popcornapps.com',
   PATIENT_APPT_EMAILID_PRODUCTION = 'helpdesk@apollo247.com',
   PATIENT_APPT_CC_EMAILID_PRODUCTION = 'prashant_sharma@apollohospitals.com,sriram.kanchan@popcornapps.com',
