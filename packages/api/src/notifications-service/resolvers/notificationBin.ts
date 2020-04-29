242432a509187b6df2332a372e8d57af4d9bc921
saiprasad <saiprasad.ashwini@popcornapps.com>
2020-04-29 10:52:12 +0530

    Initial commit

--- packages/api/src/notifications-service/resolvers/notificationBin.ts
+++ packages/api/src/notifications-service/resolvers/notificationBin.ts
@@ -8,6 +8,10 @@ import {
   NotificationBin,
 } from 'consults-service/entities';
 import { NotificationBinRepository } from 'notifications-service/repositories/notificationBinRepository';
+import CryptoJS from 'crypto-js';
+import { AphError } from 'AphError';
+import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
+import { sendNotificationSMS } from 'notifications-service/resolvers/notifications';
 
 export const notificationBinTypeDefs = gql`
   enum notificationStatus {
@@ -72,17 +76,23 @@ const insertMessage: Resolver<
   NotificationsServiceContext,
   { notificationData: Partial<NotificationBin> }
 > = async (parent, { messageInput }, { consultsDb, doctorsDb, patientsDb }) => {
+  const { fromId, toId, eventName, eventId, message, status, type } = messageInput;
+  const bytes = CryptoJS.AES.decrypt(message, process.env.NOTIFICATION_SMS_SECRECT_KEY);
+  const isMessageEncrypted = bytes.toString(CryptoJS.enc.Utf8);
+  if (!isMessageEncrypted) throw new AphError(AphErrorMessages.MESSAGE_ENCRYPTION_ERROR);
   const notificationBinRepo = consultsDb.getCustomRepository(NotificationBinRepository);
   const notificationInputs: Partial<NotificationBin> = {
-    fromId: messageInput.fromId,
-    toId: messageInput.toId,
-    eventName: messageInput.eventName,
-    eventId: messageInput.eventId,
-    message: messageInput.message,
-    status: messageInput.status,
-    type: messageInput.type,
+    fromId: fromId,
+    toId: toId,
+    eventName: eventName,
+    eventId: eventId,
+    message: message,
+    status: status,
+    type: type,
   };
   await notificationBinRepo.saveNotification(notificationInputs);
+  if (eventName == notificationEventName.APPOINTMENT) {
+  }
 
   return { notificationData: notificationInputs };
 };
