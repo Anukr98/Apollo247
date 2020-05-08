dd035627b033fe0daacfbf399e3df35ecbd75f4e
Ashok <ashokkumar.s@apollo247.org>
2020-05-08 23:05:23 +0530

    wip

--- packages/api/src/consults-service/resolvers/getOrderInvoice.ts
+++ packages/api/src/consults-service/resolvers/getOrderInvoice.ts
@@ -11,38 +11,92 @@ import _ from 'lodash';
 
 export const getOrderInvoiceTypeDefs = gql`
 type AppointmentsResult {
+  appointments : [ApptResponse]
+  doc: docResponse
+  patient: patientDetails
+}
+type patientDetails {
+  uhid: String
+  mobileNumber: String
+  emailAddress: String
+}
+type docResponse {
+  firstName: String
+  lastName: String
+  specialization: String
+}
+type ApptResponse {
+    displayId: Int
+    id: String
+    patientName: String
+    appointmentType: String
+    appointmentDateTime: DateTime
+    actualAmount: Float
+    discountedAmount: Float
+    appointmentPayments: [appointmentPayment]
+    status: String
+  }
+type appointmentPayment {
+  amountPaid: Float
+  bankTxnId: String
   id: String
+  paymentRefId: String
+  paymentStatus: String
+  paymentType: String
+  responseMessage: String
 }
   extend type Query {
-    getOrderInvoice(patientId: String): AppointmentsResult
+    getOrderInvoice(patientId: String, appointmentId: String): AppointmentsResult
   }
 `;
 
-
-type AppointmentsResult = {
+type ApptResponse = {
+  displayId: number;
   id: string;
+  patientName: string;
+  appointmentDateTime: Date;
+  actualAmount: Number;
+  discountedAmount: Number;
+  appointmentType: string
+  appointmentPayments: appointmentPayment[];
+  status: String;
 };
 
+type AppointmentsResult = {
+  appointments: ApptResponse[];
+}
+
+type appointmentPayment = {
+  amountPaid: Number;
+  bankTxnId: string;
+  id: string;
+  paymentRefId: string;
+  paymentStatus: string;
+  paymentType: string;
+  responseMessage: string;
+}
 
 const getOrderInvoice: Resolver<
   null,
-  { patientId: string },
+  { patientId: string, appointmentId: string },
   ConsultServiceContext,
   AppointmentsResult
 > = async (parent, args, { consultsDb, doctorsDb, patientsDb }) => {
-  const patientRepo = consultsDb.getCustomRepository(PatientRepository);
   const apptsRepo = consultsDb.getCustomRepository(AppointmentRepository);
-  // const docConsultRep = doctorsDb.getCustomRepository(DoctorRepository);
-  // const response = await apptsRepo.getAllAppointmentsByPatientId(args.patientId);
-  // console.log('orders Response', JSON.stringify(response, null, 2));
-  let result = [];
-  // const response = await patientsRepo.findById(args.patientId);
-  // console.log('doc Response', JSON.stringify(response, null, 2));
-  const patientDetails = await patientRepo.getPatientDetails(args.patientId);
-  console.log('patientDetails', patientDetails);
-  if (!patientDetails) throw new AphError(AphErrorMessages.INVALID_PATIENT_ID, undefined, {});
+  const docConsultRep = doctorsDb.getCustomRepository(DoctorRepository);
+  const patientsRep = patientsDb.getCustomRepository(PatientRepository);
+  const response = await apptsRepo.findByAppointmentId(args.appointmentId);
+  console.log('orders Response', JSON.stringify(response, null, 2));
 
-  return patientDetails;
+  const patientDetails = await patientsRep.findById(args.patientId);
+  console.log('orders Response', JSON.stringify(patientDetails, null, 2));
+
+  const docResponse = await docConsultRep.findDoctorByIdWithoutRelations(response[0].doctorId);
+  console.log('doc Response', JSON.stringify(docResponse, null, 2));
+
+  if (response && response.length > 0) {
+    return { appointments: response, doc: docResponse, patient: patientDetails }
+  } else throw new AphError(AphErrorMessages.INVALID_PATIENT_ID, undefined, {});
 };
 
 export const getOrderInvoiceResolvers = {
