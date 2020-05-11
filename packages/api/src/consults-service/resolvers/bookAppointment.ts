216122bf970ecbab7c374f4b448c020335ab8ad6
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-05-11 17:02:50 +0530

    fixed invalid hospital id issue

--- packages/api/src/consults-service/resolvers/bookAppointment.ts
+++ packages/api/src/consults-service/resolvers/bookAppointment.ts
@@ -182,9 +182,21 @@ const bookAppointment: Resolver<
     throw new AphError(AphErrorMessages.INVALID_DOCTOR_ID, undefined, {});
   }
   // check if hospital id is linked to doctor
-  if (docDetails.doctorHospital[0].facility.id !== appointmentInput.hospitalId) {
+  let facilityFlag = 0;
+  docDetails.doctorHospital.forEach((facility) => {
+    if (facility.id == appointmentInput.hospitalId) {
+      facilityFlag = 1;
+    }
+  });
+
+  if (facilityFlag == 0) {
     throw new AphError(AphErrorMessages.INVALID_HOSPITAL_ID, undefined, {});
   }
+
+  // if (docDetails.doctorHospital[0].facility.id !== appointmentInput.hospitalId) {
+  //   throw new AphError(AphErrorMessages.INVALID_HOSPITAL_ID, undefined, {});
+  // }
+
   //check if doctor and hospital are matched
   const facilityId = appointmentInput.hospitalId;
   if (facilityId) {
