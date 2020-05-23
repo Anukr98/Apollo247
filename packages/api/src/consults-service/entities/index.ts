8078ee0c8f8b37129c12930446f55c2002b80928
Piyush Arora <piyush.bulandi@gmail.com>
2020-05-23 16:01:01 +0530

    added refund info in the api

--- packages/api/src/consults-service/entities/index.ts
+++ packages/api/src/consults-service/entities/index.ts
@@ -104,6 +104,7 @@ export enum REQUEST_ROLES {
   PATIENT = 'PATIENT',
   JUNIOR = 'JUNIOR',
   SYSTEM = 'SYSTEM',
+  ADMIN = 'ADMIN'
 }
 
 export enum TRANSFER_STATUS {
@@ -377,9 +378,6 @@ export class AppointmentPayments extends BaseEntity {
 
   @Column({ nullable: true })
   bankName: string;
-
-  @Column({ nullable: true })
-  refundAmount: number;
 
   @Column({ nullable: true, type: 'text' })
   responseCode: string;
@@ -698,8 +696,9 @@ export enum MEDICINE_UNIT {
 
 export enum AUDIT_STATUS {
   PENDING = 'PENDING',
-  COMPLETED = 'COMPLETED',
+  COMPLETED = 'COMPLETED'
 }
+
 export type CaseSheetMedicinePrescription = {
   externalId: string;
   id: string;
@@ -1697,4 +1696,3 @@ export interface RxPdfData {
   referralSpecialtyName: string;
   referralSpecialtyDescription: string;
 }
-///////////////////////////////////////////////////////////
