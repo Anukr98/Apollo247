34be070c8a21bb642c7f2782572891ad6a1e0234
Piyush Arora <piyush.bulandi@gmail.com>
2020-05-26 16:13:56 +0530

    APP-1996 added delay in webhook response

--- paytm-integration/consult-integrations/controllers/webhook.js
+++ paytm-integration/consult-integrations/controllers/webhook.js
@@ -5,44 +5,47 @@ const { consultsOrderQuery } = require('../helpers/make-graphql-query');
 
 
 module.exports = async (req, res, next) => {
-    let orderId;
 
-    try {
-        const payload = req.body;
-        orderId = payload.ORDERID;
+    setTimeout(async () => {
+        let orderId;
+        try {
+            const payload = req.body;
+            orderId = payload.ORDERID;
 
-        logger.info(`${orderId} - consult-response-webhook - ${JSON.stringify(payload)}`);
+            logger.info(`${orderId} - consult-response-webhook - ${JSON.stringify(payload)}`);
 
-        const checksum = payload.CHECKSUMHASH;
-        delete payload.CHECKSUMHASH;
-        if (!verifychecksum(payload, process.env.PAYTM_MERCHANT_KEY_CONSULTS, checksum)) {
-            logger.error(`${orderId} - consult-response-webhook: checksum did not match - ${JSON.stringify(payload)}`);
-            return next(new Error(`checkSum did not match for order - ${orderId}`));
+            const checksum = payload.CHECKSUMHASH;
+            delete payload.CHECKSUMHASH;
+            if (!verifychecksum(payload, process.env.PAYTM_MERCHANT_KEY_CONSULTS, checksum)) {
+                logger.error(`${orderId} - consult-response-webhook: checksum did not match - ${JSON.stringify(payload)}`);
+                return next(new Error(`checkSum did not match for order - ${orderId}`));
+            }
+
+            axios.defaults.headers.common['authorization'] = process.env.API_TOKEN;
+
+            // this needs to be altered later.
+            const requestJSON = {
+                query: consultsOrderQuery(payload),
+            };
+
+            /// write medicineoirder
+            const response = await axios.post(process.env.API_URL, requestJSON);
+            logger.info(`${payload.ORDERID} - makeAppointmentPayment -  ${JSON.stringify(response.data)}`);
+
+            if (response.data.errors && response.data.errors.length) {
+                logger.info(`${orderId} - consult-payment-webhook - ${JSON.stringify(response.data.errors)}`);
+                //throw new Error(`Error Occured in makeAppointmentPayment - consult-payment-webhook - for order id: ${orderId}`);
+            }
+            res.send("webhook consumed successfully!");
+
+        } catch (e) {
+            if (e.response && e.response.data) {
+                logger.error(`${orderId} - consult-payment-webhook - ${JSON.stringify(e.response.data)}`);
+            } else {
+                logger.error(`${orderId} - consult-payment-webhook -  ${e.stack}`);
+            }
+            next(e);
         }
+    }, 10000);
 
-        axios.defaults.headers.common['authorization'] = process.env.API_TOKEN;
-
-        // this needs to be altered later.
-        const requestJSON = {
-            query: consultsOrderQuery(payload),
-        };
-
-        /// write medicineoirder
-        const response = await axios.post(process.env.API_URL, requestJSON);
-        logger.info(`${payload.ORDERID} - makeAppointmentPayment -  ${JSON.stringify(response.data)}`);
-
-        if (response.data.errors && response.data.errors.length) {
-            logger.info(`${orderId} - consult-payment-webhook - ${JSON.stringify(response.data.errors)}`);
-            //throw new Error(`Error Occured in makeAppointmentPayment - consult-payment-webhook - for order id: ${orderId}`);
-        }
-        res.send("webhook consumed successfully!");
-
-    } catch (e) {
-        if (e.response && e.response.data) {
-            logger.error(`${orderId} - consult-payment-webhook - ${JSON.stringify(e.response.data)}`);
-        } else {
-            logger.error(`${orderId} - consult-payment-webhook -  ${e.stack}`);
-        }
-        next(e);
-    }
 };
