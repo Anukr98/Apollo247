5dc394671ed9ae213d6030009f762c1c388239a9
malleshpopcornapps <popcorn@PCALP-50003.localdomain>
2020-06-01 11:45:21 +0530

    Created UI for location pincode on header

--- packages/web-patients/src/components/LocationSearch.tsx
+++ packages/web-patients/src/components/LocationSearch.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { makeStyles, createStyles } from '@material-ui/styles';
 import { Theme, Popover } from '@material-ui/core';
 import { AphTextField } from '@aph/web-ui-components';
+import { AphButton, AphDialog, AphDialogTitle, AphDialogClose } from '@aph/web-ui-components';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import { AllowLocation } from 'components/AllowLocation';
 import { useAuth } from 'hooks/authHooks';
@@ -13,10 +14,11 @@ const useStyles = makeStyles((theme: Theme) => {
       borderLeft: '0.5px solid rgba(2,71,91,0.3)',
       paddingLeft: 20,
       marginLeft: 40,
-      paddingTop: 22,
-      paddingBottom: 22,
+      paddingTop: 14,
+      paddingBottom: 14,
       marginTop: 10,
       marginBottom: 10,
+      position: 'relative',
       [theme.breakpoints.down('xs')]: {
         marginLeft: 'auto',
         borderLeft: 'none',
@@ -25,10 +27,8 @@ const useStyles = makeStyles((theme: Theme) => {
       },
     },
     locationWrap: {
-      display: 'flex',
-      alignItems: 'center',
-      cursor: 'pointer',
       maxWidth: 210,
+      cursor: 'pointer',
       [theme.breakpoints.down('xs')]: {
         maxWidth: 180,
       },
@@ -47,13 +47,21 @@ const useStyles = makeStyles((theme: Theme) => {
       right: 2,
     },
     selectedLocation: {
-      fontSize: 13,
-      fontWeight: 600,
-      color: theme.palette.secondary.light,
+      fontSize: 14,
+      fontWeight: 500,
       overflow: 'hidden',
-      textTransform: 'uppercase',
       whiteSpace: 'nowrap',
       textOverflow: 'ellipsis',
+      display: 'flex',
+      alignItems: 'center',
+      '& span:first-child': {
+        borderBottom: '2px solid #00b38e',
+      },
+      '& span:last-child': {
+        '& img': {
+          verticalAlign: 'middle',
+        },
+      },
       [theme.breakpoints.down('xs')]: {
         marginLeft: 'auto',
       },
@@ -179,6 +187,72 @@ const useStyles = makeStyles((theme: Theme) => {
         left: '0px !important',
       },
     },
+    userName: {
+      fontSize: 12,
+      color: '#01475b',
+    },
+    locationPopRoot: {
+      overflow: 'initial',
+      boxShadow: '0 0 3px 0 rgba(0, 0, 0, 0.2)',
+      border: 'solid 1px #f7f8f5',
+      '& ul': {
+        margin: 0,
+        padding: 0,
+        '& li': {
+          padding: '12px 20px',
+          fontSize: 15,
+          fontWeight: 500,
+          borderBottom: 'solid 1px #f7f8f5',
+          listStyleType: 'none',
+          cursor: 'pointer',
+          '&:last-child': {
+            borderBottom: 'none',
+          },
+        },
+      }
+    },
+    noService: {
+      position: 'absolute',
+      bottom: -2,
+      fontSize: 11,
+      color: '#890000',
+      minWidth: 140,
+    },
+    pincodeData: {
+      padding: 30,
+      fontSize: 14,
+      color: '#02475b',
+      fontWeight: 500,
+      '& h2': {
+        fontSize: 32,
+        lineHeight: '42px',
+        fontWeight: 600,
+        margin: 0,
+      },
+    },
+    bottomActions: {
+      textAlign: 'center',
+      paddingTop: 16,
+    },
+    submitBtn: {
+      backgroundColor: '#fcb716',
+      color: '#fff',
+      minWidth: 150,
+      borderRadius: 10,
+      '&:hover': {
+        backgroundColor: '#fcb716',
+        color: '#fff',        
+      },
+    },
+    disabledBtn: {
+      opacity: 0.6,
+    },
+    pincodeError: {
+      fontSize: 13,
+      color: '#890000',
+      paddingTop: 8,
+      fontWeight: 500,
+    },
   });
 });
 
@@ -206,6 +280,7 @@ export const LocationSearch: React.FC = (props) => {
   const locationRef = useRef(null);
   const [isLocationPopoverOpen, setIsLocationPopoverOpen] = React.useState<boolean>(false);
   const [isForceFullyClosePopover, setIsForceFullyClosePopover] = React.useState<boolean>(false);
+  const [isLocationPopover, setIsLocationPopover] = React.useState<boolean>(false);
 
   const searchOptions = {
     componentRestrictions: { country: ['in'] },
@@ -218,6 +293,7 @@ export const LocationSearch: React.FC = (props) => {
   const [selectedAddress, setSelectedAddress] = React.useState('');
   const [isLocationDenied, setIsLocationDenied] = React.useState<boolean>(false);
   const [detectBy, setDetectBy] = React.useState<string | null>('');
+  const [isPincodeDialogOpen, setIsPincodeDialogOpen] = React.useState<boolean>(false);
 
   const {
     currentLocation,
@@ -316,29 +392,26 @@ export const LocationSearch: React.FC = (props) => {
       <div
         className={classes.locationWrap}
         ref={locationRef}
-        onClick={() => setIsLocationPopoverOpen(true)}
+        onClick={() => setIsLocationPopover(true)}
         title={selectedAddress}
         id="locationId"
       >
-        <img
-          className={`${classes.locationIcon} ${classes.iconDesktop}`}
-          src={require('images/ic_location_on.svg')}
-          alt=""
-          title={selectedAddress}
-        />
-        <span className={classes.selectedLocation}>
-          {!isPopoverOpen && selectedAddress.length > 0
-            ? selectedAddress
-            : !isPopoverOpen && currentLocation && currentLocation.length > 0
-            ? currentLocation
-            : getAddressFromLocalStorage()}
-        </span>
-        <img
-          className={`${classes.locationIcon} ${classes.iconMobile}`}
-          src={require('images/ic_location_on.svg')}
-          alt=""
-          title={selectedAddress}
-        />
+        <div className={classes.userName}>
+          Deliver to Divya
+        </div>
+        <div className={classes.selectedLocation}>
+          <span>
+            {!isPopoverOpen && selectedAddress.length > 0
+              ? selectedAddress
+              : !isPopoverOpen && currentLocation && currentLocation.length > 0
+              ? currentLocation
+              : getAddressFromLocalStorage()}
+          </span>  
+            <span>
+              <img src={require('images/ic_dropdown_green.svg')} alt="" />
+            </span>
+        </div>
+        <div className={classes.noService}>Sorry, not serviceable here.</div>
       </div>
       <Popover
         open={isLocationPopoverOpen}
@@ -413,6 +486,36 @@ export const LocationSearch: React.FC = (props) => {
         </PlacesAutocomplete>
       </Popover>
       <Popover
+        open={isLocationPopover}
+        anchorEl={locationRef.current}
+        onClose={() => {
+          setIsLocationPopover(false);
+        }}
+        classes={{
+          paper: classes.locationPopRoot,
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <ul>
+          <li>Auto Select Location</li>
+          <li
+            onClick={() => {
+              setIsLocationPopover(false);
+              setIsPincodeDialogOpen(true);
+            }}
+          >
+            Enter Delivery Pincode
+          </li>
+        </ul>
+      </Popover>      
+      <Popover
         open={isPopoverOpen}
         anchorEl={mascotRef.current}
         anchorOrigin={{
@@ -442,6 +545,26 @@ export const LocationSearch: React.FC = (props) => {
           </div>
         </div>
       </Popover>
+      <AphDialog open={isPincodeDialogOpen} maxWidth="sm">
+        <AphDialogClose onClick={() => setIsPincodeDialogOpen(false)} title={'Close'} />
+        <div className={classes.pincodeData}>
+          <h2>Hi! :)</h2>
+          <p>Allow us to serve you better by entering your delivery pincode below.</p>
+          <AphTextField placeholder="Enter pincode here" />
+          <div className={classes.pincodeError}>Sorry, we are not servicing in your area currently. Call 1860 500 0101 for pharmacy store nearby.</div>
+          <div className={classes.bottomActions}>
+            <AphButton
+              color="primary"
+              classes={{
+                root: classes.submitBtn,
+                disabled: classes.disabledBtn,
+              }}
+            >
+              Submit
+            </AphButton>
+          </div>
+        </div>
+      </AphDialog>
     </div>
   );
 };
