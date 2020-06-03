415f7f5c161922a48ce7732c73415c1a50e1722d
Sawan Kumar <sawankumar@apollo247.org>
2020-06-03 12:43:27 +0530

    onError attribute added on chat image

--- packages/web-doctors/src/components/ConsultRoom.tsx
+++ packages/web-doctors/src/components/ConsultRoom.tsx
@@ -604,6 +604,30 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
       return rowData.message;
     }
   };
+
+  async function imageExists(image_url: string) {
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+
+    return http.status !== 404;
+  }
+
+  const handleImageError = async (e: any, imageUrl: string) => {
+    e.persist();
+    const isImageExist = await imageExists(imageUrl);
+    if (isImageExist) {
+      e.target.src = imageUrl;
+      return;
+    } else {
+      e.target.src = 'https://flevix.com/wp-content/uploads/2019/07/Spin-Preloader.gif';
+      setTimeout(() => {
+        handleImageError(e, imageUrl);
+      }, 1000 * 10);
+    }
+  };
+
   const renderChatRow = (rowData: MessagesObjectProps, index: number) => {
     const { patientDetails } = useContext(CaseSheetContext);
     if (
@@ -682,7 +706,13 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
                         <img src={require('images/pdf_thumbnail.png')} />
                       </a>
                     ) : (
-                      <img src={rowData.url} alt={rowData.url} />
+                      <img
+                        src={rowData.url}
+                        alt={rowData.url}
+                        onError={(e: any) => {
+                          handleImageError(e, rowData.url);
+                        }}
+                      />
                     )}
                     {rowData.messageDate && (
                       <div className={classes.timeStampImg}>
@@ -783,7 +813,13 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
                     className={classes.imageUpload}
                   >
                     {rowData.url.substr(-4).toLowerCase() !== '.pdf' ? (
-                      <img src={rowData.url} alt={rowData.url} />
+                      <img
+                        src={rowData.url}
+                        alt={rowData.url}
+                        onError={(e: any) => {
+                          handleImageError(e, rowData.url);
+                        }}
+                      />
                     ) : (
                       <a href={rowData.url} target="_blank">
                         <img src={require('images/pdf_thumbnail.png')} />
@@ -889,7 +925,13 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
                         <img src={require('images/pdf_thumbnail.png')} />
                       </a>
                     ) : (
-                      <img src={rowData.url} alt={rowData.url} />
+                      <img
+                        src={rowData.url}
+                        alt={rowData.url}
+                        onError={(e: any) => {
+                          handleImageError(e, rowData.url);
+                        }}
+                      />
                     )}
                     {rowData.messageDate && (
                       <div className={classes.timeStampImg}>
