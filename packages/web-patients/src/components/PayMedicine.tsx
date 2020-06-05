81e7edfd630a7b85af5a0f4d43e81c00779585b8
Shrey Saroch <shrey.s@apollo247.org>
2020-06-05 16:53:43 +0530

    APP-2284 : clearing storage in finally block

--- packages/web-patients/src/components/PayMedicine.tsx
+++ packages/web-patients/src/components/PayMedicine.tsx
@@ -622,6 +622,7 @@ export const PayMedicine: React.FC = (props) => {
       })
       .finally(() => {
         setMutationLoading(false);
+        sessionStorage.removeItem('cartValues');
       });
   };
 
@@ -666,7 +667,6 @@ export const PayMedicine: React.FC = (props) => {
             window.location.href = pgUrl;
           } else if (orderAutoId && orderAutoId > 0 && value === 'COD') {
             placeOrder(orderId, orderAutoId, false, '');
-            sessionStorage.removeItem('cartValues');
           } else if (errorMessage.length > 0) {
             setMutationLoading(false);
             setIsAlertOpen(true);
@@ -873,9 +873,8 @@ export const PayMedicine: React.FC = (props) => {
                       {mutationLoading ? (
                         <CircularProgress size={22} color="secondary" />
                       ) : (
-                        `Pay Rs.${
-                          totalWithCouponDiscount && totalWithCouponDiscount.toFixed(2)
-                        } on delivery`
+                        `Pay Rs.${totalWithCouponDiscount &&
+                          totalWithCouponDiscount.toFixed(2)} on delivery`
                       )}
                     </AphButton>
                   )}
