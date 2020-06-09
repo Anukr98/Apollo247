42964880191cc68c43f5a623a475de9e2fdc3f18
kishore1990 <kishore.s@apollo247.org>
2020-06-09 22:19:40 +0530

    APP-2395 discount price at item level fix

--- packages/api/src/profiles-service/resolvers/pharmaOrderBilled.ts
+++ packages/api/src/profiles-service/resolvers/pharmaOrderBilled.ts
@@ -185,13 +185,14 @@ const saveOrderShipmentInvoice: Resolver<
     }),
     itemDetails: JSON.stringify(
       saveOrderShipmentInvoiceInput.itemDetails.map((item) => {
+        const quantity = item.quantity / item.packSize;
         return {
           itemId: item.articleCode,
           itemName: item.articleName,
           batchId: item.batch,
-          issuedQty: Number((item.quantity / item.packSize).toFixed(2)),
+          issuedQty: Number(quantity.toFixed(2)),
           mou: item.packSize,
-          discountPrice: Number((item.packSize * item.discountPrice).toFixed(2)),
+          discountPrice: Number((item.discountPrice / quantity).toFixed(2)),
           mrp: Number((item.packSize * item.unitPrice).toFixed(2)),
         };
       })
