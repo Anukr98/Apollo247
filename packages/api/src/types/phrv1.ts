0e2cb8ef767bd6fa0155c3b639c3701fa688eaef
sushma <sushma.voleti@popcornapps.com>
2020-06-11 13:28:58 +0530

    Added phr services file and types for upload document

--- packages/api/src/types/phrv1.ts
+++ packages/api/src/types/phrv1.ts
@@ -4,3 +4,37 @@ export interface GetUsersResponse {
   errorType: null;
   response: {}[];
 }
+
+export interface LabResultsUploadRequest {
+  labTestName: string;
+  labTestSource: string;
+  labTestDate: number;
+  labTestRefferedBy: string;
+  observation: string;
+  identifier: string;
+  visitId: string;
+  additionalNotes: string;
+  labTestResults: {
+    parameterName: string;
+    result: string;
+    unit: string;
+    range: string;
+    outOfRange: Boolean;
+    resultDate: number;
+  }[];
+  testResultFiles: {
+    id: string;
+    fileName: string;
+    mimeType: string;
+    dateCreated: string;
+    content: string;
+  }[];
+  labTestFollowUpDate: number;
+}
+
+export interface LabResultsUploadResponse {
+  errorCode: number;
+  errorMsg: string;
+  errorType: string;
+  response: string;
+}
