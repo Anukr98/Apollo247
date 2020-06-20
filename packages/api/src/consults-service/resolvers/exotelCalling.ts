cc77bd89300ab9fb6e2e22c5012843d7b51a449b
Prabhat Sharma <the.prabhat@gmail.com>
2020-06-20 17:17:36 +0530

    Add record flag in exotel calls

--- packages/api/src/consults-service/resolvers/exotelCalling.ts
+++ packages/api/src/consults-service/resolvers/exotelCalling.ts
@@ -9,15 +9,13 @@ import { DoctorRepository } from 'doctors-service/repositories/doctorRepository'
 import { PatientRepository } from 'profiles-service/repositories/patientRepository';
 import { UniqueVariableNamesRule } from 'graphql';
 
-
 export const exotelTypeDefs = gql`
-
   input exotelInput {
     from: String
     to: String
     appointmentId: String
   }
-  
+
   type exotelResult {
     isError: Boolean
     from: String
@@ -31,37 +29,36 @@ export const exotelTypeDefs = gql`
   }
 `;
 
-
 type exotelInput = {
-    from: string;
-    to: string;
-    appointmentId: string;
+  from: string;
+  to: string;
+  appointmentId: string;
 };
 
 type exotelResult = {
-    isError: boolean;
-    from: string;
-    to: string;
-    response: string;
-    errorMessage: string;
+  isError: boolean;
+  from: string;
+  to: string;
+  response: string;
+  errorMessage: string;
 };
 
 type exotelInputArgs = { exotelInput: exotelInput };
 
 type exotelOutputResult = {
-    exotel: exotelResult
+  exotel: exotelResult;
 };
 
 type ExotelRequest = {
   From: string;
   To: string;
   CallerId: string | undefined;
-}
+};
 
 type callInputs = {
   exotelUrl: string | undefined;
   exotelRequest: ExotelRequest;
-}
+};
 
 interface exotelCalling {
   isError: boolean;
@@ -72,92 +69,83 @@ interface exotelCalling {
 }
 
 async function exotelCalling(callInputs: callInputs): Promise<exotelCalling> {
-
-  if(!callInputs.exotelUrl || !callInputs.exotelRequest.CallerId){
+  if (!callInputs.exotelUrl || !callInputs.exotelRequest.CallerId) {
     throw new AphError(AphErrorMessages.INVALID_EXOTEL_PARAMETERS, undefined, {});
   }
 
   let reqBody = callInputs.exotelRequest;
 
-  callInputs.exotelUrl += "?";
-  for(let [key, value] of Object.entries(reqBody)){
+  callInputs.exotelUrl += '?';
+  for (let [key, value] of Object.entries(reqBody)) {
     callInputs.exotelUrl += '&' + key + '=' + value;
   }
 
   return await fetch(callInputs.exotelUrl, {
     method: 'POST',
   })
-  .then((res) => {
+    .then((res) => {
+      console.log('API_response==>', res);
 
-    console.log('API_response==>', res);
-    
-    log(
-      'consultServiceLogger',
-      'API_CALL_RESPONSE',
-      'initateConferenceTelephoneCall()->API_CALL_RESPONSE',
-      JSON.stringify(res),
-      ''
-    );
-    
-    let exotelResult = {
-      isError : false,
-      from: callInputs.exotelRequest.From,
-      to : callInputs.exotelRequest.To,
-      response : JSON.stringify(res),
-      errorMessage : ''
-    };
+      log(
+        'consultServiceLogger',
+        'API_CALL_RESPONSE',
+        'initateConferenceTelephoneCall()->API_CALL_RESPONSE',
+        JSON.stringify(res),
+        ''
+      );
 
-    return exotelResult;
-  })
-  .catch((error) => {
-    
-    console.error('exotelError:', error);
+      let exotelResult = {
+        isError: false,
+        from: callInputs.exotelRequest.From,
+        to: callInputs.exotelRequest.To,
+        response: JSON.stringify(res),
+        errorMessage: '',
+      };
 
-    log(
-      'consultServiceLogger',
-      'API_CALL_ERROR',
-      'initateConferenceTelephoneCall()->CATCH_BLOCK',
-      '',
-      JSON.stringify(error)
-    );
+      return exotelResult;
+    })
+    .catch((error) => {
+      console.error('exotelError:', error);
 
-    throw new AphError(AphErrorMessages.EXOTEL_REQUEST_ERROR);
+      log(
+        'consultServiceLogger',
+        'API_CALL_ERROR',
+        'initateConferenceTelephoneCall()->CATCH_BLOCK',
+        '',
+        JSON.stringify(error)
+      );
 
-    let exotelResult = {
-      isError : true,
-      from: callInputs.exotelRequest.From,
-      to : callInputs.exotelRequest.To,
-      response : '',
-      errorMessage : JSON.stringify(error)
-    };
+      throw new AphError(AphErrorMessages.EXOTEL_REQUEST_ERROR);
 
-    return exotelResult;
+      let exotelResult = {
+        isError: true,
+        from: callInputs.exotelRequest.From,
+        to: callInputs.exotelRequest.To,
+        response: '',
+        errorMessage: JSON.stringify(error),
+      };
 
-  });
+      return exotelResult;
+    });
 }
 
 const initateConferenceTelephoneCall: Resolver<
-null,
-exotelInputArgs,
-ConsultServiceContext,
-exotelCalling
+  null,
+  exotelInputArgs,
+  ConsultServiceContext,
+  exotelCalling
 > = async (parent, { exotelInput }, { consultsDb, doctorsDb, patientsDb }) => {
-  
-
   const exotelCallerId: string | undefined = process.env.EXOTEL_CALLER_ID;
   let exotelUrl: string | undefined = process.env.EXOTEL_API_URL;
 
-  if(!exotelInput.from && !exotelInput.to){
-    
+  if (!exotelInput.from && !exotelInput.to) {
     // check if appointmentId exist
-    if(!exotelInput.appointmentId){
+    if (!exotelInput.appointmentId) {
       throw new AphError(AphErrorMessages.INVALID_APPOINTMENT_ID, undefined, {});
     }
-    
+
     const apptsRepo = consultsDb.getCustomRepository(AppointmentRepository);
-    const appt = await apptsRepo.findById(
-      exotelInput.appointmentId,
-    );
+    const appt = await apptsRepo.findById(exotelInput.appointmentId);
 
     // check if appointment exist
     if (!appt) {
@@ -166,44 +154,40 @@ exotelCalling
 
     // lookup with patientId
     const patientRepo = patientsDb.getCustomRepository(PatientRepository);
-    const patient = await patientRepo.findById(
-      appt.patientId,
-    );
+    const patient = await patientRepo.findById(appt.patientId);
 
-    if(!patient){
+    if (!patient) {
       throw new AphError(AphErrorMessages.GET_PATIENTS_ERROR, undefined, {});
     }
 
     // lookup with doctorId
     const doctorRepo = doctorsDb.getCustomRepository(DoctorRepository);
-    const doctor = await doctorRepo.findById(
-      appt.doctorId,
-    );
+    const doctor = await doctorRepo.findById(appt.doctorId);
 
-    if(!doctor){
+    if (!doctor) {
       throw new AphError(AphErrorMessages.GET_DOCTORS_ERROR, undefined, {});
     }
 
     // we have got both phone numbers as doctor.mobileNumber and patient.mobileNumber
     let exotelRequest = {
-      'From': doctor.mobileNumber, 
-      'To': patient.mobileNumber,
-      'CallerId': exotelCallerId
+      From: doctor.mobileNumber,
+      To: patient.mobileNumber,
+      CallerId: exotelCallerId,
+      Record: true,
+      RecordingChannels: 'single',
     };
-    
-    return await exotelCalling({exotelUrl, exotelRequest});
 
+    return await exotelCalling({ exotelUrl, exotelRequest });
   }
 
-  
-  if(exotelInput.from && !exotelInput.to){
+  if (exotelInput.from && !exotelInput.to) {
     throw new AphError(AphErrorMessages.INVALID_PARAMETERS, undefined, {});
   }
-  
-  if(!exotelInput.from && exotelInput.to){
+
+  if (!exotelInput.from && exotelInput.to) {
     throw new AphError(AphErrorMessages.INVALID_PARAMETERS, undefined, {});
   }
-  
+
   // const reg = new RegExp(/^\d{10}$/);
   // if(!reg.test(exotelInput.from) || !reg.test(exotelInput.to)){
   //   throw new AphError(AphErrorMessages.INVALID_PARAMETERS, undefined, {});
@@ -213,16 +197,17 @@ exotelCalling
   // check for validity of both numbers.
 
   let exotelRequest = {
-    'From': exotelInput.from,
-    'To': exotelInput.to,
-    'CallerId': exotelCallerId
+    From: exotelInput.from,
+    To: exotelInput.to,
+    CallerId: exotelCallerId,
+    Record: true,
+    RecordingChannels: 'single',
   };
-  return await exotelCalling({exotelUrl, exotelRequest})
-
-}
+  return await exotelCalling({ exotelUrl, exotelRequest });
+};
 
 export const exotelCallingResolvers = {
-    Query: {
-        initateConferenceTelephoneCall,
-    },
+  Query: {
+    initateConferenceTelephoneCall,
+  },
 };
