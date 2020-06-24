d27cbd03204f4365d60f8f5142e51a07ddcee5da
kanchanpopcornapps <sriram.kanchan@popcornapps.com>
2020-06-24 10:55:51 +0530

    APP-2387: fixed error message

--- packages/api/src/profiles-service/resolvers/getMedicineOrdersOMSList.ts
+++ packages/api/src/profiles-service/resolvers/getMedicineOrdersOMSList.ts
@@ -292,7 +292,7 @@ const getMedicineOrderOMSDetails: Resolver<
   ProfilesServiceContext,
   MedicineOrderOMSDetailsResult
 > = async (parent, args, { profilesDb }) => {
-  let medicineOrderDetails: any = {};
+  let medicineOrderDetails: any = '';
   if (args.billNumber && args.billNumber != '' && args.billNumber != '0') {
     const ordersResp = await fetch(
       process.env.PRISM_GET_OFFLINE_ORDERS
@@ -371,6 +371,9 @@ const getMedicineOrderOMSDetails: Resolver<
           medicineOrderDetails = offlineList;
         }
       });
+    }
+    if (medicineOrderDetails == '' || medicineOrderDetails == null) {
+      throw new AphError(AphErrorMessages.INVALID_MEDICINE_ORDER_ID, undefined, {});
     }
   } else {
     const patientRepo = profilesDb.getCustomRepository(PatientRepository);
