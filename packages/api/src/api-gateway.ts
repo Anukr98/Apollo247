01f380a89913876856bbf2303cf17e4cb13e82f1
Pratyush Singh <pratyush@redesyn.com>
2020-06-25 13:28:28 +0530

    fix cors

--- packages/api/src/api-gateway.ts
+++ packages/api/src/api-gateway.ts
@@ -98,7 +98,10 @@ export type Resolver<Parent, Args, Context, Result> = (
     '104.211.90.216',
     '104.211.216.178',
     'https://apollo247.com',
-    'https://*.apollo247.com',
+    'https://uatdoctors.apollo247.com',
+    'https://uatpatients.apollo247.com',
+    'https://stagingpatients.apollo247.com',
+    'https://stagingdoctors.apollo247.com',
   ];
 
   const logger = winstonLogger.loggers.get('apiGatewayLogger');
