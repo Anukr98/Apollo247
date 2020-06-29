7474f906c93a2ded41b6d327c5431f65c124957b
amandalekhya <54703416+Alekhya837@users.noreply.github.com>
2020-06-29 13:20:03 +0530

    APP-2478-D
    null condition check

--- packages/web-patients/src/components/Specialities.tsx
+++ packages/web-patients/src/components/Specialities.tsx
@@ -159,12 +159,16 @@ export const Specialities: React.FC<SpecialitiesProps> = (props) => {
                           />
                           <div className={classes.spContent}>
                             <div>{specialityDetails.name}</div>
-                            <div className={classes.specialityDetails}>
-                              {specialityDetails.shortDescription}
-                            </div>
-                            <div className={classes.symptoms}>
-                              {getSymptoms(specialityDetails.symptoms)}
-                            </div>
+                            {specialityDetails.shortDescription && (
+                              <div className={classes.specialityDetails}>
+                                {specialityDetails.shortDescription}
+                              </div>
+                            )}
+                            {specialityDetails.symptoms && (
+                              <div className={classes.symptoms}>
+                                {getSymptoms(specialityDetails.symptoms)}
+                              </div>
+                            )}
                             <span className={classes.rightArrow}>
                               <img src={require('images/ic_arrow_right.svg')} />
                             </span>
