a7a829f55164e42d6358e6afcf93ffed964e7ec1
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2020-07-03 20:14:48 +0530

    APP-2847: uncomment alertStore from medicineOrderDetails

--- packages/mobile-patients/src/graphql/profiles.ts
+++ packages/mobile-patients/src/graphql/profiles.ts
@@ -1435,7 +1435,7 @@ export const GET_MEDICINE_ORDER_OMS_DETAILS = gql`
         deliveryType
         currentStatus
         patientAddressId
-        # alertStore
+        alertStore
         medicineOrdersStatus {
           id
           orderStatus
