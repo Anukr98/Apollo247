6f769247338fd44b92f7b6a8fc3e7755e51d93d4
Anup PurushottamKemaye <anup.purushottam@popcornapps.com>
2020-07-07 10:04:08 +0530

    APP-2881 fixed
    APP-2881 fixed

--- packages/web-doctors/src/components/ConsultTabs.tsx
+++ packages/web-doctors/src/components/ConsultTabs.tsx
@@ -398,6 +398,7 @@ export const ConsultTabs: React.FC = () => {
   const [occupationHistory, setOccupationHistory] = useState<string>('');
   const [referralError, setReferralError] = useState<boolean>(false);
   const [updatedDate, setUpdatedDate] = useState<string>('');
+  const [casesheetVersion, setCasesheetVersion] = useState<number>(1);
 
   const [appointmentStatus, setAppointmentStatus] = useState<string>('');
   const [sentToPatient, setSentToPatient] = useState<boolean>(false);
@@ -648,6 +649,7 @@ export const ConsultTabs: React.FC = () => {
           _data!.data!.getCaseSheet!.caseSheetDetails!.sentToPatient
             ? setSentToPatient(_data!.data!.getCaseSheet!.caseSheetDetails!.sentToPatient)
             : setSentToPatient(false);
+            _data!.data!.getCaseSheet!.caseSheetDetails!.version ? setCasesheetVersion(_data.data.getCaseSheet.caseSheetDetails.version) : setCasesheetVersion(1);
           if (
             _data.data &&
             _data.data.getCaseSheet &&
@@ -1763,6 +1765,7 @@ export const ConsultTabs: React.FC = () => {
             setOccupationHistory,
             updatedDate,
             setUpdatedDate,
+            casesheetVersion
           }}
         >
           <Scrollbars
