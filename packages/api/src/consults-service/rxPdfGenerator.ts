7490c6a951ec0ae42083f69ea800f0da00aa54af
Mohd Asad <mohd.asad@apollo247.org>
2020-07-10 15:45:47 +0530

    fix

--- packages/api/src/consults-service/rxPdfGenerator.ts
+++ packages/api/src/consults-service/rxPdfGenerator.ts
@@ -607,6 +607,7 @@ export const generateRxPdfDocument = (rxPdfData: RxPdfData): typeof PDFDocument 
         if (prescription.since.length > 0) textArray.push('Since: ' + prescription.since);
         if (prescription.howOften) textArray.push('How Often: ' + prescription.howOften);
         if (prescription.severity) textArray.push('Severity: ' + prescription.severity);
+        if (prescription.details) textArray.push('Details: ' + prescription.details);
 
         doc
           .fontSize(12)
@@ -621,14 +622,14 @@ export const generateRxPdfDocument = (rxPdfData: RxPdfData): typeof PDFDocument 
           .text(`${textArray.join('  |  ')}`, margin + 15)
           .moveDown(0.8);
 
-        if (prescription.details) {
-          doc
-            .fontSize(12)
-            .font(assetsDir + '/fonts/IBMPlexSans-Regular.ttf')
-            .fillColor('#666666')
-            .text(`${prescription.details}`, margin + 15)
-            .moveDown(0.8);
-        }
+        // if (prescription.details) {
+        //   doc
+        //     .fontSize(12)
+        //     .font(assetsDir + '/fonts/IBMPlexSans-Regular.ttf')
+        //     .fillColor('#666666')
+        //     .text(`${prescription.details}`, margin + 15)
+        //     .moveDown(0.8);
+        // }
 
         if (doc.y > doc.page.height - 150) {
           pageBreak();
