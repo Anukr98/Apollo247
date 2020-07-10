1d738271a9e59e70abaec127b55a35b18b68c9bc
hithaprani <hithaprani.vakkanti@popcornapps.com>
2020-07-10 11:55:09 +0530

    QA 1.0207

--- packages/mobile-doctors/src/helpers/AppConfig.ts
+++ packages/mobile-doctors/src/helpers/AppConfig.ts
@@ -36,8 +36,8 @@ const ConfigurationDev = {
 const ConfigurationQA = {
   ...commonConfigs,
   LOG_ENVIRONMENT: 'release',
-  iOS_Version: '1.0206',
-  Android_Version: '1.0206',
+  iOS_Version: '1.0207',
+  Android_Version: '1.0207',
 };
 
 //Prod
