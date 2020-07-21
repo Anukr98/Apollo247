153bef0fb2f5f8110aaf9c12c62b314461154ba0
Bhuvan <bhuvanachandra_r@apollo247.org>
2020-07-21 11:06:38 +0530

    One Apollo Redemption

--- packages/mobile-patients/src/strings/AppConfig.ts
+++ packages/mobile-patients/src/strings/AppConfig.ts
@@ -19,7 +19,7 @@ export enum AppEnv {
   DEVReplica = 'DEVReplica',
 }
 
-const APP_ENV: AppEnv = AppEnv.PROD as AppEnv; // For respective API environments in the app.
+const APP_ENV: AppEnv = AppEnv.DEV as AppEnv; //Change to AppEnv.(DEV, QA, PROD) for respective API environments in the app. Also don't forget to change src/helpers/apiRoutes.ts
 
 const pharmaToken201 = 'Bearer 2o1kd4bjapqifpb27fy7tnbivu8bqo1d';
 const pharmaTokenYXV = 'YXV0aF91c2VyOnN1cGVyc2VjcmV0X3Rhd';
