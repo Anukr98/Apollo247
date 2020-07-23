10d7f6d099fb869f7f3e5555903919c8c437e959
Ankit Bisht <>
2020-07-23 13:18:26 +0530

    APP-3074: resolution set to 640x480 in OTPublisher at doctor side

--- packages/mobile-doctors/src/components/Chat/AudioVideoCotext.tsx
+++ packages/mobile-doctors/src/components/Chat/AudioVideoCotext.tsx
@@ -748,6 +748,7 @@ export const AudioVideoProvider: React.FC = (props) => {
                 audioTrack: audioEnabled,
                 audioVolume: 100,
                 name: name,
+                resolution: '640x480',
               }}
               eventHandlers={publisherEventHandlers}
             />
