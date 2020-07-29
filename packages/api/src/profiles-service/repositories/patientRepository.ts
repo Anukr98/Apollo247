0a1cdacef4ef12060ff0e1f8a418ab30bc0f8838
kishore1990 <kishore.s@apollo247.org>
2020-07-29 16:11:36 +0530

    APP-3352 fix linked patient ids

--- packages/api/src/profiles-service/repositories/patientRepository.ts
+++ packages/api/src/profiles-service/repositories/patientRepository.ts
@@ -565,7 +565,7 @@ export class PatientRepository extends Repository<Patient> {
         });
       }
     } else {
-      primaryPatientIds.push(patientId);
+      primaryPatientIds.push(patientDetails.id);
     }
     return primaryPatientIds;
   }
