a9ec1fb44495944a89a8611ae3a9af65a1d51b3c
sankeeth <sankeerth.obulam@popcornapps.com>
2020-07-30 10:35:44 +0530

    APP-3328: added trycatch

--- packages/mobile-patients/src/components/ConsultRoom/ConsultOverlay.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultOverlay.tsx
@@ -447,15 +447,18 @@ export const ConsultOverlay: React.FC<ConsultOverlayProps> = (props) => {
           }
           console.log('apptmt', apptmt);
           // If amount is zero don't redirect to PG
-          if (props.callSaveSearch !== 'true') {
-            saveSearchDoctor(client, props.doctor ? props.doctor.id : '', props.patientId);
 
-            saveSearchSpeciality(
-              client,
-              props.doctor && props.doctor.specialty && props.doctor.specialty.id,
-              props.patientId
-            );
-          }
+          try {
+            if (props.callSaveSearch !== 'true') {
+              saveSearchDoctor(client, props.doctor ? props.doctor.id : '', props.patientId);
+
+              saveSearchSpeciality(
+                client,
+                props.doctor && props.doctor.specialty && props.doctor.specialty.id,
+                props.patientId
+              );
+            }
+          } catch (error) {}
 
           makePayment(
             g(apptmt, 'id')!,
