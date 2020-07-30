0ba354cf51f4c299afcce294ffbba0c41b70443f
Piyush Arora <piyush.bulandi@gmail.com>
2020-07-30 13:54:30 +0530

    APP-3404 passing uhid in CustomerId

--- packages/api/src/profiles-service/resolvers/oneApollo.ts
+++ packages/api/src/profiles-service/resolvers/oneApollo.ts
@@ -77,7 +77,7 @@ const getOneApolloUser: Resolver<
         MobileNumber: mobNumberIN,
         Gender: patient.gender,
         StoreCode: storeCode,
-        CustomerId: mobNumberIN,
+        CustomerId: patient.uhid,
       });
       if (userCreateResponse.Success) {
         response = await oneApollo.getOneApolloUser(mobNumberIN);
