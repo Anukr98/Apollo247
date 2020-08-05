43ef20a406c5aa5716f2ed98f3ec94296959da53
Ramarao <ramarao.jaligama@popcornapps.com>
2020-08-05 10:15:59 +0530

    APP-3398 Appointments page

--- packages/web-patients/src/components/AppContainer.tsx
+++ packages/web-patients/src/components/AppContainer.tsx
@@ -21,6 +21,7 @@ import { MedicineLanding } from 'components/Medicine/MedicineLanding';
 import { ViewAllBrands } from 'components/Medicine/ViewAllBrands';
 import { SearchByBrand } from 'components/Medicine/SearchByBrand';
 import { Appointments } from 'components/ConsultRoom/Appointments';
+import { AppointmentsList } from 'components/Consult/V2/AppointmentsList';
 import { ChatRoom } from 'components/ChatRoom/ChatRoom';
 import { PrescriptionsLanding } from 'components/Prescriptions/PrescriptionsLanding';
 import { MyAccount } from 'components/MyAccount/MyAccount';
@@ -179,6 +180,7 @@ const App: React.FC = () => {
             component={PrescriptionsLanding}
           />
           <AuthRouted exact path={clientRoutes.appointments()} component={Appointments} />
+          <AuthRouted exact path={clientRoutes.appointmentsList()} component={AppointmentsList} />
           <AuthRouted exact path={clientRoutes.appointmentSuccess()} component={Appointments} />
           <AuthRouted
             exact
--- packages/web-patients/src/helpers/commonHelpers.ts
+++ packages/web-patients/src/helpers/commonHelpers.ts
@@ -45,6 +45,7 @@ const locationRoutesBlackList: string[] = [
   '/address-book',
   '/aboutUs',
   '/appointments',
+  '/appointmentslist',
   '/faq',
   '/needHelp',
   '/my-payments',
@@ -63,8 +64,8 @@ const getDeviceType = (): DEVICETYPE => {
     return /Android/i.test(userAgent)
       ? DEVICETYPE.ANDROID
       : /iPhone/i.test(userAgent)
-      ? DEVICETYPE.IOS
-      : null;
+        ? DEVICETYPE.IOS
+        : null;
   } else {
     return DEVICETYPE.DESKTOP;
   }
@@ -367,7 +368,7 @@ const getAvailability = (nextAvailability: string, differenceInMinutes: number, 
   } else if (isAvailableAfterTomorrow) {
     return `${message} in ${
       nextAvailabilityMoment.diff(tomorrowAvailabilityTime, 'days') + 1 // intentionally added + 1 as we need to consider 6 am as next day
-    } days`;
+      } days`;
   } else if (!isAvailableTomorrow && differenceInMinutes >= 120) {
     return `${message} at ${nextAvailabilityMoment.format('hh:mm A')}`;
   } else {
@@ -375,7 +376,6 @@ const getAvailability = (nextAvailability: string, differenceInMinutes: number, 
   }
 };
 
- 
 const isActualUser = () => {
   const botPattern =
     '(googlebot/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
@@ -383,10 +383,9 @@ const isActualUser = () => {
   const userAgent = navigator.userAgent;
   return !re.test(userAgent);
 }
- 
 const getStoreName = (storeAddress: string) => {
   const store = JSON.parse(storeAddress);
-  return store && store.storename ? _upperFirst(_lowerCase(store.storename)) : ''; 
+  return store && store.storename ? _upperFirst(_lowerCase(store.storename)) : '';
 };
 
 export {
