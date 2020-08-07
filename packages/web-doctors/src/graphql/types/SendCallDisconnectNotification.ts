97b582cd68d3db3d68e5430ee8765e9a8b3377c8
Sawan Kumar <sawankumar@apollo247.org>
2020-08-07 15:34:30 +0530

    sendCallDisconnectNotification api added on call disconnect before answer

---
+++ packages/web-doctors/src/graphql/types/SendCallDisconnectNotification.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { APPT_CALL_TYPE } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: SendCallDisconnectNotification
+// ====================================================
+
+export interface SendCallDisconnectNotification_sendCallDisconnectNotification {
+  __typename: "EndCallResult";
+  status: boolean;
+}
+
+export interface SendCallDisconnectNotification {
+  sendCallDisconnectNotification: SendCallDisconnectNotification_sendCallDisconnectNotification;
+}
+
+export interface SendCallDisconnectNotificationVariables {
+  appointmentId: string;
+  callType: APPT_CALL_TYPE;
+}
