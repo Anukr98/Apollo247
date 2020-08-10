84da5cc1196292dfce0f2c87540fad07d0091222
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2020-08-10 16:02:56 +0530

    APP-3525: initial commit, graphql query of getMedicineOrderOMSDetailsWithAddress

--- packages/mobile-patients/src/graphql/profiles.ts
+++ packages/mobile-patients/src/graphql/profiles.ts
@@ -1457,6 +1457,114 @@ export const UPDATE_DIAGNOSTIC_ORDER = gql`
   }
 `;
 
+
+export const GET_MEDICINE_ORDER_OMS_DETAILS_WITH_ADDRESS = gql`
+  query getMedicineOrderOMSDetailsWithAddress($patientId: String, $orderAutoId: Int, $billNumber: String) {
+    getMedicineOrderOMSDetailsWithAddress(
+      patientId: $patientId
+      orderAutoId: $orderAutoId
+      billNumber: $billNumber
+    ) {
+      medicineOrderDetails {
+        id
+        createdDate
+        orderAutoId
+        billNumber
+        coupon
+        devliveryCharges
+        prismPrescriptionFileId
+        couponDiscount
+        productDiscount
+        redeemedAmount
+        estimatedAmount
+        prescriptionImageUrl
+        orderTat
+        orderType
+        shopAddress
+        packagingCharges
+        deliveryType
+        currentStatus
+        patientAddressId
+        alertStore
+        medicineOrderLineItems {
+          medicineSKU
+          medicineName
+          price
+          mrp
+          quantity
+          isMedicine
+          mou
+          isPrescriptionNeeded
+        }
+        medicineOrderPayments {
+          id
+          paymentType
+          amountPaid
+          paymentRefId
+          paymentStatus
+          paymentDateTime
+          responseCode
+          responseMessage
+          bankTxnId
+        }
+        medicineOrdersStatus {
+          id
+          orderStatus
+          statusDate
+          hideStatus
+          statusMessage
+          customReason
+        }
+        medicineOrderShipments {
+          id
+          siteId
+          siteName
+          apOrderNo
+          updatedDate
+          currentStatus
+          itemDetails
+          medicineOrdersStatus {
+            id
+            orderStatus
+            statusDate
+            hideStatus
+          }
+          medicineOrderInvoice {
+            id
+            siteId
+            remarks
+            requestType
+            vendorName
+            billDetails
+            itemDetails
+          }
+        }
+        patient {
+          mobileNumber
+          id
+          firstName
+          lastName
+          addressList {
+            id
+            addressLine1
+            addressLine2
+            city
+            state
+            zipcode
+          }
+        }
+        medicineOrderAddress {
+          addressLine1
+          addressLine2
+          city
+          state
+          zipcode
+        }
+      }
+    }
+  }
+`;
+
 export const GET_MEDICINE_ORDER_OMS_DETAILS = gql`
   query getMedicineOrderOMSDetails($patientId: String, $orderAutoId: Int, $billNumber: String) {
     getMedicineOrderOMSDetails(
