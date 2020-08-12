0ad142e4faeda36bbfd657a46131d054c960cb9e
Piyush Arora <piyush.bulandi@gmail.com>
2020-08-12 10:33:03 +0530

    APP-3626 removed medcineOrderCancelled from pharmaUpdateOrderStatus

--- packages/api/src/profiles-service/resolvers/pharmaUpdateOrderStatus.ts
+++ packages/api/src/profiles-service/resolvers/pharmaUpdateOrderStatus.ts
@@ -24,7 +24,6 @@ import { AphError } from 'AphError';
 import { AphErrorMessages } from '@aph/universal/dist/AphErrorMessages';
 import {
   NotificationType,
-  medicineOrderCancelled,
   sendMedicineOrderStatusNotification,
 } from 'notifications-service/resolvers/notifications';
 import { format, addMinutes, parseISO } from 'date-fns';
@@ -167,8 +166,13 @@ const updateOrderStatus: Resolver<
       statusMessage: updateOrderStatusInput.reasonCode,
     };
     await medicineOrdersRepo.saveMedicineOrderStatus(orderStatusAttrs, orderDetails.orderAutoId);
-  //  medicineOrderCancelled(orderDetails, updateOrderStatusInput.reasonCode, profilesDb);
-    calculateRefund(orderDetails, 0, profilesDb, medicineOrdersRepo,updateOrderStatusInput.reasonCode);
+    calculateRefund(
+      orderDetails,
+      0,
+      profilesDb,
+      medicineOrdersRepo,
+      updateOrderStatusInput.reasonCode
+    );
   }
 
   if (
@@ -345,8 +349,6 @@ const updateOrderStatus: Resolver<
         postEvent(postBody);
       }
       if (status == MEDICINE_ORDER_STATUS.CANCELLED) {
-        //medicineOrderCancelled(orderDetails, updateOrderStatusInput.reasonCode, profilesDb);
-
         //post order cancelled event to webEngage
         const postBody: Partial<WebEngageInput> = {
           userId: orderDetails.patient.mobileNumber,
@@ -377,7 +379,13 @@ const updateOrderStatus: Resolver<
         },
         0
       );
-      calculateRefund(orderDetails, totalOrderBilling, profilesDb, medicineOrdersRepo,updateOrderStatusInput.reasonCode);
+      calculateRefund(
+        orderDetails,
+        totalOrderBilling,
+        profilesDb,
+        medicineOrdersRepo,
+        updateOrderStatusInput.reasonCode
+      );
     }
   }
 
