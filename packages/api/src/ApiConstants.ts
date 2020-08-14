f0c28cd5356d9eae84cdcfb5ee0e3294a205d9c0
Naman <naman.gupta@apollo247.org>
2020-08-14 18:27:41 +0530

    APP-3453 removed unused references which we are comfortable with until we have a service account

--- packages/api/src/ApiConstants.ts
+++ packages/api/src/ApiConstants.ts
@@ -189,7 +189,6 @@ export enum ApiConstants {
 
   //medicine COD email configuration starts
   MEDICINE_SUPPORT_EMAILID = 'sushma.voleti@popcornapps.com',
-  MEDICINE_SUPPORT_CC_EMAILID = 'sumeeth.kumar@popcornapps.com,',
   MEDICINE_SUPPORT_EMAILID_PRODUCTION = 'hd.chennai@apollopharmacy.org',
   MEDICINE_SUPPORT_CC_EMAILID_PRODUCTION = '',
   //medicine COD email configuration ends
@@ -246,7 +245,6 @@ export enum ApiConstants {
   FROM_MAIL = 'info@popcornapps.com',
   APPT_MAIL_SUBJECT = 'Your appointment has been booked successfully',
   FROM_NAME = 'Admin',
-  PATIENT_APPT_EMAILID = 'sriram.kanchan@popcornapps.com',
   PATIENT_APPT_CC_EMAILID = 'chanti.reddy@popcornapps.com',
   PATIENT_APPT_CC_EMAILID_TRIGGER = 'chanti.reddy@popcornapps.com',
   PATIENT_APPT_EMAILID_PRODUCTION = 'helpdesk@apollo247.com',
