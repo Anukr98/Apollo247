52daba19f4badc770ac3940219b9174451ab72f8
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2020-08-17 14:56:36 +0530

    APP-3569: graphql types added

---
+++ packages/mobile-patients/src/graphql/types/alertMedicineOrderPickup.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { AlertMedicineOrderPickupInput } from "./globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: alertMedicineOrderPickup
+// ====================================================
+
+export interface alertMedicineOrderPickup_alertMedicineOrderPickup {
+  __typename: "AlertMedicineOrderPickupResult";
+  status: boolean | null;
+  message: string | null;
+}
+
+export interface alertMedicineOrderPickup {
+  alertMedicineOrderPickup: alertMedicineOrderPickup_alertMedicineOrderPickup;
+}
+
+export interface alertMedicineOrderPickupVariables {
+  alertMedicineOrderPickupInput?: AlertMedicineOrderPickupInput | null;
+}
---
+++ packages/mobile-patients/src/graphql/types/getPastAppointmentsCount.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: getPastAppointmentsCount
+// ====================================================
+
+export interface getPastAppointmentsCount_getPastAppointmentsCount {
+  __typename: "PastAppointmentsCountResult";
+  count: number | null;
+  completedCount: number | null;
+  yesCount: number | null;
+  noCount: number | null;
+}
+
+export interface getPastAppointmentsCount {
+  getPastAppointmentsCount: getPastAppointmentsCount_getPastAppointmentsCount;
+}
+
+export interface getPastAppointmentsCountVariables {
+  doctorId: string;
+  patientId: string;
+  appointmentId: string;
+}
