f7e9685c69db16fb1d5ab39b9b6042e7d35d8b5f
Ashish Anand <ashishanand@Ashishs-MacBook-Pro.local>
2020-08-20 19:07:14 +0530

    APP-3502

--- packages/web-doctors/src/components/case-sheet/panels/FollowUp.tsx
+++ packages/web-doctors/src/components/case-sheet/panels/FollowUp.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { AphTextField, AphCustomDropdown } from '@aph/web-ui-components';
 import { MenuItem } from '@material-ui/core';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import { isValid } from 'date-fns/esm';
 const useStyles = makeStyles(() => ({
   header: {
     fontStyle: 'normal',
@@ -35,16 +36,45 @@ const useStyles = makeStyles(() => ({
     color: '#00B38E',
     marginTop: 20,
   },
+  infoErrorWrapper: {
+    display: 'flex',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 12,
+    lineHeight: '16px',
+    letterSpacing: 1,
+    color: 'red',
+    marginTop: 20,
+  },
 }));
 
 export const FollowUp = () => {
   const classes = useStyles({});
+  const [value, setValue] = useState(null);
+  const [errorType, setErrorType] = useState('');
+  const minError = 'Value cannot be less than default value';
+  const maxError = 'Value cannot be more than 30 days';
+
   return (
     <div>
       <span className={classes.header}> {'Set your patient follow up chat days limit.'}</span>
       <br />
-      <AphTextField type={'number'} className={classes.followUpInput} />
+      <AphTextField
+        type={'number'}
+        className={classes.followUpInput}
+        value={value}
+        onChange={(e) => {
+          let val = e.target.value;
+          let intValue = parseInt(val, 10);
+          if (val === '' || (intValue >= 0 && intValue <= 30)) setValue(val);
+        }}
+        onBlur={(e) => {
+          if (e.target.value === '') setValue(0);
+        }}
+      />
+
       <span className={classes.daysLabel}>{'Days'}</span>
+      {/* <span>{errorType === 'min' && minError}</span> */}
       <span className={classes.infoWrapper}>
         <InfoOutlinedIcon
           style={{
