d1775e3c924bf6a4c0e22fd5deea4e9d0805fe20
akgp <ajay.k@apollo247.org>
2020-08-21 18:22:35 +0530

    ormconfig changes for run revert

--- packages/api/ormconfig.ts
+++ packages/api/ormconfig.ts
@@ -1,21 +1,41 @@
-
 module.exports = [
-    {
-        "name": 'consults-db',
-        "type": "postgres",
-        "cli": {
-            "migrationsDir": "src/consults-service/database/migration"
-        },
+  {
+    name: 'consults-db',
+    type: 'postgres',
+    host: 'localhost',
+    port: 5437,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'consults_local',
+    migrations: ['dist/migration/consults/*.js'],
+    cli: {
+      migrationsDir: 'src/consults-service/database/migration',
     },
-    {
-        "name": 'doctors-db',
-        "cli": {
-            "migrationsDir": "src/doctors-service/database/migration"
-        }
+  },
+  {
+    name: 'doctors-db',
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'doctors_local',
+    migrations: ['dist/migration/doctors/*.js'],
+    cli: {
+      migrationsDir: 'src/doctors-service/database/migration',
     },
-    {
-        "name": 'profiles-db',
-        "cli": {
-            "migrationsDir": "src/profiles-service/database/migration"
-        }
-    }]
+  },
+  {
+    name: 'profiles-db',
+    type: 'postgres',
+    host: 'profiles-db',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'profiles_local',
+    migrations: ['dist/migration/profiles/*.js'],
+    cli: {
+      migrationsDir: 'src/profiles-service/database/migration',
+    },
+  },
+];
