8dd4e1c9b70484c03be967f59c81509ee54a2336
Ankit Bisht <>
2020-08-25 11:16:48 +0530

    APP-4019: Rolling back

--- packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
@@ -833,23 +833,26 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
   }, [voipDeviceToken])
 
   const callVoipDeviceTokenAPI = async () => {
-    const input = {
-      patientId: currentPatient ? currentPatient.id : '',
-      voipToken: voipDeviceToken
+    const asyncVoipToken = await AsyncStorage.getItem(LocalStrings.voipDeviceToken);
+    const parsedToken = asyncVoipToken && JSON.parse(asyncVoipToken);
+    if (voipDeviceToken !== parsedToken) {
+      const input = {
+        patientId: currentPatient ? currentPatient.id : '',
+        voipToken: voipDeviceToken
+      }
+      client.mutate<addVoipPushToken, addVoipPushTokenVariables>({
+        mutation: SAVE_VOIP_DEVICE_TOKEN,
+        variables: {
+          voipPushTokenInput: input
+        },
+        fetchPolicy: 'no-cache'
+      }).then((data: any) => {
+        AsyncStorage.setItem(LocalStrings.voipDeviceToken, JSON.stringify(voipDeviceToken));
+      }).catch((e) => {
+        CommonBugFender('ConsultRoom_callDeviceVoipTokenAPI', e);
+        console.log('Error occured while sending voip token', e);
+      })
     }
-
-    client.mutate<addVoipPushToken, addVoipPushTokenVariables>({
-      mutation: SAVE_VOIP_DEVICE_TOKEN,
-      variables: {
-        voipPushTokenInput: input
-      },
-      fetchPolicy: 'no-cache'
-    }).then((data: any) => {
-      console.log("voip data", data)
-    }).catch((e) => {
-      CommonBugFender('ConsultRoom_callDeviceVoipTokenAPI', e);
-      console.log('Error occured while sending voip token', e);
-    })
   }
 
   const getTokenforCM = async () => {
