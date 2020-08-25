5061aa8f94582957b0ea848f5502b810cd144ed2
Mohd Asad <mohd.asad@apollo247.org>
2020-08-25 11:43:39 +0530

    migrations-added

--- packages/api/src/consults-service/database/migration/1596692647718-ConsultQueueItem.ts
+++ packages/api/src/consults-service/database/migration/1596692647718-ConsultQueueItem.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 export class ConsultQueueItem1596692647711 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE consult_queue_item ALTER COLUMN "appointmentId" SET DATA TYPE UUID USING (uuid_generate_v4())`);
+        await queryRunner.query(`ALTER TABLE consult_queue_item ALTER COLUMN "appointmentId" SET DATA TYPE UUID`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
