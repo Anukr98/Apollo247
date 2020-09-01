ca8cd0b9d466b4bea6df906b4501441c4f87383f
akgp <ajay.k@apollo247.org>
2020-09-01 17:36:32 +0530

    review changes for create and update date

--- packages/api/src/doctors-service/database/migration/1598957231678-APP-3739-DoctorHelpLineTable.ts
+++ packages/api/src/doctors-service/database/migration/1598957231678-APP-3739-DoctorHelpLineTable.ts
@@ -2,10 +2,16 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class APP3739DoctorHelpLineTable1598957231678 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TABLE  doctor_help_line(
-      id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-      "doctorType" character varying,
-      "mobileNumber" character varying)`);
+    await queryRunner.query(
+      `CREATE TABLE "doctor_help_line" (
+        "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
+        "doctorType" character varying NOT NULL, 
+        "mobileNumber" character varying NOT NULL, 
+        "createdDate" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+        "updatedAt" TIMESTAMP, 
+        CONSTRAINT "UQ_92aa603f88b1d2cde2655cf0052" UNIQUE ("doctorType"), 
+        CONSTRAINT "PK_ca18038443917b4ae6c1a875735" PRIMARY KEY ("id"))`
+    );
     await queryRunner.query(`INSERT INTO doctor_help_line ("doctorType", "mobileNumber") values 
     ('APOLLO', '04048217273'),
     ('APOLLO_HOMECARE', '04048217273'),
