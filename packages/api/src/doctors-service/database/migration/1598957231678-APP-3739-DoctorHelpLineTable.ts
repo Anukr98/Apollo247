0a25d6ba9a0ef79ab4690e7b8da210baefb09228
akgp <ajay.k@apollo247.org>
2020-09-01 16:44:11 +0530

    migration added for doctor help line numbers APP-3739

---
+++ packages/api/src/doctors-service/database/migration/1598957231678-APP-3739-DoctorHelpLineTable.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class APP3739DoctorHelpLineTable1598957231678 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE  doctor_help_line(
+      id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+      "doctorType" character varying,
+      "mobileNumber" character varying)`);
+    await queryRunner.query(`INSERT INTO doctor_help_line ("doctorType", "mobileNumber") values 
+    ('APOLLO', '04048217273'),
+    ('APOLLO_HOMECARE', '04048217273'),
+    ('CLINIC', '04048217273'),
+    ('CRADLE', '04048217273'),
+    ('FERTILITY', '04048217273'),
+    ('PAYROLL', '04048217273'),
+    ('STAR_APOLLO', '04048217273'),
+    ('SPECTRA', '04048217273'),
+    ('SUGAR', '04048217273'),
+    ('WHITE_DENTAL', '04048217273'),
+    ('DOCTOR_CONNECT', '04045208102');`);
+  }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "doctorHelpLine"`);
+  }
+}
