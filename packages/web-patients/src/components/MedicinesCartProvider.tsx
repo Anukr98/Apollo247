329c2001162e86af2e6c1ffd583d61df21be29b0
yaswanthkoneri <yaswanth.k@apollo247.org>
2020-09-11 17:17:38 +0530

    Product disc = MRP on cart fix

--- packages/web-patients/src/components/MedicinesCartProvider.tsx
+++ packages/web-patients/src/components/MedicinesCartProvider.tsx
@@ -80,11 +80,11 @@ export interface MedicineCartContextProps {
   removeCartItems: ((itemId: MedicineCartItem['arrSku']) => void) | null;
   removeFreeCartItems: (() => void) | null;
   updateCartItem:
-    | ((itemUpdates: Partial<MedicineCartItem> & { id: MedicineCartItem['id'] }) => void)
-    | null;
+  | ((itemUpdates: Partial<MedicineCartItem> & { id: MedicineCartItem['id'] }) => void)
+  | null;
   updateCartItemPrice:
-    | ((itemUpdates: Partial<MedicineCartItem> & { id: MedicineCartItem['id'] }) => void)
-    | null;
+  | ((itemUpdates: Partial<MedicineCartItem> & { id: MedicineCartItem['id'] }) => void)
+  | null;
   updateCartItemQty: ((item: MedicineCartItem) => void) | null;
   cartTotal: number;
   storePickupPincode: string | null;
@@ -97,8 +97,8 @@ export interface MedicineCartContextProps {
   setStoreAddressId: ((deliveryAddressId: string) => void) | null;
   deliveryAddresses: GetPatientAddressList_getPatientAddressList_addressList[];
   setDeliveryAddresses:
-    | ((deliveryAddresses: GetPatientAddressList_getPatientAddressList_addressList[]) => void)
-    | null;
+  | ((deliveryAddresses: GetPatientAddressList_getPatientAddressList_addressList[]) => void)
+  | null;
   clearCartInfo: (() => void) | null;
   addMultipleCartItems: ((items: MedicineCartItem[]) => void) | null;
   prescriptions: PrescriptionFormat[] | null;
@@ -471,7 +471,7 @@ export const MedicinesCartProvider: React.FC = (props) => {
   const cartTotal: MedicineCartContextProps['cartTotal'] = parseFloat(
     cartItems
       .reduce((currTotal, currItem) => {
-        if (currItem.special_price == 0) {
+        if (currItem.special_price == 0 && currItem.couponFree) {
           return currTotal + currItem.quantity * currItem.special_price;
         } else {
           return currTotal + currItem.quantity * (Number(currItem.special_price) || currItem.price);
