c2a02b7a648d4a0b0b9080d3c2ff6f1e1fe6da25
akgp <ajay.k@apollo247.org>
2020-09-15 21:40:57 +0530

    query in one call

--- packages/api/src/consults-service/database/migration/1600167902800-APP-3469.ts
+++ packages/api/src/consults-service/database/migration/1600167902800-APP-3469.ts
@@ -2,12 +2,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class APP34691600167902800 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "case_sheet" ADD "clinicalObservationNotes" text`);
-    await queryRunner.query(`ALTER TABLE "case_sheet" ADD "diagonasticTestResult" text`);
+    await queryRunner.query(
+      `ALTER TABLE "case_sheet" ADD "clinicalObservationNotes" text, ADD "diagonasticTestResult" text`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "case_sheet" DROP COLUMN "clinicalObservationNotes"`);
-    await queryRunner.query(`ALTER TABLE "case_sheet" DROP COLUMN "diagonasticTestResult"`);
+    await queryRunner.query(
+      `ALTER TABLE "case_sheet" DROP COLUMN "clinicalObservationNotes", DROP COLUMN "diagonasticTestResult"`
+    );
   }
 }
