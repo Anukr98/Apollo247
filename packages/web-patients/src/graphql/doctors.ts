b36e5240455e4bb8007d1031e25524b28b89096b
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2020-09-16 11:02:58 +0530

    added chat days on doctor details page

--- packages/web-patients/src/graphql/doctors.ts
+++ packages/web-patients/src/graphql/doctors.ts
@@ -22,6 +22,7 @@ export const GET_DOCTOR_DETAILS_BY_ID = gql`
       physicalConsultationFees
       qualification
       doctorType
+      chatDays
       starTeam {
         associatedDoctor {
           fullName
