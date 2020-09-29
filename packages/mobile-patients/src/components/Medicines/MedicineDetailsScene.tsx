e4ffd391de7bbe44d41f579737636e780b346507
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2020-09-29 20:54:37 +0530

    APP-4557: fire event one time on page load

--- packages/mobile-patients/src/components/Medicines/MedicineDetailsScene.tsx
+++ packages/mobile-patients/src/components/Medicines/MedicineDetailsScene.tsx
@@ -356,6 +356,19 @@ export const MedicineDetailsScene: React.FC<MedicineDetailsSceneProps> = (props)
         const productDetails = g(data, 'productdp', '0' as any);
         if (productDetails) {
           setmedicineDetails(productDetails || {});
+          const showOutOfStockView = productDetails?.sell_online
+            ? (!showDeliverySpinner && !deliveryTime) || deliveryError || isOutOfStock
+            : false;
+          if (typeof movedFrom !== 'undefined' && typeof isOutOfStock !== 'undefined') {
+            // webengage event when page is opened from different sources
+            const eventAttributes: WebEngageEvents[WebEngageEventName.PRODUCT_PAGE_VIEWED] = {
+              source: movedFrom,
+              ProductId: sku,
+              ProductName: medicineName,
+              'Stock availability': showOutOfStockView ? 'No' : 'Yes',
+            };
+            postWebEngageEvent(WebEngageEventName.PRODUCT_PAGE_VIEWED, eventAttributes);
+          }
           trackTagalysViewEvent(productDetails);
           if (_deliveryError) {
             setTimeout(() => {
@@ -607,17 +620,6 @@ export const MedicineDetailsScene: React.FC<MedicineDetailsSceneProps> = (props)
     const showOutOfStockView = medicineDetails?.sell_online
       ? (!showDeliverySpinner && !deliveryTime) || deliveryError || isOutOfStock
       : false;
-
-    if (typeof movedFrom !== 'undefined' && typeof isOutOfStock !== 'undefined') {
-      // webengage event when page is opened from different sources
-      const eventAttributes: WebEngageEvents[WebEngageEventName.PRODUCT_PAGE_VIEWED] = {
-        source: movedFrom,
-        ProductId: sku,
-        ProductName: medicineName,
-        'Stock availability': showOutOfStockView ? 'No' : 'Yes',
-      };
-      postWebEngageEvent(WebEngageEventName.PRODUCT_PAGE_VIEWED, eventAttributes);
-    }
 
     return (
       notServiceable ? null :
