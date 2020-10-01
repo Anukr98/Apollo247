53c18b76436c19de5f0e92cfb4092b942ec45374
Bhuvan <bhuvanachandra12342@gmail.com>
2020-10-01 12:19:22 +0530

    APP-4901: Delivery fee fix

--- packages/mobile-patients/src/components/ShoppingCartProvider.tsx
+++ packages/mobile-patients/src/components/ShoppingCartProvider.tsx
@@ -376,12 +376,9 @@ export const ShoppingCartProvider: React.FC = (props) => {
   );
 
   const deliveryCharges =
-    !deliveryType || deliveryType == MEDICINE_DELIVERY_TYPE.STORE_PICKUP
-      ? 0
-      : deliveryType == MEDICINE_DELIVERY_TYPE.HOME_DELIVERY &&
-        cartTotal > 0 &&
-        cartTotal - productDiscount - couponDiscount <
-          AppConfig.Configuration.MIN_CART_VALUE_FOR_FREE_DELIVERY
+    cartTotal > 0 &&
+    cartTotal - productDiscount - couponDiscount <
+      AppConfig.Configuration.MIN_CART_VALUE_FOR_FREE_DELIVERY
       ? AppConfig.Configuration.DELIVERY_CHARGES
       : 0;
 
