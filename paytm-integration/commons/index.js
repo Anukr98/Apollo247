ca0d6829cfbfb7c54e20debd00bd77a686ba9a31
Shrey Saroch <shrey.s@apollo247.org>
2020-10-06 11:03:56 +0530

    removal of unused code

--- paytm-integration/commons/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.getAddressDetails = require('./getAddressDetails').getAddressDetails;
-module.exports.getCache = require('./connectRedis').getCache;
-module.exports.CreateUserSubscription = require('./exotel').CreateUserSubscription;
-module.exports.getCallDetails = require('./exotel').getCallDetails;
