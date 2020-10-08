132a571d413fd5022a92ea15b960a63e4383f466
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2020-10-08 17:21:27 +0530

    APP-4604: upload prescription event correction

--- packages/mobile-patients/src/components/Consult/ChatRoom.tsx
+++ packages/mobile-patients/src/components/Consult/ChatRoom.tsx
@@ -6523,6 +6523,7 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   const uploadPrescriptionPopup = () => {
     return (
       <UploadPrescriprionPopup
+        type={'nonCartFlow'}
         heading="Attach File(s)"
         instructionHeading="Instructions For Uploading Files"
         instructions={[
