43ccc9b1885d9caa5df9998bf613b4af01e96b2f
Anup PurushottamKemaye <anup.purushottam@popcornapps.com>
2020-10-09 10:41:54 +0530

    APP-4911 implementation
    APP-4911 implementation

--- packages/web-doctors/src/graphql/consults.ts
+++ packages/web-doctors/src/graphql/consults.ts
@@ -155,11 +155,13 @@ export const END_CALL_NOTIFICATION = gql`
     $appointmentCallId: String
     $patientId: String
     $numberOfParticipants: Int
+    $endVoipCall: Boolean
   ) {
     endCallNotification(
       appointmentCallId: $appointmentCallId
       patientId: $patientId
       numberOfParticipants: $numberOfParticipants
+      endVoipCall: $endVoipCall
     ) {
       status
     }
