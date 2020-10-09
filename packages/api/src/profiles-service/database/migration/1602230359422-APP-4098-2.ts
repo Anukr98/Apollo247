78810a0f2931b90041dd829bc79a395efce6ba81
saurabh0119 <64277172+saurabh0119@users.noreply.github.com>
2020-10-09 16:35:19 +0530

    Update 1602230359422-APP-4098-2.ts

--- packages/api/src/profiles-service/database/migration/1602230359422-APP-4098-2.ts
+++ packages/api/src/profiles-service/database/migration/1602230359422-APP-4098-2.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class APP409821602230359422 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "medicine_order_shipments" ALTER COLUMN "updatedDate" TIMESTAMP default CURRENT_TIMESTAMP;`
+      `ALTER TABLE "medicine_order_shipments" ALTER COLUMN "updatedDate" SET default CURRENT_TIMESTAMP;`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "medicine_order_shipments" ALTER COLUMN "updatedDate" TIMESTAMP default NULL;`
+      `ALTER TABLE "medicine_order_shipments" ALTER COLUMN "updatedDate" SET default NULL;`
     );
   }
 }
