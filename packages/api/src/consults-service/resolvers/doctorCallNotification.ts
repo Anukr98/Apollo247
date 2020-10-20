cd51f5641ac65dcba3c6be37d3b85092dff842d2
Mohd Asad <mohd.asad@apollo247.org>
2020-10-20 10:29:22 +0530

    fix

--- packages/api/src/consults-service/resolvers/doctorCallNotification.ts
+++ packages/api/src/consults-service/resolvers/doctorCallNotification.ts
@@ -259,6 +259,23 @@ const sendCallNotification: Resolver<
       args.numberOfParticipants,
       args.patientId
     );
+
+    const pushNotificationSessionInput = {
+      appointmentId: args.appointmentId,
+      notificationType: NotificationType.INITIATE_SENIOR_APPT_SESSION,
+    };
+
+    if (args.doctorType === DOCTOR_CALL_TYPE.JUNIOR) {
+      pushNotificationSessionInput.notificationType = NotificationType.INITIATE_JUNIOR_APPT_SESSION;
+    }
+
+    const sessionNotificationResult = await sendNotification(
+      pushNotificationSessionInput,
+      patientsDb,
+      consultsDb,
+      doctorsDb
+    );
+
   } else {
     const pushNotificationInput = {
       appointmentId: args.appointmentId,
