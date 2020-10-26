3a3857fe835af7c07a619dcf5d02db8ad93958ed
vishnu-apollo247 <vishnu.r@apollo247.org>
2020-10-26 16:19:54 +0530

    APP-4941: fixed API level upgrade crash issue

---
+++ packages/mobile-patients/replaceOTFile.sh
@@ -0,0 +1,2 @@
+#replace RCTTiming.m in react-native/React/modules
+cat OTSubcriberViewManager.java > node_modules/opentok-react-native/android/src/main/java/com/opentokreactnative/OTSubcriberViewManager.java
