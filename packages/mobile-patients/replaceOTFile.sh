808bdd6e6d09a8d069c5f8c6df3a1c47e2694b2b
vishnu-apollo247 <vishnu.r@apollo247.org>
2020-10-27 10:08:15 +0530

    APP-4885: removed extra fields from event

--- packages/mobile-patients/replaceOTFile.sh
+++ packages/mobile-patients/replaceOTFile.sh
@@ -1,2 +1,1 @@
-#replace RCTTiming.m in react-native/React/modules
-cat OTSubcriberViewManager.java > node_modules/opentok-react-native/android/src/main/java/com/opentokreactnative/OTSubcriberViewManager.java
+cat OTSubscriberViewManager.java > node_modules/opentok-react-native/android/src/main/java/com/opentokreactnative/OTSubscriberViewManager.java
