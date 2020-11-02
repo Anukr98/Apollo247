cbb01226fd8089fc0fc2d22b650231c3c5a8a1f9
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2020-11-02 13:14:27 +0530

    APP-5500: doctor card click event added istopdoc attribute

--- packages/mobile-patients/src/helpers/webEngageEvents.ts
+++ packages/mobile-patients/src/helpers/webEngageEvents.ts
@@ -990,6 +990,7 @@ export interface WebEngageEvents {
     Fee: number;
     'Doctor Speciality': string;
     Rank: number;
+    Is_TopDoc?: YesOrNo;
   };
   [WebEngageEventName.DOCTOR_CARD_CONSULT_CLICK]: {
     'Patient Name': string;
