e41ce33d2edfd71f15c8414052c91276d1ab1ba9
Saifuddin <saifuddin@popcornapps.com>
2020-11-04 20:32:27 +0530

    APP-5130: code review changes

--- packages/mobile-patients/src/components/HealthRecords/AddRecord.tsx
+++ packages/mobile-patients/src/components/HealthRecords/AddRecord.tsx
@@ -326,7 +326,7 @@ export const AddRecord: React.FC<AddRecordProps> = (props) => {
   };
 
   const isValid = () => {
-    const validRecordDetails1 = recordType && dateOfTest && testName && docName ? true : false;
+    const validRecordDetails1 = recordType && dateOfTest && testName && docName;
     const valid = isTestRecordParameterFilled().map((item) => {
       return {
         maxmin: (item.maximum || item.minimum) && item.maximum! > item.minimum!,
@@ -339,14 +339,13 @@ export const AddRecord: React.FC<AddRecordProps> = (props) => {
       };
     });
 
-    let message =
-      dateOfTest === ''
-        ? 'Enter Record date'
-        : testName === ''
-        ? 'Enter Record name'
-        : docName === ''
-        ? 'Enter Record doctor’s name'
-        : '';
+    let message = !dateOfTest
+      ? 'Enter Record date'
+      : !testName
+      ? 'Enter Record name'
+      : !docName
+      ? 'Enter Record doctor’s name'
+      : '';
 
     message === '' &&
       valid.forEach((item) => {
@@ -355,7 +354,7 @@ export const AddRecord: React.FC<AddRecordProps> = (props) => {
         }
       });
 
-    const finval = validRecordDetails1 ? true : false;
+    const finval = validRecordDetails1;
 
     return {
       isvalid: finval,
@@ -392,19 +391,19 @@ export const AddRecord: React.FC<AddRecordProps> = (props) => {
         description: 'Please add document',
       });
       return false;
-    } else if (_.isEmpty(dateOfTest)) {
+    } else if (!dateOfTest) {
       showAphAlert!({
         title: 'Alert!',
         description: 'Enter Record date',
       });
       return false;
-    } else if (_.isEmpty(testName)) {
+    } else if (!testName) {
       showAphAlert!({
         title: 'Alert!',
         description: 'Enter Record name',
       });
       return false;
-    } else if (_.isEmpty(docName)) {
+    } else if (!docName) {
       showAphAlert!({
         title: 'Alert!',
         description: 'Enter Record prescribed by',
