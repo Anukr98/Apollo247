42429bc76024ea1e7eaaa13d1f8fe0eaaee6ca20
Ankit Bisht <>
2020-12-01 22:00:32 +0530

    APP-5803: willBlur on consult code reverted

--- packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
@@ -403,15 +403,6 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
   };
 
   useEffect(() => {
-    const _willBlur = props.navigation.addListener('willBlur', (payload) => {
-      setBannerData && setBannerData([]);
-    });
-    return () => {
-      _willBlur && _willBlur.remove();
-    };
-  });
-
-  useEffect(() => {
     //TODO: if deeplinks is causing issue comment handleDeepLink here and uncomment in SplashScreen useEffect
     // handleDeepLink(props.navigation);
     isserviceable();
