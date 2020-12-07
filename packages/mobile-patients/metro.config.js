73992a5d68a8ec08ce76ba9bd20a0da53cf77db0
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2020-12-07 19:21:05 +0530

    APP-3569: remove unused modules

--- packages/mobile-patients/metro.config.js
+++ packages/mobile-patients/metro.config.js
@@ -4,15 +4,6 @@
  *
  * @format
  */
-const path = require('path');
-
-const extraNodeModules = {
-  '@aph/api-schema': path.resolve(__dirname + '/../api-schema'),
-  // '@aph/mobile-ui-components': path.resolve(__dirname + '/../mobile-ui-components'),
-  // '@aph/shared-ui-components': path.resolve(__dirname + '/../shared-ui-components'),
-  '@aph/universal': path.resolve(__dirname + '/../universal'),
-};
-const watchFolders = Object.values(extraNodeModules);
 
 module.exports = {
   transformer: {
@@ -24,8 +15,4 @@ module.exports = {
       },
     }),
   },
-  resolver: {
-    extraNodeModules,
-  },
-  watchFolders,
 };
