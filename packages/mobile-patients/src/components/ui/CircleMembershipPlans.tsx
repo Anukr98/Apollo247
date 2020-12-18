7ac5e66988e40d135fe0842f177c655debd28fbd
Ankit Bisht <>
2020-12-18 23:08:41 +0530

    APP-6258: letting user to choose plan from HC

--- packages/mobile-patients/src/components/ui/CircleMembershipPlans.tsx
+++ packages/mobile-patients/src/components/ui/CircleMembershipPlans.tsx
@@ -10,12 +10,15 @@ import {
   Dimensions,
   ScrollView,
   Platform,
+  Alert,
 } from 'react-native';
-import { useAppCommonData } from '@aph/mobile-patients/src/components/AppCommonDataProvider';
 import { theme } from '@aph/mobile-patients/src/theme/theme';
 import string from '@aph/mobile-patients/src/strings/strings.json';
 import { useApolloClient } from 'react-apollo-hooks';
-import { GET_PLAN_DETAILS_BY_PLAN_ID } from '@aph/mobile-patients/src/graphql/profiles';
+import {
+  GET_PLAN_DETAILS_BY_PLAN_ID,
+  CREATE_USER_SUBSCRIPTION,
+} from '@aph/mobile-patients/src/graphql/profiles';
 import { GetPlanDetailsByPlanId } from '@aph/mobile-patients/src/graphql/types/GetPlanDetailsByPlanId';
 import { CommonBugFender } from '@aph/mobile-patients/src/FunctionHelpers/DeviceHelper';
 import { AppConfig } from '@aph/mobile-patients/src/strings/AppConfig';
@@ -34,6 +37,15 @@ import {
   WebEngageEvents,
 } from '@aph/mobile-patients/src/helpers/webEngageEvents';
 import { useAllCurrentPatients } from '@aph/mobile-patients/src/hooks/authHooks';
+import {
+  CreateUserSubscription,
+  CreateUserSubscriptionVariables,
+} from '@aph/mobile-patients/src/graphql/types/CreateUserSubscription';
+import {
+  one_apollo_store_code,
+  PaymentStatus,
+} from '@aph/mobile-patients/src/graphql/types/globalTypes';
+import { useUIElements } from '@aph/mobile-patients/src/components/UIElementsProvider';
 
 const { width } = Dimensions.get('window');
 interface CircleMembershipPlansProps extends NavigationScreenProps {
@@ -50,11 +62,13 @@ interface CircleMembershipPlansProps extends NavigationScreenProps {
   buyNow?: boolean;
   source?: 'Pharma' | 'Product Detail' | 'Pharma Cart' | 'Diagnostic' | 'Consult';
   from?: string;
+  healthCredits?: number;
+  onPurchaseViaHCCallback?: (res: any) => void;
 }
 
 export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (props) => {
   const [membershipPlans, setMembershipPlans] = useState<any>(props.membershipPlans || []);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [spinning, setSpinning] = useState<boolean>(true);
   const {
     isConsultJourney,
     isDiagnosticJourney,
@@ -67,11 +81,12 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
     buyNow,
     source,
     from,
+    healthCredits,
+    onPurchaseViaHCCallback,
   } = props;
   const client = useApolloClient();
   const planId = AppConfig.Configuration.CIRCLE_PLAN_ID;
   const circleStaticMonthlySavings = AppConfig.Configuration.CIRCLE_STATIC_MONTHLY_SAVINGS;
-  const { circleSubscription } = useAppCommonData();
   const {
     circlePlanSelected,
     setCirclePlanSelected,
@@ -90,7 +105,12 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
   } = useShoppingCart();
   const { setIsDiagnosticCircleSubscription } = useDiagnosticsCart();
   const { currentPatient } = useAllCurrentPatients();
-
+  const { setLoading } = useUIElements();
+  const storeCode =
+    Platform.OS === 'ios' ? one_apollo_store_code.IOSCUS : one_apollo_store_code.ANDCUS;
+  const amountToPay = defaultCirclePlan
+    ? defaultCirclePlan?.currentSellingPrice
+    : circlePlanSelected?.currentSellingPrice;
   const planDimension = isModal ? 100 : 120;
   const defaultPlanDimension = isModal ? 130 : 120;
   const isIos = Platform.OS === 'ios';
@@ -104,12 +124,13 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
     if (!props.membershipPlans || props.membershipPlans?.length === 0) {
       fetchCarePlans();
     } else {
-      setLoading(false);
+      setSpinning(false);
       onEndApiCall && onEndApiCall();
     }
     if (buyNow && props.membershipPlans?.length > 0) {
-      selectDefaultPlan && selectDefaultPlan(props.membershipPlans);
-      setAutoCirlcePlanAdded && setAutoCirlcePlanAdded(true);
+      setDefaultCirclePlan && setDefaultCirclePlan(null);
+      // selectDefaultPlan && selectDefaultPlan(props.membershipPlans);
+      // setAutoCirlcePlanAdded && setAutoCirlcePlanAdded(true);
     }
   }, []);
 
@@ -161,10 +182,10 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
           }
         }
       }
-      setLoading(false);
+      setSpinning(false);
       onEndApiCall && onEndApiCall();
     } catch (error) {
-      setLoading(false);
+      setSpinning(false);
       onEndApiCall && onEndApiCall();
       CommonBugFender('CareSelectPlans_GetPlanDetailsByPlanId', error);
     }
@@ -378,7 +399,7 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
   const renderSubscriptionPlans = () => {
     return (
       <View style={styles.subscriptionContainer}>
-        <ContentLoader loading={loading} active containerStyles={{ marginTop: 10 }}>
+        <ContentLoader loading={spinning} active containerStyles={{ marginTop: 10 }}>
           {isModal && renderHeaderTitle()}
           <View style={isModal ? [styles.careBannerView, { paddingBottom: 5 }] : {}}>
             <View style={styles.careTextContainer}>
@@ -552,9 +573,48 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
     circleWebEngageEvent(WebEngageEventName.VC_NON_CIRCLE_REMOVES_CART);
   };
 
+  const onPurchasePlanThroughHC = async () => {
+    setLoading && setLoading(true);
+    try {
+      const res = await client.mutate<CreateUserSubscription, CreateUserSubscriptionVariables>({
+        mutation: CREATE_USER_SUBSCRIPTION,
+        variables: {
+          userSubscription: {
+            mobile_number: currentPatient?.mobileNumber,
+            plan_id: planId,
+            sub_plan_id: defaultCirclePlan
+              ? defaultCirclePlan?.subPlanId
+              : circlePlanSelected?.subPlanId,
+            storeCode,
+            FirstName: currentPatient?.firstName,
+            LastName: currentPatient?.lastName,
+            payment_reference: {
+              amount_paid: 0,
+              payment_status: PaymentStatus.TXN_SUCCESS,
+              purchase_via_HC: true,
+              HC_used: amountToPay,
+            },
+            transaction_date_time: new Date().toISOString(),
+          },
+        },
+        fetchPolicy: 'no-cache',
+      });
+      setLoading && setLoading(false);
+      if (res?.data?.CreateUserSubscription?.success) {
+        onPurchaseViaHCCallback!(res);
+      } else {
+        Alert.alert('Apollo', `${res?.data?.CreateUserSubscription?.message}`);
+      }
+    } catch (error) {
+      setLoading && setLoading(false);
+      CommonBugFender('ConsultRoom_createUserCircleSubscription', error);
+    }
+  };
+
   const renderAddToCart = () => {
     return (
       <Button
+        disabled={!defaultCirclePlan && !circlePlanSelected}
         title={
           buyNow && from !== string.banner_context.PHARMACY_HOME
             ? string.circleDoctors.upgrade
@@ -569,7 +629,14 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
           autoSelectDefaultPlan(membershipPlans);
           closeModal && closeModal();
           if (buyNow && from !== string.banner_context.PHARMACY_HOME) {
-            props.navigation.navigate(AppRoutes.CircleSubscription, { from: from, soruce: source });
+            if (healthCredits && healthCredits >= amountToPay) {
+              onPurchasePlanThroughHC();
+            } else {
+              props.navigation.navigate(AppRoutes.CircleSubscription, {
+                from: from,
+                soruce: source,
+              });
+            }
           } else {
             setDefaultCirclePlan && setDefaultCirclePlan(null);
           }
@@ -650,7 +717,7 @@ export const CircleMembershipPlans: React.FC<CircleMembershipPlansProps> = (prop
   };
   return (
     <View style={isModal ? {} : [styles.careBannerView, props.style]}>
-      {circlePlanSelected && !isModal && !autoCirlcePlanAdded && !loading && !defaultCirclePlan
+      {circlePlanSelected && !isModal && !autoCirlcePlanAdded && !spinning && !defaultCirclePlan
         ? renderCarePlanAdded()
         : renderSubscribeCareContainer()}
     </View>
