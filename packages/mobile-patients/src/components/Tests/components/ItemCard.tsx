3272bfa1015eaddfb524845dcc455ecccf2742d0
Bhavna <bhavna.c@apollo247.org>
2021-02-16 16:57:06 +0530

    APP-6267 :  removed commented code

--- packages/mobile-patients/src/components/Tests/components/ItemCard.tsx
+++ packages/mobile-patients/src/components/Tests/components/ItemCard.tsx
@@ -312,10 +312,6 @@ export const ItemCard: React.FC<ItemCardProps> = (props) => {
   }
 
   function onPress(item: any, packageCalculatedMrp: number, pricesForItem: any) {
-    // if (sourceScreen == AppRoutes.TestDetails) {
-    //   return;
-    // }
-
     const specialPrice = pricesForItem?.specialPrice!;
     const price = pricesForItem?.price!; //more than price (black)
     const circlePrice = pricesForItem?.circlePrice!;
