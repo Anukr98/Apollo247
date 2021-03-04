7c94f9d1d49ddc79b106cff74751ceb84f7daf16
Bhuvan <bhuvanachandra12342@gmail.com>
2021-03-04 15:16:07 +0530

    APP-8390: added minimum support version check to payment options

--- packages/mobile-patients/src/graphql/profiles.ts
+++ packages/mobile-patients/src/graphql/profiles.ts
@@ -4614,6 +4614,21 @@ export const GET_BANK_OPTIONS = gql`
   }
 `;
 
+export const GET_PAYMENT_METHODS = gql`
+  query getPaymentMethods($is_mobile: Boolean) {
+    getPaymentMethods(is_mobile: $is_mobile) {
+      name
+      minimum_supported_version
+      payment_methods {
+        image_url
+        payment_method_name
+        payment_method_code
+        minimum_supported_version
+      }
+    }
+  }
+`;
+
 export const CREATE_ORDER = gql`
   mutation createOrder($order_input: OrderInput) {
     createOrder(order_input: $order_input) {
