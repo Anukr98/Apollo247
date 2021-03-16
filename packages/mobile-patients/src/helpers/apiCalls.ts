3cc432179d0a000648c88fc46a7581994b7dd42b
Shikha Jangid <shikhajangid@Shikhas-MacBook-Pro.local>
2021-03-16 12:41:16 +0530

    APP-8382: serviciable api config change

--- packages/mobile-patients/src/helpers/apiCalls.ts
+++ packages/mobile-patients/src/helpers/apiCalls.ts
@@ -659,10 +659,10 @@ export const getStoreInventoryApi = (
 export const pinCodeServiceabilityApi247 = (
   pincode: string
 ): Promise<AxiosResponse<{ response: boolean }>> => {
-  const url = `${config.UATTAT_CONFIG[0]}/v2/serviceable?pincode=${pincode}`;
+  const url = `${config.SERVICEABLE_CONFIG[0]}/${pincode}`;
   return Axios.get(url, {
     headers: {
-      Authorization: config.UATTAT_CONFIG[1],
+      Authorization: config.SERVICEABLE_CONFIG[1],
     },
   });
 };
@@ -1139,7 +1139,10 @@ export const getDiagnosticTestDetails = (
   });
 };
 
-export const getDiagnosticListingWidget = (pageName: string, widgetName: string): Promise<AxiosResponse<any>> => {
+export const getDiagnosticListingWidget = (
+  pageName: string,
+  widgetName: string
+): Promise<AxiosResponse<any>> => {
   const baseurl = config.DRUPAL_CONFIG[0];
   const getWidgets = `${baseurl}/${pageName}/${widgetName}`;
   return Axios.get(getWidgets, {
