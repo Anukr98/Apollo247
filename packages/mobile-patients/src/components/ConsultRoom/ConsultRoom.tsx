34359bb8fe391e69ad9834daf41434829e630e84
bhavna-chaurasia <bhavna.c@apollo247.org>
2021-04-01 17:59:10 +0530

    APP-8737 : logout case handled

--- packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
@@ -855,7 +855,9 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
 
   //for prohealth option
   useEffect(() => {
-    checkIsProhealthActive();
+    if (currentPatient?.id) {
+      checkIsProhealthActive();
+    }
   }, [currentPatient]);
 
   //to be called only when the user lands via app launch
@@ -3568,13 +3570,13 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
               });
             } catch (e) {
               setLoading?.(false);
-              CommonBugFender('opening_ProHealthwebView_ConsultRoom', e)
+              CommonBugFender('opening_ProHealthwebView_ConsultRoom', e);
             }
           }
         });
       } catch (e) {
         setLoading?.(false);
-        CommonBugFender('regenerateJWTToken_ConsultRoom', e)
+        CommonBugFender('regenerateJWTToken_ConsultRoom', e);
       }
     }
     setLoading?.(false);
