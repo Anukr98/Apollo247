86f704faf93b6715c6c3cfb682a1b8746fda3eae
Abdul Muqeeth Taj <muqeeth.taj@popcornapps.com>
2021-04-01 13:09:14 +0530

    APP-7561: Update need help qurey

--- packages/mobile-patients/src/graphql/types/GetHelpSectionQueries.ts
+++ packages/mobile-patients/src/graphql/types/GetHelpSectionQueries.ts
@@ -7,12 +7,176 @@
 // GraphQL query operation: GetHelpSectionQueries
 // ====================================================
 
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_cta {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_ctaNonCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_ctaCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content {
+  __typename: "NeedHelpContent";
+  title: string | null;
+  text: string | null;
+  cta: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_cta | null;
+  ctaNonCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_ctaNonCircle | null;
+  ctaCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content_ctaCircle | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_cta {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_ctaNonCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_ctaCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content {
+  __typename: "NeedHelpContent";
+  title: string | null;
+  text: string | null;
+  cta: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_cta | null;
+  ctaNonCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_ctaNonCircle | null;
+  ctaCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content_ctaCircle | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_cta {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_ctaNonCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_ctaCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content {
+  __typename: "NeedHelpContent";
+  title: string | null;
+  text: string | null;
+  cta: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_cta | null;
+  ctaNonCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_ctaNonCircle | null;
+  ctaCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content_ctaCircle | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_cta {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_ctaNonCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_ctaCircle {
+  __typename: "NeedHelpQueryCta";
+  title: string | null;
+  appRoute: string | null;
+  webRoute: string | null;
+  appRouteParams: any | null;
+  webRouteParams: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content {
+  __typename: "NeedHelpContent";
+  title: string | null;
+  text: string | null;
+  cta: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_cta | null;
+  ctaNonCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_ctaNonCircle | null;
+  ctaCircle: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content_ctaCircle | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_queries {
+  __typename: "NeedHelpQuery";
+  id: string | null;
+  title: string | null;
+  nonOrderQueries: (string | null)[] | null;
+  queriesByOrderStatus: any | null;
+}
+
+export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries {
+  __typename: "NeedHelpQuery";
+  id: string | null;
+  title: string | null;
+  nonOrderQueries: (string | null)[] | null;
+  queriesByOrderStatus: any | null;
+  content: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_content | null;
+  queries: (GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries_queries | null)[] | null;
+}
+
 export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries {
   __typename: "NeedHelpQuery";
   id: string | null;
   title: string | null;
   nonOrderQueries: (string | null)[] | null;
   queriesByOrderStatus: any | null;
+  content: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_content | null;
+  queries: (GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries_queries | null)[] | null;
 }
 
 export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries {
@@ -21,6 +185,7 @@ export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_que
   title: string | null;
   nonOrderQueries: (string | null)[] | null;
   queriesByOrderStatus: any | null;
+  content: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_content | null;
   queries: (GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries_queries | null)[] | null;
 }
 
@@ -30,6 +195,7 @@ export interface GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries {
   title: string | null;
   nonOrderQueries: (string | null)[] | null;
   queriesByOrderStatus: any | null;
+  content: GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_content | null;
   queries: (GetHelpSectionQueries_getHelpSectionQueries_needHelpQueries_queries | null)[] | null;
 }
 
