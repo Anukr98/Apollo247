025a4ac4b482b0842ce69c1e87a643c2bd22b3c4
Mohammad Asad <asad.m@apollo247.org>
2021-04-09 11:33:29 +0530

    followup only for online APP-7942

--- packages/mobile-patients/src/components/ConsultRoom/DoctorDetails.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/DoctorDetails.tsx
@@ -332,6 +332,9 @@ export interface DoctorDetailsProps extends NavigationScreenProps {}
 export const DoctorDetails: React.FC<DoctorDetailsProps> = (props) => {
   const consultedWithDoctorBefore = props.navigation.getParam('consultedWithDoctorBefore');
   const [displayoverlay, setdisplayoverlay] = useState<boolean>(false);
+  const [follow_up_chat_message_visibility, set_follow_up_chat_message_visibility] = useState<
+    boolean
+  >(true);
   const [consultMode, setConsultMode] = useState<ConsultMode>(ConsultMode.ONLINE);
   const [onlineSelected, setOnlineSelected] = useState<boolean>(true);
   const [doctorDetails, setDoctorDetails] = useState<getDoctorDetailsById_getDoctorDetailsById>();
@@ -645,6 +648,8 @@ export const DoctorDetails: React.FC<DoctorDetailsProps> = (props) => {
       } else if (modeOfConsult.includes(ConsultMode.PHYSICAL)) {
         setConsultType(ConsultMode.PHYSICAL);
         setOnlineSelected(false);
+        set_follow_up_chat_message_visibility(false);
+        console.log('csk only physical', set_follow_up_chat_message_visibility);
       } else {
         setConsultType(ConsultMode.BOTH);
       }
@@ -870,10 +875,12 @@ export const DoctorDetails: React.FC<DoctorDetailsProps> = (props) => {
                 )}
               </View>
               <View style={styles.separatorStyle} />
-              <View style={styles.followUpChatMessageViewStyle}>
-                <CTGrayChat style={styles.followUpChatImageStyle} />
-                <Text style={styles.followUpChatMessageStyle}>{followUpChatMessage}</Text>
-              </View>
+              {follow_up_chat_message_visibility && (
+                <View style={styles.followUpChatMessageViewStyle}>
+                  <CTGrayChat style={styles.followUpChatImageStyle} />
+                  <Text style={styles.followUpChatMessageStyle}>{followUpChatMessage}</Text>
+                </View>
+              )}
               <View
                 style={[
                   styles.onlineConsultView,
@@ -909,6 +916,7 @@ export const DoctorDetails: React.FC<DoctorDetailsProps> = (props) => {
                     style={{ height: consultViewHeight }}
                     onPress={() => {
                       setOnlineSelected(true);
+                      set_follow_up_chat_message_visibility(true);
                       const eventAttributes: WebEngageEvents[WebEngageEventName.TYPE_OF_CONSULT_SELECTED] = {
                         'Doctor Speciality': g(doctorDetails, 'specialty', 'name')!,
                         'Patient Name': `${g(currentPatient, 'firstName')} ${g(
@@ -1031,6 +1039,7 @@ export const DoctorDetails: React.FC<DoctorDetailsProps> = (props) => {
   };
 
   const onPressMeetInPersonCard = () => {
+    set_follow_up_chat_message_visibility(false);
     const eventAttributes: WebEngageEvents[WebEngageEventName.TYPE_OF_CONSULT_SELECTED] = {
       'Doctor Speciality': g(doctorDetails, 'specialty', 'name')!,
       'Patient Name': `${g(currentPatient, 'firstName')} ${g(currentPatient, 'lastName')}`,
