ab8c449f0f414137c7d571f525d70a1438862278
Gufran Khurshid <gufran.k@apollo247.org>
2021-04-13 06:07:05 +0000

    Merged in feature/APP-6546-reduce-app-sizes-optimization-two (pull request #1136)
    Removed fonts & resources exclusion
    
    * APP-6546-reduce-app-size-optimization-two Removed unused fonts
    
    * APP-6546-reduce-app-sizes-optimization-two reverted ios fonts
    
    * APP-6546-reduce-app-sizes-optimization-two removed commented code
    
    Approved-by: AnkitSingh Bisht
    Approved-by: Shikha Jangid
    Approved-by: Bhavna Chaurasia
    Approved-by: Muqeeth Taj

--- packages/mobile-patients/src/theme/fonts.ts
+++ packages/mobile-patients/src/theme/fonts.ts
@@ -2,21 +2,7 @@ import { Platform, TextStyle } from 'react-native';
 
 type AphOpacity = 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
 type AphTextColors = '#01475b' | '#02475b' | '#0087ba' | '#fc9916' | '#890000' | '#fff' | string;
-type AphFont =
-  | 'TI'
-  | 'T'
-  | 'SBI'
-  | 'SB'
-  | 'R'
-  | 'MI'
-  | 'M'
-  | 'LI'
-  | 'L'
-  | 'I'
-  | 'ELI'
-  | 'EL'
-  | 'B'
-  | 'BI';
+type AphFont = 'SB' | 'R' | 'M' | 'B';
 
 const getFont = (key: AphFont, size: number): TextStyle => {
   let fontStyle = Fonts.IBMPlexSansBold(size);
@@ -24,44 +10,14 @@ const getFont = (key: AphFont, size: number): TextStyle => {
     case 'B':
       fontStyle = Fonts.IBMPlexSansBold(size);
       break;
-    case 'BI':
-      fontStyle = Fonts.IBMPlexSansBoldItalic(size);
-      break;
-    case 'EL':
-      fontStyle = Fonts.IBMPlexSansExtraLight(size);
-      break;
-    case 'ELI':
-      fontStyle = Fonts.IBMPlexSansExtraLightItalic(size);
-      break;
-    case 'I':
-      fontStyle = Fonts.IBMPlexSansItalic(size);
-      break;
-    case 'L':
-      fontStyle = Fonts.IBMPlexSansLight(size);
-      break;
-    case 'LI':
-      fontStyle = Fonts.IBMPlexSansLightItalic(size);
-      break;
     case 'M':
       fontStyle = Fonts.IBMPlexSansMedium(size);
-      break;
-    case 'MI':
-      fontStyle = Fonts.IBMPlexSansMediumItalic(size);
       break;
     case 'R':
       fontStyle = Fonts.IBMPlexSansRegular(size);
       break;
     case 'SB':
       fontStyle = Fonts.IBMPlexSansSemiBold(size);
-      break;
-    case 'SBI':
-      fontStyle = Fonts.IBMPlexSansSemiBoldItalic(size);
-      break;
-    case 'T':
-      fontStyle = Fonts.IBMPlexSansThin(size);
-      break;
-    case 'TI':
-      fontStyle = Fonts.IBMPlexSansThinItalic(size);
       break;
   }
   return fontStyle;
@@ -83,24 +39,6 @@ export const getTextStyle = (
 });
 
 const Fonts = {
-  IBMPlexSansThinItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-ThinItalic',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansThin: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-Thin',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansSemiBoldItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-SemiBoldItalic',
-      fontSize: s,
-    };
-  },
   IBMPlexSansSemiBold: (s = 12) => {
     return {
       fontFamily: 'IBMPlexSans-SemiBold',
@@ -113,12 +51,7 @@ const Fonts = {
       fontSize: s,
     };
   },
-  IBMPlexSansMediumItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-MediumItalic',
-      fontSize: s,
-    };
-  },
+
   IBMPlexSansMedium: (s = 12) => {
     return {
       fontFamily: 'IBMPlexSans-Medium',
@@ -133,45 +66,9 @@ const Fonts = {
       }),
     };
   },
-  IBMPlexSansLightItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-LightItalic',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansLight: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-Light',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-Italic',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansExtraLightItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-ExtraLightItalic',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansExtraLight: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-ExtraLight',
-      fontSize: s,
-    };
-  },
   IBMPlexSansBold: (s = 12) => {
     return {
       fontFamily: 'IBMPlexSans-Bold',
-      fontSize: s,
-    };
-  },
-  IBMPlexSansBoldItalic: (s = 12) => {
-    return {
-      fontFamily: 'IBMPlexSans-BoldItalic',
       fontSize: s,
     };
   },
