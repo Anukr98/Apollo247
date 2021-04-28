9d3e8e7dbd39668bdb3c91032b84ee8f2bfde9e9
AnkitSingh Bisht <ankitsingh.b@apollo247.org>
2021-04-28 10:55:55 +0000

    Merged in 5.5.3_bugFixes_ankit (pull request #1250)
    5.5.3 bugFixes ankit
    
    * APP-9659: removed unwanted loading
    
    * APP-8965: phr bug fixes
    
    * APP-9750: added NSUserTrackingUsageDescription permission
    
    Approved-by: Gufran Khurshid
    Approved-by: Shikha Jangid

--- packages/mobile-patients/src/components/HealthRecords/HealthConditionScreen.tsx
+++ packages/mobile-patients/src/components/HealthRecords/HealthConditionScreen.tsx
@@ -192,6 +192,7 @@ export const HealthConditionScreen: React.FC<HealthConditionScreenProps> = (prop
   const [prismAuthToken, setPrismAuthToken] = useState<string>(
     props.navigation?.getParam('authToken') || ''
   );
+  const [searchQuery, setSearchQuery] = useState({});
   const { phrSession, setPhrSession } = useAppCommonData();
 
   useEffect(() => {
@@ -330,6 +331,12 @@ export const HealthConditionScreen: React.FC<HealthConditionScreenProps> = (prop
       }
       setSearchLoading(true);
       const search = _.debounce(onSearchHealthRecords, 500);
+      setSearchQuery((prevSearch: any) => {
+        if (prevSearch.cancel) {
+          prevSearch.cancel();
+        }
+        return search;
+      });
       search(value);
     }
   };
