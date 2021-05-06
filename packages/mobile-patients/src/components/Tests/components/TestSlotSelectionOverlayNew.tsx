2a49ba2a4a472368f2df31a0aee9c88f5ab9209f
Bhawana Shanker <bhawana.s@apollo247.org>
2021-05-06 12:12:35 +0530

    APP-9321 : ui completed

--- packages/mobile-patients/src/components/Tests/components/TestSlotSelectionOverlayNew.tsx
+++ packages/mobile-patients/src/components/Tests/components/TestSlotSelectionOverlayNew.tsx
@@ -25,7 +25,15 @@ import { theme } from '@aph/mobile-patients/src/theme/theme';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import { useApolloClient } from 'react-apollo-hooks';
-import { Dimensions, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import { useDiagnosticsCart } from '@aph/mobile-patients/src/components/DiagnosticsCartProvider';
 import {
   getDiagnosticSlotsCustomized,
@@ -51,7 +59,7 @@ const { width, height } = Dimensions.get('window');
 export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewProps> = (props) => {
   const { isTodaySlotUnavailable, maxDate } = props;
   const { cartItems } = useDiagnosticsCart();
-  const [selectedDayTab, setSelectedDayTab] = useState(0)
+  const [selectedDayTab, setSelectedDayTab] = useState(0);
   const [slotInfo, setSlotInfo] = useState<TestSlot | undefined>(props.slotInfo);
   const [slots, setSlots] = useState<TestSlot[]>(props.slots);
   const [date, setDate] = useState<Date>(props.date);
@@ -68,9 +76,46 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
   const isSameDate = moment().isSame(moment(date), 'date');
   const itemId = props.itemId;
   const cartItemsWithId = cartItems?.map((item) => Number(item?.id));
-
+  const [selectedDate, setSelectedDate] = useState('12');
   type UniqueSlotType = typeof uniqueSlots[0];
-
+  const datearray = [
+    {
+      date: '12',
+      day: 'Mon',
+    },
+    {
+      date: '13',
+      day: 'Tue',
+    },
+    {
+      date: '14',
+      day: 'Wed',
+    },
+    {
+      date: '15',
+      day: 'Thu',
+    },
+    {
+      date: '16',
+      day: 'Fri',
+    },
+    {
+      date: '17',
+      day: 'Sat',
+    },
+    {
+      date: '18',
+      day: 'Sun',
+    },
+    {
+      date: '19',
+      day: 'Mon',
+    },
+    {
+      date: '20',
+      day: 'Tue',
+    },
+  ];
   const fetchSlots = (updatedDate?: Date) => {
     let dateToCheck = !!updatedDate ? updatedDate : date;
     setChangedDate(dateToCheck);
@@ -163,17 +208,37 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
         title: 'Evening',
       },
     ];
-
+    // console.log('dropDownOptions :>> ', dropDownOptions);
+    const wait = [
+      { data: { endTime: '06:00', startTime: '06:00' }, key: '06:00 AM', value: '06:00 AM' },
+      { data: { endTime: '06:30', startTime: '06:30' }, key: '06:30 AM', value: '06:30 AM' },
+      { data: { endTime: '07:00', startTime: '07:00' }, key: '07:00 AM', value: '07:00 AM' },
+      { data: { endTime: '07:30', startTime: '07:30' }, key: '07:30 AM', value: '07:30 AM' },
+      { data: { endTime: '08:00', startTime: '08:00' }, key: '08:00 AM', value: '08:00 AM' },
+      { data: { endTime: '08:30', startTime: '08:30' }, key: '08:30 AM', value: '08:30 AM' },
+      { data: { endTime: '09:00', startTime: '09:00' }, key: '09:00 AM', value: '09:00 AM' },
+      { data: { endTime: '09:30', startTime: '09:30' }, key: '09:30 AM', value: '09:30 AM' },
+      { data: { endTime: '10:00', startTime: '10:00' }, key: '10:00 AM', value: '10:00 AM' },
+      { data: { endTime: '10:30', startTime: '10:30' }, key: '10:30 AM', value: '10:30 AM' },
+    ];
     return (
       <View>
         <View style={styles.dayPhaseContainer}>
           {dayPhaseArray.map((item, index) => (
-            <TouchableOpacity style={styles.dayPhaseStyle}>
+            <TouchableOpacity
+              style={[styles.dayPhaseStyle,{
+                borderBottomColor: selectedDayTab == index ? theme.colors.APP_GREEN : theme.colors.WHITE,
+                borderBottomWidth: selectedDayTab == index ? 4 : 0,
+              }]}
+              onPress={() => {
+                setSelectedDayTab(index);
+              }}
+            >
               {selectedDayTab == index ? item?.activeImage : item?.inactiveImage}
               <Text
                 style={{
                   ...theme.fonts.IBMPlexSansMedium(16),
-                  color: theme.colors.LIGHT_BLUE,
+                  color: selectedDayTab == index ? theme.colors.APP_GREEN : theme.colors.LIGHT_BLUE,
                   padding: 10,
                 }}
               >
@@ -182,122 +247,137 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
             </TouchableOpacity>
           ))}
         </View>
-      </View>
-    );
-  };
-
-  const renderCalendarView = () => {
-    /**
-     * as soon as current date has no slot selection, then change it to next date (autoselection)
-     */
-
-    const isCurrentDateSlotUnavailable = isSameDate && uniqueSlots?.length == 0;
-    //date is the selected date & minDate : date to show.
-    const dateToHighlight =
-      isCurrentDateSlotUnavailable && isDateAutoSelected
-        ? moment(date)
-            .add(1, 'day')
-            .toDate()
-        : date;
-
-    const minDateToShow = props.isReschdedule
-      ? new Date()
-      : !!isTodaySlotUnavailable && isTodaySlotUnavailable
-      ? moment(new Date())
-          .add(1, 'day')
-          .toDate()
-      : new Date();
-    const datearray = [
-      {
-        date:'12',
-        day:'Mon'
-      },
-      {
-        date:'13',
-        day:'Tue'
-      },
-      {
-        date:'14',
-        day:'Wed'
-      },
-      {
-        date:'15',
-        day:'Thu'
-      },
-      {
-        date:'16',
-        day:'Fri'
-      },
-      {
-        date:'17',
-        day:'Sat'
-      },{
-        date:'18',
-        day:'Sun'
-      },
-      {
-        date:'19',
-        day:'Mon'
-      },
-      {
-        date:'20',
-        day:'Tue'
-      },
-    ]
-    return (
-      <View>
-        <Text
-          style={[
-            {
-              ...theme.fonts.IBMPlexSansMedium(17),
-              color: theme.colors.LIGHT_BLUE,
-              padding: 15,
-            },
-            props.headingTextStyle,
-          ]}
-        >
-          {' '}
-          {props.heading}
-        </Text>
-        <View style={{borderColor:'#e7e7e7',borderWidth:1,borderRadius:10,margin:10,padding:10}}>
-          <Text style={{
-              ...theme.fonts.IBMPlexSansMedium(14),
-              color: theme.colors.LIGHT_BLUE,
-              padding: 10,
-            }}>February 2021</Text>
-          <ScrollView 
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          >
-            {
-              datearray.map((item)=>(
-                  <View style={{paddingHorizontal:10,marginHorizontal:10,backgroundColor:'#00B38E'}}>
-                    <Text>{item?.date}</Text>
-                    <Text>{item?.day}</Text>
-                  </View>
-                )
-                )
-            } 
-            
-          </ScrollView>
+        <View>
+          <FlatList
+            keyExtractor={(_, index) => index.toString()}
+            data={dropDownOptions}
+            contentContainerStyle={styles.timeContainer}
+            numColumns={4}
+            renderItem={({ item, index }) => (
+              <View style={styles.dateContentStyle}>
+                <Text style={styles.dateTextStyle}>{item?.value}</Text>
+              </View>
+            )}
+          />
         </View>
       </View>
     );
   };
 
+  const renderCalendarView = () => {
+          /**
+           * as soon as current date has no slot selection, then change it to next date (autoselection)
+           */
+
+          const isCurrentDateSlotUnavailable = isSameDate && uniqueSlots?.length == 0;
+          //date is the selected date & minDate : date to show.
+          const dateToHighlight =
+            isCurrentDateSlotUnavailable && isDateAutoSelected
+              ? moment(date)
+                  .add(1, 'day')
+                  .toDate()
+              : date;
+
+          const minDateToShow = props.isReschdedule
+            ? new Date()
+            : !!isTodaySlotUnavailable && isTodaySlotUnavailable
+            ? moment(new Date())
+                .add(1, 'day')
+                .toDate()
+            : new Date();
+
+          return (
+            <View>
+              <Text
+                style={[
+                  {
+                    ...theme.fonts.IBMPlexSansMedium(17),
+                    color: theme.colors.LIGHT_BLUE,
+                    padding: 15,
+                  },
+                  props.headingTextStyle,
+                ]}
+              >
+                {' '}
+                {props.heading}
+              </Text>
+              <View
+                style={{
+                  borderColor: '#cccccc',
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  margin: 10,
+                  padding: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    ...theme.fonts.IBMPlexSansMedium(15),
+                    color: theme.colors.LIGHT_BLUE,
+                    padding: 10,
+                  }}
+                >
+                  {`${moment().format('MMMM')} ${moment().format('YYYY')}`}
+                </Text>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  {datearray.map((item, index) => (
+                    <TouchableOpacity
+                      onPress={() => {
+                        setSelectedDate(item?.date);
+                      }}
+                      style={[
+                        styles.dateContainer,
+                        {
+                          backgroundColor:
+                            selectedDate == item?.date ? theme.colors.APP_GREEN : 'transparent',
+                        },
+                      ]}
+                    >
+                      <Text
+                        style={[
+                          styles.dateStyle,
+                          {
+                            color: selectedDate == item?.date ? 'white' : theme.colors.SHERPA_BLUE,
+                          },
+                        ]}
+                      >
+                        {item?.date}
+                      </Text>
+                      <Text
+                        style={[
+                          styles.dayStyle,
+                          {
+                            color: selectedDate == item?.date ? 'white' : theme.colors.SHERPA_BLUE,
+                          },
+                        ]}
+                      >
+                        {item?.day}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+              </View>
+            </View>
+          );
+        };;
+
   const isDoneBtnDisabled = !date || !slotInfo;
   const infoPanel = () => {
     return (
       <View style={styles.warningbox}>
-        <InfoIconRed/>
-        <Text style={{
-              ...theme.fonts.IBMPlexSansRegular(10),
-              color: theme.colors.LIGHT_BLUE,
-              padding: 10,
-            }}>Phelbo will arrive within 30 minutes of the slot time</Text>
+        <InfoIconRed />
+        <Text
+          style={{
+            ...theme.fonts.IBMPlexSansRegular(10),
+            color: theme.colors.LIGHT_BLUE,
+            padding: 10,
+          }}
+        >
+          Phelbo will arrive within 30 minutes of the slot time
+        </Text>
       </View>
-    )
-  }
+    );
+  };
   const renderBottomButton = (
     <Button
       style={{ margin: 16, marginTop: 5, width: 'auto' }}
@@ -334,10 +414,10 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
 const styles = StyleSheet.create({
   containerStyle: {
     flex: 1,
-    backgroundColor: theme.colors.DEFAULT_BACKGROUND_COLOR,
+    backgroundColor: theme.colors.WHITE,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    marginTop: 120,
+    marginTop: 160,
   },
   phrOverlayStyle: {
     padding: 0,
@@ -351,22 +431,57 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   warningbox: {
-    backgroundColor:'#FCFDDA',
-    flexDirection:'row',
-    borderRadius:10,
-    margin:15,
-    alignItems:'center',
-    justifyContent:'flex-start',
-    paddingHorizontal:15,
-    paddingVertical:10
+    backgroundColor: '#FCFDDA',
+    flexDirection: 'row',
+    borderRadius: 10,
+    margin: 15,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
   },
-  dayPhaseContainer:{
-    flexDirection:'row',
-    justifyContent:'space-evenly'
+  dateContainer: {
+    width: 75,
+    height: 75,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.APP_GREEN,
+  },
+  dayPhaseContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
   dayPhaseStyle: {
-    alignItems:'center',
-    flexDirection:'row',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  dateStyle: {
+    ...theme.viewStyles.text('M', 17, 'white'),
+  },
+  dayStyle: {
+    ...theme.viewStyles.text('M', 12, 'white'),
+  },
+  timeContainer: {
+    width: '94%',
+    alignSelf: 'center',
+    // backgroundColor: '#c8c8c8',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateContentStyle: {
+    width: 68,
+    height: 42,
+    margin: 12,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.DEFAULT_BACKGROUND_COLOR,
+  },
+  dateTextStyle:{
+    ...theme.viewStyles.text('M', 12, theme.colors.SHERPA_BLUE),
   },
   optionsView: {
     flexDirection: 'row',
