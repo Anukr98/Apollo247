c88d482fef1d0ffe8d806910499945eaf7202510
Ankit Bisht <ankitsingh.b@apollo247.org>
2021-05-08 11:45:57 +0530

    APP-9996: Added a fix to call save device token api

--- packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
+++ packages/mobile-patients/src/components/ConsultRoom/ConsultRoom.tsx
@@ -839,9 +839,11 @@ export const ConsultRoom: React.FC<ConsultRoomProps> = (props) => {
   const saveDeviceNotificationToken = async (id: string) => {
     try {
       const savedToken = await AsyncStorage.getItem('deviceToken');
+      const oneTimeApiCall = await AsyncStorage.getItem('saveTokenDeviceApiCall');
       const token = await messaging().getToken();
-      if (savedToken && JSON.parse(savedToken) !== token) {
+      if (!oneTimeApiCall || !savedToken || (savedToken && JSON.parse(savedToken) !== token)) {
         saveTokenDevice(client, token, id);
+        await AsyncStorage.setItem('saveTokenDeviceApiCall', 'called');
       }
     } catch (error) {}
   };
