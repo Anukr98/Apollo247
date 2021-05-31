8b9dbfd12a02aec8a5d0fad88068bf4bcb11b0e0
Bhuvan <bhuvanachandra12342@gmail.com>
2021-05-31 13:10:45 +0530

    APP-10703: env update

--- packages/mobile-patients/android/app/src/main/java/com/apollopatient/MainApplication.java
+++ packages/mobile-patients/android/app/src/main/java/com/apollopatient/MainApplication.java
@@ -83,7 +83,7 @@ public class MainApplication extends Application implements ReactApplication {
                 });
 
                 WebEngageConfig webEngageConfig = new WebEngageConfig.Builder()
-                        .setWebEngageKey("in~~c2ab3533")
+                        .setWebEngageKey("in~~c2ab3529")
                         .setDebugMode(false) // only in development mode
                         .build();
 
