e986693d2e072d0955bc6de27a0c19b7a95034e9
Ankit Bisht <ankitsingh.b@apollo247.org>
2021-06-08 17:40:21 +0530

    APP-10903: keeping patient inside the A/V session until manually disconnected

--- packages/mobile-patients/src/components/Consult/ChatRoom.tsx
+++ packages/mobile-patients/src/components/Consult/ChatRoom.tsx
@@ -2457,10 +2457,8 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
     connected: (event: string) => {
       openTokWebEngageEvents(WebEngageEventName.DOCTOR_SUBSCRIBER_CONNECTED, JSON.stringify(event));
       setSnackbarState(false);
-      if (!subscriberConnected.current) {
-        playJoinSound();
-        callToastStatus.current = callConnected;
-      }
+      playJoinSound();
+      callToastStatus.current = callConnected;
       subscriberConnected.current = true;
       setTimeout(() => {
         callToastStatus.current = '';
@@ -3342,14 +3340,11 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
         message.message.message === 'Audio call ended' ||
         message.message.message === 'Video call ended'
       ) {
-        resetCurrentRetryAttempt();
+        if (message?.message?.id !== currentPatient?.id) {
+          // call has been ended by doctor
+          callToastStatus.current = 'Call has been ended by doctor.';
+        }
         setTimeout(() => {
-          setCallMinimize(false);
-          AsyncStorage.setItem('callDisconnected', 'true');
-          setOnSubscribe(false);
-          callhandelBack = true;
-          setIsCall(false);
-          setIsAudioCall(false);
           addMessages(message);
         }, 2000);
       } else if (message.message.message === covertVideoMsg) {
@@ -3403,10 +3398,6 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
         setDoctorJoinedChat && setDoctorJoinedChat(false);
         setDoctorJoined(false);
       } else if (message.message.message === endCallMsg) {
-        resetCurrentRetryAttempt();
-        callStatus.current = disconnecting;
-        callToastStatus.current = '';
-        subscriberConnected.current = false;
         setTimeout(() => {
           try {
             const event = _.debounce(() => {
@@ -3420,14 +3411,6 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
             });
             event();
           } catch (error) {}
-          setCallMinimize(false);
-          AsyncStorage.setItem('callDisconnected', 'true');
-          AsyncStorage.getItem('leftSoundPlayed').then((data) => {
-            if (!JSON.parse(data || 'false')) {
-              stopSound();
-              playDisconnectSound();
-            }
-          });
         }, 2000);
       } else if (message.message.message === exotelCall) {
         addMessages(message);
@@ -6244,25 +6227,6 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
       </View>
     );
   };
-
-  useEffect(() => {
-    if (
-      patientJoinedCall.current &&
-      !subscriberConnected.current &&
-      callTimer === avgTimeForDoctorToJoinInMinutes * 60
-    ) {
-      callStatus.current = ' ';
-      callToastStatus.current = disconnecting;
-      isErrorToast.current = true;
-      setTimeout(() => {
-        if (isAudio.current) {
-          handleEndAudioCall();
-        } else {
-          handleEndCall();
-        }
-      }, 1000);
-    }
-  }, [callTimer]);
 
   const handleEndCall = (playSound: boolean = true) => {
     APICallAgain(false);
