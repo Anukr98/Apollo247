9d49ecf83463c852e0802d125be02b77f9e6117d
bhavna-chaurasia <bhavna.c@apollo247.org>
2021-06-11 08:25:12 +0530

    APP-11111 - report fix for very old orders

--- packages/mobile-patients/src/components/Tests/PostOrderJourney/TestOrderDetails.tsx
+++ packages/mobile-patients/src/components/Tests/PostOrderJourney/TestOrderDetails.tsx
@@ -252,7 +252,7 @@ export const TestOrderDetails: React.FC<TestOrderDetailsProps> = (props) => {
     const getVisitId = selectedOrder?.visitNo;
     getPatientPrismMedicalRecordsApi(
       client,
-      currentPatient?.id,
+      selectedOrder?.patientId,
       [MedicalRecordType.TEST_REPORT],
       'Diagnostics'
     )
