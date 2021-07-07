22cd3c72d6a774ed5328f8b39c0ec725c282dadd
GufranK <gufran.k@apollo247.org>
2021-07-07 17:37:52 +0530

    APP-11996 Doctor call disconnection

--- packages/mobile-patients/src/components/Consult/ChatRoom.tsx
+++ packages/mobile-patients/src/components/Consult/ChatRoom.tsx
@@ -775,7 +775,7 @@ const urlRegEx = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|png|JPG|PNG|jfif|jpeg|JPEG
 export interface ChatRoomProps extends NavigationScreenProps {}
 export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [startCallConnectionUpdateBT, setStartCallConnectionUpdateBT] = useState<any>();
+  const [startCallConnectionUpdateBT, setStartCallConnectionUpdateBT] = useState<number>(0);
   const fromIncomingCall = props.navigation.state.params!.isCall;
   const { isIphoneX } = DeviceHelper();
   const [contentHeight, setContentHeight] = useState(40);
@@ -1555,6 +1555,10 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
         }, 10000);
       } else {
         setTextChange(false);
+
+        AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+          BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+        });
         BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
       }
     }
@@ -2209,6 +2213,8 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   };
 
   const updateStatusOfCall = async (input: CheckCallConnectionInput) => {
+    console.log('check  API call ---- updateStatusOfCall ---- ', input);
+
     let data;
     try {
       data = await client.mutate<checkCallConnection, checkCallConnectionVariables>({
@@ -2437,6 +2443,8 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
       );
       stopTimer();
       startTimer(0);
+      console.log('check publisherEventHandlers --- streamCreated --- ');
+
       startCallConnectionUpdateFx();
     },
     streamDestroyed: (event: string) => {
@@ -2447,6 +2455,9 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
       patientJoinedCall.current = false;
       // subscriberConnected.current = false;
       endVoipCall();
+      AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+        BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+      });
       BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
     },
     error: (error: string) => {
@@ -2502,6 +2513,9 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
       patientJoinedCall.current = false;
       subscriberConnected.current = false;
       endVoipCall();
+      AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+        BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+      });
       BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
     },
     otrnError: (error: string) => {
@@ -3372,8 +3386,24 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
       ) {
         if (message?.message?.id !== currentPatient?.id) {
           // call has been ended by doctor
+
+          AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+            BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+          });
+
+          // Disconnecting the call as doctor disconnected --------- */
+          callStatus.current = disconnecting;
           callToastStatus.current = 'Call has been ended by doctor.';
-          BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
+          isErrorToast.current = true;
+          setTimeout(() => {
+            hideCallUI();
+          }, 2000);
+          if (isAudio.current) {
+            handleEndAudioCall();
+          } else {
+            handleEndCall();
+          }
+          ////------------ */
         }
         //resetCurrentRetryAttempt();
         setTimeout(() => {
@@ -3437,7 +3467,11 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
         setDoctorJoinedChat && setDoctorJoinedChat(false);
         setDoctorJoined(false);
       } else if (message.message.message === endCallMsg) {
+        AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+          BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+        });
         BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
+
         //Intentionally commented
         //resetCurrentRetryAttempt();
         // callStatus.current = disconnecting;
@@ -5829,10 +5863,11 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   };
 
   const startCallConnectionUpdateFx = () => {
-    const startCallConnectionUpdate = BackgroundTimer.setInterval(() => {
+    const startCallConnectionUpdateBT = BackgroundTimer.setInterval(() => {
       updateStatusOfCall({ appointmentId: appointmentData.id, patientId: patientId });
     }, 30000);
-    setStartCallConnectionUpdateBT(startCallConnectionUpdate);
+    setStartCallConnectionUpdateBT(startCallConnectionUpdateBT);
+    AsyncStorage.setItem('startCallConnectionUpdateBT', startCallConnectionUpdateBT.toString());
   };
 
   const onPressJoinBtn = () => {
@@ -6195,6 +6230,9 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
     if (isIos()) {
       RNCallKeep.endAllCalls();
     }
+    AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+      BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+    });
     BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
   };
 
@@ -6354,7 +6392,11 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   // }, [callTimer]);
 
   const handleEndCall = (playSound: boolean = true) => {
+    AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+      BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+    });
     BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
+
     APICallAgain(false);
     resetCurrentRetryAttempt();
     setTimeout(() => {
@@ -6404,6 +6446,11 @@ export const ChatRoom: React.FC<ChatRoomProps> = (props) => {
   };
 
   const handleEndAudioCall = (playSound: boolean = true) => {
+    AsyncStorage.getItem('startCallConnectionUpdateBT').then((data) => {
+      BackgroundTimer.clearInterval(Number.parseInt(data || '0'));
+    });
+    BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
+
     BackgroundTimer.clearInterval(startCallConnectionUpdateBT);
     APICallAgain(false);
     resetCurrentRetryAttempt();
