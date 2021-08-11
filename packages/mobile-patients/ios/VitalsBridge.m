2d133a36e8b6b014e889cd91840ec9e8cf899e5f
Apple <satyam.sharma@neosoftmail.com>
2021-08-11 19:10:48 +0530

    APP-12600-H: Resolved Suggested conflicts

--- packages/mobile-patients/ios/VitalsBridge.m
+++ packages/mobile-patients/ios/VitalsBridge.m
@@ -1,24 +1,24 @@
-// //
-// //  VitalsBridge.m
-// //  ApolloPatient
-// //
-// //  Created by Ranjith Kumar on 12/26/19.
-// //  Copyright © 2019 Facebook. All rights reserved.
-// //
+//
+//  VitalsBridge.m
+//  ApolloPatient
+//
+//  Created by Ranjith Kumar on 12/26/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
 
-// #import "VitalsBridge.h"
+#import "VitalsBridge.h"
 
-// @implementation VitalsBridge
+@implementation VitalsBridge
 
-// - (NSArray<NSString *> *)supportedEvents
-// {
-//   return @[@"ViewWillAppear"];
-// }
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"ViewWillAppear"];
+}
 
-// @end
-// @interface RCT_EXTERN_MODULE(Vitals, NSObject)
+@end
+@interface RCT_EXTERN_MODULE(Vitals, NSObject)
 
-// RCT_EXTERN_METHOD(vitalsToExport: (NSString)token buildSpecify:(NSString)specify comingFrom:(NSString)name)
-// RCT_EXTERN_METHOD(goToReactNative: (NSString)token)
+RCT_EXTERN_METHOD(vitalsToExport: (NSString)token buildSpecify:(NSString)specify comingFrom:(NSString)name)
+RCT_EXTERN_METHOD(goToReactNative: (NSString)token)
 
-// @end
+@end
