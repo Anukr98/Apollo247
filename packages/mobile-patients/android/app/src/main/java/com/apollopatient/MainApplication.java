ad1245a8db0789dcfb4e4d0d768622c2daccee13
Mohammad Asad <asad.m@apollo247.org>
2021-08-17 17:18:23 +0530

    APP-12600-H

--- packages/mobile-patients/android/app/src/main/java/com/apollopatient/MainApplication.java
+++ packages/mobile-patients/android/app/src/main/java/com/apollopatient/MainApplication.java
@@ -52,7 +52,6 @@ public class MainApplication extends Application implements ReactApplication {
                     packages.add(new RNAppSignatureHelperPackage());
                     packages.add(new LinearGradientPackage());
                     packages.add(new GetReferrerPackage());
-                    packages.add(new AppReviewPackage());
 
                     return packages;
                 }
