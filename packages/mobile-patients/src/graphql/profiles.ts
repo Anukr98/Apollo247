1fc23bc64f91e6f2e14c71b78a5be9fa64fb693f
Bhavna <bhavna.c@apollo247.org>
2021-08-18 17:00:53 +0530

    APP-13169 : pincode added

--- packages/mobile-patients/src/graphql/profiles.ts
+++ packages/mobile-patients/src/graphql/profiles.ts
@@ -1965,8 +1965,8 @@ export const GET_DIAGNOSTICS_HC_CHARGES = gql`
 `;
 
 export const GET_DIAGNOSTICS_BY_ITEMIDS_AND_CITYID = gql`
-  query findDiagnosticsByItemIDsAndCityID($cityID: Int!, $itemIDs: [Int]!) {
-    findDiagnosticsByItemIDsAndCityID(cityID: $cityID, itemIDs: $itemIDs) {
+  query findDiagnosticsByItemIDsAndCityID($cityID: Int!, $itemIDs: [Int]!, $pincode:Int) {
+    findDiagnosticsByItemIDsAndCityID(cityID: $cityID, itemIDs: $itemIDs, pincode: $pincode) {
       diagnostics {
         id
         itemId
