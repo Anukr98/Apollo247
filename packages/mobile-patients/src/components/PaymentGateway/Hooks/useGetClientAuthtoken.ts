a3a844233ac843c49d6806103b7a4994d3f3b6e0
Bhuvan <bhuvanachandra12342@gmail.com>
2021-09-01 14:59:16 +0530

    APP-13419: Cred Pay integration

--- packages/mobile-patients/src/components/PaymentGateway/Hooks/useGetClientAuthtoken.ts
+++ packages/mobile-patients/src/components/PaymentGateway/Hooks/useGetClientAuthtoken.ts
@@ -1,33 +1,28 @@
 import { useEffect, useState } from 'react';
-import Axios, { AxiosResponse, Canceler } from 'axios';
-import { AppConfig } from '@aph/mobile-patients/src/strings/AppConfig';
+import { useApolloClient } from 'react-apollo-hooks';
+import { GET_JUSPAY_CLIENTAUTH_TOKEN } from '@aph/mobile-patients/src/graphql/profiles';
 
-export const useGetClientAuthToken = (customer_id: string) => {
-  const [token, setToken] = useState<any>([]);
+export const useGetClientAuthToken = (customer_id: string, businessLine: string) => {
+  const [clientAuthToken, setClientAuthToken] = useState<any>([]);
+  const client = useApolloClient();
 
-  const getJuspayClientToken = (): Promise<AxiosResponse<any>> => {
-    const baseUrl =
-      AppConfig?.APP_ENV == 'PROD' ? 'https://api.juspay.in' : 'https://sandbox.juspay.in';
-    const url = `${baseUrl}/v2/customers/${customer_id}`;
-    return Axios.post(
-      url,
-      { 'options.get_client_auth_token': 'true' },
-      {
-        headers: {
-          your_api_key: '2927ED6D69D4087837CEB2BA03FD4A',
-          'x-merchantid': 'apollopharm',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
+  const getJuspayClientToken = () => {
+    return client.query({
+      query: GET_JUSPAY_CLIENTAUTH_TOKEN,
+      variables: {
+        customer_id: customer_id,
+        is_pharma_juspay: businessLine == 'pharma' ? true : false,
+        get_client_auth_token: true,
+      },
+      fetchPolicy: 'no-cache',
+    });
   };
 
   const initiate = async () => {
     try {
-      console.log('inside initiate');
       const response = await getJuspayClientToken();
       const { data } = response;
-      console.log('data >>', data);
+      setClientAuthToken(data?.getCustomer?.juspay?.client_auth_token);
     } catch (error) {
       console.log(error);
     }
@@ -37,5 +32,5 @@ export const useGetClientAuthToken = (customer_id: string) => {
     initiate();
   }, []);
 
-  return { token };
+  return { clientAuthToken };
 };
