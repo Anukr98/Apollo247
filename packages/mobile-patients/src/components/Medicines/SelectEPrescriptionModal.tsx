fce017ce249ae4bd5307b1249786e0cd80ede30e
Shikha Jangid <shikhajangid@MacBook-Pro.local>
2021-09-07 14:36:55 +0530

    APP-13692: discharge summary on select prescription

--- packages/mobile-patients/src/components/Medicines/SelectEPrescriptionModal.tsx
+++ packages/mobile-patients/src/components/Medicines/SelectEPrescriptionModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-apollo-hooks';
+import { useApolloClient, useQuery } from 'react-apollo-hooks';
 import {
   SafeAreaView,
   StyleSheet,
@@ -49,6 +49,8 @@ import {
 import { TrackerBig } from '@aph/mobile-patients/src/components/ui/Icons';
 import Pdf from 'react-native-pdf';
 import { SelectEprescriptionCard } from '@aph/mobile-patients/src/components/Medicines/Components/SelectEprescriptionCard';
+import { getPatientPrismMedicalRecordsApi } from '@aph/mobile-patients/src/helpers/clientCalls';
+import { getPatientPrismMedicalRecords_V3_getPatientPrismMedicalRecords_V3_hospitalizations_response } from '@aph/mobile-patients/src/graphql/types/getPatientPrismMedicalRecords_V3';
 
 const { width, height } = Dimensions.get('window');
 
@@ -197,6 +199,7 @@ export interface SelectEPrescriptionModalProps {
 
 export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> = (props) => {
   const [selectedPrescription, setSelectedPrescription] = useState<{ [key: string]: boolean }>({});
+  const [selectedDischargeSummary, setSelectedDischargeSummary] = useState<{ [key: string]: boolean }>({});
   const [showPreview, setShowPreview] = useState<boolean>(false);
   const [imageUrl, setImageUrl] = useState<string>('');
   const [imageIndex, setImageIndex] = useState<string>('');
@@ -204,9 +207,15 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
   const loadRecordsStep = 12;
   const [healthRecordIndex, setHealthRecordIndex] = useState<number>(loadRecordsStep);
   const [refreshHealthRecords, setRefreshHealthRecords] = useState<boolean>(false);
+  const [dischargeSummaryData, setDischargeSummaryData] = useState<any[]>([])
   const { currentPatient, profileAllPatients } = useAllCurrentPatients();
   const { getPatientApiCall } = useAuth();
+  const client = useApolloClient();
   const DATE_FORMAT = 'DD MMM YYYY';
+
+  useEffect(() => {
+    getLatestHospitalizationRecords();
+  }, [])
 
   useEffect(() => {
     if (!currentPatient) {
@@ -319,23 +328,116 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
     setCombination(sordByDate(mergeArray));
   }, [labResults, prescriptions]);
 
+  const getLatestHospitalizationRecords = () => {
+    getPatientPrismMedicalRecordsApi(client, currentPatient?.id, [
+      MedicalRecordType.HOSPITALIZATION,
+    ])
+      .then((data: any) => {
+        const hospitalizationsData = g(
+          data,
+          'getPatientPrismMedicalRecords_V3',
+          'hospitalizations',
+          'response'
+        );
+        if (hospitalizationsData?.length) formatDischargeSummaryData(hospitalizationsData);
+      })
+      .catch((error) => {
+        CommonBugFender('SelectEPrescriptionModal_getLatestHospitalizationRecords', error);
+        currentPatient && handleGraphQlError(error);
+      });
+  };
+
+  const formatDischargeSummaryData = (
+    summaryData: getPatientPrismMedicalRecords_V3_getPatientPrismMedicalRecords_V3_hospitalizations_response[]
+  ) => {
+    const dischargeSummary = summaryData.map((item) =>
+    ({
+      id: item?.id,
+      date: moment(item?.dateOfHospitalization || item?.date).format(DATE_FORMAT),
+      uploadedUrl: item?.hospitalizationFiles?.[0]?.file_Url + '/' + item?.hospitalizationFiles?.[0]?.fileName,
+      doctorName: item?.doctorName || 'Hospitalizations',
+      forPatient: (currentPatient && currentPatient.firstName) || '',
+      medicines: '',
+      message: item?.dateOfHospitalization ? 
+        `Date of Hospitalization: ${moment(item?.dateOfHospitalization).format('DD-MMM-YYYY')}\n` :  `` +
+        item?.dateOfDischarge ? `Date of Discharge: ${moment(item?.dateOfDischarge).format('DD-MMM-YYYY')}\n` : `-` + 
+          item?.diagnosisNotes ? `Diagnosis Notes: ${item?.diagnosisNotes}` : ``
+    } as any)
+    )
+      .filter((item) => !!item.uploadedUrl)
+      .sort(
+        (a, b) =>
+          moment(b.date, DATE_FORMAT)
+            .toDate()
+            .getTime() -
+          moment(a.date, DATE_FORMAT)
+            .toDate()
+            .getTime());
+    setDischargeSummaryData(dischargeSummary);
+  };
+
+  const renderDischargeSummaryList = () => {
+    return (
+      <View>
+        <Text style={styles.sectionHeadings}>
+          {`Discharge Summary`}
+        </Text>
+        <FlatList
+          data={dischargeSummaryData}
+          renderItem={renderDischargeSummary}
+          keyExtractor={(item) => item.id}
+          numColumns={3}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+        />
+      </View>
+    );
+  };
+
+  const renderDischargeSummary = ({ item, index }) => {
+    const isPdf = item?.uploadedUrl?.split('.')?.pop() === 'pdf';
+    const selected = !!selectedDischargeSummary[item?.id];
+    const heading = item?.doctorName || '';
+    const dateOfPrescription = item?.date;
+    return (
+      <SelectEprescriptionCard
+        selected={selected}
+        isPdf={isPdf}
+        url={item?.uploadedUrl || ''}
+        heading={heading}
+        date={dateOfPrescription}
+        onLongPressCard={() => {
+          setIsPdfPrescription(isPdf);
+          setImageUrl(item?.uploadedUrl);
+          setImageIndex(index.toString());
+          setShowPreview(true);
+        }}
+        onPressCard={() => {
+          setSelectedDischargeSummary({
+            ...selectedDischargeSummary,
+            [item.id]: !selected,
+          });
+        }}
+      />
+    );
+  };
+
   const sordByDate = (
     array: { type: 'medical' | 'lab' | 'health' | 'hospital' | 'prescription'; data: any }[]
   ) => {
     return array.sort(({ data: data1 }, { data: data2 }) => {
       let date1 = new Date(
         data1.testDate ||
-          data1.labTestDate ||
-          data1.appointmentDate ||
-          data1.dateOfHospitalization ||
-          data1.date
+        data1.labTestDate ||
+        data1.appointmentDate ||
+        data1.dateOfHospitalization ||
+        data1.date
       );
       let date2 = new Date(
         data2.testDate ||
-          data2.labTestDate ||
-          data2.appointmentDate ||
-          data2.dateOfHospitalization ||
-          data2.date
+        data2.labTestDate ||
+        data2.appointmentDate ||
+        data2.dateOfHospitalization ||
+        data2.date
       );
       return date1 > date2 ? -1 : date1 < date2 ? 1 : data2.id - data1.id;
     });
@@ -392,33 +494,33 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
   const formattedEPrescriptions = ePrescriptions
     .map(
       (item) =>
-        ({
-          id: item!.id,
-          date: moment(item!.quoteDateTime).format(DATE_FORMAT),
-          uploadedUrl: item!.prescriptionImageUrl,
-          doctorName: `Meds Rx ${(item!.id && item!.id.substring(0, item!.id.indexOf('-'))) || ''}`, // item.referringDoctor ? `Dr. ${item.referringDoctor}` : ''
-          forPatient: (currentPatient && currentPatient.firstName) || '',
-          medicines: getMedicines(item!.medicineOrderLineItems! || []),
-          prismPrescriptionFileId: item!.prismPrescriptionFileId,
-        } as EPrescription)
+      ({
+        id: item!.id,
+        date: moment(item!.quoteDateTime).format(DATE_FORMAT),
+        uploadedUrl: item!.prescriptionImageUrl,
+        doctorName: `Meds Rx ${(item!.id && item!.id.substring(0, item!.id.indexOf('-'))) || ''}`, // item.referringDoctor ? `Dr. ${item.referringDoctor}` : ''
+        forPatient: (currentPatient && currentPatient.firstName) || '',
+        medicines: getMedicines(item!.medicineOrderLineItems! || []),
+        prismPrescriptionFileId: item!.prismPrescriptionFileId,
+      } as EPrescription)
     )
     .concat(
       ePrescriptionsFromConsults.map(
         (item) =>
-          ({
-            id: item!.id,
-            appointmentId: item?.id,
-            date: moment(item!.appointmentDateTime).format(DATE_FORMAT),
-            uploadedUrl: getBlobUrl(item?.caseSheet),
-            doctorName: item!.doctorInfo ? `${item!.doctorInfo.fullName}` : '',
-            forPatient: (currentPatient && currentPatient.firstName) || '',
-            medicines: (
-              (getCaseSheet(item!.caseSheet as any) || { medicinePrescription: [] })
-                .medicinePrescription || []
-            )
-              .map((item) => item!.medicineName)
-              .join(', '),
-          } as EPrescription)
+        ({
+          id: item!.id,
+          appointmentId: item?.id,
+          date: moment(item!.appointmentDateTime).format(DATE_FORMAT),
+          uploadedUrl: getBlobUrl(item?.caseSheet),
+          doctorName: item!.doctorInfo ? `${item!.doctorInfo.fullName}` : '',
+          forPatient: (currentPatient && currentPatient.firstName) || '',
+          medicines: (
+            (getCaseSheet(item!.caseSheet as any) || { medicinePrescription: [] })
+              .medicinePrescription || []
+          )
+            .map((item) => item!.medicineName)
+            .join(', '),
+        } as EPrescription)
       )
     )
     .filter((item) => !!item.uploadedUrl)
@@ -544,7 +646,7 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
     const selected = selectedHealthRecord?.findIndex((i) => i === imageIndex.toString()) > -1;
     return (
       <Overlay
-        onRequestClose={() => {}}
+        onRequestClose={() => { }}
         isVisible={showPreview}
         onBackdropPress={() => setShowPreview(false)}
       >
@@ -633,6 +735,8 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
   const onPressUpload = () => {
     CommonLogEvent('SELECT_PRESCRIPTION_MODAL', 'Formatted e prescription');
     const submitValues = prescriptionUpto6months?.filter((item) => selectedPrescription[item!.id]);
+    const selectedDS = dischargeSummaryData?.filter((item) => selectedDischargeSummary[item?.id]);
+    if (selectedDS.length) submitValues.push(...selectedDS);
     if (combination) {
       combination.forEach(({ type, data }, index) => {
         if (selectedHealthRecord?.findIndex((i) => i === index.toString()) > -1) {
@@ -651,19 +755,17 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
             message += `UHID: ${(currentPatient && currentPatient.uhid) || '-'}\n`;
             message += `Test Date: ${date || '-'}\n`;
             message += `${data?.observation ? `Observation Notes: ${data?.observation}\n` : ``}`;
-            message += `${
-              data?.additionalNotes ? `Additional Notes: ${data?.additionalNotes}\n` : ``
-            }`;
+            message += `${data?.additionalNotes ? `Additional Notes: ${data?.additionalNotes}\n` : ``
+              }`;
             message += `---------------\n`;
             (data?.labTestResults || []).forEach((record: any) => {
               if (record) {
                 if (record.parameterName) {
                   message += `${record.parameterName}\n`;
-                  message += `${
-                    record.result
+                  message += `${record.result
                       ? `Result: ${record.result} ${record.unit ? record.unit || '' : ''}\n`
                       : ``
-                  }`;
+                    }`;
                 } else {
                   message += `Summary: ${record.result}`;
                 }
@@ -693,12 +795,10 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
             message += `Test Name: ${name}\n`;
             message += `UHID: ${(currentPatient && currentPatient.uhid) || '-'}\n`;
             message += `Test Date: ${moment(date).format('DD-MMM-YYYY') || '-'}\n`;
-            message += `${
-              data?.observation ? `Observation Notes: ${data?.additionalNotes}\n` : ``
-            }`;
-            message += `${
-              data?.additionalNotes ? `Additional Notes: ${data?.additionalNotes}\n` : ``
-            }`;
+            message += `${data?.observation ? `Observation Notes: ${data?.additionalNotes}\n` : ``
+              }`;
+            message += `${data?.additionalNotes ? `Additional Notes: ${data?.additionalNotes}\n` : ``
+              }`;
             message += `---------------\n`;
             (data?.medicalRecordParameters || []).forEach((record: any) => {
               if (record) {
@@ -747,6 +847,7 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
       });
     }
     setSelectedHealthRecord([]);
+    setSelectedDischargeSummary({});
     props.onSubmit(submitValues);
   };
 
@@ -784,7 +885,9 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
         title={'UPLOAD'}
         disabled={
           Object.keys(selectedPrescription)?.filter((item) => selectedPrescription?.[item])
-            ?.length == 0 && selectedHealthRecord.length === 0
+            ?.length == 0 && selectedHealthRecord.length === 0 &&
+            Object.keys(selectedDischargeSummary)?.filter((item) => selectedDischargeSummary?.[item])
+            ?.length == 0
         }
         onPress={onPressUpload}
         style={styles.buttonCta}
@@ -813,6 +916,7 @@ export const SelectEPrescriptionModal: React.FC<SelectEPrescriptionModalProps> =
                   renderSteps()}
                 {!!prescriptionUpto6months.length && renderEPrescriptions(false)}
                 {!!combination?.length && props.displayPrismRecords && renderHealthRecords()}
+                {!!dischargeSummaryData?.length && renderDischargeSummaryList()}
                 {!!prescriptionOlderThan6months.length && renderEPrescriptions(true)}
                 <View style={{ height: 12 }} />
               </>
