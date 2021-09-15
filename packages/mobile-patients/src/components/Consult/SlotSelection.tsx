40d30367511b8b48d382348f0eb291db9159f545
Abhishek <abhishek@unthinkable.co>
2021-09-15 18:01:14 +0000

    Merged in APP-12820-patient-app-share-patient-phr (pull request #1898)
    APP-12820 Patient App - Share PHR Details while Booking Appointment
    
    * APP-12820 Patient App - Share PHR Details while Booking Appointment
    
    * APP-12820 Patient APP - Share Patient PHR - PR Fix
    
    * Merge branch 'release/v5.9.0' of bitbucket.org:apollo247/aph-mobile-patients into APP-12820-patient-app-share-patient-phr
    
    * Merge branch 'release/v5.9.0' of bitbucket.org:apollo247/aph-mobile-patients into APP-12820-patient-app-share-patient-phr
    
    * Merge branch 'release/v5.9.0' of bitbucket.org:apollo247/aph-mobile-patients into APP-12820-patient-app-share-patient-phr
    
    * APP-12820 API Key Change
    
    * Merged release/v5.9.0 into APP-12820-patient-app-share-patient-phr
    
    Approved-by: Shikha Jangid
    Approved-by: Nishant Srivastava

--- packages/mobile-patients/src/components/Consult/SlotSelection.tsx
+++ packages/mobile-patients/src/components/Consult/SlotSelection.tsx
@@ -50,6 +50,8 @@ import {
   VideoActiveIcon,
   HospitalPhrSearchIcon,
   Online,
+  CheckBoxFilled,
+  CheckBox,
 } from '@aph/mobile-patients/src/components/ui/Icons';
 import {
   renderDoctorDetailsShimmer,
@@ -78,6 +80,7 @@ import {
   BOOKINGSOURCE,
   DEVICETYPE,
   PLAN,
+  HealthRecordConsentStatus,
 } from '@aph/mobile-patients/src/graphql/types/globalTypes';
 import string from '@aph/mobile-patients/src/strings/strings.json';
 import { AppRoutes } from '@aph/mobile-patients/src/components/NavigatorContainer';
@@ -125,6 +128,7 @@ export const SlotSelection: React.FC<SlotSelectionProps> = (props) => {
   const consultedWithDoctorBefore = props.navigation.getParam('consultedWithDoctorBefore');
   const callSaveSearch = props.navigation.getParam('callSaveSearch');
   const slotSelected = useRef<boolean>(false);
+  const [sharePHR, setSharePHR] = useState<boolean>(true);
 
   const bothConsultTabs = [
     {
@@ -741,6 +745,29 @@ export const SlotSelection: React.FC<SlotSelectionProps> = (props) => {
     );
   };
 
+ const renderSharePHR = () => {
+   return (
+     <View style={styles.sharePHRContainer}>
+       <TouchableOpacity
+         activeOpacity={1}
+         onPress={() => {
+           setSharePHR(!sharePHR);
+         }}
+         style={styles.checkBoxStyleContainer}
+       >
+         {sharePHR ? (
+           <CheckBoxFilled style={styles.checkBoxStyle} resizeMode={'contain'} />
+         ) : (
+           <CheckBox style={styles.checkBoxStyle} resizeMode={'contain'} />
+         )}
+       </TouchableOpacity>
+       <Text style={styles.sharePHRText}>
+         {string.common.sharePHR}
+       </Text>
+     </View>
+   );
+ };
+
   const renderFooterComponent = () => {
     if (loadTotalSlots) return;
     return (
@@ -753,6 +780,7 @@ export const SlotSelection: React.FC<SlotSelectionProps> = (props) => {
             }}
             isSelected={whatsAppUpdate}
           />
+          {renderSharePHR()}
           {!isOnlineSelected && (
             <Text style={styles.physicalConsultNoteTxt}>Note: Pay at Reception is available.</Text>
           )}
@@ -808,7 +836,9 @@ export const SlotSelection: React.FC<SlotSelectionProps> = (props) => {
     };
 
     callWEGEvent(WebEngageEventName.CONSULT_NOW_CLICKED, eventAttributes);
-
+    const healthRecordConsentStatus = sharePHR
+      ? HealthRecordConsentStatus.GRANTED
+      : HealthRecordConsentStatus.NOT_GRANTED;
     const appointmentInput: BookAppointmentInput = {
       patientId: currentPatient?.id,
       doctorId,
@@ -826,8 +856,8 @@ export const SlotSelection: React.FC<SlotSelectionProps> = (props) => {
               plan: PLAN.CARE_PLAN,
             }
           : null,
+      healthRecordsConsent: healthRecordConsentStatus,
     };
-
     const passProps = {
       doctor: doctorDetails,
       tabs: tabs,
@@ -1204,9 +1234,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   emptyComponentView: {
-    flexDirection: 'row', 
-    flexWrap: 'wrap', 
-    marginStart: 40, 
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginStart: 40,
     marginTop: 20,
   },
+  sharePHRContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    marginHorizontal: Platform.OS === 'ios' ? 45 : 60,
+    alignItems: 'center'
+  },
+  sharePHRText: {
+    ...theme.fonts.IBMPlexSansMedium(12),
+    color: theme.colors.LIGHT_BLUE,
+  },
+  checkBoxStyleContainer: {
+    width: 25,
+    height: 25,
+    marginLeft: 5,
+  },
+  checkBoxStyle: { width: 18, height: 18 },
 });
