d72bc4e5a2d27c0a6b18092740aa39ed866b31d3
Ishawari Singh <ishawari.singh@unthinkable.co>
2021-10-01 15:13:26 +0530

    APP-13759: close modal on backdrop press

--- packages/mobile-patients/src/components/SuggestedQuantityNudge/SuggestedQuantityNudge.tsx
+++ packages/mobile-patients/src/components/SuggestedQuantityNudge/SuggestedQuantityNudge.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import Modal from 'react-native-modal';
 import { PhrCloseIcon } from '@aph/mobile-patients/src/components/ui/Icons';
 import { theme } from '@aph/mobile-patients/src/theme/theme';
@@ -57,92 +57,98 @@ export const SuggestedQuantityNudge: React.FC<SuggestedQuantityNudgeProps> = (pr
 
   return (
     <Modal isVisible={showSuggestedQuantityNudge} backdropOpacity={0.3} style={styles.modal}>
-      <View style={styles.mainView}>
-        <View style={styles.bottomSheetContainer}>
-          <View style={styles.crossContainer}>
-            <TouchableOpacity
-              onPress={() => {
-                onPressCloseBottomSheet();
-              }}
-            >
-              <PhrCloseIcon style={styles.closeIcon} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.modalHeaderContainer}>
-            <Text numberOfLines={1} style={styles.titleText}>
-              {title}
-            </Text>
-          </View>
-          <View style={styles.mainTextContainer}>
-            <Text style={styles.mainText}>
-              {mainText}
-              <Text style={styles.itemQuantityStyle}>{itemQuantity}</Text>
-              <Text style={styles.mainText}>{mainText1}</Text>
-            </Text>
-          </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View style={styles.cartContainer}>
-              <TouchableOpacity
-                onPress={() => {
-                  decreaseQuantity(selectedQuantity);
-                }}
-                style={styles.buttonStyles}
-              >
-                <Text
-                  style={[
-                    styles.buttonTextStyles,
-                    selectedQuantity === 2 ? styles.greyedStyle : {},
-                  ]}
-                >
-                  -
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.buttonStyles}>
-                <Text style={[styles.buttonTextStyles, styles.quantityStyle]}>
-                  {selectedQuantity}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  increaseQuantity(selectedQuantity);
-                }}
-                style={[styles.buttonStyles, styles.lastButtonStyles]}
-              >
-                <Text
-                  style={[
-                    styles.buttonTextStyles,
-                    selectedQuantity === maxQuantity ? styles.greyedStyle : {},
-                  ]}
-                >
-                  +
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.addButtonContainer}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          onPressCloseBottomSheet();
+        }}
+      >
+        <View style={styles.mainView}>
+          <View style={styles.bottomSheetContainer}>
+            <View style={styles.crossContainer}>
               <TouchableOpacity
                 onPress={() => {
                   onPressCloseBottomSheet();
-                  if (cartItems.find(({ id }) => id?.toUpperCase() === sku?.toUpperCase())) {
-                    updateCartItem?.({
-                      id: sku,
-                      quantity: selectedQuantity,
-                    });
-                    setCurrentProductQuantityInCart(selectedQuantity);
-                  }
                 }}
               >
-                {packForm ? (
-                  <Text style={styles.addButtonTextStyles}>
-                    Add {selectedQuantity} {packForm}(s)
-                  </Text>
-                ) : (
-                  <Text style={styles.addButtonTextStyles}>Add {selectedQuantity} Item(s)</Text>
-                )}
+                <PhrCloseIcon style={styles.closeIcon} />
               </TouchableOpacity>
+            </View>
+            <View style={styles.modalHeaderContainer}>
+              <Text numberOfLines={1} style={styles.titleText}>
+                {title}
+              </Text>
+            </View>
+            <View style={styles.mainTextContainer}>
+              <Text style={styles.mainText}>
+                {mainText}
+                <Text style={styles.itemQuantityStyle}>{itemQuantity}</Text>
+                <Text style={styles.mainText}>{mainText1}</Text>
+              </Text>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={styles.cartContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    decreaseQuantity(selectedQuantity);
+                  }}
+                  style={styles.buttonStyles}
+                >
+                  <Text
+                    style={[
+                      styles.buttonTextStyles,
+                      selectedQuantity === 2 ? styles.greyedStyle : {},
+                    ]}
+                  >
+                    -
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonStyles}>
+                  <Text style={[styles.buttonTextStyles, styles.quantityStyle]}>
+                    {selectedQuantity}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    increaseQuantity(selectedQuantity);
+                  }}
+                  style={[styles.buttonStyles, styles.lastButtonStyles]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonTextStyles,
+                      selectedQuantity === maxQuantity ? styles.greyedStyle : {},
+                    ]}
+                  >
+                    +
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.addButtonContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    onPressCloseBottomSheet();
+                    if (cartItems.find(({ id }) => id?.toUpperCase() === sku?.toUpperCase())) {
+                      updateCartItem?.({
+                        id: sku,
+                        quantity: selectedQuantity,
+                      });
+                      setCurrentProductQuantityInCart(selectedQuantity);
+                    }
+                  }}
+                >
+                  {packForm ? (
+                    <Text style={styles.addButtonTextStyles}>
+                      Add {selectedQuantity} {packForm}(s)
+                    </Text>
+                  ) : (
+                    <Text style={styles.addButtonTextStyles}>Add {selectedQuantity} Item(s)</Text>
+                  )}
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
@@ -165,11 +171,11 @@ const styles = StyleSheet.create({
   crossContainer: {
     position: 'absolute',
     right: 14,
-    top: 29,
+    top: 25,
   },
   closeIcon: {
-    height: 16,
-    width: 16,
+    height: 22,
+    width: 22,
   },
   modalHeaderContainer: {
     width: 248,
