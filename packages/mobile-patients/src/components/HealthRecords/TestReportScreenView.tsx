9f8b67c04a28452d552cb694d25025839da13868
Pavan Kumar <pavankumar.m@apollo247.org>
2021-11-17 06:03:26 +0000

    APP-11120: Pull request changes

--- packages/mobile-patients/src/components/HealthRecords/TestReportScreenView.tsx
+++ packages/mobile-patients/src/components/HealthRecords/TestReportScreenView.tsx
@@ -364,7 +364,7 @@ export const TestReportViewScreen: React.FC<TestReportViewScreenProps> = (props)
   };
 
   const asyncFetchDailyData = async (dataToFetch: any) => {
-    const apiArray: any[] = []; // const arrData = [1,2,3,4,5,6,7,8,9,0]
+    const apiArray: any[] = [];
     dataToFetch?.labTestResults?.map((item: any) => {
       apiArray.push({ parameterName: item?.parameterName, testName: dataToFetch?.labTestName });
     });
