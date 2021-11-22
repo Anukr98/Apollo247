5c8cb8a71c80d5c4696ddefa6fa0cfdb4881f927
Bhawana Shanker <bhawana.s@apollo247.org>
2021-11-22 17:07:17 +0530

    APP-14761 : fix logic for cta

--- packages/mobile-patients/src/components/Tests/TestWidgetListing.tsx
+++ packages/mobile-patients/src/components/Tests/TestWidgetListing.tsx
@@ -48,12 +48,19 @@ export const TestWidgetListing: React.FC<TestWidgetListingProps> = (props) => {
   };
   const callToOrderDetails = AppConfig.Configuration.DIAGNOSTICS_CITY_LEVEL_CALL_TO_ORDER;
   const ctaDetailArray = callToOrderDetails?.ctaDetailsOnCityId;
+  const isCtaDetailDefault = callToOrderDetails?.ctaDetailsDefault?.ctaProductPageArray?.includes(CALL_TO_ORDER_CTA_PAGE_ID.TESTLISTING);
   const ctaDetailMatched = ctaDetailArray?.filter((item: any) => {
+    if (item?.cityId == cityId) {
       if (item?.ctaProductPageArray?.includes(CALL_TO_ORDER_CTA_PAGE_ID.TESTLISTING)) {
         return item;
       } else {
-        return null
+        return null;
       }
+    } else if (isCtaDetailDefault) {
+      return callToOrderDetails?.ctaDetailsDefault;
+    } else {
+      return null;
+    }
   });
 
   const renderCallToOrder = () => {
