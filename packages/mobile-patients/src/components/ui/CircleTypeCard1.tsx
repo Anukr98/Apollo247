6edc3c8c211c7e7a2e24f880d809d5d3322b8a2c
Mohammad Asad <asad.m@apollo247.org>
2021-11-23 15:18:29 +0530

    APP-13770 hsr circle expiring soon

--- packages/mobile-patients/src/components/ui/CircleTypeCard1.tsx
+++ packages/mobile-patients/src/components/ui/CircleTypeCard1.tsx
@@ -49,42 +49,33 @@ export const CircleTypeCard1: React.FC<CircleTypeCard1Props> = (props) => {
             source={require('@aph/mobile-patients/src/components/ui/icons/circleLogo.webp')}
           />
         </View>
-
-        <View style={styles.subPlanTwo}>
-          <Text style={{ ...theme.viewStyles.text('M', 12, '#02475B', 1, 16) }}>Savings:</Text>
-          <Text style={{ ...theme.viewStyles.text('M', 20, '#02475B', 1, 25) }}>₹{savings}</Text>
-          <View style={{ width: width }}>
-            {credits ? (
-              <View style={styles.planContainer}>
-                <Text style={{ ...theme.viewStyles.text('M', 12, '#666666', 0.6, 16) }}>
-                  Available Health Credits:
-                </Text>
-                <Text style={{ ...theme.viewStyles.text('M', 12, '#666666', 1, 16) }}>
-                  {' '}
-                  {credits || 0}
-                </Text>
-              </View>
-            ) : null}
+        <LinearGradientComponent
+          style={styles.subPlanTwoThreeDevider}
+          colors={[theme.colors.LIGHT_BLUE, theme.colors.LIGHT_BLUE]}
+        >
+          <View style={styles.subPlanTwo}>
+            <Text style={{ ...theme.viewStyles.text('M', 13, theme.colors.WHITE, 1, 20) }}>
+              You saved ₹{savings || 'Unable to Load'} with circle
+            </Text>
+            <Text
+              style={{
+                ...theme.viewStyles.text('M', 10, theme.colors.WHITE, 1, 20),
+                fontStyle: 'italic',
+              }}
+            >
+              Plan Expiring on {expiry}
+            </Text>
           </View>
-        </View>
 
-        <View style={styles.subPlanThree}>
-          <Text style={{ ...theme.viewStyles.text('M', 12, '#02475B', 1, 16) }}>
-            Plan Expires In:
-          </Text>
-          <Text style={{ ...theme.viewStyles.text('M', 20, '#02475B', 1, 25) }}>
-            {expiry} {expiry == '1' ? `day` : `days`}
-          </Text>
-        </View>
-
-        <View style={styles.subPlanFour}>
-          <Text
-            style={{ width: 155, height: 32, ...theme.viewStyles.text('B', 15, '#FC9916', 1, 18) }}
-            onPress={onButtonPress}
-          >
-            RENEW NOW
-          </Text>
-        </View>
+          <View style={styles.subPlanThree}>
+            <Text
+              style={{ ...theme.viewStyles.text('B', 15, '#FC9916', 1, 18) }}
+              onPress={onButtonPress}
+            >
+              RENEW
+            </Text>
+          </View>
+        </LinearGradientComponent>
       </View>
     </View>
   );
