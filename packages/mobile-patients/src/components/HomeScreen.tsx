870b234b6f48ce6e0d530d85f7fcf1b2d33e07c5
Mohammad Asad <asad.m@apollo247.org>
2021-11-24 14:45:37 +0530

    APP-16262

--- packages/mobile-patients/src/components/HomeScreen.tsx
+++ packages/mobile-patients/src/components/HomeScreen.tsx
@@ -3420,7 +3420,10 @@ export const HomeScreen: React.FC<HomeScreenProps> = (props) => {
     const textForNotch = getNotchText(item?.expired_at, item?.notch_text?.text);
     let offerDesignTemplate = getTemplateStyle(item?.template_name);
     return (
-      <TouchableOpacity activeOpacity={1} onPress={() => onOfferCtaPressed(item, index + 1)}>
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => textForNotch !== 'Offer Expired' && onOfferCtaPressed(item, index + 1)}
+      >
         <LinearGradientVerticalComponent
           colors={[
             offerDesignTemplate?.banner_bg_color?.primary_color,
