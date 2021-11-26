338685a989c2e009c053947a96a5087136c8fc7d
Ishawari Singh <ishawari.singh@unthinkable.co>
2021-11-26 11:30:42 +0530

    APP-16459: removed unnecessary console statement

--- packages/mobile-patients/src/components/ProductDetailPage/Components/CouponSectionPDP.tsx
+++ packages/mobile-patients/src/components/ProductDetailPage/Components/CouponSectionPDP.tsx
@@ -22,7 +22,6 @@ export const CouponSectionPDP = (props: { offersData: SpecialOffersCouponsData[]
     array[position].knowMoreOption = !array[position].knowMoreOption;
     setCouponData(array);
   };
-  console.log('cashback');
 
   const renderIndividualCoupon = (item: SpecialOffersCouponsData, index: number) => {
     return (
