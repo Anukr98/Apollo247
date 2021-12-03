0a8524ff64843d9a00ed990324cb5b2b7c0aac9f
Shikha Jangid <shikha.j@apollo247.org>
2021-12-03 17:20:31 +0530

    APP-14684: cherry pick commit 2a13c96

--- packages/mobile-patients/src/graphql/profiles.ts
+++ packages/mobile-patients/src/graphql/profiles.ts
@@ -6806,3 +6806,83 @@ export const BOOK_PACKAGE_CONSULT = gql`
     }
   }
 `;
+
+export const SERVER_CART_FETCH_CART = gql`
+  query fetchCart($patientId: String!) {
+    fetchCart(patientId: $patientId) {
+      statusCode
+      errorMessage
+      cartMessage
+      data {
+        patientId
+        amount {
+          estimatedAmount
+          deliveryCharges
+          isDeliveryFree
+          cartSavings
+          couponSavings
+          totalCashBack
+          cartTotal
+          packagingCharges
+          circleSavings {
+            membershipCashBack
+            circleDelivery
+          }
+        }
+        noOfShipments
+        longitude
+        latitude
+        zipcode
+        patientAddressId
+        couponDetails {
+          coupon
+          couponMessage
+          valid
+        }
+        prescriptionDetails {
+          prescriptionImageUrl
+          prismPrescriptionFileId
+        }
+        prescriptionType
+        appointmentId
+        subscriptionDetails {
+          userSubscriptionId
+          planId
+          subPlanId
+          type
+          planAmount
+          currentSellingPrice
+          validDuration
+          durationInMonth
+        }
+        medicineOrderCartLineItems {
+          sku
+          magentoId
+          magentoStatus
+          name
+          quantity
+          price
+          sellingPrice
+          mou
+          couponDiscountPrice
+          thumbnail
+          isExpress
+          isPrescriptionRequired
+          subcategory
+          typeId
+          urlKey
+          isInStock
+          maxOrderQty
+          sellOnline
+          tat
+          tatDuration
+          isCouponApplicable
+          cashback
+          isShippable
+          freeProduct
+          shipmentNo
+        }
+      }
+    }
+  }
+`;
