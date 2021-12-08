a7bcaf57b9bc6130f65d95cda7aad27625d216e6
Apple <satyam.sharma@neosoftmail.com>
2021-12-08 18:27:12 +0530

    APP-16931: Integrate Referrar Banner With QA Remote Key

--- packages/mobile-patients/src/components/HomeScreen.tsx
+++ packages/mobile-patients/src/components/HomeScreen.tsx
@@ -2801,7 +2801,8 @@ export const HomeScreen: React.FC<HomeScreenProps> = (props) => {
 
   const firebaseRemoteConfigForReferrer = async () => {
     try {
-      const bannerConfig = await remoteConfig().getValue('Referrer_Banner');
+      const RefBannerKey = AppConfig.APP_ENV == 'PROD' ? 'Referrer_Banner' : 'QA_Referrer_Banner';
+      const bannerConfig = await remoteConfig().getValue(RefBannerKey);
       setReferrerAvailable(bannerConfig.asBoolean());
     } catch (e) {}
   };
@@ -5726,6 +5727,7 @@ export const HomeScreen: React.FC<HomeScreenProps> = (props) => {
                   : renderHeadings('Offers For You')}
                 {offersListCache.length === 0 && offersListLoading && renderOffersForYouShimmer()}
                 {(offersListCache.length > 0 || !offersListLoading) && renderOffersForYou()}
+                {isReferrerAvailable && renderReferralBanner()}
                 {!appointmentLoading && currentAppointments === '0'
                   ? null
                   : renderHeadings('My Doctors')}
@@ -5733,7 +5735,7 @@ export const HomeScreen: React.FC<HomeScreenProps> = (props) => {
                   ? null
                   : renderListView('Active Appointments', 'normal')}
                 <View>{renderAllConsultedDoctors()}</View>
-                {isReferrerAvailable && renderReferralBanner()}
+
                 {renderHeadings('Circle Membership and More')}
                 {isCircleMember === '' && circleDataLoading && renderCircleShimmer()}
                 <View>{isCircleMember === 'yes' && !circleDataLoading && renderCircle()}</View>
