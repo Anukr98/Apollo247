1071fbd5da5d0a0a605a87cb240fbe4b21ef04b3
Gufran Khurshid <gufran.k@apollo247.org>
2021-12-08 10:07:17 +0000

    Merged in feature/APP-17081-remote-configurable-tat-text (pull request #2469)
    APP-17081 Done TAT configurable
    
    * APP-17081 Done TAT configurable
    
    
    Approved-by: Asad Mohammad
    Approved-by: Nishant Srivastava

--- packages/mobile-patients/src/components/ui/Icons.tsx
+++ packages/mobile-patients/src/components/ui/Icons.tsx
@@ -896,9 +896,7 @@ export const CashbackIcon: React.FC<IconProps> = (props) => (
   />
 );
 
-export const DeliveryInIcon: React.FC<IconProps> = (props) => (
-  <IconBase style={{ height: 26, width: 115 }} {...props} source={require('./icons/ribbon.webp')} />
-);
+
 
 export const ToggleOn: React.FC<IconProps> = (props) => (
   <IconBase
