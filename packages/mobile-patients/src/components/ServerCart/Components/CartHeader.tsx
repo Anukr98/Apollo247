a6dc1aba0821d2c43f4972348608862753765291
RanveerSingh <sainiranveersingh@gmail.com>
2021-12-13 13:31:50 +0530

    APP-17164: cart header event encapsulated with try catch

--- packages/mobile-patients/src/components/ServerCart/Components/CartHeader.tsx
+++ packages/mobile-patients/src/components/ServerCart/Components/CartHeader.tsx
@@ -19,36 +19,39 @@ export const CartHeader: React.FC<CartHeaderProps> = (props) => {
   const { currentPatient } = useAllCurrentPatients()
 
   const postPharmacyCartAddItemsClickedEvent = async () => {
-    let prescription_required = false, total_discount = 0, subtotal = 0;
-    for (let i = 0; i < shoppingCart?.serverCartItems?.length; i++) {
-      if (shoppingCart?.serverCartItems[i]?.isPrescriptionRequired === '1') {
-        prescription_required = true;
-        break;
+    try {
+      let prescription_required = false, total_discount = 0, subtotal = 0;
+      for (let i = 0; i < shoppingCart?.serverCartItems?.length; i++) {
+        if (shoppingCart?.serverCartItems[i]?.isPrescriptionRequired === '1') {
+          prescription_required = true;
+          break;
+        }
       }
+      shoppingCart?.serverCartItems?.forEach(item => {
+        subtotal += item?.price
+        if(item?.sellingPrice)
+          total_discount += (item?.price - item?.sellingPrice)
+      })
+      const pincode = shoppingCart?.addresses?.find(item => item?.id === shoppingCart?.cartAddressId)?.zipcode
+      const eventAttributes: CleverTapEvents[CleverTapEventName.PHARMACY_CART_ADD_ITEMS_CLICKED] = {
+        user: currentPatient?.firstName,
+        mobile_number: currentPatient?.mobileNumber,
+        user_type: await AsyncStorage.getItem("PharmacyUserType"),
+        circle_member: shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Added'],
+        circle_membership_value: shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Value'] ? shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Value'] : 0,
+        cartItems: shoppingCart?.serverCartItems,
+        prescription_required,
+        total_discount,
+        total_items_in_cart: shoppingCart?.serverCartItems?.length,
+        subtotal,
+        coupon: shoppingCart?.cartCoupon ? shoppingCart?.cartCoupon?.coupon : null,
+        customerId: currentPatient?.id,
+        pincode,
+        order_value: shoppingCart?.serverCartAmount?.estimatedAmount
+      }
+      postCleverTapEvent(CleverTapEventName.PHARMACY_CART_ADD_ITEMS_CLICKED, eventAttributes)
     }
-    shoppingCart?.serverCartItems?.forEach(item => {
-      subtotal += item?.price
-      if(item?.sellingPrice)
-        total_discount += (item?.price - item?.sellingPrice)
-    })
-    const pincode = shoppingCart?.addresses?.find(item => item?.id === shoppingCart?.cartAddressId)?.zipcode
-    const eventAttributes: CleverTapEvents[CleverTapEventName.PHARMACY_CART_ADD_ITEMS_CLICKED] = {
-      user: currentPatient?.firstName,
-      mobile_number: currentPatient?.mobileNumber,
-      user_type: await AsyncStorage.getItem("PharmacyUserType"),
-      circle_member: shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Added'],
-      circle_membership_value: shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Value'] ? shoppingCart?.pharmacyCircleAttributes?.['Circle Membership Value'] : 0,
-      cartItems: shoppingCart?.serverCartItems,
-      prescription_required,
-      total_discount,
-      total_items_in_cart: shoppingCart?.serverCartItems?.length,
-      subtotal,
-      coupon: shoppingCart?.cartCoupon ? shoppingCart?.cartCoupon?.coupon : null,
-      customerId: currentPatient?.id,
-      pincode,
-      order_value: shoppingCart?.serverCartAmount?.estimatedAmount
-    }
-    postCleverTapEvent(CleverTapEventName.PHARMACY_CART_ADD_ITEMS_CLICKED, eventAttributes)
+    catch(err) {}
   }
 
   const headerRightComponent = () => {
