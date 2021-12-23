5d6853d081905fcc1ceb696e4bf3822c5d3b2f74
bhavna-chaurasia <bhavna.c@apollo247.org>
2021-12-23 10:49:51 +0530

    APP-17366 : slot selection ui fix

--- packages/mobile-patients/src/components/Tests/components/TestSlotSelectionOverlayNew.tsx
+++ packages/mobile-patients/src/components/Tests/components/TestSlotSelectionOverlayNew.tsx
@@ -358,7 +358,12 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
   const renderSlotSelectionView = () => {
     const distanceCharges = overallDistanceCharge;
     return (
-      <View style={[styles.slotsView,{height: distanceCharges > 0 && !props.isReschdedule ? '65%' : '75%' }]}>
+      <View
+        style={[
+          styles.slotsView,
+          { height: distanceCharges > 0 && !props.isReschdedule ? '65%' : '75%' },
+        ]}
+      >
         <View style={styles.dayPhaseContainer}>
           {dayPhaseArray?.map((item, index) => (
             <TouchableOpacity
@@ -387,7 +392,12 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
             </TouchableOpacity>
           ))}
         </View>
-        <View style={[styles.slotsList,{height: distanceCharges > 0 && !props.isReschdedule ? '80%' : '90%' }]}>
+        <View
+          style={[
+            styles.slotsList,
+            { height: distanceCharges > 0 && !props.isReschdedule ? '80%' : '90%' },
+          ]}
+        >
           {!!overallSlotsArray && overallSlotsArray?.length != 0 ? (
             <FlatList
               bounces={false}
@@ -435,9 +445,7 @@ export const TestSlotSelectionOverlayNew: React.FC<TestSlotSelectionOverlayNewPr
         style={[
           styles.dateContentStyle,
           {
-            backgroundColor: isSelected
-              ? theme.colors.APP_GREEN
-              : theme.colors.SLOT_GRAY,
+            backgroundColor: isSelected ? theme.colors.APP_GREEN : theme.colors.SLOT_GRAY,
           },
         ]}
       >
@@ -807,7 +815,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   slotsList: { marginBottom: 20, marginTop: 10, backgroundColor: 'white' },
-  slotsView: { marginBottom: 30, paddingBottom: 30 },
+  slotsView: { marginBottom: 30, paddingBottom: 30, flex: 1 },
 });
 
 TestSlotSelectionOverlayNew.defaultProps = {
