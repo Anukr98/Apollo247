2d7c2797dadc5ded4932e8c0079ebd692910bdbb
Nishant Srivastava <nishant.srivastava@apollo247.org>
2021-12-24 17:54:31 +0530

    APP-15358: payment card fixes for refund

--- packages/mobile-patients/src/components/MyPayments/PaymentStatus/components/FooterButton.tsx
+++ packages/mobile-patients/src/components/MyPayments/PaymentStatus/components/FooterButton.tsx
@@ -13,11 +13,12 @@ interface FooterButtonProps {
   item: any;
   paymentFor: string;
   navigationProps: any;
+  paymentStatus: string;
 }
 const FooterButton: FC<FooterButtonProps> = (props) => {
   const { SUCCESS, FAILED, REFUND } = PaymentConstants;
   const statusItemValues = () => {
-    const { paymentFor, item } = props;
+    const { paymentFor, item, paymentStatus } = props;
     let status = 'PENDING';
     let orderID = 0;
     if (paymentFor === 'consult') {
@@ -27,7 +28,7 @@ const FooterButton: FC<FooterButtonProps> = (props) => {
       const paymentInfo = PaymentOrders?.paymentStatus ? PaymentOrders : appointmentPayments[0];
       if (!paymentInfo) {
         status = 'PENDING';
-      } else if (refundInfo.length) {
+      } else if (refundInfo.length || paymentStatus == REFUND) {
         status = REFUND;
       } else {
         status = paymentInfo?.paymentStatus;
