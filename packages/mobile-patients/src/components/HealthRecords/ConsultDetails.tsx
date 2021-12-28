8168c29b43e949bca55f4d93dd5295719244d919
Abhishek <Abhishek@unthinkable.co>
2021-12-28 16:06:35 +0530

    APP-16188-Removed Commented Code

--- packages/mobile-patients/src/components/HealthRecords/ConsultDetails.tsx
+++ packages/mobile-patients/src/components/HealthRecords/ConsultDetails.tsx
@@ -433,7 +433,6 @@ export const ConsultDetails: React.FC<ConsultDetailsProps> = (props) => {
   }, []);
 
   const getAddressList = () => {
-    // setLoading && setLoading(true);
     client
       .query<getPatientAddressList, getPatientAddressListVariables>({
         query: GET_PATIENT_ADDRESS_LIST,
@@ -455,12 +454,10 @@ export const ConsultDetails: React.FC<ConsultDetailsProps> = (props) => {
             if (address) {
               address?.latitude && setDefaultAddress(address);
             } else {
-              // setLoading && setLoading(false);
               setPrescAvailability('unavailable');
               setTestAvailability('unavailable');
             }
           } else {
-            // setLoading && setLoading(false);
             setPrescAvailability('unavailable');
             setTestAvailability('unavailable');
           }
@@ -1160,8 +1157,6 @@ export const ConsultDetails: React.FC<ConsultDetailsProps> = (props) => {
     const { isAllMedicineAtPincode, isPartialMedicineAtPincode, noPincode } = tatContent.find(
       (item) => item.isMedicine
     );
-    console.log(noPincode, defaultAddress?.zipcode, tat);
-
     return prescAvailability !== 'unavailable' && defaultAddress?.zipcode && tat ? (
       <Text style={styles.tatText}>
         {prescAvailability == 'available'
