82c486cb9eb146fca4e452f79fd6200710194bda
Bhavna Chaurasia <bhavna.c@apollo247.org>
2022-01-07 05:24:51 +0000

    Merged in APP-18324 (pull request #2681)
    APP-18324
    
    * APP-17017 : removed logs
    
    * Merge branch 'release/v6.2.1' of https://bitbucket.org/apollo247/aph-mobile-patients into release/v6.2.1
    
    * Merge branch 'release/v6.2.1' of https://bitbucket.org/apollo247/aph-mobile-patients into release/v6.2.1
    
    * Merge branch 'release/v6.2.1' of https://bitbucket.org/apollo247/aph-mobile-patients into release/v6.2.1
    
    * Merge branch 'release/v6.2.1' of https://bitbucket.org/apollo247/aph-mobile-patients into release/v6.2.1
    
    * Merge branch 'release/v6.2.1' of https://bitbucket.org/apollo247/aph-mobile-patients into release/v6.2.1
    
    * APP-18324 : review page disclaimer text updated
    
    
    Approved-by: Bhuvan Reddy
    Approved-by: Gufran Khurshid

--- packages/mobile-patients/src/strings/AppConfig.ts
+++ packages/mobile-patients/src/strings/AppConfig.ts
@@ -372,6 +372,7 @@ const appStaticVariables = {
       phoneNumber: string.diagnostics.uploadPrescriptionWhatsapp.whatsappPhoneNumber,
     },
   ],
+  DIAGNOSTIC_REVIEW_ORDER_DISCLAIMER_TEXT: string.diagnosticsCartPage.reviewPagePolicyText
 };
 
 const DEV_top_specialties = [
