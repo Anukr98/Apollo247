74f8913c246a4a97c69f51eb7f87939136d18529
bhavna chaurasia <bhavna.c@apollo247.org>
2022-02-02 11:19:50 +0530

    APP-19495 : nan fix

--- packages/mobile-patients/src/components/Tests/TestCartPage/ReviewOrder.tsx
+++ packages/mobile-patients/src/components/Tests/TestCartPage/ReviewOrder.tsx
@@ -2646,6 +2646,14 @@ export const ReviewOrder: React.FC<ReviewOrderProps> = (props) => {
           if (toPayPrice == 0) {
             try {
               const res = await createJusPayOrder(payId!);
+              const defaultClevertapEventParams = {
+                mobileNumber: currentPatient?.mobileNumber,
+                vertical: 'diagnostics',
+                displayId: orderDetails?.displayId,
+                paymentId: payId,
+                amount: toPayPrice,
+                availableHc: healthCredits,
+              };
               if (res?.data?.createOrderV2?.payment_status == 'TXN_SUCCESS') {
                 _navigatetoOrderStatus(
                   true,
@@ -2653,7 +2661,10 @@ export const ReviewOrder: React.FC<ReviewOrderProps> = (props) => {
                   eventAttributes,
                   orderInfo,
                   payId!,
-                  verticalSpecificData
+                  verticalSpecificData,
+                  true,
+                  defaultClevertapEventParams,
+                  toPayPrice
                 );
               } else {
                 renderAlert(string.common.tryAgainLater);
@@ -2818,7 +2829,8 @@ export const ReviewOrder: React.FC<ReviewOrderProps> = (props) => {
             eventAttributes,
             orderInfo,
             paymentId,
-            verticalSpecificData
+            verticalSpecificData,
+            false
           );
         }
       } else {
@@ -2836,18 +2848,36 @@ export const ReviewOrder: React.FC<ReviewOrderProps> = (props) => {
     eventAttributes: WebEngageEvents[WebEngageEventName.DIAGNOSTIC_CHECKOUT_COMPLETED],
     orderInfo: any,
     paymentId: string | number,
-    verticalSpecificData: any
+    verticalSpecificData: any,
+    fromZeroPayment: boolean,
+    defaultClevertapEventParams?: any,
+    amount?: string | number
   ) {
     setLoading?.(false);
-    props.navigation.navigate(AppRoutes.PaymentStatusDiag, {
-      paymentId: paymentId,
-      orderDetails: orderInfo,
-      isCOD: isCOD,
-      eventAttributes,
-      paymentStatus: paymentStatus,
-      isModify: isModifyFlow ? modifiedOrder : null,
-      verticalSpecificData,
-    });
+    if (fromZeroPayment) {
+      props.navigation.navigate(AppRoutes.PaymentStatusDiag, {
+        paymentId: paymentId,
+        orderDetails: orderInfo,
+        isCOD: isCOD,
+        eventAttributes,
+        paymentStatus: paymentStatus,
+        isModify: isModifyFlow ? modifiedOrder : null,
+        verticalSpecificData,
+        defaultClevertapEventParams: defaultClevertapEventParams,
+        isCircleAddedToCart: isCircleAddedToCart,
+        amount: amount,
+      });
+    } else {
+      props.navigation.navigate(AppRoutes.PaymentStatusDiag, {
+        paymentId: paymentId,
+        orderDetails: orderInfo,
+        isCOD: isCOD,
+        eventAttributes,
+        paymentStatus: paymentStatus,
+        isModify: isModifyFlow ? modifiedOrder : null,
+        verticalSpecificData,
+      });
+    }
   }
 
   const saveModifyOrder = (orderInfo: saveModifyDiagnosticOrderInput) =>
