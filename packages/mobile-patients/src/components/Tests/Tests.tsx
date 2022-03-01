25c9e9275c4520443afd397dcd4f77112379d6ac
bhavna chaurasia <bhavna.c@apollo247.org>
2022-03-01 16:58:06 +0530

    APP-17004 : UI fix

--- packages/mobile-patients/src/components/Tests/Tests.tsx
+++ packages/mobile-patients/src/components/Tests/Tests.tsx
@@ -3048,7 +3048,7 @@ export const Tests: React.FC<TestsProps> = (props) => {
           <Button
             title={nameFormater(string.diagnostics.goToCart, 'upper')}
             onPress={() => _navigateToPatientsPage()}
-            style={{ width: '90%' }}
+            style={{ width: winWidth / 2.3 }}
           />
         </View>
       </View>
@@ -3602,7 +3602,7 @@ const styles = StyleSheet.create({
   },
   itemFirst: {
     flexDirection: 'row',
-    width: '100%',
+    width: '90%',
     marginVertical: 10,
     paddingHorizontal: 10,
     justifyContent: 'space-between',
